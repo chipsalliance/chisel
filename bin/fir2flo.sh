4e8aa6245b43cc3ed3afbf19ad60dc37183ed0f4
Jim Lawson <ucbjrl@berkeley.edu>
2015-07-21 11:01:15 -0700

    Check for errors.

--- bin/fir2flo.sh
+++ bin/fir2flo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 $CHISEL_BIN/firrtl -i $1.fir -o $1.flo -X flo # -x X # -p c # tkwTgc
-$CHISEL_BIN/filter < $1.flo > tmp; mv tmp $1.flo
+if [ $? ] ; then
+   $CHISEL_BIN/filter < $1.flo > tmp && mv tmp $1.flo
+fi
--- bin/flo2app.sh
+++ bin/flo2app.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo FLO-LLVM DONE
 flo-llvm $1.flo #  --vcdtmp
-echo FLO-LLVM DONE
-flo-llvm-release $1.flo --harness > $1-harness.cpp
-echo FLO-LLVM-RELEASE DONE
-clang++ -o $1 $1-harness.cpp $1.o
-echo GPP DONE
+if [ $? ] ; then
+  echo FLO-LLVM DONE
+  flo-llvm-release $1.flo --harness > $1-harness.cpp
+fi
+if [ $? ] ; then
+  echo FLO-LLVM-RELEASE DONE
+  clang++ -o $1 $1-harness.cpp $1.o
+  echo GPP DONE
+fi
