6d4232c93e50c7a0efc6dbadb1007e307d71de11
Jim Lawson <ucbjrl@berkeley.edu>
2015-10-09 15:28:06 -0700

    Add support for running all known tests.

--- src/test/scala/chiselTests/main.scala
+++ src/test/scala/chiselTests/main.scala
@@ -4,18 +4,32 @@ import Chisel._
 object MiniChisel {
   def main(gargs: Array[String]): Unit = {
 
-    require(gargs.length > 0, "Need a test name argument")
+    require(gargs.length > 0 && gargs(0)(0) != '-', "Need a test name argument")
     val name   = gargs(0)
-    require(name(0) != '-', "Need a test name as the first argument")
-    val args   = gargs.slice(1, gargs.length) 
-    name match {
-      case "BundleWire" => chiselMain(args, () => Module(new BundleWire(16)))
-      case "ComplexAssign" => chiselMain(args, () => Module(new ComplexAssign(32)))
-      case "GCD" => chiselMain(args, () => Module(new GCD()))
-      case "MulLookup" => chiselMain(args, () => Module(new MulLookup(2)))
-      case "Stack" => chiselMain(args, () => Module(new Stack(16)))
-      case "Tbl" => chiselMain(args, () => Module(new Tbl(8, 42)))
-      case _ => println(" skipping " + name)
+    val nameIndex = 0
+    val optionIndex = gargs.indexWhere { arg => arg(0) == '-' }
+    val numberOfOptions = if (optionIndex == -1) {
+      0
+    } else {
+      gargs.length - optionIndex
+    }
+    val numberOfNames = gargs.length - numberOfOptions
+    val testNames: Array[String]  =  if (name == "all") {
+      Array[String]("BundleWire", "ComplexAssign", "GCD", "MulLookup", "Stack", "Tbl")
+    } else {
+      gargs.slice(nameIndex, nameIndex + numberOfNames)  
+    }
+    val args = gargs.slice(optionIndex, optionIndex + numberOfOptions) 
+    for (testName <- testNames) {
+      testName match {
+        case "BundleWire" => chiselMain(args, () => Module(new BundleWire(16)))
+        case "ComplexAssign" => chiselMain(args, () => Module(new ComplexAssign(32)))
+        case "GCD" => chiselMain(args, () => Module(new GCD()))
+        case "MulLookup" => chiselMain(args, () => Module(new MulLookup(2)))
+        case "Stack" => chiselMain(args, () => Module(new Stack(16)))
+        case "Tbl" => chiselMain(args, () => Module(new Tbl(8, 42)))
+        case _ => println(" skipping " + name)
+      }
     }
   }
 }
