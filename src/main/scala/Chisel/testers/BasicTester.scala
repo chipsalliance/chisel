3e34e66416e86286c4396941c665cf9b0d28f050
ducky <richard.lin@berkeley.edu>
2015-12-11 15:26:47 -0800

    Add stop() in BasicTester as a replacement for io.done

--- src/main/scala/Chisel/testers/BasicTester.scala
+++ src/main/scala/Chisel/testers/BasicTester.scala
@@ -3,6 +3,10 @@
 package Chisel.testers
 import Chisel._
 
+import internal._
+import internal.Builder.pushCommand
+import firrtl._
+
 class BasicTester extends Module {
   val io = new Bundle {
     val done = Bool()
@@ -12,4 +16,10 @@ class BasicTester extends Module {
   io.error := UInt(0)
 
   def popCount(n: Long): Int = n.toBinaryString.count(_=='1')
+
+  /** Ends the test, reporting success.
+    */
+  def stop() {
+    pushCommand(Stop(Node(clock), 0))
+  }
 }
