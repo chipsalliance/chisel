c6370278e40614c04bf0dcc66d50fc40b202d706
chick <chick@qrhino.com>
2015-12-21 10:27:57 -0800

    add functions to check events specifications

--- src/main/scala/Chisel/testers/DecoupledTester.scala
+++ src/main/scala/Chisel/testers/DecoupledTester.scala
@@ -14,6 +14,7 @@ import scala.collection.mutable.ArrayBuffer
 abstract class DecoupledTester extends BasicTester {
   def device_under_test : Module
   var io_info : IOAccessor = null
+  var num_events = 0
 
   case class Step(input_map: mutable.HashMap[Data,Int], output_map: mutable.HashMap[Data,Int])
   val test_actions = new ArrayBuffer[Step]()
@@ -25,10 +26,23 @@ abstract class DecoupledTester extends BasicTester {
     test_actions.last.output_map(io_port) = value
   }
 
+  /**
+   *   validate that all pokes ports are members of the same DecoupledIO
+   */
+  def verify_all_ports_are_same_decoupled(pokes:Seq[Tuple2[Data, Int]]) : Boolean = {
+    pokes.flatMap {
+      case (port, value) => io_info.find_parent_decoupled_port_name(io_info.port_to_name(port))
+    }.size == 1
+  }
+  /**
+   *   validate that all pokes ports are members of the same ValidIO
+   */
+  def verify_all_ports_are_same_valid(pokes:Seq[Tuple2[Data, Int]]) : Boolean = {
+    pokes.flatMap {
+      case (port, value) => io_info.find_parent_valid_port_name(io_info.port_to_name(port))
+    }.size == 1
+  }
 
-
-  //TODO: validate that all pokes ports are members of the same DecoupledIO
-  //TODO: validate the same for expects ports
   /**
    * create an event in which poke values will be loaded when corresponding ready
    * expect values will be validated when corresponding valid occurs
@@ -37,9 +51,13 @@ abstract class DecoupledTester extends BasicTester {
    */
   def event(pokes: Seq[Tuple2[Data, Int]], expects: Seq[Tuple2[Data, Int]]): Unit = {
     test_actions += new Step(new mutable.HashMap[Data, Int], new mutable.HashMap[Data, Int])
+
+    //TODO: validate that each event refers to 1 and only 1 decoupled/valid
+
     for( (port, value) <- pokes) poke(port, value)
     for( (port, value) <- expects) poke(port, value)
 
+    num_events += 1
   }
 
   def finish(): Unit = {
