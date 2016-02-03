7441d8c5c556f2a8c2feb46d76ef051841ef71de
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2016-02-02 23:31:38 -0800

    Add some Chisel2 driver emulation methods
    I don't want to have to maintain a big rocket-chip fork to have it run
    through Chisel3, so instead I'm adding back some of the driver routines
    that existed in Chisel2.

--- src/main/scala/Chisel/Main.scala
+++ src/main/scala/Chisel/Main.scala
@@ -2,7 +2,15 @@
 
 package Chisel
 
+import java.io.File
+
 @deprecated("chiselMain doesn't exist in Chisel3", "3.0") object chiselMain {
   def apply[T <: Module](args: Array[String], gen: () => T) =
     Predef.assert(false)
+
+  def run[T <: Module] (args: Array[String], gen: () => T) = {
+    def circuit = Driver.elaborate(gen)
+    def output_file = new File(Driver.targetDir + "/" + circuit.name + ".fir")
+    Driver.dumpFirrtl(circuit, Option(output_file))
+  }
 }
