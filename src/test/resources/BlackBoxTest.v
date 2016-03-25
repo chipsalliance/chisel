189bdb436162890da29d06c931b48e7b0c179e49
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-03-25 15:59:39 -0700

    Update BlackBoxTest naming to match FIRRTL $ name expansion

--- src/test/resources/BlackBoxTest.v
+++ src/test/resources/BlackBoxTest.v
@@ -1,17 +1,17 @@
 module BlackBoxInverter(
     input  [0:0] clk,
     input  [0:0] reset,
-    output [0:0] io_in,
-    output [0:0] io_out
+    output [0:0] io$in,
+    output [0:0] io$out
 );
-  assign io_out = !io_in;
+  assign io$out = !io$in;
 endmodule
 
 module BlackBoxPassthrough(
     input  [0:0] clk,
     input  [0:0] reset,
-    output [0:0] io_in,
-    output [0:0] io_out
+    output [0:0] io$in,
+    output [0:0] io$out
 );
-  assign io_out = io_in;
+  assign io$out = io$in;
 endmodule
