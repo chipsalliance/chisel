31252cd9776bedeb96b91e837e4b060fe38a7351
Andrew Waterman <waterman@cs.berkeley.edu>
2016-04-22 14:55:33 -0700

    Split ValidIf from within PrimOps

--- src/main/scala/firrtl/passes/Passes.scala
+++ src/main/scala/firrtl/passes/Passes.scala
@@ -1260,6 +1260,7 @@ object SplitExp extends Pass {
             e map (split_exp_e(i + 1)) match {
                case (e:DoPrim) => if (i > 0) split(e) else e
                case (e:Mux) => if (i > 0) split(e) else e
+               case (e:ValidIf) => if (i > 0) split(e) else e
                case (e) => e
             }
          }
