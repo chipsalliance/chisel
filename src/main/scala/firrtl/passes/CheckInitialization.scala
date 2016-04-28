c32b1d4a13bb0f1c35f1f4e074f4740a32755258
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-04-28 13:46:29 -0700

    Make style and spacing of Check Initialization more idiomatic Scala

--- src/main/scala/firrtl/passes/CheckInitialization.scala
+++ src/main/scala/firrtl/passes/CheckInitialization.scala
@@ -31,56 +31,56 @@ import firrtl._
 import firrtl.Utils._
 import firrtl.Mappers._
 
-// Datastructures
-import scala.collection.mutable.ArrayBuffer
+object CheckInitialization extends Pass {
+  def name = "Check Initialization"
+  var mname = ""
+  class RefNotInitialized(info: Info, name: String) extends PassException(s"$info : [module $mname]  Reference $name is not fully initialized.")
+  def run(c: Circuit): Circuit = {
+    val errors = collection.mutable.ArrayBuffer[PassException]()
+    def checkInitM(m: InModule): Unit = {
+      def getName(e: Expression): String = {
+        e match {
+          case e: WRef => e.name
+          case e: WSubField => getName(e.exp) + "." + e.name
+          case e: WSubIndex => getName(e.exp) + "[" + e.value + "]"
+          case e =>
+            error("Shouldn't be here")
+            ""
+        }
+      }
+      def hasVoidQ(e: Expression): Boolean = {
+        var void = false
+        def hasVoid(e: Expression): Expression = {
+          e match {
+            case e: WVoid =>
+              void = true
+              e
+            case e => e map hasVoid
+          }
+        }
+        hasVoid(e)
+        void
+      }
+      def checkInitS(s: Stmt): Stmt = {
+        s match {
+          case s: Connect =>
+            if (hasVoidQ(s.exp)) errors += new RefNotInitialized(s.info, getName(s.loc))
+            s
+          case s => s map checkInitS
+        }
+      }
+      checkInitS(m.body)
+    }
 
-object CheckInitialization extends Pass with StanzaPass {
-   def name = "Check Initialization"
-   var mname = ""
-   class RefNotInitialized (info:Info, name:String) extends PassException(s"${info} : [module ${mname}  Reference ${name} is not fully initialized.")
-   def run (c:Circuit): Circuit = {
-      val errors = ArrayBuffer[PassException]()
-      def check_init_m (m:InModule) : Unit = {
-         def get_name (e:Expression) : String = {
-            (e) match { 
-               case (e:WRef) => e.name
-               case (e:WSubField) => get_name(e.exp) + "." + e.name
-               case (e:WSubIndex) => get_name(e.exp) + "[" + e.value + "]"
-               case (e) => error("Shouldn't be here"); ""
-            }
-         }
-         def has_voidQ (e:Expression) : Boolean = {
-            var void = false
-            def has_void (e:Expression) : Expression = {
-               (e) match { 
-                  case (e:WVoid) => void = true; e
-                  case (e) => e map (has_void)
-               }
-            }
-            has_void(e)
-            void
-         }
-         def check_init_s (s:Stmt) : Stmt = {
-            (s) match { 
-               case (s:Connect) => {
-                  if (has_voidQ(s.exp)) errors += new RefNotInitialized(s.info,get_name(s.loc))
-                  s
-               }
-               case (s) => s map (check_init_s)
-            }
-         }
-         check_init_s(m.body)
+    c.modules foreach { m =>
+      mname = m.name
+      m match {
+        case m: InModule => checkInitM(m)
+        case m => false
       }
-         
-      for (m <- c.modules) {
-         mname = m.name
-         (m) match  { 
-            case (m:InModule) => check_init_m(m)
-            case (m) => false
-         }
-      }
+    }
 
     if (errors.nonEmpty) throw new PassExceptions(errors)
     c
-   }
+  }
 }
