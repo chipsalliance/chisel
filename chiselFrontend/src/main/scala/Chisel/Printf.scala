da-steve101/chisel3

Implementation of source locators

chiselFrontend/src/main/scala/Chisel/Printf.scala
@@ -1,0 +1,38 @@
+// See LICENSE for license details.
+
+package Chisel
+
+import scala.language.experimental.macros
+
+import internal._
+import internal.Builder.pushCommand
+import internal.firrtl._
+import internal.sourceinfo.{SourceInfo, PrintTransform}
+
+object printf { // scalastyle:ignore object.name
+  /** Prints a message in simulation.
+    *
+    * Does not fire when in reset (defined as the encapsulating Module's
+    * reset). If your definition of reset is not the encapsulating Module's
+    * reset, you will need to gate this externally.
+    *
+    * May be called outside of a Module (like defined in a function), so
+    * functions using printf make the standard Module assumptions (single clock
+    * and single reset).
+    *
+    * @param fmt printf format string
+    * @param data format string varargs containing data to print
+    */
+  def apply(fmt: String, data: Bits*): Unit = macro PrintTransform.apply
+
+  def do_apply(fmt: String, data: Bits*)(implicit sourceInfo: SourceInfo) {
+    when (!(Builder.dynamicContext.currentModule.get.reset)) {
+      printfWithoutReset(fmt, data:_*)
+    }
+  }
+
+  private[Chisel] def printfWithoutReset(fmt: String, data: Bits*)(implicit sourceInfo: SourceInfo) {
+    val clock = Builder.dynamicContext.currentModule.get.clock
+    pushCommand(Printf(sourceInfo, Node(clock), fmt, data.map((d: Bits) => d.ref)))
+  }
+}

chiselFrontend/src/main/scala/Chisel/When.scala
@@ -2,9 +2,12 @@
 
 package Chisel
 
+import scala.language.experimental.macros
+
 import internal._
 import internal.Builder.pushCommand
 import internal.firrtl._
+import internal.sourceinfo.{SourceInfo, WhenTransform}
 
 object when {  // scalastyle:ignore object.name
   /** Create a `when` condition block, where whether a block of logic is
@@ -24,8 +27,10 @@ object when {  // scalastyle:ignore object.name
     * }
     * }}}
     */
-  def apply(cond: Bool)(block: => Unit): WhenContext = {
-    new WhenContext(cond, !cond)(block)
+  def apply(cond: Bool)(block: => Unit): WhenContext = macro WhenTransform.apply
+
+  def do_apply(cond: Bool, block: => Unit)(implicit sourceInfo: SourceInfo) = {
+    new WhenContext(sourceInfo, cond, !cond, block)
   }
 }
 
@@ -36,21 +41,25 @@ object when {  // scalastyle:ignore object.name
   * that both the condition is true and all the previous conditions have been
   * false.
   */
-class WhenContext(cond: Bool, prevCond: => Bool)(block: => Unit) {
+class WhenContext(sourceInfo: SourceInfo, cond: Bool, prevCond: => Bool, block: => Unit) {
   /** This block of logic gets executed if above conditions have been false
     * and this condition is true.
     */
-  def elsewhen (elseCond: Bool)(block: => Unit): WhenContext = {
-    new WhenContext(prevCond && elseCond, prevCond && !elseCond)(block)
+  def elsewhen (elseCond: Bool)(block: => Unit): WhenContext = macro WhenTransform.elsewhen
+
+  def do_elsewhen (elseCond: Bool, block: => Unit)(implicit sourceInfo: SourceInfo): WhenContext = {
+    new WhenContext(sourceInfo, prevCond && elseCond, prevCond && !elseCond, block)
   }
 
   /** This block of logic gets executed only if the above conditions were all
     * false. No additional logic blocks may be appended past the `otherwise`.
     */
-  def otherwise(block: => Unit): Unit =
-    new WhenContext(prevCond, null)(block)
+  def otherwise(block: => Unit): Unit = macro WhenTransform.otherwise
 
-  pushCommand(WhenBegin(cond.ref))
+  def do_otherwise(block: => Unit)(implicit sourceInfo: SourceInfo): Unit =
+    new WhenContext(sourceInfo, prevCond, null, block)
+
+  pushCommand(WhenBegin(sourceInfo, cond.ref))
   block
-  pushCommand(WhenEnd())
+  pushCommand(WhenEnd(sourceInfo))
 }

chipsalliance/chisel

chiselFrontend/src/main/scala/Chisel/Printf.scala
@@ -1,0 +1,36 @@
+// See LICENSE for license details.
+
+package Chisel
+
+import scala.language.experimental.macros
+
+import internal._
+import internal.Builder.pushCommand
+import internal.firrtl._
+import internal.sourceinfo.SourceInfo
+
+object printf { // scalastyle:ignore object.name
+  /** Prints a message in simulation.
+    *
+    * Does not fire when in reset (defined as the encapsulating Module's
+    * reset). If your definition of reset is not the encapsulating Module's
+    * reset, you will need to gate this externally.
+    *
+    * May be called outside of a Module (like defined in a function), so
+    * functions using printf make the standard Module assumptions (single clock
+    * and single reset).
+    *
+    * @param fmt printf format string
+    * @param data format string varargs containing data to print
+    */
+  def apply(fmt: String, data: Bits*)(implicit sourceInfo: SourceInfo) {
+    when (!(Builder.dynamicContext.currentModule.get.reset)) {
+      printfWithoutReset(fmt, data:_*)
+    }
+  }
+
+  private[Chisel] def printfWithoutReset(fmt: String, data: Bits*)(implicit sourceInfo: SourceInfo) {
+    val clock = Builder.dynamicContext.currentModule.get.clock
+    pushCommand(Printf(sourceInfo, Node(clock), fmt, data.map((d: Bits) => d.ref)))
+  }
+}

chiselFrontend/src/main/scala/Chisel/When.scala
@@ -2,9 +2,12 @@
 
 package Chisel
 
+import scala.language.experimental.macros
+
 import internal._
 import internal.Builder.pushCommand
 import internal.firrtl._
+import internal.sourceinfo.{SourceInfo}
 
 object when {  // scalastyle:ignore object.name
   /** Create a `when` condition block, where whether a block of logic is
@@ -24,8 +27,8 @@ object when {  // scalastyle:ignore object.name
     * }
     * }}}
     */
-  def apply(cond: Bool)(block: => Unit): WhenContext = {
-    new WhenContext(cond, !cond)(block)
+  def apply(cond: Bool)(block: => Unit)(implicit sourceInfo: SourceInfo): WhenContext = {
+    new WhenContext(sourceInfo, cond, !cond, block)
   }
 }
 
@@ -36,21 +39,21 @@ object when {  // scalastyle:ignore object.name
   * that both the condition is true and all the previous conditions have been
   * false.
   */
-class WhenContext(cond: Bool, prevCond: => Bool)(block: => Unit) {
+final class WhenContext(sourceInfo: SourceInfo, cond: Bool, prevCond: => Bool, block: => Unit) {
   /** This block of logic gets executed if above conditions have been false
     * and this condition is true.
     */
-  def elsewhen (elseCond: Bool)(block: => Unit): WhenContext = {
-    new WhenContext(prevCond && elseCond, prevCond && !elseCond)(block)
+  def elsewhen (elseCond: Bool)(block: => Unit)(implicit sourceInfo: SourceInfo): WhenContext = {
+    new WhenContext(sourceInfo, prevCond && elseCond, prevCond && !elseCond, block)
   }
 
   /** This block of logic gets executed only if the above conditions were all
     * false. No additional logic blocks may be appended past the `otherwise`.
     */
-  def otherwise(block: => Unit): Unit =
-    new WhenContext(prevCond, null)(block)
+  def otherwise(block: => Unit)(implicit sourceInfo: SourceInfo): Unit =
+    new WhenContext(sourceInfo, prevCond, null, block)
 
-  pushCommand(WhenBegin(cond.ref))
+  pushCommand(WhenBegin(sourceInfo, cond.ref))
   block
-  pushCommand(WhenEnd())
+  pushCommand(WhenEnd(sourceInfo))
 }