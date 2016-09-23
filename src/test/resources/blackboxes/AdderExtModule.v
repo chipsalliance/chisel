e25c6f7a5e4e1bfbfcb8345288be478caa469525
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-09-22 19:09:36 -0700

    Add ExtModule Tests

---
+++ src/test/resources/blackboxes/AdderExtModule.v
@@ -0,0 +1,8 @@
+
+module AdderExtModule(
+  input [15:0] foo,
+  output [15:0] bar
+);
+  assign bar = foo + 1;
+endmodule
+
---
+++ src/test/resources/blackboxes/SimpleExtModule.v
@@ -0,0 +1,8 @@
+
+module SimpleExtModule(
+  input [15:0] foo,
+  output [15:0] bar
+);
+  assign bar = foo;
+endmodule
+
