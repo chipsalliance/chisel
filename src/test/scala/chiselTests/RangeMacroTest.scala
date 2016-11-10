a17c0d9b66fae72553b80126359372cdfb612819
chick <chick@qrhino.com>
2016-11-09 16:23:52 -0800

    simple test that range interpolator works with UInt factory method

--- src/test/scala/chiselTests/RangeMacroTest.scala
+++ src/test/scala/chiselTests/RangeMacroTest.scala
@@ -8,15 +8,6 @@ import org.scalatest._
 import org.scalatest.prop._
 import chisel3.testers.BasicTester
 
-package object rangeMacroTest {
-
-implicit class ChiselRange(val sc: StringContext) extends AnyVal {
-  def range(args: Any*): Unit = macro chisel3.internal.RangeTransform.apply
-}
-
-}
-
-import rangeMacroTest._
 
 /** Comprehensive test of static range parsing functionality.
   * Note: negative (failure) conditions can't be tested because they will fail at compile time,
