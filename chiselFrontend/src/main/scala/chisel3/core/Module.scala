c74d1ae3286f4f6b0c86328b39e4fcd3814d635c
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-11-17 13:27:16 -0800

    Check that concrete implementations of Module make io a [lazy] val
    Also add test to ensure Chisel allows users to rely on lazy semantics of a lazy
    val io

--- chiselFrontend/src/main/scala/chisel3/core/Module.scala
+++ chiselFrontend/src/main/scala/chisel3/core/Module.scala
@@ -40,6 +40,8 @@ object Module {
                      "This is probably due to rewrapping a Module instance with Module()." +
                      sourceInfo.makeMessage(" See " + _))
     }
+    if (m.io != m.io) throwException("Concrete implementations of Module must make io a val or lazy val")
+
     Builder.currentModule = parent // Back to parent!
 
     val ports = m.computePorts
@@ -139,8 +141,12 @@ extends HasId {
       case Some(c) => getRef fullName c
     }
 
-  /** IO for this Module. At the Scala level (pre-FIRRTL transformations),
+  /** IO for this Module.
+    *
+    * At the Scala level (pre-FIRRTL transformations),
     * connections in and out of a Module may only go through `io` elements.
+    *
+    * @note While this is a def, concrete implementations *must* make it a val or lazy val
     */
   def io: Record
   val clock = Port(Input(Clock()))
