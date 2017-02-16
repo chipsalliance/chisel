b69e787c0a698b7fb703ccd8d24003f83207e296
Jack Koenig <jack.koenig3@gmail.com>
2017-02-16 11:21:11 -0800

    [skip chisel tests] Add ability to skip Chisel tests in Travis (#444)

---
+++ .run_chisel_tests.sh
@@ -0,0 +1,11 @@
+set -e
+# Skip chisel tests if the commit message says to
+if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip chisel tests]"* ]]; then
+  exit 0
+else
+  git clone https://github.com/ucb-bar/chisel3.git
+  mkdir -p chisel3/lib
+  cp utils/bin/firrtl.jar chisel3/lib
+  cd chisel3
+  sbt clean test
+fi
