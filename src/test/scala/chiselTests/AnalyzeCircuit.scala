d1e9a9f0fa810c6f8a5e1e57611c0eb206d38715
chick <chick@qrhino.com>
2017-03-14 14:21:53 -0700

    Some style cleanup on AnalyzeCircuit

--- src/test/scala/chiselTests/AnalyzeCircuit.scala
+++ src/test/scala/chiselTests/AnalyzeCircuit.scala
@@ -1,26 +1,16 @@
 // See LICENSE for license details.
 
-package chisel3tutorialtests
+package chiselTests
 
-import chisel3.tutorial.lesson1.{AnalyzeModule, AnalyzeCircuit}
+import chisel3.tutorial.lesson1.AnalyzeModule
 
 import chisel3._
-import chisel3.experimental.ChiselAnnotation
 
-import logger.{LazyLogging, LogLevel}
+import logger.LogLevel
 
 import firrtl.FirrtlExecutionSuccess
 
 import org.scalatest.{FreeSpec, Matchers}
-import java.io.{ByteArrayOutputStream, PrintStream}
-
-class OutputCaptor {
-  val byteArrayOutputStream = new ByteArrayOutputStream()
-  val printStream = new PrintStream(byteArrayOutputStream)
-  def getOutputStrings: Seq[String] = {
-    byteArrayOutputStream.toString.split("""\n""")
-  }
-}
 
 class MyModule(analyze: Boolean) extends Module
     with AnalyzeModule {
@@ -40,14 +30,13 @@ class MyModule(analyze: Boolean) extends Module
 class AnalyzeModuleTester extends FreeSpec with Matchers {
   "Can run tutorial to print mux count" - {
     "Can turn on Analyze on MyModule" in {
-      val captor = new OutputCaptor
       logger.Logger.log2StringBuffer()
       logger.Logger.setLevel(classOf[chisel3.tutorial.lesson1.AnalyzeCircuit], LogLevel.Info)
       // logger.Logger.setLevel(LogLevel.Info)
       Driver.execute(Array("-X", "low", "--target-dir", "test_run_dir"), () => new MyModule(true)) match {
         case ChiselExecutionSuccess(_, _, Some(firrtlResult: FirrtlExecutionSuccess)) =>
           val messagesLogged = logger.Logger.getStringBuffer.get.toString()
-          println(s"messages logged\n$messagesLogged")
+          println(s"messages logged----\n$messagesLogged\n-----")
           messagesLogged.contains("muxes!") should be(true)
       }
     }
