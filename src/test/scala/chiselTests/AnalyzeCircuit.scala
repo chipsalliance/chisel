269248b52c0621b03f928eaf0ea57a7b18848352
Adam Izraelevitz <azidar@gmail.com>
2017-03-14 12:44:13 -0700

    Update to new logger funcs

--- src/test/scala/chiselTests/AnalyzeCircuit.scala
+++ src/test/scala/chiselTests/AnalyzeCircuit.scala
@@ -22,7 +22,9 @@ class OutputCaptor {
   }
 }
 
-class MyModule(analyze: Boolean) extends Module with AnalyzeModule {
+class MyModule(analyze: Boolean) extends Module
+    with AnalyzeModule {
+  if(analyze) setAnalyze()
   val io = IO(new Bundle {
     val tval = Input(UInt(16.W))
     val fval = Input(UInt(16.W))
@@ -33,20 +35,18 @@ class MyModule(analyze: Boolean) extends Module with AnalyzeModule {
   when(io.cond) {
     io.out := io.tval
   }
-  if(analyze) setAnalyze()
 }
 
 class AnalyzeModuleTester extends FreeSpec with Matchers {
   "Can run tutorial to print mux count" - {
     "Can turn on Analyze on MyModule" in {
       val captor = new OutputCaptor
-      logger.Logger.reset()
-      logger.Logger.setOutput(captor.printStream)
       logger.Logger.setLevel(logger.LogLevel.Info)
+      logger.Logger.log2StringBuffer()
       //logger.Logger.setClassLogLevels(Map("chisel3.tutorial.lesson1.AnalyzeCircuit" -> logger.LogLevel.Info))
       Driver.execute(Array("-X", "low", "--target-dir", "test_run_dir"), () => new MyModule(true)) match {
         case ChiselExecutionSuccess(_, _, Some(firrtlResult: FirrtlExecutionSuccess)) =>
-          val messagesLogged = captor.getOutputStrings
+          val messagesLogged = logger.Logger.getStringBuffer.get
           println(messagesLogged)
           messagesLogged.contains("muxes!") should be(true)
       }
