0bcea6e67ebe7de1ec4a1f6a7de0547a3b1d1c00
Jack Koenig <jack.koenig3@gmail.com>
2017-04-07 14:37:50 -0700

    Change Enum to emit minimum widths of 1
    Fixes #554

---
+++ src/test/scala/chiselTests/EnumSpec.scala
@@ -0,0 +1,19 @@
+// See LICENSE for license details.
+
+package chiselTests
+
+import chisel3._
+import chisel3.util.Enum
+import chisel3.testers.BasicTester
+
+class EnumSpec extends ChiselFlatSpec {
+
+  "1-entry Enums" should "work" in {
+    assertTesterPasses(new BasicTester {
+      val onlyState :: Nil = Enum(1)
+      val wire = Wire(init = onlyState)
+      chisel3.assert(wire === onlyState)
+      stop()
+    })
+  }
+}
