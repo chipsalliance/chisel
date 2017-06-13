6bd6f3b014acee01d2f8fa823776d586646083d0
Jack Koenig <koenig@sifive.com>
2017-06-13 10:42:20 -0700

    Canonicalize spacing in RemoveValidIf

--- src/main/scala/firrtl/passes/RemoveValidIf.scala
+++ src/main/scala/firrtl/passes/RemoveValidIf.scala
@@ -7,22 +7,22 @@ import firrtl.ir._
 
 // Removes ValidIf as an optimization
 object RemoveValidIf extends Pass {
-   // Recursive. Removes ValidIf's
-   private def onExp(e: Expression): Expression = {
-      e map onExp match {
-         case ValidIf(cond, value, tpe) => value
-         case x => x
-      }
-   }
-   // Recursive.
-   private def onStmt(s: Statement): Statement = s map onStmt map onExp
+  // Recursive. Removes ValidIf's
+  private def onExp(e: Expression): Expression = {
+    e map onExp match {
+      case ValidIf(cond, value, tpe) => value
+      case x => x
+    }
+  }
+  // Recursive.
+  private def onStmt(s: Statement): Statement = s map onStmt map onExp
 
-   private def onModule(m: DefModule): DefModule = {
-      m match {
-         case m: Module => Module(m.info, m.name, m.ports, onStmt(m.body))
-         case m: ExtModule => m
-      }
-   }
+  private def onModule(m: DefModule): DefModule = {
+    m match {
+      case m: Module => Module(m.info, m.name, m.ports, onStmt(m.body))
+      case m: ExtModule => m
+    }
+  }
 
-   def run(c: Circuit): Circuit = Circuit(c.info, c.modules.map(onModule), c.main)
+  def run(c: Circuit): Circuit = Circuit(c.info, c.modules.map(onModule), c.main)
 }
