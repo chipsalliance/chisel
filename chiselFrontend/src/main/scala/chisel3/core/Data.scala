5ee1934f68579f5c9627982604dd6d52247d9d7d
Jack Koenig <jack.koenig3@gmail.com>
2017-06-14 14:44:07 -0700

    Make deprecated debug point to and call dontTouch

--- chiselFrontend/src/main/scala/chisel3/core/Data.scala
+++ chiselFrontend/src/main/scala/chisel3/core/Data.scala
@@ -20,9 +20,9 @@ object Direction {
   object Unspecified extends Direction("unspecified") { override def flip: Direction = Input }
 }
 
-@deprecated("debug doesn't do anything in Chisel3 as no pruning happens in the frontend", "chisel3")
+@deprecated("Use dontTouch instead of debug in chisel3", "chisel3")
 object debug {  // scalastyle:ignore object.name
-  def apply (arg: Data): Data = arg
+  def apply(arg: Data)(implicit compileOptions: CompileOptions): Data = dontTouch(arg)
 }
 
 object DataMirror {
