43b1ebf6f4fcff41c58f1f9d5fedd17f60328c04
Jim Lawson <ucbjrl@berkeley.edu>
2017-07-11 11:08:39 -0700

    Have library code inherit compileOptions from the enclosing Module (if it exists).

--- src/main/scala/chisel3/util/util.scala
+++ src/main/scala/chisel3/util/util.scala
@@ -2,6 +2,8 @@
 
 package chisel3
 
+import chisel3.core.{CompileOptions, UserModule}
+
 /** The util package provides extensions to core chisel for common hardware components and utility functions.
   *
   */
@@ -12,4 +14,12 @@ package object util {
   val ValidIO = chisel3.util.Valid
   val DecoupledIO = chisel3.util.Decoupled
 
+  // A significant issue is which CompileOptions should be in effect when we elaborate utility extensions.
+  // If there is a parent module with compile options, use that, otherwise, materialize the default.
+  // Note: This can be overridden either by passing in explicit CompileOptions, or adding an implicit at the
+  //  site where we invoke the utility class/method.
+  implicit val inheritedCompileOptions = chisel3.internal.Builder.currentModule match {
+    case Some(u: UserModule) => u.compileOptions
+    case _ => CompileOptions.materialize
+  }
 }
