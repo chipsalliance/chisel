6297e00aa845cf6cc1275c05d313f3237e69de9d
Albert Magyar <albert.magyar@gmail.com>
2017-08-17 17:55:44 -0700

    Use firrtl elses in elsewhen/otherwise case emission (#510)
    Preprocess chisel3 IR before emission to determing whether
    whens have alternatives.

--- src/test/scala/chiselTests/When.scala
+++ src/test/scala/chiselTests/When.scala
@@ -53,11 +53,36 @@ class OverlappedWhenTester() extends BasicTester {
   }
 }
 
+class NoOtherwiseOverlappedWhenTester() extends BasicTester {
+  val cnt = Counter(4)
+  when(true.B) { cnt.inc() }
+
+  val out = Wire(UInt(3.W))
+  when(cnt.value <= 0.U) {
+    out := 1.U
+  } .elsewhen (cnt.value <= 1.U) {
+    out := 2.U
+  } .elsewhen (cnt.value <= 2.U) {
+    out := 3.U
+  } .elsewhen (cnt.value <= 3.U) {
+    out := 0.U
+  }
+
+  assert(out === cnt.value + 1.U)
+
+  when(cnt.value === 3.U) {
+    stop()
+  }
+}
+
 class WhenSpec extends ChiselFlatSpec {
   "When, elsewhen, and otherwise with orthogonal conditions" should "work" in {
     assertTesterPasses{ new WhenTester }
   }
   "When, elsewhen, and otherwise with overlapped conditions" should "work" in {
     assertTesterPasses{ new OverlappedWhenTester }
+  }
+  "When and elsewhen without otherwise with overlapped conditions" should "work" in {
+    assertTesterPasses{ new NoOtherwiseOverlappedWhenTester }
   }
 }
