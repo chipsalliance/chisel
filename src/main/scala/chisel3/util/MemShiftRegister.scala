fb2a432477a3401950f1daeb696c86711e2940db
Stephen Tridgell <stephen@tridgell.net>
2017-08-21 11:51:45 +1000

    Always display the last output

--- src/main/scala/chisel3/util/MemShiftRegister.scala
+++ src/main/scala/chisel3/util/MemShiftRegister.scala
@@ -75,7 +75,7 @@ class MemShiftRegister[T <: Data]( genType : T, n : Int ) extends Module {
     when ( io.en ) {
       myMem.write( cntr._1, io.in )
     }
-    io.out := myMem.read( readAddr, io.en )
+    io.out := myMem.read( readAddr, true.B )
     io.cntrWrap := cntr._2
   }
 }
