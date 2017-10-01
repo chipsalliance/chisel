b103bc89a6819115651b244df191f00969a539af
Jack Koenig <jack.koenig3@gmail.com>
2017-10-01 14:09:02 -0700

    Add script for formally comparing emitted Verilog
    Also add Travis test for running this script on PRs

---
+++ .run_formal_checks.sh
@@ -0,0 +1,17 @@
+set -e
+# Run formal check only for PRs
+if [ $TRAVIS_PULL_REQUEST = "false" ]; then
+    echo "Not a pull request, no formal check"
+    exit 0
+elif [[ $TRAVIS_COMMIT_MESSAGE == *"[skip formal checks]"* ]]; then
+    echo "Commit message says to skip formal checks"
+    exit 0
+else
+    # $TRAVIS_BRANCH is branch targeted by PR
+    # Travis does a shallow clone, checkout PR target so that we have it
+    # THen return to previous branch so HEAD points to the commit we're testing
+    git remote set-branches origin $TRAVIS_BRANCH && git fetch
+    git checkout $TRAVIS_BRANCH
+    git checkout -
+    bash ./scripts/formal_equiv.sh HEAD $TRAVIS_BRANCH
+fi
