df93cd456759966e711288019f5c43f4b70800aa
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-11-08 19:08:24 -0500

    [wip] Add BoringUtils annotator
    This adds an annotator that provides a linkage to the FIRRTL
    WiringTransform. This enables synthesizable cross module references
    between one source and multiple sinks without changing IO (the
    WiringTransform bores through the hierarchy).
    
    Per WiringTransform, this will connect sources to their closest
    sinks (as determined by BFS) or fail if ownership is indeterminate.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

---
+++ src/main/scala/chisel3/util/BoringUtils.scala
@@ -0,0 +1,51 @@
+// See LICENSE for license details.
+
+package chisel3.util
+
+import chisel3._
+import chisel3.internal._
+import chisel3.core.ChiselAnnotation
+import _root_.firrtl.passes.wiring.WiringTransform
+
+/** Utilities for generating synthesizeable cross module references.
+  *
+  * @example {{{
+  * class ModuleA extends Module with BoringUtils {
+  *   val a = Reg(Bool())
+  *   addSource(a, "unique_identifier")
+  * }
+  * class ModuleB extends Module with BoringUtils {
+  *   val b = Wire(Bool())
+  *   addSink(b, "unique_identifier")
+  * }
+  * class ModuleC extends Module with BoringUtils {
+  *   val c = Wire(Bool())
+  *   addSink(c, "unique_identifier")
+  * }
+  * }}}
+  */
+trait BoringUtils {
+  self: Module =>
+
+  /** Add a named source cross module reference
+    *
+    * @param component source circuit component
+    * @param name unique identifier for this source
+    */
+  def addSource(component: InstanceId, name: String): Unit = {
+    annotate(
+      ChiselAnnotation(component, classOf[WiringTransform], s"source $name"))
+  }
+
+  /** Add a named sink cross module reference. Multiple sinks may map to
+    * the same source.
+    *
+    * @param component sink circuit component
+    * @param name unique identifier for this sink that must resolve to
+    * a source identifier
+    */
+  def addSink(component: InstanceId, name: String): Unit = {
+    annotate(
+      ChiselAnnotation(component, classOf[WiringTransform], s"sink $name"))
+  }
+}
