hstogether/firrtl

Add RemoveWires transform
This transform replaces all wires with nodes in a legal, flow-forward
order

src/main/scala/firrtl/LoweringCompilers.scala
@@ -86,6 +86,7 @@ class MiddleFirrtlToLowFirrtl extends CoreTransform {
     passes.InferWidths,
     passes.Legalize,
     new firrtl.transforms.RemoveReset,
+    new firrtl.transforms.RemoveWires,
     new firrtl.transforms.CheckCombLoops)
 }
 

chipsalliance/chisel

src/main/scala/firrtl/LoweringCompilers.scala
@@ -86,7 +86,8 @@ class MiddleFirrtlToLowFirrtl extends CoreTransform {
     passes.InferWidths,
     passes.Legalize,
     new firrtl.transforms.RemoveReset,
-    new firrtl.transforms.CheckCombLoops)
+    new firrtl.transforms.CheckCombLoops,
+    new firrtl.transforms.RemoveWires)
 }
 
 /** Runs a series of optimization passes on LowFirrtl