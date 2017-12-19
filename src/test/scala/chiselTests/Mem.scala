2d2787de4ce2cabebac2f785d6fd59c816918756
ducky <richard.lin@berkeley.edu>
2017-12-18 20:30:51 -0800

    Fix test

--- src/test/scala/chiselTests/Mem.scala
+++ src/test/scala/chiselTests/Mem.scala
@@ -6,21 +6,21 @@ import chisel3._
 import chisel3.util._
 import chisel3.testers.BasicTester
 
+class MemVecTester extends BasicTester {
+  val mem = Mem(2, Vec(2, UInt(8.W)))
+
+  // Circuit style tester is definitely the wrong abstraction here
+  val (cnt, wrap) = Counter(true.B, 2)
+  mem(0)(0) := 1.U
+
+  when (cnt === 1.U) {
+    assert(mem.read(0.U)(0) === 1.U)
+    stop()
+  }
+}
+
 class MemorySpec extends ChiselPropSpec {
   property("Mem of Vec should work") {
-    assertTesterPasses { new BasicTester {
-      val mem = Mem(2, Vec(2, UInt(8.W)))
-
-      // Circuit style tester is definitely the wrong abstraction here
-      val (cnt, wrap) = Counter(true.B, 2)
-      when (cnt === 0.U) {
-        mem(0.U)(0) := 1.U
-        mem(0.U)(1) := 2.U
-      } .elsewhen (cnt === 1.U) {
-        assert(mem.read(0.U)(0) === 1)
-        assert(mem(0.U)(1) === 2)
-        stop()
-      }
-    } }
+    assertTesterPasses { new MemVecTester }
   }
 }
