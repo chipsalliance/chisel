b6e04afc0ce69bd29117ca4488d9e892a0162361
Jim Lawson <ucbjrl@berkeley.edu>
2017-12-19 18:54:12 -0800

    Use traditional bash tests.

--- .install-firrtl.sh
+++ .install-firrtl.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+set -ev
 git clone --depth 10 https://github.com/freechipsproject/firrtl
 cd firrtl
 git checkout master
 git pull
 FIRRTL_SHA1=`git rev-parse HEAD`
 BUILT_FIRRTL_SHA1=""
-if test -s $HOME/.ivy2/firrtl.sha1; then
+if [ -s $HOME/.ivy2/firrtl.sha1 ]; then
   BUILT_FIRRTL_SHA1=`cat $HOME/.ivy2/firrtl.sha1`
 fi
-if test $FIRRTL_SHA1 \!= $BUILT_FIRRTL_SHA1; then
+if [ $FIRRTL_SHA1 != $BUILT_FIRRTL_SHA1 ]; then
   sbt +clean +publishLocal
   echo $FIRRTL_SHA1 > $HOME/.ivy2/firrtl.sha1
 fi
