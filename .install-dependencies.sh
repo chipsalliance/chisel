2eab35ad864353314443bd3432a00a6ab7935e54
Jim Lawson <ucbjrl@berkeley.edu>
2017-12-19 18:00:50 -0800

    Cache verilator; try not to rebuild firrtl if we have a suitable version already built.

--- .install-dependencies.sh
+++ .install-dependencies.sh
@@ -3,8 +3,4 @@ set -ev
 # Install Verilator (http://www.veripool.org/projects/verilator/wiki/Installing)
 bash .install_verilator.sh
 # Install firrtl
-git clone --depth 10 https://github.com/freechipsproject/firrtl
-cd firrtl
-git checkout master
-git pull
-sbt +clean +publishLocal
+bash .install-firrtl.sh
