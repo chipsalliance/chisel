2cd9fe01af05012d33d00ee3d9b880b85644b276
Jim Lawson <ucbjrl@berkeley.edu>
2018-02-12 15:34:49 -0800

    Try without Travis build stages.

--- .install-dependencies.sh
+++ .install-dependencies.sh
@@ -2,6 +2,6 @@
 set -ev
 # If you want to run the tests requiring a backend, you'll need to install verilator.
 # Install Verilator (http://www.veripool.org/projects/verilator/wiki/Installing)
-bash .install_verilator.sh
+#bash .install_verilator.sh
 # Install firrtl
 bash .install-firrtl.sh
