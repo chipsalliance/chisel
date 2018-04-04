4669b3682b43c30fe0d211576f2818e48dbae326
Jim Lawson <ucbjrl@berkeley.edu>
2018-04-04 15:13:27 -0700

    Use peek/pokes - can't use wires since we aren't in a Module anymore.

--- src/test/scala/chisel3/tests/GCD.scala
+++ src/test/scala/chisel3/tests/GCD.scala
@@ -17,11 +17,15 @@ class GCD extends Module {
   })
   val x = Reg(UInt(32.W))
   val y = Reg(UInt(32.W))
+  io.v := false.B
   when (x > y)   { x := x -% y }
   .otherwise     { y := y -% x }
   when (io.e) { x := io.a; y := io.b }
   io.z := x
   io.v := y === 0.U
+//  when ((x % 100.U) === 0.U) {
+//    printf("a %d, b %d, e %d, z %d, v %d\n", io.a, io.b, io.e, io.z, io.v)
+//  }
 }
 
 /*
@@ -55,41 +59,26 @@ class GCDSpec extends PropSpec with ChiselScalatestTester with PropertyChecks {
     elaborate { new GCD }
   }
 */
-  private val backendNames = Array[String] ("firrtl")
+  private val backendNames = Array[String] ("firrtl", "jni", "verilator")
   for (backendName <- backendNames) {
     val options = new TesterOptionsManager {
       testerOptions = TesterOptions(backendName = backendName)
     }
 
 
-    property("GCDTester should return the correct result (with $backendName)") {
-      forAll (gcds) { (a: Int, b: Int, z: Int) =>
-        test(new Module{
-  val io = IO(new Bundle {
-    val a  = Input(UInt(32.W))
-    val b  = Input(UInt(32.W))
-    val e  = Input(Bool())
-    val z  = Output(UInt(32.W))
-    val v  = Output(Bool())
-  })
-  val x = Reg(UInt(32.W))
-  val y = Reg(UInt(32.W))
-  when (x > y)   { x := x -% y }
-  .otherwise     { y := y -% x }
-  when (io.e) { x := io.a; y := io.b }
-  io.z := x
-  io.v := y === 0.U
-	}, options) { dut =>
-          val first = RegInit(true.B)
-          dut.io.a := a.U
-          dut.io.b := b.U
-          dut.io.e := first
-          when(first) { first := false.B }
-          when(!first && dut.io.v) {
-            dut.io.z.expect(z.U)
-          }.otherwise {
+    property(s"GCDTester should return the correct result (with $backendName)") {
+      test(new GCD, options) { dut =>
+        forAll (gcds) { (a: Int, b: Int, z: Int) =>
+          dut.io.a.poke(a.U)
+          dut.io.b.poke(b.U)
+          dut.io.e.poke(true.B)
+
+          do {
             dut.clock.step()
-	  }
+            dut.io.e.poke(false.B)
+          } while(!dut.io.v.peek().litToBoolean)
+
+          dut.io.z.expect(z.U)
         }
       }
     }
