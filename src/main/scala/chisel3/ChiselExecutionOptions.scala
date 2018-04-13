98c5679259ecac017c0a4b6bee2c547b5322e0d7
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-04-13 03:02:17 -0400

    Add ChiselExecutionOptions documentation
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/chisel3/ChiselExecutionOptions.scala
+++ src/main/scala/chisel3/ChiselExecutionOptions.scala
@@ -3,36 +3,63 @@
 package chisel3
 
 import firrtl.ExecutionOptionsManager
-import firrtl.annotations.NoTargetAnnotation
+import firrtl.annotations.{
+  Annotation,
+  NoTargetAnnotation }
 
-sealed trait ChiselOption
+/** Indicates that a subclass is an [[firrtl.annotation.Annotation]] with
+  * an option consummable by [[HasChiselExecutionOptions]]
+  *
+  * This must be mixed into a subclass of [[annotaiton.Annotation]]
+  */
+sealed trait ChiselOption { self: Annotation => }
+
+/** Disables FIRRTL compiler execution
+  *  - deasserts [[ChiselExecutionOptions.runFirrtlCompiler]]
+  *  - equivalent to command line option `-chnrf/--no-run-firrtl`
+  */
 case object NoRunFirrtlAnnotation extends NoTargetAnnotation with ChiselOption
+
+/** Disable saving CHIRRTL to an intermediate file
+  *  - deasserts [[ChiselExecutionOptions.saveChirrtl]]
+  *  - equivalent to command line option `--dont-save-chirrtl`
+  */
 case object DontSaveChirrtlAnnotation extends NoTargetAnnotation with ChiselOption
+
+/** Disable saving CHIRRTL-time annotaitons to an intermediate file
+  *  - deasserts [[ChiselExecutionOptions.saveAnnotations]]
+  *  - equivalent to command line option `--dont-save-annotations`
+  */
 case object DontSaveAnnotationsAnnotation extends NoTargetAnnotation with ChiselOption
 
-//TODO: provide support for running firrtl as separate process, could alternatively be controlled by external driver
-/**
-  * Options that are specific to chisel.
+// TODO: provide support for running firrtl as separate process, could
+//       alternatively be controlled by external driver
+/** Options that control the execution of the Chisel compiler
   *
-  * @param runFirrtlCompiler when true just run chisel, when false run chisel then compile its output with firrtl
+  * @param runFirrtlCompiler run the FIRRTL compiler if true
+  * @param saveChirrtl save CHIRRTL output to a file if true
+  * @param saveAnnotations save CHIRRTL-time annotations to a file if true
   * @note this extends FirrtlExecutionOptions which extends CommonOptions providing easy access to down chain options
   */
 case class ChiselExecutionOptions (
   runFirrtlCompiler: Boolean = true,
-  saveChirrtl: Boolean = true,
-  saveAnnotations: Boolean = true
+  saveChirrtl: Boolean       = true,
+  saveAnnotations: Boolean   = true
 )
 
 trait HasChiselExecutionOptions {
   self: ExecutionOptionsManager =>
 
+  /** A [[ChiselExecutionOptions]] object generated from processing all
+    * Chisel command line options
+    */
   lazy val chiselOptions: ChiselExecutionOptions = options
     .collect{ case opt: ChiselOption => opt }
-    .foldLeft(ChiselExecutionOptions())( (old, x) =>
+    .foldLeft(ChiselExecutionOptions())( (c, x) =>
       x match {
-        case NoRunFirrtlAnnotation => old.copy(runFirrtlCompiler = false)
-        case DontSaveChirrtlAnnotation => old.copy(saveChirrtl = false)
-        case DontSaveAnnotationsAnnotation => old.copy(saveAnnotations = false)
+        case NoRunFirrtlAnnotation         => c.copy(runFirrtlCompiler = false)
+        case DontSaveChirrtlAnnotation     => c.copy(saveChirrtl = false)
+        case DontSaveAnnotationsAnnotation => c.copy(saveAnnotations = false)
       } )
 
   parser.note("Chisel Options")
