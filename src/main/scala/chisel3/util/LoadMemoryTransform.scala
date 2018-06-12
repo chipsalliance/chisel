76a9b91d85aa7a7b8fc46a8941faf9780d7b4cf1
chick <chick@qrhino.com>
2018-06-12 12:56:22 -0700

    Work in progress Building out transform and pass now

--- src/main/scala/chisel3/util/LoadMemoryTransform.scala
+++ src/main/scala/chisel3/util/LoadMemoryTransform.scala
@@ -2,14 +2,20 @@
 
 package chisel3.util
 
+import chisel3._
 import chisel3.core.RunFirrtlTransform
-import chisel3.experimental.ChiselAnnotation
 import chisel3.internal.InstanceId
-import firrtl.{CircuitForm, CircuitState, LowForm, TargetDirAnnotation, Transform}
-import firrtl.annotations.{Named, SingleTargetAnnotation}
+import firrtl.{CircuitForm, CircuitState, LowForm, TargetDirAnnotation, Transform, WRef, WSubField}
+import firrtl.annotations.{Annotation, Named, SingleTargetAnnotation}
+import firrtl.ir.{Circuit, DefMemory, DefModule, Statement}
+import firrtl.passes.MemPortUtils.MemPortMap
+import firrtl.passes.Pass
+import firrtl.Mappers._
+import firrtl.passes.memlib.DefAnnotatedMemory
+import firrtl.passes.memlib.MemTransformUtils.updateStmtRefs
 
 case class ChiselLoadMemoryAnnotation(target: InstanceId, fileName: String)
-  extends ChiselAnnotation
+  extends chisel3.core.ChiselAnnotation
     with RunFirrtlTransform {
 
   def transformClass : Class[LoadMemoryTransform] = classOf[LoadMemoryTransform]
@@ -21,6 +27,72 @@ case class LoadMemoryAnnotation(target: Named, value1: String) extends SingleTar
   def duplicate(n: Named): LoadMemoryAnnotation = this.copy(target = n)
 }
 
+
+class CreateBindableMemoryLoaders(val annotations: Seq[Annotation]) extends Pass {
+
+  def createMemProto(m: DefAnnotatedMemory): DefAnnotatedMemory = {
+    val rports = m.readers.indices map (i => s"R$i")
+    val wports = m.writers.indices map (i => s"W$i")
+    val rwports = m.readwriters.indices map (i => s"RW$i")
+    m copy (readers = rports, writers = wports, readwriters = rwports)
+  }
+
+  /** Maps the serialized form of all memory port field names to the
+    *    corresponding new memory port field Expression.
+    *  E.g.:
+    *    - ("m.read.addr") becomes (m.R0.addr)
+    */
+  def getMemPortMap(m: DefAnnotatedMemory, memPortMap: MemPortMap) {
+    val defaultFields = Seq("addr", "en", "clk")
+    val rFields = defaultFields :+ "data"
+    val wFields = rFields :+ "mask"
+    val rwFields = defaultFields ++ Seq("wmode", "wdata", "rdata", "wmask")
+
+    def updateMemPortMap(ports: Seq[String], fields: Seq[String], newPortKind: String): Unit =
+      for ((p, i) <- ports.zipWithIndex; f <- fields) {
+        val newPort = WSubField(WRef(m.name), newPortKind + i)
+        val field = WSubField(newPort, f)
+        memPortMap(s"${m.name}.$p.$f") = field
+      }
+    updateMemPortMap(m.readers, rFields, "R")
+    updateMemPortMap(m.writers, wFields, "W")
+    updateMemPortMap(m.readwriters, rwFields, "RW")
+  }
+
+  /**
+    * Search modules for memories that match annotations
+    * @param currentModule          current being checked
+    * @param namePrefix             progressive path to module
+    * @return
+    */
+  def updateMemMods(currentModule: DefModule, namePrefix: String = ""): DefModule = {
+
+    def updateMemStmts(memPortMap: MemPortMap)(s: Statement): Statement = s match {
+      case m: DefAnnotatedMemory =>
+        val updatedMem = createMemProto(m)
+        getMemPortMap(m, memPortMap)
+        updatedMem
+      case m: DefMemory =>
+        println(m)
+        m
+      case s => s map updateMemStmts(memPortMap)
+    }
+
+    val memPortMap = new MemPortMap
+    (currentModule map updateMemStmts(memPortMap)
+      map updateStmtRefs(memPortMap))
+  }
+
+  /**
+    * run the pass
+    * @param c
+    * @return
+    */
+  def run(c: Circuit): Circuit = {
+    c.copy(modules = c.modules.map { module => updateMemMods(module) })
+  }
+}
+
 //noinspection ScalaStyle
 class LoadMemoryTransform extends Transform {
   def inputForm  : CircuitForm = LowForm
@@ -29,11 +101,13 @@ class LoadMemoryTransform extends Transform {
   def execute(state: CircuitState): CircuitState = {
     val targetDir = state.annotations.collectFirst { case td: TargetDirAnnotation => td }
     println(s"target dir is ${targetDir.getOrElse("no dir")}")
-    val processedAnnotations = state.annotations.map {
-      case LoadMemoryAnnotation(t, value) => LoadMemoryAnnotation(t, value + ":seen")
-      case other => other
-    }
+    val processedAnnotations = state.annotations.flatMap {
+      case loadMemoryAnnotation: LoadMemoryAnnotation => Some(loadMemoryAnnotation)
+      case other                                      => None
+    } ++ (if(targetDir.isDefined) Seq(targetDir.get) else Seq())
+
     println(s"got here")
+    (new CreateBindableMemoryLoaders(processedAnnotations)).run(state.circuit)
     state.copy(annotations = processedAnnotations)
   }
 }
--- src/test/scala/chiselTests/LoadMemoryFromFileSpec.scala
+++ src/test/scala/chiselTests/LoadMemoryFromFileSpec.scala
@@ -11,18 +11,37 @@ class UsesMem(memoryDepth: Int, memoryType: Data) extends Module {
   val io = IO(new Bundle {
     val address = Input(UInt(memoryType.getWidth.W))
     val value   = Output(memoryType)
+    val value2  = Output(memoryType)
   })
 
   val memory = Mem(memoryDepth, memoryType)
 
-  chisel3.experimental.annotate(ChiselLoadMemoryAnnotation(memory, "./mem.txt"))
+  chisel3.experimental.annotate(ChiselLoadMemoryAnnotation(memory, "./mem1.txt"))
+  io.value := memory(io.address)
+
+  val low = Module(new UsesMemLow(memoryDepth, memoryType))
+
+  low.io.address := io.address
+  io.value2 := low.io.value
+}
+
+class UsesMemLow(memoryDepth: Int, memoryType: Data) extends Module {
+  val io = IO(new Bundle {
+    val address = Input(UInt(memoryType.getWidth.W))
+    val value   = Output(memoryType)
+  })
+
+  val memory = Mem(memoryDepth, memoryType)
+
+  chisel3.experimental.annotate(ChiselLoadMemoryAnnotation(memory, "./mem2.txt"))
   io.value := memory(io.address)
 }
+
 
 class LoadMemoryFromFileSpec extends FreeSpec with Matchers {
   "Users can specify a source file to load memory from" in {
     Driver.execute(
-      args = Array("-X", "˚verilog", "--target-dir", "test_run_dir"),
+      args = Array("-X", "verilog", "--target-dir", "test_run_dir"),
       dut = () => new UsesMem(memoryDepth = 8, memoryType = UInt(16.W))
     )
   }
