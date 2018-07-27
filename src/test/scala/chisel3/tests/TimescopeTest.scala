261240ac43393c48315c590bb49a31aaa9a4e995
ducky <richard.lin@berkeley.edu>
2018-07-27 16:39:02 -0700

    Add more broken tests

---
+++ src/test/scala/chisel3/tests/TimescopeTest.scala
@@ -0,0 +1,58 @@
+package chisel3.tests
+
+import org.scalatest._
+
+import chisel3._
+import chisel3.tester._
+
+class TimescopeTest extends FlatSpec with ChiselScalatestTester {
+  behavior of "Testers2"
+
+  it should "revert signals at end of timescopes" in {
+    test(new Module {
+      val io = IO(new Bundle {
+        val in = Input(UInt(8.W))
+        val out = Output(UInt(8.W))
+      })
+      io.out := io.in
+    }) { c =>
+      c.io.in.poke(0.U)
+      c.io.out.expect(0.U)
+      timescope {
+        c.io.in.poke(1.U)
+        c.io.out.expect(1.U)
+        c.clock.step()
+        c.io.out.expect(1.U)
+      }
+      c.io.out.expect(0.U)
+      c.clock.step()
+      c.io.out.expect(0.U)
+    }
+  }
+
+  it should "allow combinational operations within timescopes" in {
+    test(new Module {
+      val io = IO(new Bundle {
+        val in = Input(UInt(8.W))
+        val out = Output(UInt(8.W))
+      })
+      io.out := io.in
+    }) { c =>
+      c.io.in.poke(0.U)
+      c.io.out.expect(0.U)
+      timescope {
+        c.io.in.poke(1.U)
+        c.io.out.expect(1.U)
+        c.io.in.poke(2.U)
+        c.io.out.expect(2.U)
+        c.clock.step()
+        c.io.out.expect(2.U)
+      }
+      c.io.out.expect(0.U)
+      c.io.in.poke(3.U)
+      c.io.out.expect(3.U)
+      c.clock.step()
+      c.io.out.expect(3.U)
+    }
+  }
+}
