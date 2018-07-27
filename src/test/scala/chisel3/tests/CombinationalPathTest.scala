a41d7df935d6be3ba07f4981e80f50ee5e1be65b
ducky <richard.lin@berkeley.edu>
2018-07-27 16:34:32 -0700

    actual tests

--- src/test/scala/chisel3/tests/CombinationalPathTest.scala
+++ src/test/scala/chisel3/tests/CombinationalPathTest.scala
@@ -8,31 +8,67 @@ import chisel3.tester._
 class CombinationalPathTest extends FlatSpec with ChiselScalatestTester {
   behavior of "Testers2"
 
-  it should "detect combinational loops" in {
-    // TODO this test should fail
-    test(new Module {
-      val io = IO(new Bundle {
-        val in1 = Input(UInt(8.W))
-        val in2 = Input(UInt(8.W))
-        val out1 = Output(UInt(8.W))
-        val out2 = Output(UInt(8.W))
-      })
-      io.out1 := io.in1 + io.in2
-
-      val innerModule = Module(new Module {
+  it should "detect combinationally-dependent operations across threads" in {
+    assertThrows[ThreadOrderDependentException] {
+      test(new Module {
         val io = IO(new Bundle {
-          val in = Input(UInt(8.W))
-          val out = Output(UInt(8.W))
+          val in = Input(Bool())
+          val out = Output(Bool())
         })
         io.out := io.in
-      })
-      innerModule.io.in := io.in1
-      io.out2 := innerModule.io.out
-    }) { c =>
-      fork {
-        c.io.in1.poke(1.U)
-      } .fork {
-        c.io.in2.poke(2.U)
+      }) { c =>
+        fork {
+          c.io.in.poke(true.B)
+        } .fork {
+          c.io.out.expect(true.B)
+        }
+      }
+    }
+  }
+
+  it should "detect combinationally-dependent operations through internal modules" in {
+    assertThrows[ThreadOrderDependentException] {
+      test(new Module {
+        val io = IO(new Bundle {
+          val in = Input(Bool())
+          val out = Output(Bool())
+        })
+
+        val innerModule = Module(new Module {
+          val io = IO(new Bundle {
+            val in = Input(Bool())
+            val out = Output(Bool())
+          })
+          io.out := io.in
+        })
+
+        innerModule.io.in := io.in
+        io.out := innerModule.io.out
+      }) { c =>
+        fork {
+          c.io.in.poke(true.B)
+        } .fork {
+          c.io.out.expect(true.B)
+        }
+      }
+    }
+  }
+
+  it should "detect combinational paths across operations" in {
+    assertThrows[ThreadOrderDependentException] {
+      test(new Module {
+        val io = IO(new Bundle {
+          val in1 = Input(Bool())
+          val in2 = Input(Bool())
+          val out = Output(Bool())
+        })
+        io.out := io.in1 || io.in2
+      }) { c =>
+        fork {
+          c.io.in1.poke(true.B)
+        } .fork {
+          c.io.out.expect(true.B)
+        }
       }
     }
   }
