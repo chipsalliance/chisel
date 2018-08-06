9aa4fd0d95e96d14f840dcc3e52e47efe3b983a1
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-08-06 14:25:15 -0400

    Update BoringUtils documentation
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/chisel3/util/experimental/BoringUtils.scala
+++ src/main/scala/chisel3/util/experimental/BoringUtils.scala
@@ -17,36 +17,103 @@ import chisel3.internal.Namespace
   */
 class BoringUtilsException(message: String) extends Exception(message)
 
-/** Utilities for generating synthesizeable cross module references
-  * ("boring" through a hierarchy) from one source module to one or more
-  * sink modules.
+/** Utilities for generating synthesizeable cross module references that
+  * "bore" through the hieararchy. The underlying cross module connects
+  * are handled by FIRRTL's Wiring Transform
+  * ([[firrtl.passes.wiring.WiringTransform]]).
   *
-  * @example {{{
-  * import chisel3.util.experimental.BoringUtils
-  * class ModuleA extends Module {
-  *   val a = Reg(Bool())
-  *   BoringUtils.addSource(a, "unique_identifier")
+  * Consider the following exmple where you want to connect a component in
+  * one module to a component in another. Module `Constant` has a wire
+  * tied to `42` and `Expect` will assert unless connected to `42`:
+  * {{{
+  * class Constant extends Module {
+  *   val io = IO(new Bundle{})
+  *   val x = Wire(UInt(6.W))
+  *   x := 42.U
   * }
-  * class ModuleB extends Module {
-  *   val b = Wire(Bool())
-  *   BoringUtils.addSink(b, "unique_identifier")
-  * }
-  * class ModuleC extends Module {
-  *   val c = Wire(Bool())
-  *   BoringUtils.addSink(c, "unique_identifier")
+  * class Expect extends Module {
+  *   val io = IO(new Bundle{})
+  *   val y = Wire(UInt(6.W))
+  *   y := 0.U
+  *   // This assertion will fail unless we bore!
+  *   chisel3.assert(y === 42.U, "y should be 42 in module Expect")
   * }
   * }}}
+  *
+  * We can then connect `x` to `y` using [[BoringUtils]] without
+  * modifiying the Chisel IO of `Constant`, `Expect`, or modules that may
+  * instantiate them. There are two approaches to do this:
+  *
+  * 1. Hierarchical boring using [[BoringUtils.bore]]
+  *
+  * 2. Non-hierarchical boring using [[BoringUtils.addSink]]/[[BoringUtils.addSource]]
+  *
+  * ===Hierarchical Boring===
+  *
+  * Hierarchcical boring involves connecting one sink instance to another
+  * source instance in a parent module. Below, module `Top` contains an
+  * instance of `Cosntant` and `Expect`. Using [[BoringUtils.bore]], we
+  * can connect `constant.x` to `expect.y`.
+  *
+  * {{{
+  * class Top extends Module {
+  *   val io = IO(new Bundle{})
+  *   val constant = Module(new Constant)
+  *   val expect = Module(new Expect)
+  *   BoringUtils.bore(constant.x, Seq(expect.y))
+  * }
+  * }}}
+  *
+  * ===Non-hierarchical Boring===
+  *
+  * Non-hierarchical boring involves connections from sources to sinks
+  * that cannot see each other. Here, `x` is described as a source and
+  * given a name, `uniqueId`, and `y` is described as a sink with the same
+  * name. This is equivalent to the hierarchical boring example above, but
+  * requires no modifications to `Top`.
+  *
+  * {{{
+  * class Constant extends Module {
+  *   val io = IO(new Bundle{})
+  *   val x = Wire(UInt(6.W))
+  *   x := 42.U
+  *   BoringUtils.addSource(x, "uniqueId")
+  * }
+  * class Expect extends Module {
+  *   val io = IO(new Bundle{})
+  *   val y = Wire(UInt(6.W))
+  *   y := 0.U
+  *   // This assertion will fail unless we bore!
+  *   chisel3.assert(y === 42.U, "y should be 42 in module Expect")
+  *   BoringUtils.addSink(y, "uniqueId")
+  * }
+  * class Top extends Module {
+  *   val io = IO(new Bundle{})
+  *   val constant = Module(new Constant)
+  *   val expect = Module(new Expect)
+  * }
+  * }}}
+  *
+  * ==Comments==
+  *
+  * Both hierarchical and non-hierarchical boring emit FIRRTL annotations
+  * that describe sources and sinks. These are matched by a `name` key
+  * that indicates they should be wired together. Hierarhical boring
+  * safely generates this name automatically. Non-hierarchical boring
+  * unsafely relies on user input to generate this name. Use of
+  * non-hierarchical naming may result in naming conflicts that the user
+  * must handle.
+  *
+  * The automatic generation of hierarchical names relies on a global,
+  * mutable namespace. This is currently persistent across circuit
+  * elaborations.
   */
 object BoringUtils {
-  /** A global namespace for boring ids */
+  /* A global namespace for boring ids */
   private val namespace: SyncVar[Namespace] = new SyncVar
   namespace.put(Namespace.empty)
 
-  /** Get a new name from the global namespace
-    *
-    * @param value the name you'd like to get
-    * @return the name safe in the global namespace
-    */
+  /* Get a new name (value) from the namespace */
   private def newName(value: String): String = {
     val ns = namespace.take()
     val valuex = ns.name(value)
@@ -54,16 +121,20 @@ object BoringUtils {
     valuex
   }
 
-  /** Check if a name exists in the namespace */
+  /* True if the requested name (value) exists in the namespace */
   private def checkName(value: String): Boolean = namespace.get.contains(value)
 
   /** Add a named source cross module reference
     *
     * @param component source circuit component
     * @param name unique identifier for this source
-    * @param dedup enable dedupblication of modules
-    * @param forceUnique fail if a non-unique name parameter is used
-    * @return the mangled name used
+    * @param dedup enable dedupblication of modules (necessary if other
+    * instances exist in the design that are not sources)
+    * @param uniqueName if true, this will use a non-conflicting name from
+    * the global namespace
+    * @return the name used
+    * @note if a uniqueName is not specified, the returned name may differ
+    * from the user-provided name
     */
   def addSource(component: NamedComponent, name: String, dedup: Boolean = false, uniqueName: Boolean = false): String = {
     val id = if (uniqueName) { newName(name) } else { name }
@@ -85,7 +156,12 @@ object BoringUtils {
     *
     * @param component sink circuit component
     * @param name unique identifier for this sink that must resolve to
-    * a source identifier
+    * @param dedup enable deduplication on sink component (necessary if
+    * other instances exist in the design that are not sinks) a source
+    * identifier
+    * @param forceExists if true, require that the provided `name` paramater
+    * already exists in the global namespace
+    * @throws BoringUtilsException if name is expected to exist and it doesn't
     */
   def addSink(component: InstanceId, name: String, dedup: Boolean = false, forceExists: Boolean = false): Unit = {
     if (forceExists && !checkName(name)) {
@@ -109,6 +185,10 @@ object BoringUtils {
     *
     * @param source a source component
     * @param sinks one or more sink components
+    * @return the name of the signal used to connect the source to the
+    * sinks
+    * @note the returned name will be based on the name of the source
+    * component
     */
   def bore(source: Data, sinks: Seq[Data]): String = {
     lazy val genName = addSource(source, source.instanceName, true, true)
