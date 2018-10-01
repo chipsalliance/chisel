6d873c93993b37754d874331216584dea7822584
Abert Chen <albert.chen@sifive.com>
2018-10-01 13:09:28 -0700

    add HasBlackBoxPath trait

--- src/main/scala/chisel3/util/BlackBoxUtils.scala
+++ src/main/scala/chisel3/util/BlackBoxUtils.scala
@@ -4,12 +4,15 @@ package chisel3.util
 
 import chisel3._
 import chisel3.experimental.{ChiselAnnotation, RunFirrtlTransform}
-import firrtl.transforms.{BlackBoxResourceAnno, BlackBoxInlineAnno, BlackBoxSourceHelper}
+import firrtl.transforms.{BlackBoxPathAnno, BlackBoxResourceAnno, BlackBoxInlineAnno, BlackBoxSourceHelper}
 
 trait HasBlackBoxResource extends BlackBox {
   self: BlackBox =>
 
-  def setResource(blackBoxResource: String): Unit = {
+  @deprecated("Use addResource instead", "1.2")
+  def setResource(blackBoxResource: String): Unit = addResource(blackBoxResource)
+
+  def addResource(blackBoxResource: String): Unit = {
     val anno = new ChiselAnnotation with RunFirrtlTransform {
       def toFirrtl = BlackBoxResourceAnno(self.toNamed, blackBoxResource)
       def transformClass = classOf[BlackBoxSourceHelper]
@@ -24,6 +27,18 @@ trait HasBlackBoxInline extends BlackBox {
   def setInline(blackBoxName: String, blackBoxInline: String): Unit = {
     val anno = new ChiselAnnotation with RunFirrtlTransform {
       def toFirrtl = BlackBoxInlineAnno(self.toNamed, blackBoxName, blackBoxInline)
+      def transformClass = classOf[BlackBoxSourceHelper]
+    }
+    chisel3.experimental.annotate(anno)
+  }
+}
+
+trait HasBlackBoxPath extends BlackBox {
+  self: BlackBox =>
+
+  def addPath(blackBoxPath: String): Unit = {
+    val anno = new ChiselAnnotation with RunFirrtlTransform {
+      def toFirrtl = BlackBoxPathAnno(self.toNamed, blackBoxPath)
       def transformClass = classOf[BlackBoxSourceHelper]
     }
     chisel3.experimental.annotate(anno)
