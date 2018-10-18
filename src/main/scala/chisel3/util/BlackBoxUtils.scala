0e2c2147402f84ce0930e4d3d5dc7db3692e9102
Abert Chen <albert.chen@sifive.com>
2018-10-18 14:55:57 -0700

    add ScalaDoc

--- src/main/scala/chisel3/util/BlackBoxUtils.scala
+++ src/main/scala/chisel3/util/BlackBoxUtils.scala
@@ -12,6 +12,14 @@ trait HasBlackBoxResource extends BlackBox {
   @deprecated("Use addResource instead", "3.2")
   def setResource(blackBoxResource: String): Unit = addResource(blackBoxResource)
 
+  /** Copies a resource file to the target directory
+    *
+    * Resource files are located in project_root/src/main/resources/.
+    * Example of adding the resource file project_root/src/main/resources/blackbox.v:
+    * {{{
+    * addResource("/blackbox.v")
+    * }}}
+    */
   def addResource(blackBoxResource: String): Unit = {
     val anno = new ChiselAnnotation with RunFirrtlTransform {
       def toFirrtl = BlackBoxResourceAnno(self.toNamed, blackBoxResource)
@@ -36,6 +44,12 @@ trait HasBlackBoxInline extends BlackBox {
 trait HasBlackBoxPath extends BlackBox {
   self: BlackBox =>
 
+  /** Copies a file to the target directory
+    *
+    * This works with absolute and relative paths. Relative paths are relative
+    * to the current working directory, which is generally not the same as the
+    * target directory.
+    */
   def addPath(blackBoxPath: String): Unit = {
     val anno = new ChiselAnnotation with RunFirrtlTransform {
       def toFirrtl = BlackBoxPathAnno(self.toNamed, blackBoxPath)
