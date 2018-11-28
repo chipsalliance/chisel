205fe11ad9d1be35718978e0217bd158d547c10d
Jack Koenig <koenig@sifive.com>
2018-11-27 19:51:05 -0800

    Add some width tests

--- src/test/scala/chiselTests/WidthSpec.scala
+++ src/test/scala/chiselTests/WidthSpec.scala
@@ -14,4 +14,99 @@ class WidthSpec extends ChiselFlatSpec {
     1.U.getWidth should be (1)
     1.S.getWidth should be (2)
   }
+
+  behavior of "WireInit (Single Argument)"
+
+  it should "set width if passed a literal with forced width" in {
+    assertKnownWidth(4) {
+      WireInit(3.U(4.W))
+    }
+  }
+
+  it should "NOT set width if passed a literal without a forced width" in {
+    assertInferredWidth(4) {
+      val w = WireInit(3.U)
+      w := 3.U(4.W)
+      w
+    }
+  }
+
+  it should "NOT set width if passed a non-literal" in {
+    assertInferredWidth(4) {
+      val w = WireInit(3.U(4.W))
+      WireInit(w)
+    }
+  }
+
+  behavior of "WireInit (Double Argument)"
+
+  it should "set the width if the template type has a set width" in {
+    assertKnownWidth(4) {
+      WireInit(UInt(4.W), 0.U)
+    }
+    assertKnownWidth(4) {
+      WireInit(UInt(4.W), 0.U(2.W))
+    }
+  }
+
+  it should "infer the width if the template type has no width" in {
+    val templates = Seq(
+      () => 0.U, () => 0.U(2.W), () => WireInit(0.U), () => WireInit(0.U(2.W))
+    )
+    for (gen <- templates) {
+      assertInferredWidth(4) {
+        val w = WireInit(UInt(), gen())
+        w := 0.U(4.W)
+        w
+      }
+    }
+  }
+
+  behavior of "RegInit (Single Argument)"
+
+  it should "set width if passed a literal with forced width" in {
+    assertKnownWidth(4) {
+      RegInit(3.U(4.W))
+    }
+  }
+
+  it should "NOT set width if passed a literal without a forced width" in {
+    assertInferredWidth(4) {
+      val w = RegInit(3.U)
+      w := 3.U(4.W)
+      w
+    }
+  }
+
+  it should "NOT set width if passed a non-literal" in {
+    assertInferredWidth(4) {
+      val w = WireInit(3.U(4.W))
+      RegInit(w)
+    }
+  }
+
+  behavior of "RegInit (Double Argument)"
+
+  it should "set the width if the template type has a set width" in {
+    assertKnownWidth(4) {
+      RegInit(UInt(4.W), 0.U)
+    }
+    assertKnownWidth(4) {
+      RegInit(UInt(4.W), 0.U(2.W))
+    }
+  }
+
+  it should "infer the width if the template type has no width" in {
+    val templates = Seq(
+      () => 0.U, () => 0.U(2.W), () => WireInit(0.U), () => WireInit(0.U(2.W))
+    )
+    for (gen <- templates) {
+      assertInferredWidth(4) {
+        val w = RegInit(UInt(), gen())
+        w := 0.U(4.W)
+        w
+      }
+    }
+  }
 }
+
