a26d0c4e966b97ff0c848df51b55540d0f93af1b
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-01-30 12:06:00 -0500

    Add chisel3.stage.phases.Elaborate Phase
    This adds an Elaborate Phase that expands ChiselGeneratorAnnotations
    into ChiselCircuitAnnotations and deletes the original.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

---
+++ src/main/scala/chisel3/stage/phases/Elaborate.scala
@@ -0,0 +1,43 @@
+// See LICENSE for license details.
+
+package chisel3.stage.phases
+
+import java.io.{PrintWriter, StringWriter}
+
+import chisel3.{ChiselException, Module}
+import chisel3.internal.ErrorLog
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselCircuitAnnotation, ChiselOptions}
+
+import firrtl.AnnotationSeq
+import firrtl.options.{OptionsException, Phase}
+import firrtl.options.Viewer.view
+
+/** Elaborate all [[chisel3.stage.ChiselGeneratorAnnotation]]s into [[chisel3.stage.ChiselCircuitAnnotation]]s.
+  */
+class Elaborate extends Phase {
+
+  /**
+    * @todo Change this to print to STDERR (`Console.err.println`)
+    */
+  def transform(annotations: AnnotationSeq): AnnotationSeq = annotations.flatMap {
+    case a: ChiselGeneratorAnnotation =>
+      try {
+        Some(a.elaborate)
+      } catch {
+        case e: OptionsException => throw e
+        case e: ChiselException =>
+          val copts = view[ChiselOptions](annotations)
+          val stackTrace = if (!copts.printFullStackTrace) {
+            e.chiselStackTrace
+          } else {
+            val s = new StringWriter
+            e.printStackTrace(new PrintWriter(s))
+            s.toString
+          }
+          Predef.augmentString(stackTrace).lines.foreach(line => println(s"${ErrorLog.errTag} $line"))
+          Some(a)
+      }
+    case a => Some(a)
+  }
+
+}
