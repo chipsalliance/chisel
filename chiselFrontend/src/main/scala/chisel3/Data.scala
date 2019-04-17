2dcc7229c1e4089119cace6575798cfc48189f8d
Jim Lawson <ucbjrl@berkeley.edu>
2019-04-17 10:50:24 -0700

    Move DontCare back into Data.scala inside package internal

--- chiselFrontend/src/main/scala/chisel3/Data.scala
+++ chiselFrontend/src/main/scala/chisel3/Data.scala
@@ -642,7 +642,7 @@ object WireDefault {
   }
 
   /** @usecase def apply[T <: Data](t: T, init: DontCare.type): T
-    *          Construct a [[Wire]] with a type template and a [[DontCare]] default
+    *   Construct a [[Wire]] with a type template and a [[DontCare]] default
     *   @param t The type template used to construct this [[Wire]]
     *   @param init The default connection to this [[Wire]], can only be [[DontCare]]
     *   @note This is really just a specialized form of `apply[T <: Data](t: T, init: T): T` with [[DontCare]]
@@ -673,4 +673,37 @@ object WireDefault {
     }).asInstanceOf[T]
     apply(model, init)
   }
+}
+
+package internal {
+/** RHS (source) for Invalidate API.
+  * Causes connection logic to emit a DefInvalid when connected to an output port (or wire).
+  */
+private[chisel3] object InternalDontCare extends Element {
+  // This object should be initialized before we execute any user code that refers to it,
+  //  otherwise this "Chisel" object will end up on the UserModule's id list.
+  // We make it private to chisel3 so it has to be accessed through the package object.
+
+  private[chisel3] override val width: Width = UnknownWidth()
+
+  bind(DontCareBinding(), SpecifiedDirection.Output)
+  override def cloneType: this.type = DontCare
+
+  override def toString: String = "DontCare()"
+
+  override def litOption: Option[BigInt] = None
+
+  def toPrintable: Printable = PString("DONTCARE")
+
+  private[chisel3] def connectFromBits(that: Bits)(implicit sourceInfo:  SourceInfo, compileOptions: CompileOptions): Unit = { // scalastyle:ignore line.size.limit
+    Builder.error("connectFromBits: DontCare cannot be a connection sink (LHS)")
+  }
+
+  def do_asUInt(implicit sourceInfo: chisel3.internal.sourceinfo.SourceInfo, compileOptions: CompileOptions): UInt = { // scalastyle:ignore line.size.limit
+    Builder.error("DontCare does not have a UInt representation")
+    0.U
+  }
+  // DontCare's only match themselves.
+  private[chisel3] def typeEquivalent(that: Data): Boolean = that == DontCare
+}
 }
