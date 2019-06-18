ec3e03092b83ae45c0b4988c630f279560d438b0
Jim Lawson <ucbjrl@berkeley.edu>
2019-06-18 16:31:59 -0700

    Respond to comments - don't be lazy. Independently calculate the bit length to verify correct operation.

--- src/test/scala/chiselTests/Math.scala
+++ src/test/scala/chiselTests/Math.scala
@@ -16,7 +16,7 @@ class Math extends ChiselPropSpec {
         val n = (1 << width) + offset
         if (n > 0) {
           val d = unsignedBitLength(n)
-          val t = if (n == 1) 1 else BigInt(n).bitLength
+          val t = if (offset < 0) width else width + 1
           d shouldEqual (t)
         }
       }
@@ -29,7 +29,18 @@ class Math extends ChiselPropSpec {
         for ( mult <- List(-1, +1)) {
           val n = ((1 << (width - 1)) + offset) * mult
           val d = signedBitLength(n)
-          val t = BigInt(n).bitLength + 1
+          val t = n match {
+            case -1 => 1
+            case 0 => 1
+            case 1 => 2
+            case 2 => 3
+            case _ =>
+              if (n > 0) {
+                if (offset < 0) width else width + 1
+              } else {
+                if (offset > 0) width + 1 else width
+              }
+          }
           d shouldEqual (t)
         }
       }
