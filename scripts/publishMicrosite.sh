d1d9f6e9714764581c75135fb06898091b8c5a94
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-08-02 18:21:23 -0400

    Bespoke publishing script
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -5,4 +5,12 @@ git config --global user.email "schuyler.eldridge@gmail.com"
 git config --global user.name "seldridge"
 git config --global push.default simple
 
-sbt ++$TRAVIS_SCALA_VERSION docs/publishMicrosite
+(
+  cd $HOME
+  git clone --branch gh-pages git@github.com:freechipsproject/www.chisel-lang.org site
+  cd site
+  rm -rf *
+  cp $TRAVIS_BUILD_DIR/docs/target/site/* .
+  git commit -m "Published from $TRAVIS_COMMIT"
+  git push origin gh-pages
+)
