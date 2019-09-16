abejgonzalez/firrtl

Deprecate Gender and add implicit Flow conversion
Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

src/main/scala/firrtl/passes/Checks.scala
@@ -482,7 +482,7 @@ object CheckFlows extends Pass {
     case SourceFlow => "source"
     case SinkFlow => "sink"
     case UnknownFlow => "unknown"
-    case DuplexFlow => "sourceOrSink"
+    case DuplexFlow => "duplex"
   }
 
   class WrongFlow(info:Info, mname: String, expr: String, wrong: Flow, right: Flow) extends PassException(
@@ -575,4 +575,21 @@ object CheckFlows extends Pass {
     errors.trigger()
     c
   }
+}
+
+@deprecated("Use 'CheckFlows'. This will be removed in 1.4", "1.3")
+object CheckGenders {
+
+  implicit def toStr(g: Gender): String = g match {
+    case MALE => "source"
+    case FEMALE => "sink"
+    case UNKNOWNGENDER => "unknown"
+    case BIGENDER => "sourceOrSink"
+  }
+
+  def run(c: Circuit): Circuit = CheckGenders.run(c)
+
+  @deprecated("Use 'CheckFlows.WrongFlow'. This will be removed in 1.4", "1.3")
+  class WrongGender(info:Info, mname: String, expr: String, wrong: Flow, right: Flow) extends PassException(
+    s"$info: [module $mname]  Expression $expr is used as a $wrong but can only be used as a $right.")
 }

chipsalliance/chisel

src/main/scala/firrtl/passes/Checks.scala
@@ -551,7 +551,7 @@ object CheckFlows extends Pass {
     case SourceFlow => "source"
     case SinkFlow => "sink"
     case UnknownFlow => "unknown"
-    case DuplexFlow => "sourceOrSink"
+    case DuplexFlow => "duplex"
   }
 
   class WrongFlow(info:Info, mname: String, expr: String, wrong: Flow, right: Flow) extends PassException(
@@ -644,4 +644,21 @@ object CheckFlows extends Pass {
     errors.trigger()
     c
   }
+}
+
+@deprecated("Use 'CheckFlows'. This object will be removed in 1.3", "1.2")
+object CheckGenders {
+
+  implicit def toStr(g: Gender): String = g match {
+    case MALE => "source"
+    case FEMALE => "sink"
+    case UNKNOWNGENDER => "unknown"
+    case BIGENDER => "sourceOrSink"
+  }
+
+  def run(c: Circuit): Circuit = CheckFlows.run(c)
+
+  @deprecated("Use 'CheckFlows.WrongFlow'. This class will be removed in 1.3", "1.2")
+  class WrongGender(info:Info, mname: String, expr: String, wrong: Flow, right: Flow) extends PassException(
+    s"$info: [module $mname]  Expression $expr is used as a $wrong but can only be used as a $right.")
 }