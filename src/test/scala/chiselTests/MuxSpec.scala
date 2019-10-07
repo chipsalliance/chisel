2f3daab305b85a0b688d4147251cddc0dd901d75
Albert Magyar <albert.magyar@gmail.com>
2019-10-07 13:08:47 -0700

    Add test case for exhaustive MuxLookup optimization

--- src/test/scala/chiselTests/MuxSpec.scala
+++ src/test/scala/chiselTests/MuxSpec.scala
@@ -3,6 +3,7 @@
 package chiselTests
 
 import chisel3._
+import chisel3.util.{MuxLookup, log2Ceil}
 import chisel3.testers.BasicTester
 
 class MuxTester extends BasicTester {
@@ -24,4 +25,38 @@ class MuxSpec extends ChiselFlatSpec {
   "Mux" should "pass basic checks" in {
     assertTesterPasses { new MuxTester }
   }
+}
+
+class MuxLookupWrapper(keyWidth: Int, default: Int, mapping: () => Seq[(UInt, UInt)]) extends RawModule {
+  val outputWidth = log2Ceil(default).max(keyWidth) // make room for default value
+  val key = IO(Input(UInt(keyWidth.W)))
+  val output = IO(Output(UInt(outputWidth.W)))
+  output := MuxLookup(key, default.U, mapping())
+}
+
+class MuxLookupExhaustiveSpec extends ChiselPropSpec {
+  val keyWidth = 2
+  val default = 9 // must be less than 10 to avoid hex/decimal mismatches
+
+  // Assumes there are no temps with '9' in the name -> fails conservatively
+  // Assumes no binary recoding in output
+
+  val incomplete = { () => Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 3.U) }
+  property("The default value should not be optimized away for an incomplete MuxLookup") {
+    val c = Driver.emit { () => new MuxLookupWrapper(keyWidth, default, incomplete) }
+    c.contains(default.toString) should be (true) // not optimized away
+  }
+
+  val exhaustive = { () => Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 3.U, 3.U -> 0.U) }
+  property("The default value should be optimized away for an exhaustive MuxLookup") {
+    val c = Driver.emit { () => new MuxLookupWrapper(keyWidth, default, exhaustive) }
+    c.contains(default.toString) should be (false) // optimized away
+  }
+
+  val overlap = { () => Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 3.U, 4096.U -> 0.U) }
+  property("The default value should be optimized away for a MuxLookup with 2^{keyWidth} non-distinct mappings") {
+    val c = Driver.emit { () => new MuxLookupWrapper(keyWidth, default, overlap) }
+    c.contains(default.toString) should be (true) // not optimized away
+  }
+
 }
