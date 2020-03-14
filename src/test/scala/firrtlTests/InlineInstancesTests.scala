7ddbdc36506865731ba633c54d5110b77576362a
Jack Koenig <koenig@sifive.com>
2020-03-13 19:49:25 -0700

    Make InlineInstances invalidate ResolveKinds
    Fixes #1453

--- src/test/scala/firrtlTests/InlineInstancesTests.scala
+++ src/test/scala/firrtlTests/InlineInstancesTests.scala
@@ -2,9 +2,14 @@
 
 package firrtlTests
 
+import firrtl._
 import firrtl.annotations._
-import firrtl.passes.{InlineAnnotation, InlineInstances}
+import firrtl.passes.{InlineAnnotation, InlineInstances, ResolveKinds}
 import firrtl.transforms.NoCircuitDedupAnnotation
+import firrtl.stage.TransformManager
+import firrtl.options.Dependency
+
+import FirrtlCheckers._
 
 /**
  * Tests inline instances transformation
@@ -536,6 +541,28 @@ class InlineInstancesTests extends LowTransformSpec {
          DummyAnno(top.instOf("i_bar", "NestedNoInline").ref("foo_b"))
        )
      )
+  }
+
+  "InlineInstances" should "properly invalidate ResolveKinds" in {
+    val input =
+      """circuit Top :
+        |  module Top :
+        |    input a : UInt<32>
+        |    output b : UInt<32>
+        |    inst i of Inline
+        |    i.a <= a
+        |    b <= i.b
+        |  module Inline :
+        |    input a : UInt<32>
+        |    output b : UInt<32>
+        |    b <= a""".stripMargin
+
+    val state = CircuitState(parse(input), ChirrtlForm, Seq(inline("Inline")))
+    val manager = new TransformManager(Seq(Dependency[InlineInstances], Dependency(ResolveKinds)))
+    val result = manager.execute(state)
+
+    result shouldNot containTree { case WRef("i_a", _, PortKind, _) => true }
+    result should    containTree { case WRef("i_a", _, WireKind, _) => true }
   }
 }
 
