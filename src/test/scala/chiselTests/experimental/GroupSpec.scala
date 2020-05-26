fb7e612609427fbfc8ce8803ee70f717fb47e603
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2020-05-26 15:14:11 -0700

    Added more tests

--- src/test/scala/chiselTests/experimental/GroupSpec.scala
+++ src/test/scala/chiselTests/experimental/GroupSpec.scala
@@ -4,59 +4,115 @@ package chiselTests.experimental
 
 import chiselTests.ChiselFlatSpec
 import chisel3._
+import chisel3.RawModule
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselMain}
 import chisel3.util.experimental.group
 import firrtl.analyses.InstanceGraph
-import firrtl.options.{Dependency, TargetDirAnnotation}
+import firrtl.options.TargetDirAnnotation
 import firrtl.stage.CompilerAnnotation
-import firrtl.{EmittedFirrtlCircuitAnnotation, FirrtlExecutionSuccess, LowFirrtlCompiler, LowFirrtlEmitter, ir => fir}
+import firrtl.{LowFirrtlCompiler, ir => fir}
 
 import scala.collection.mutable
 
 class GroupSpec extends ChiselFlatSpec {
 
-  class MyModule extends Module {
-    val io = IO(new Bundle{
-      val a = Input(Bool())
-      val b = Output(Bool())
-    })
-    val reg1 = RegInit(0.U)
-    reg1 := io.a
-    val reg2 = RegNext(reg1)
-    io.b := reg2
-    group(Seq(reg1, reg2), "DosRegisters", "doubleReg")
-  }
-
   def collectInstances(c: fir.Circuit, top: Option[String] = None): Seq[String] = new InstanceGraph(c)
     .fullHierarchy.values.flatten.toSeq
     .map( v => (top.getOrElse(v.head.name) +: v.tail.map(_.name)).mkString(".") )
 
-  def collectRegisters(m: fir.DefModule): Seq[String] = {
-    val regs = mutable.ArrayBuffer[String]()
+  def collectDeclarations(m: fir.DefModule): Set[String] = {
+    val decs = mutable.HashSet[String]()
     def onStmt(s: fir.Statement): fir.Statement = s.mapStmt(onStmt) match {
-      case r: fir.DefRegister => regs += r.name; r
+      case d: fir.IsDeclaration => decs += d.name; d
       case other => other
     }
     m.mapStmt(onStmt)
-    regs
+    decs.toSet
   }
 
-  "Module Grouping" should "compile to low FIRRTL" in {
-    val firrtlCircuit = (ChiselMain.stage.run(
+  def lower[T <: RawModule](gen: () => T): fir.Circuit = {
+    (ChiselMain.stage.run(
       Seq(
         CompilerAnnotation(new LowFirrtlCompiler()),
         TargetDirAnnotation("test_run_dir"),
-        ChiselGeneratorAnnotation( () => new MyModule)
+        ChiselGeneratorAnnotation(gen)
       )
     ) collectFirst {
       case firrtl.stage.FirrtlCircuitAnnotation(circuit) => circuit
     }).get
+  }
 
+  "Module Grouping" should "compile to low FIRRTL" in {
+
+    class MyModule extends Module {
+      val io = IO(new Bundle{
+        val a = Input(Bool())
+        val b = Output(Bool())
+      })
+      val reg1 = RegInit(0.U)
+      reg1 := io.a
+      val reg2 = RegNext(reg1)
+      io.b := reg2
+      group(Seq(reg1, reg2), "DosRegisters", "doubleReg")
+    }
+
+    val firrtlCircuit = lower(() => new MyModule)
     firrtlCircuit.modules.collect {
       case m: fir.Module if m.name == "MyModule" =>
-        Nil should be (collectRegisters(m))
+        Set("doubleReg") should be (collectDeclarations(m))
       case m: fir.Module if m.name == "DosRegisters" =>
-        Seq("reg1", "reg2") should be (collectRegisters(m))
+        Set("reg1", "reg2") should be (collectDeclarations(m))
+    }
+    val instances = collectInstances(firrtlCircuit, Some("MyModule")).toSet
+    Set("MyModule", "MyModule.doubleReg") should be (instances)
+  }
+
+  "Module Grouping" should "not include intermediate registers" in {
+    class MyModule extends Module {
+      val io = IO(new Bundle{
+        val a = Input(Bool())
+        val b = Output(Bool())
+      })
+      val reg1 = RegInit(0.U)
+      reg1 := io.a
+      val reg2 = RegNext(reg1)
+      val reg3 = RegNext(reg2)
+      io.b := reg3
+      group(Seq(reg1, reg3), "DosRegisters", "doubleReg")
+    }
+
+    val firrtlCircuit = lower(() => new MyModule)
+    firrtlCircuit.modules.collect {
+      case m: fir.Module if m.name == "MyModule" =>
+        Set("reg2", "doubleReg") should be (collectDeclarations(m))
+      case m: fir.Module if m.name == "DosRegisters" =>
+        Set("reg1", "reg3") should be (collectDeclarations(m))
+    }
+    val instances = collectInstances(firrtlCircuit, Some("MyModule")).toSet
+    Set("MyModule", "MyModule.doubleReg") should be (instances)
+  }
+
+  "Module Grouping" should "include intermediate wires" in {
+    class MyModule extends Module {
+      val io = IO(new Bundle{
+        val a = Input(Bool())
+        val b = Output(Bool())
+      })
+      val reg1 = RegInit(0.U)
+      reg1 := io.a
+      val wire = WireInit(reg1)
+      val reg3 = RegNext(wire)
+      io.b := reg3
+      group(Seq(reg1, reg3), "DosRegisters", "doubleReg")
+    }
+
+    val firrtlCircuit = lower(() => new MyModule)
+    println(firrtlCircuit.serialize)
+    firrtlCircuit.modules.collect {
+      case m: fir.Module if m.name == "MyModule" =>
+        Set("doubleReg") should be (collectDeclarations(m))
+      case m: fir.Module if m.name == "DosRegisters" =>
+        Set("reg1", "reg3", "wire") should be (collectDeclarations(m))
     }
     val instances = collectInstances(firrtlCircuit, Some("MyModule")).toSet
     Set("MyModule", "MyModule.doubleReg") should be (instances)
