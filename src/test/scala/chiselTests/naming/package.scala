38a18a2907dc5e00798411e65318803bbc77c71d
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2020-05-27 10:32:26 -0700

    Add documentation to aspectTest

--- src/test/scala/chiselTests/naming/package.scala
+++ src/test/scala/chiselTests/naming/package.scala
@@ -7,13 +7,17 @@ import chisel3.aop.Aspect
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage, NoRunFirrtlCompilerAnnotation}
 import firrtl.AnnotationSeq
 
-import scala.reflect.ClassTag
-
 package object naming {
-  def run[T <: RawModule](gen: () => T, annotations: AnnotationSeq): AnnotationSeq = {
+  private def run[T <: RawModule](gen: () => T, annotations: AnnotationSeq): AnnotationSeq = {
     new ChiselStage().run(Seq(ChiselGeneratorAnnotation(gen), NoRunFirrtlCompilerAnnotation) ++ annotations)
   }
-  def aspectTest[T <: RawModule](gen: () => T)(f: T => Unit)(implicit ctag: ClassTag[T]): Unit = {
+
+  /** A tester which runs generator and uses an aspect to check the returned object
+    * @param gen function to generate a Chisel module
+    * @param f a function to check the Chisel module
+    * @tparam T the Chisel module class
+    */
+  def aspectTest[T <: RawModule](gen: () => T)(f: T => Unit): Unit = {
     case object BuiltAspect extends Aspect[T] {
       override def toAnnotation(top: T): AnnotationSeq = {f(top); Nil}
     }
