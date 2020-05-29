5ea1bba6bdae43c4757559bbc5829fd5693aff67
Albert Magyar <albert.magyar@gmail.com>
2020-05-28 19:52:35 -0700

    Add prototype for lint rule: referential equality on hardware

--- scalafix/input/src/main/scala/fix/ChiselLinter.scala
+++ scalafix/input/src/main/scala/fix/ChiselLinter.scala
@@ -7,4 +7,7 @@ import chisel3._
 
 object ChiselLinter {
   123.U(6) // assert: ChiselLinter
+  val x = 3.U
+  val y = 3.U
+  y == x // assert: ChiselLinter
 }
--- scalafix/rules/src/main/scala/fix/ChiselLinter.scala
+++ scalafix/rules/src/main/scala/fix/ChiselLinter.scala
@@ -9,12 +9,30 @@ case class AmbiguousLiteralExtract(lit: Lit.Int) extends Diagnostic {
     s"Passing an Int to .U is usually a mistake: it does not set the width but does a bit extract."
 }
 
+case class ReferentialEqualityOnData(eq: Term.ApplyInfix) extends Diagnostic {
+  override def position: Position = eq.pos
+  override def message: String =
+    s"Using == on a hardware type (e.g. UInt) is usually a mistake: did you mean to use the === operator?"
+}
+
 class ChiselLinter extends SemanticRule("ChiselLinter") {
+
+  private def isChiselType(symbol: Symbol)(implicit doc: SemanticDocument): Boolean = symbol.info match {
+    case Some(i) =>
+      i.signature match {
+        case TypeSignature(_, _, upper: TypeRef) => upper.symbol.toString.contains("chisel3")
+        case MethodSignature(_, _, tr: TypeRef) => tr.symbol.toString.contains("chisel3")
+        case _ => false
+      }
+    case _ => false
+  }
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
       case Term.Apply(Term.Select(Lit.Int(_), Term.Name("U")), Seq(il: Lit.Int)) =>
         Patch.lint(AmbiguousLiteralExtract(il))
+      case eq @ Term.ApplyInfix(lhs, Term.Name("=="), _, _) if (isChiselType(lhs.symbol)) =>
+        Patch.lint(ReferentialEqualityOnData(eq))
     }.asPatch
   }
 
