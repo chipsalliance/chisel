03ed04f1ede451a08f2e1679ef2c1601e561df17
Albert Magyar <albert.magyar@gmail.com>
2020-05-28 18:47:49 -0700

    Add lint rule for ambiguous lit extract: e.g. 123.U(5)

--- scalafix/rules/src/main/scala/fix/ChiselLinter.scala
+++ scalafix/rules/src/main/scala/fix/ChiselLinter.scala
@@ -3,8 +3,8 @@ package fix
 import scalafix.v1._
 import scala.meta._
 
-case class AmbiguousLiteralExtract(pos: Position) extends Diagnostic {
-  override def position: Position = pos
+case class AmbiguousLiteralExtract(lit: Lit.Int) extends Diagnostic {
+  override def position: Position = lit.pos
   override def message: String =
     s"Passing an Int to .U is usually a mistake: it does not set the width but does a bit extract."
 }
@@ -12,7 +12,10 @@ case class AmbiguousLiteralExtract(pos: Position) extends Diagnostic {
 class ChiselLinter extends SemanticRule("ChiselLinter") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    ???
+    doc.tree.collect {
+      case Term.Apply(Term.Select(Lit.Int(_), Term.Name("U")), Seq(il: Lit.Int)) =>
+        Patch.lint(AmbiguousLiteralExtract(il))
+    }.asPatch
   }
 
 }
