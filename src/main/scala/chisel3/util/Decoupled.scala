734f450f77bdd067f27c148793822d81d24d01ea
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2020-06-16 16:48:39 -0700

    Added back transit name to queue

--- src/main/scala/chisel3/util/Decoupled.scala
+++ src/main/scala/chisel3/util/Decoupled.scala
@@ -288,8 +288,7 @@ object Queue
       q.io.enq.valid := enq.valid // not using <> so that override is allowed
       q.io.enq.bits := enq.bits
       enq.ready := q.io.enq.ready
-      //TransitName(q.io.deq, q)
-      q.io.deq
+      TransitName(q.io.deq, q)
     }
   }
 
