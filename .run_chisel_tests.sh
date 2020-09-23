8a993e85e989e2d89b12fe154366597df3f903b9
Jack Koenig <koenig@sifive.com>
2020-09-23 10:13:17 -0700

    Improve Travis Chisel tests (#1903)

--- .run_chisel_tests.sh
+++ .run_chisel_tests.sh
@@ -11,11 +11,10 @@ TREADLE_BRANCH="master"
 if git log --format=%B --no-merges ${TRAVIS_COMMIT_RANGE/.../..} | grep '\[skip chisel tests\]'; then
   exit 0
 else
+  sbt $SBT_ARGS publishLocal
   git clone https://github.com/freechipsproject/treadle.git --single-branch -b ${TREADLE_BRANCH} --depth 10
-  (cd treadle && sbt $SBT_ARGS +publishLocal)
+  (cd treadle && sbt $SBT_ARGS publishLocal)
   git clone https://github.com/ucb-bar/chisel3.git --single-branch -b ${CHISEL_BRANCH}
-  mkdir -p chisel3/lib
-  cp utils/bin/firrtl.jar chisel3/lib
   cd chisel3
-  sbt "set concurrentRestrictions in Global += Tags.limit(Tags.Test, 2)" clean test
+  sbt $SBT_ARGS test
 fi
