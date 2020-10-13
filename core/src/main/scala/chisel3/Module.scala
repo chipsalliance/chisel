929d3667452cd0366ca78e0e690437f7f4137993
Jack Koenig <koenig@sifive.com>
2020-10-12 21:02:27 -0700

    Make it possible to GC Data instances
    No longer create a pointer from parent to every HasId, only do it by
    default for BaseModules and MemBases. Add pointer from parent to Data
    upon binding the Data.
    
    * Add MemTypeBinding for port types of Mems
    
    This binding is similar to the SampleElementBinding for Vecs in that
    these Data are not truly hardware, but are represented in the FIRRTL IR
    and thus need some representation.
    
    * Call _onModuleClose on unbound Records
    
    This maintains some corner-case behavior that is nevertheless relied
    upon. It ensures that refs are set for the elements of Records, even if
    they are not bound to any real hardware.
    
    (cherry picked from commit 0a0d7c6aac4326f2127d6d95efa5a4e10c81946c)

--- core/src/main/scala/chisel3/Module.scala
+++ core/src/main/scala/chisel3/Module.scala
@@ -160,6 +160,8 @@ package experimental {
     */
   // TODO: seal this?
   abstract class BaseModule extends HasId {
+    _parent.foreach(_.addId(this))
+
     //
     // Builder Internals - this tracks which Module RTL construction belongs to.
     //
@@ -326,6 +328,17 @@ package experimental {
       }
 
       names
+    }
+
+    /** Invokes _onModuleClose on HasIds found via reflection but not bound to hardware
+      * (thus not part of _ids)
+      * This maintains old naming behavior for non-hardware Data
+      */
+    private[chisel3] def closeUnboundIds(names: HashMap[HasId, String]): Unit = {
+      val idLookup = _ids.toSet
+      for ((id, _) <- names if !idLookup(id)) {
+        id._onModuleClose
+      }
     }
 
     /** Compatibility function. Allows Chisel2 code which had ports without the IO wrapper to
