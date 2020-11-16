b249814df95f1bf3e8eae49ff84f41e1c85f8d29
Jiuyang Liu <liu@jiuyang.me>
2020-11-16 20:20:52 +0000

    make LazyLogging log to console by default. (#1961)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/main/scala/logger/Logger.scala
+++ src/main/scala/logger/Logger.scala
@@ -58,7 +58,7 @@ private class LoggerState {
   val classLevels = new scala.collection.mutable.HashMap[String, LogLevel.Value]
   val classToLevelCache = new scala.collection.mutable.HashMap[String, LogLevel.Value]
   var logClassNames = false
-  var stream:             PrintStream = System.out
+  var stream:             PrintStream = Console.out
   var fromInvoke:         Boolean = false // this is used to not have invokes re-create run-state
   var stringBufferOption: Option[Logger.OutputCaptor] = None
 
