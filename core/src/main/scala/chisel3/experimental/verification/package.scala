b5bdf045410bc8a4e46a716c55b64ff34a006dde
Pialex99 <alexandre.pinazza@epfl.ch>
2020-12-07 15:20:59 +0100

    Delayed assertions done

--- core/src/main/scala/chisel3/experimental/verification/package.scala
+++ core/src/main/scala/chisel3/experimental/verification/package.scala
@@ -3,74 +3,70 @@
 package chisel3
 package experimental
 
-import chisel3.{Bool, CompileOptions}
+import chisel3.{ Bool, CompileOptions }
 import chisel3.internal.Builder
-import chisel3.internal.Builder.pushCommand
-import chisel3.internal.firrtl.{Formal, Verification}
+import chisel3.internal.Builder.{ pushCommand, pushOp }
+import chisel3.internal.firrtl.{ ILit, PrimOp, DefPrim, Formal, Verification }
 import chisel3.internal.sourceinfo.SourceInfo
 
 package object verification {
   implicit class Delay[T <: Data](data: T) {
-    def in(delay : Int)(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): T = {
-      
-      val regs = for (i <- 0 until delay)
-        yield Reg(data.cloneTypeFull)
-      
-      regs.foldRight(data) { (current, last) =>
-        current := last
-        current
-      }
+    def in(delay: BigInt)(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): T = {
+      val clock = Builder.forcedClock
+      scala.Predef.require(delay > 0, "A signal can only be mesured in a positive amount of clock cycles")
+      requireIsHardware(data, "")
+      pushOp(DefPrim(sourceInfo, data.cloneTypeFull, PrimOp.InOp, data.ref, ILit(delay)))
     }
   }
   object assert {
-    def apply(predicate: Bool, msg: String = "")(
-      implicit sourceInfo: SourceInfo,
-      compileOptions: CompileOptions): Unit = {
+    def apply(
+      predicate: Bool,
+      msg: String = ""
+    )(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): Unit = {
       val clock = Builder.forcedClock
-      pushCommand(Verification(Formal.Assert, sourceInfo, clock.ref,
-        predicate.ref, msg))
+      pushCommand(Verification(Formal.Assert, sourceInfo, clock.ref, predicate.ref, msg))
     }
   }
 
   object assume {
-    def apply(predicate: Bool, msg: String = "")(
-      implicit sourceInfo: SourceInfo,
-      compileOptions: CompileOptions): Unit = {
+    def apply(
+      predicate: Bool,
+      msg: String = ""
+    )(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): Unit = {
       val clock = Builder.forcedClock
-      pushCommand(Verification(Formal.Assume, sourceInfo, clock.ref,
-        predicate.ref, msg))
+      pushCommand(Verification(Formal.Assume, sourceInfo, clock.ref, predicate.ref, msg))
     }
   }
 
   object cover {
-    def apply(predicate: Bool, msg: String = "")(
-      implicit sourceInfo: SourceInfo,
-      compileOptions: CompileOptions): Unit = {
+    def apply(
+      predicate: Bool,
+      msg: String = ""
+    )(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): Unit = {
       val clock = Builder.forcedClock
-      pushCommand(Verification(Formal.Cover, sourceInfo, clock.ref,
-        predicate.ref, msg))
+      pushCommand(Verification(Formal.Cover, sourceInfo, clock.ref, predicate.ref, msg))
     }
   }
 
   object require {
-    def apply(predicate: Bool, msg: String = "")(
-      implicit sourceInfo: SourceInfo,
-      compileOptions: CompileOptions): Unit = {
+    def apply(
+      predicate: Bool,
+      msg: String = ""
+    )(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): Unit = {
       val clock = Builder.forcedClock
       //push an assume to the parent
-      
-      pushCommand(Verification(Formal.Require, sourceInfo, clock.ref,
-        predicate.ref, msg))
+
+      pushCommand(Verification(Formal.Require, sourceInfo, clock.ref, predicate.ref, msg))
     }
   }
 
   object ensure {
-    def apply(predicate: Bool, msg: String = "")(
-      implicit sourceInfo: SourceInfo,
-      compileOptions: CompileOptions): Unit = {
+    def apply(
+      predicate: Bool,
+      msg: String = ""
+    )(implicit sourceInfo: SourceInfo, compileOptions: CompileOptions): Unit = {
       val clock = Builder.forcedClock
-      pushCommand(Verification(Formal.Ensure, sourceInfo, clock.ref,
-        predicate.ref, msg))
+      pushCommand(Verification(Formal.Ensure, sourceInfo, clock.ref, predicate.ref, msg))
     }
   }
 }
