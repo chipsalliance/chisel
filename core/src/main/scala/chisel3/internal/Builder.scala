871ac227442c3cb3884dc2d8867b4e862c5af18f
Megan Wachs <megan@sifive.com>
2021-01-11 07:19:17 -0800

    Make `toTarget` fail if called on a Literal (or would otherwise not serialize properly) (#1714)
    * Add (failing) Test for Data toTarget calls
    
    Add scaladoc and clean up test
    
    * Builder: don't let .toTarget pass if it won't be able to deserialize properly later
    
    * Update src/test/scala/chiselTests/ReferenceTargetSpec.scala
    
    * Rename and simplify tests for literal toTarget
    
    (cherry picked from commit b578aa5e45e4ac5c2a71dda42367863a577ccaa4)

--- core/src/main/scala/chisel3/internal/Builder.scala
+++ core/src/main/scala/chisel3/internal/Builder.scala
@@ -9,6 +9,7 @@ import chisel3.experimental._
 import chisel3.internal.firrtl._
 import chisel3.internal.naming._
 import _root_.firrtl.annotations.{CircuitName, ComponentName, IsMember, ModuleName, Named, ReferenceTarget}
+import _root_.firrtl.annotations.AnnotationUtils.{validComponentName}
 import chisel3.internal.Builder.Prefix
 import logger.LazyLogging
 
@@ -276,6 +277,7 @@ private[chisel3] trait NamedComponent extends HasId {
     */
   final def toTarget: ReferenceTarget = {
     val name = this.instanceName
+    if (!validComponentName(name)) throwException(s"Illegal component name: $name (note: literals are illegal)")
     import _root_.firrtl.annotations.{Target, TargetToken}
     Target.toTargetTokens(name).toList match {
       case TargetToken.Ref(r) :: components => ReferenceTarget(this.circuitName, this.parentModName, Nil, r, components)
