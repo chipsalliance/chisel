edfbcadd8b353504f8138d98c76f61c34e5bff08
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2021-02-03 23:36:01 -0500

    Fix stack trace trimming across ChiselStage
    Fix bug in stack trace trimming behavior. Now, the following is what
    happens:
    
    1. The Builder, if catching accumulated errors, will now throw a
       ChiselException with a Scala-trimmed Stack trace. Previously, this
       would throw the full excpetion.
    2. The Elaborate phase handles stack trace trimming. By default, any
       Throwable thrown during elaboration will have its stack
       trace *mutably* trimmed and is rethrown. A logger.error is printed
       stating that there was an error during elaboration and how the user
       can turn on the full stack trace. If the --full-stacktrace option
       is on, then the Throwable is not caught and only the first
       logger.error (saying that elaboration failed) will be printed.
    3. ChiselStage (the class), ChiselStage$ (the object), and ChiselMain
       all inherit the behavior of (2).
    
    Mutable stack trace trimming behavior is moved into an implicit
    class (previously this was defined on ChiselException only) so this
    can be applied to any Throwable.
    
    No StageErrors are now thrown anymore. However, StageErrors may still
    be caught by ChiselMain (since it is a StageMain).
    
    Testing is added for ChiselMain, ChiselStage, and ChiselStage$ to test
    all this behavior.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/main/scala/chisel3/stage/phases/Elaborate.scala
+++ src/main/scala/chisel3/stage/phases/Elaborate.scala
@@ -6,6 +6,7 @@ import java.io.{PrintWriter, StringWriter}
 
 import chisel3.ChiselException
 import chisel3.internal.ErrorLog
+import chisel3.internal.ExceptionHelpers.ThrowableHelpers
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselOptions}
 import firrtl.AnnotationSeq
 import firrtl.options.Viewer.view
@@ -21,8 +22,18 @@ class Elaborate extends Phase {
   override def invalidates(a: Phase) = false
 
   def transform(annotations: AnnotationSeq): AnnotationSeq = annotations.flatMap {
-    case a: ChiselGeneratorAnnotation => a.elaborate
-    case a                            => Some(a)
+    case a: ChiselGeneratorAnnotation => try {
+      a.elaborate
+    } catch {
+      /* if any throwable comes back and we're in "stack trace trimming" mode, then print an error and trim the stack trace
+       */
+      case scala.util.control.NonFatal(a) =>
+        if (!view[ChiselOptions](annotations).printFullStackTrace) {
+          a.trimStackTraceToUserCode()
+        }
+        throw(a)
+    }
+    case a => Some(a)
   }
 
 }
