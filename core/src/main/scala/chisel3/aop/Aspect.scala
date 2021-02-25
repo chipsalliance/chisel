ac0dc0e470999e754172ded7224eade451d63d53
Jiuyang Liu <liujiuyang1994@gmail.com>
2021-02-25 22:26:22 +0000

    add scaladoc, rename confusing name.

--- core/src/main/scala/chisel3/aop/Aspect.scala
+++ core/src/main/scala/chisel3/aop/Aspect.scala
@@ -12,7 +12,10 @@ import firrtl.AnnotationSeq
   * @tparam T Type of top-level module
   */
 abstract class Aspect[T <: RawModule] extends Annotation with Unserializable with NoTargetAnnotation {
-  var annotations: AnnotationSeq = Seq()
+  /** variable to save [[AnnotationSeq]] from [[chisel3.stage.phases.AspectPhase]]
+    * to be used at [[chisel3.aop.injecting.InjectorAspect]], exposes annotations to [[chisel3.internal.DynamicContext]]
+    */
+  private[aop] var annotationsInAspect: AnnotationSeq = Seq()
   /** Convert this Aspect to a seq of FIRRTL annotation
     * @param top
     * @return
@@ -24,7 +27,7 @@ abstract class Aspect[T <: RawModule] extends Annotation with Unserializable wit
     * @return
     */
   private[chisel3] def resolveAspect(top: RawModule, remainingAnnotations: AnnotationSeq): AnnotationSeq = {
-    annotations = remainingAnnotations
+    annotationsInAspect = remainingAnnotations
     toAnnotation(top.asInstanceOf[T])
   }
 }
