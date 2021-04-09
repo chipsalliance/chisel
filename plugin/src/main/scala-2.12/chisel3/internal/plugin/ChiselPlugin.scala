953f965e3b213281c57b92cdbcfc9178222e96f9
Jiuyang Liu <liu@jiuyang.me>
2021-04-09 04:58:56 +0000

    Revert "Make plugin autoclonetype always on (#1826)"
    This reverts commit 26461d500f402310cb3adf914b636be3a3a8e442.

--- plugin/src/main/scala-2.12/chisel3/internal/plugin/ChiselPlugin.scala
+++ plugin/src/main/scala-2.12/chisel3/internal/plugin/ChiselPlugin.scala
@@ -5,9 +5,8 @@ package chisel3.internal.plugin
 import scala.tools.nsc
 import nsc.Global
 import nsc.plugins.{Plugin, PluginComponent}
-import scala.reflect.internal.util.NoPosition
 
-private[plugin] case class ChiselPluginArguments(var useBundlePlugin: Boolean = true) {
+private[plugin] case class ChiselPluginArguments(var useBundlePlugin: Boolean = false) {
   def useBundlePluginOpt = "useBundlePlugin"
   def useBundlePluginFullOpt = s"-P:chiselplugin:$useBundlePluginOpt"
 }
@@ -25,8 +24,7 @@ class ChiselPlugin(val global: Global) extends Plugin {
   override def init(options: List[String], error: String => Unit): Boolean = {
     for (option <- options) {
       if (option == arguments.useBundlePluginOpt) {
-        val msg = s"'${arguments.useBundlePluginFullOpt}' is now default behavior, you can stop using the scalacOption."
-        global.reporter.warning(NoPosition, msg)
+        arguments.useBundlePlugin = true
       } else {
         error(s"Option not understood: '$option'")
       }
