e6e37d413426ebdb6c262c60b3b9987710c4a4d7
Boyang Han <yqszxx@gmail.com>
2021-04-10 01:29:45 -0700

    Temporarily fix compile error

--- src/main/scala/chisel3/util/experimental/decode.scala
+++ src/main/scala/chisel3/util/experimental/decode.scala
@@ -7,7 +7,8 @@ import chisel3.util.experimental.minimizer._
 import scala.collection.mutable.ArrayBuffer
 
 object decode {
-  def apply(addr: UInt, default: BitPat, mapping: Iterable[(BitPat, BitPat)], minimizer: Minimizer = QMCMinimizer()): UInt = {
+  val minimizer = QMCMinimizer()
+  def apply(addr: UInt, default: BitPat, mapping: Iterable[(BitPat, BitPat)]): UInt = {
     val minimizedTable = minimizer.minimize(default, mapping.toSeq)
     val (inputs, orPlaneOutputs) = pla(minimizedTable)
     inputs := addr
@@ -19,28 +20,26 @@ object decode {
     outputs
   }
 
-  def apply(addr: UInt, default: Seq[BitPat], mappingIn: Iterable[(BitPat, Seq[BitPat])], minimizer: Minimizer = QMCMinimizer()): Seq[UInt] = {
+  def apply(addr: UInt, default: Seq[BitPat], mappingIn: Iterable[(BitPat, Seq[BitPat])]): Seq[UInt] = {
     val mapping = ArrayBuffer.fill(default.size)(ArrayBuffer[(BitPat, BitPat)]())
     for ((key, values) <- mappingIn)
       for ((value, i) <- values.zipWithIndex)
         mapping(i) += key -> value
     for ((thisDefault, thisMapping) <- default.zip(mapping))
-      yield apply(addr, thisDefault, thisMapping, minimizer)
+      yield apply(addr, thisDefault, thisMapping)
   }
 
-  def apply(addr: UInt, default: Seq[BitPat], mappingIn: List[(UInt, Seq[BitPat])], minimizer: Minimizer = QMCMinimizer()): Seq[UInt] =
+  def apply(addr: UInt, default: Seq[BitPat], mappingIn: List[(UInt, Seq[BitPat])]): Seq[UInt] =
     apply(
       addr,
       default,
-      mappingIn.map(m => (BitPat(m._1), m._2)).asInstanceOf[Iterable[(BitPat, Seq[BitPat])]],
-      minimizer
+      mappingIn.map(m => (BitPat(m._1), m._2)).asInstanceOf[Iterable[(BitPat, Seq[BitPat])]]
     )
 
-  def apply(addr: UInt, onSet: Iterable[UInt], offSet: Iterable[UInt], minimizer: Minimizer = QMCMinimizer()): Bool =
+  def apply(addr: UInt, onSet: Iterable[UInt], offSet: Iterable[UInt]): Bool =
     apply(
       addr,
       BitPat.dontCare(1),
-      onSet.map(BitPat(_) -> BitPat("b1")) ++ offSet.map(BitPat(_) -> BitPat("b0")),
-      minimizer
+      onSet.map(BitPat(_) -> BitPat("b1")) ++ offSet.map(BitPat(_) -> BitPat("b0"))
     ).asBool()
 }
