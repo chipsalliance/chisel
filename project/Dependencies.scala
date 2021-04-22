493988e6e8d0b3cdb80f93248ccd01424be31658
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2021-04-22 10:48:07 -0400

    Bump project dependencies
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
-  lazy val chisel3 = "edu.berkeley.cs" %% "chisel3" % "3.4.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
+  lazy val chisel3 = "edu.berkeley.cs" %% "chisel3" % "3.4.3"
 }
