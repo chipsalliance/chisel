5ec9f4a41108c45518fcc28a33935d59c8c65419
Boyang Han <yqszxx@gmail.com>
2021-05-11 05:55:38 -0700

    Add minimized form of test cases

--- src/test/scala/chiselTests/util/experimental/minimizer/MinimizerSpec.scala
+++ src/test/scala/chiselTests/util/experimental/minimizer/MinimizerSpec.scala
@@ -48,8 +48,8 @@ class MinimizerSpec extends AnyFlatSpec with Matchers {
     TruthTable(
       Map(
         BitPat("b000") -> BitPat("b0"),
-        // (BitPat("b001") -> BitPat("b?")),  // same as default, can be omitted
-        // (BitPat("b010") -> BitPat("b?")),  // same as default, can be omitted
+        // BitPat("b001") -> BitPat("b?"),  // same as default, can be omitted
+        // BitPat("b010") -> BitPat("b?"),  // same as default, can be omitted
         BitPat("b011") -> BitPat("b0"),
         BitPat("b100") -> BitPat("b1"),
         BitPat("b101") -> BitPat("b1"),
@@ -60,14 +60,8 @@ class MinimizerSpec extends AnyFlatSpec with Matchers {
     ),
     TruthTable(
       Map(
-        BitPat("b000") -> BitPat("b0"),
-        // (BitPat("b001") -> BitPat("b?")),  // same as default, can be omitted
-        // (BitPat("b010") -> BitPat("b?")),  // same as default, can be omitted
-        BitPat("b011") -> BitPat("b0"),
-        BitPat("b100") -> BitPat("b1"),
-        BitPat("b101") -> BitPat("b1"),
-        BitPat("b110") -> BitPat("b0"),
-        BitPat("b111") -> BitPat("b1")
+        BitPat("b10?") -> BitPat("b1"),
+        BitPat("b1?1") -> BitPat("b1"),
       ),
       BitPat("b?")
     )
@@ -78,25 +72,19 @@ class MinimizerSpec extends AnyFlatSpec with Matchers {
       Map(
         BitPat("b000") -> BitPat("b0"),
         BitPat("b001") -> BitPat("b0"),
-        // (BitPat("b010") -> BitPat("b?")),  // same as default, can be omitted
+        // BitPat("b010") -> BitPat("b?"),  // same as default, can be omitted
         (BitPat("b011") -> BitPat("b0")),
-        // (BitPat("b100") -> BitPat("b?")),  // same as default, can be omitted
-        // (BitPat("b101") -> BitPat("b?")),  // same as default, can be omitted
-        (BitPat("b110") -> BitPat("b1")),
-        (BitPat("b111") -> BitPat("b0"))
+        // BitPat("b100") -> BitPat("b?"),  // same as default, can be omitted
+        // BitPat("b101") -> BitPat("b?"),  // same as default, can be omitted
+        BitPat("b110") -> BitPat("b1"),
+        BitPat("b111") -> BitPat("b0")
       ),
       BitPat("b?")
     ),
     TruthTable(
       Map(
-        BitPat("b000") -> BitPat("b0"),
-        BitPat("b001") -> BitPat("b0"),
-        // (BitPat("b010") -> BitPat("b?")),  // same as default, can be omitted
-        (BitPat("b011") -> BitPat("b0")),
-        // (BitPat("b100") -> BitPat("b?")),  // same as default, can be omitted
-        // (BitPat("b101") -> BitPat("b?")),  // same as default, can be omitted
-        (BitPat("b110") -> BitPat("b1")),
-        (BitPat("b111") -> BitPat("b0"))
+        BitPat("b?10") -> BitPat("b1"),
+        // BitPat("b1?0") -> BitPat("b1"),  // both are ok
       ),
       BitPat("b?")
     ),
