ddce800313fe740092b20777dcd33a7c9a5804c9
Jiuyang Liu <liu@jiuyang.me>
2021-05-19 04:29:39 +0000

    remove InnerModule, since it should test in other places.

--- src/test/scala/chiselTests/ImplicitDriverSpec.scala
+++ src/test/scala/chiselTests/ImplicitDriverSpec.scala
@@ -2,30 +2,10 @@
 
 package chiselTests
 
-import chisel3._
 import chisel3.stage._
-import chisel3.stage.NoRunFirrtlCompilerAnnotation
-import chisel3.util.HasBlackBoxInline
 import org.scalatest.flatspec.AnyFlatSpec
 
 class ImplicitDriverSpec extends AnyFlatSpec {
-  class InnerModule extends Module {
-    class GCDIO extends Bundle {
-      val a = Input(UInt(32.W))
-      val b = Input(UInt(32.W))
-      val e = Input(Bool())
-      val z = Output(UInt(32.W))
-      val v = Output(Bool())
-    }
-    val io = IO(new GCDIO)
-    class GCDBlackBox extends BlackBox with HasBlackBoxInline {
-      val io = IO(new GCDIO)
-      setInline("gcd_blackbox.v", (new GCD).toVerilogString)
-    }
-    val bb = Module(new GCDBlackBox)
-    io <> bb.io
-  }
-
   "implicit driver" should "emit verilog without error" in {
     (new GCD).toVerilogString
   }
@@ -43,8 +23,5 @@ class ImplicitDriverSpec extends AnyFlatSpec {
   }
   "implicit driver" should "execute with annotations" in {
     (new GCD).execute()(Seq(NoRunFirrtlCompilerAnnotation))
-  }
-  "implicit driver" should "work under the builder context" in {
-    (new InnerModule).toVerilogString
   }
 }
