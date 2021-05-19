e1039328d351320873debccfeec6a524c25ad476
Jiuyang Liu <liu@jiuyang.me>
2021-05-19 03:21:21 +0000

    add an example to invoke emitVerilog under Builder context.

--- src/test/scala/chiselTests/ImplicitDriverSpec.scala
+++ src/test/scala/chiselTests/ImplicitDriverSpec.scala
@@ -2,10 +2,29 @@
 
 package chiselTests
 
+import chisel3._
+import chisel3.util.HasBlackBoxInline
 import chisel3.util.experimental.ImplicitDriver
 import org.scalatest.flatspec.AnyFlatSpec
 
 class ImplicitDriverSpec extends AnyFlatSpec {
+  class InnerModule extends Module {
+    class GCDIO extends Bundle {
+      val a = Input(UInt(32.W))
+      val b = Input(UInt(32.W))
+      val e = Input(Bool())
+      val z = Output(UInt(32.W))
+      val v = Output(Bool())
+    }
+    val io = IO(new GCDIO)
+    class GCDBlackBox extends BlackBox with HasBlackBoxInline {
+      val io = IO(new GCDIO)
+      setInline("gcd_blackbox.v", (new GCD).emitVerilog)
+    }
+    val bb = Module(new GCDBlackBox)
+    io <> bb.io
+  }
+
   "implicit driver" should "emit verilog without error" in {
     (new GCD).emitVerilog
   }
@@ -17,5 +36,8 @@ class ImplicitDriverSpec extends AnyFlatSpec {
   }
   "implicit driver" should "emit system verilog without error" in {
     (new GCD).emitSystemVerilog
+  }
+  "implicit driver" should "work under the builder context" in {
+    (new InnerModule).emitVerilog
   }
 }
