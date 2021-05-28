5bb6a379c4dfbd6d6fe5988721fb37b6fce22d3e
Jiuyang Liu <liu@jiuyang.me>
2021-05-28 08:39:07 +0000

    deprecate getPorts with modulePorts.

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -36,6 +36,7 @@ abstract class RawModule(implicit moduleCompileOptions: CompileOptions)
   //
   // For debuggers/testers, TODO: refactor out into proper public API
   private var _firrtlPorts: Option[Seq[firrtl.Port]] = None
+  @deprecated("Use DataMirror.modulePorts instead.", "Chisel 3.5")
   lazy val getPorts = _firrtlPorts.get
 
   val compileOptions = moduleCompileOptions
