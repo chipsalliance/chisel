c11fdf33981365927ea085b7aee095933a4a5ddf
Jiuyang Liu <liu@jiuyang.me>
2021-06-14 14:58:01 +0000

    add documentation for DecodeTableAnnotation.

--- src/main/scala/chisel3/util/experimental/decode/DecodeTableAnnotation.scala
+++ src/main/scala/chisel3/util/experimental/decode/DecodeTableAnnotation.scala
@@ -4,6 +4,15 @@ package chisel3.util.experimental.decode
 
 import firrtl.annotations.{Annotation, ReferenceTarget, SingleTargetAnnotation}
 
+/** DecodeTableAnnotation used to store a decode result for a specific [[TruthTable]].
+  * This is useful for saving large [[TruthTable]] during a elaboration time.
+  *
+  * @note user should manage the correctness of [[minimizedTable]].
+  *
+  * @param target output wire of a decoder.
+  * @param truthTable input [[TruthTable]] encoded in a serialized [[TruthTable]].
+  * @param minimizedTable minimized [[truthTable]] encoded in a serialized [[TruthTable]].
+  */
 case class DecodeTableAnnotation(
   target:         ReferenceTarget,
   truthTable:     String,
