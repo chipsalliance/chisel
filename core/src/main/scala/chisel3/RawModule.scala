a091889dad2f6849f76650d297c3b6e1cb48d278
Jack Koenig <koenig@sifive.com>
2021-06-28 14:07:03 -0700

    Set refs for ModuleClone and ClonePorts in less hacky way
    (cherry picked from commit b87107ad41e948de9da9c349505de414b1a9db7f)

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -8,6 +8,7 @@ import scala.language.experimental.macros
 
 import chisel3.experimental.BaseModule
 import chisel3.internal._
+import chisel3.internal.BaseModule.ModuleClone
 import chisel3.internal.Builder._
 import chisel3.internal.firrtl._
 import chisel3.internal.sourceinfo.UnlocatableSourceInfo
@@ -75,6 +76,7 @@ abstract class RawModule(implicit moduleCompileOptions: CompileOptions)
     // All suggestions are in, force names to every node.
     for (id <- getIds) {
       id match {
+        case id: ModuleClone => id.setRefAndPortsRef(_namespace) // special handling
         case id: BaseModule => id.forceName(None, default=id.desiredName, _namespace)
         case id: MemBase[_] => id.forceName(None, default="MEM", _namespace)
         case id: Data  =>
