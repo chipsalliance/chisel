8477fa8cb0e4b384411b32264418a474905701bf
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2021-07-14 14:04:22 -0600

    Set position of bad @public to be @public, not @instance

--- macros/src/main/scala/chisel3/internal/InstanceHandle.scala
+++ macros/src/main/scala/chisel3/internal/InstanceHandle.scala
@@ -6,19 +6,36 @@ import scala.reflect.macros.whitebox
 import scala.reflect.macros.blackbox
 
 
-object InstanceHandleMacro {
-  def impl(c: blackbox.Context)(annottees: c.Expr[Any]*) = {
-    import c.universe._
-    val inputs = annottees.map(_.tree).toList
-    val (annottee, expandees) = inputs match {
-      case (param: ValDef) :: (rest @ (_ :: _)) => (param, rest)
-      case (param: TypeDef) :: (rest @ (_ :: _)) => (param, rest)
-      case _ => (EmptyTree, inputs)
-    }
-    //println((annottee, expandees))
-    val outputs = expandees
-    c.Expr[Any](Block(outputs, Literal(Constant(()))))
-  }
+object publicMacro {
+  //def impl(c: blackbox.Context)(annottees: c.Tree*): c.Tree = {
+  //  import c.universe._
+  //  import Flag._
+  //  val result = {
+  //    //println(annottees)
+  //    val tpname = TypeName(c.internal.enclosingOwner.toString.stripPrefix("class "))
+  //    println(tpname)
+
+  //    annottees.toList match {
+  //      case Seq(x@q"val $valname: $tpe = $name") if valname.toString() == name.toString() =>
+  //         val name2 = TypeName(valname.+(c.freshName()))
+  //         val extension = q"def $valname: $tpe = module(_.$valname)"
+  //         println(s"POSITION: ${extension.pos}")
+  //         q""" implicit class $name2(module: chisel3.Instance[$tpname]) { $extension } """
+  //      case Seq(x@q"val $valname: $tpe = $_") =>
+  //         val name2 = TypeName(valname.+(c.freshName()))
+  //         val extension = q"def $valname: $tpe = module(_.$valname)"
+  //         println(s"XPOSITION: ${extension.pos}")
+  //         q"""
+  //           implicit class $name2(module: chisel3.Instance[$tpname]) { $extension }
+
+  //           $x
+  //           """
+  //    }
+  //  }
+  //  val x = q"..$result"
+  //  println(x)
+  //  x
+  //}
 }
 object instanceMacro {
   def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
@@ -35,11 +52,11 @@ object instanceMacro {
           val name2 = TypeName(tpname.+(c.freshName()))
           val extensions = scala.collection.mutable.ArrayBuffer[c.universe.Tree]()
           val newBodies = stats.flatMap {
-            case q"@public val $tpname: $tpe = $name" if tpname.toString() == name.toString() =>
-              extensions += q"def $tpname: $tpe = module(_.$tpname)"
+            case x@q"@public val $tpname: $tpe = $name" if tpname.toString() == name.toString() =>
+              extensions += atPos(x.pos)(q"def $tpname: $tpe = module(_.$tpname)")
               Nil
             case x@q"@public val $tpname: $tpe = $_" =>
-              extensions += q"def $tpname: $tpe = module(_.$tpname)"
+              extensions += atPos(x.pos)(q"def $tpname: $tpe = module(_.$tpname)")
               Seq(x)
             case other => Seq(other)
           }
@@ -71,5 +88,5 @@ class instance extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro instanceMacro.impl
 }
 class public extends StaticAnnotation {
-  //def macroTransform(annottees: Any*): Any = macro InstanceHandleMacro.impl
+  //def macroTransform(annottees: Any*): Any = macro publicMacro.impl
 }
