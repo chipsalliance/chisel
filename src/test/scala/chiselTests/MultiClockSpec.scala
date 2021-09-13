jared-barocsi/chisel3

refactor all tests.

src/test/scala/chiselTests/MultiClockSpec.scala
@@ -4,8 +4,9 @@ package chiselTests
 
 import chisel3._
 import chisel3.util.Counter
-import chisel3.testers.{BasicTester, TesterDriver}
+import chisel3.testers.TesterDriver
 import chisel3.stage.ChiselStage
+import chiselTests.testers.BasicTester
 
 /** Multi-clock test of a Reg using a different clock via withClock */
 class ClockDividerTest extends BasicTester {
@@ -120,7 +121,7 @@ class MultiClockSpec extends ChiselFlatSpec {
   }
 
   it should "scope ports of memories" in {
-    assertTesterPasses(new MultiClockMemTest, annotations = TesterDriver.verilatorOnly)
+    assertTesterPasses(new MultiClockMemTest, annotations = chiselTests.testers.TesterDriver.verilatorOnly)
   }
 
   it should "return like a normal Scala block" in {

chipsalliance/chisel

src/test/scala/chiselTests/MultiClockSpec.scala
@@ -3,9 +3,9 @@
 package chiselTests
 
 import chisel3._
-import chisel3.util.Counter
-import chisel3.testers.{BasicTester, TesterDriver}
 import chisel3.stage.ChiselStage
+import chisel3.util.Counter
+import chiselTests.testers.BasicTester
 
 /** Multi-clock test of a Reg using a different clock via withClock */
 class ClockDividerTest extends BasicTester {
@@ -120,7 +120,7 @@ class MultiClockSpec extends ChiselFlatSpec {
   }
 
   it should "scope ports of memories" in {
-    assertTesterPasses(new MultiClockMemTest, annotations = TesterDriver.verilatorOnly)
+    assertTesterPasses(new MultiClockMemTest, annotations = chiselTests.testers.TesterDriver.verilatorOnly)
   }
 
   it should "return like a normal Scala block" in {