cb66b7f8eb07e00a618ae36dbb5c013da521cbc0
Abongwa Bonalais <81782853+Burnleydev1@users.noreply.github.com>
2021-10-15 02:27:35 +0100

    Update Arbiter.scala

--- src/main/scala/chisel3/util/Arbiter.scala
+++ src/main/scala/chisel3/util/Arbiter.scala
@@ -100,7 +100,6 @@ class LockingArbiter[T <: Data](gen: T, n: Int, count: Int, needsLock: Option[T 
   * consumer.io.in <> arb.io.out
   * }}}
   */
-
 class RRArbiter[T <: Data](val gen: T, val n: Int) extends LockingRRArbiter[T](gen, n, 1)
 
 /** Hardware module that is used to sequence n producers into 1 consumer.
@@ -116,7 +115,6 @@ class RRArbiter[T <: Data](val gen: T, val n: Int) extends LockingRRArbiter[T](g
   * consumer.io.in <> arb.io.out
   * }}}
   */
-
 class Arbiter[T <: Data](val gen: T, val n: Int) extends Module {
   val io = IO(new ArbiterIO(gen, n))
 
