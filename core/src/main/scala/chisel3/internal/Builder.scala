d64d3f3cc6286ba1c6efa6b59738795be114dc01
Aditya Naik <91489422+adkian-sifive@users.noreply.github.com>
2021-10-20 00:13:34 -0400

    Update computeName and callsites (#2192)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit d6907893f019ee86573dc81768884150e541dba3)
    
    # Conflicts:
    #	core/src/main/scala/chisel3/internal/Builder.scala

--- core/src/main/scala/chisel3/internal/Builder.scala
+++ core/src/main/scala/chisel3/internal/Builder.scala
@@ -152,7 +152,7 @@ private[chisel3] trait HasId extends InstanceId {
     * @param defaultSeed Optionally provide default seed for computing the name
     * @return the name, if it can be computed
     */
-  def computeName(defaultPrefix: Option[String], defaultSeed: Option[String]): Option[String] = {
+  private[chisel3] def _computeName(defaultPrefix: Option[String], defaultSeed: Option[String]): Option[String] = {
     /** Computes a name of this signal, given the seed and prefix
       * @param seed
       * @param prefix
@@ -196,7 +196,7 @@ private[chisel3] trait HasId extends InstanceId {
   // (e.g. tried to suggest a name to part of a Record)
   private[chisel3] def forceName(prefix: Option[String], default: =>String, namespace: Namespace): Unit =
     if(_ref.isEmpty) {
-      val candidate_name = computeName(prefix, Some(default)).get
+      val candidate_name = _computeName(prefix, Some(default)).get
       val available_name = namespace.name(candidate_name)
       setRef(Ref(available_name))
     }
@@ -214,6 +214,23 @@ private[chisel3] trait HasId extends InstanceId {
   private[chisel3] def getRef: Arg = _ref.get
   private[chisel3] def getOptionRef: Option[Arg] = _ref
 
+<<<<<<< HEAD
+=======
+  private def refName(c: Component): String = _ref match {
+    case Some(arg) => arg fullName c
+    case None => _computeName(None, None).get
+  }
+
+  // Helper for reifying views if they map to a single Target
+  private[chisel3] def reifyTarget: Option[Data] = this match {
+    case d: Data => reifySingleData(d) // Only Data can be views
+    case bad => throwException(s"This shouldn't be possible - got $bad with ${_parent}")
+  }
+
+  // Helper for reifying the parent of a view if the view maps to a single Target
+  private[chisel3] def reifyParent: BaseModule = reifyTarget.flatMap(_._parent).getOrElse(ViewParent)
+
+>>>>>>> d6907893 (Update computeName and callsites (#2192))
   // Implementation of public methods.
   def instanceName: String = _parent match {
     case Some(p) => p._component match {
