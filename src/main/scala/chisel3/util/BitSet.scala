2cdf88a8b59713386e1ba5e51175b67d2b89a9e7
Jiuyang Liu <liu@jiuyang.me>
2021-10-27 16:44:25 +0800

    implement intersect subtract union.

--- src/main/scala/chisel3/util/BitSet.scala
+++ src/main/scala/chisel3/util/BitSet.scala
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chisel3.util
 
-trait BitSetFamily {
+trait BitSetFamily { bsf =>
   val terms: Set[BitSet]
   assert(terms.map(_.width).size < 1)
 
@@ -8,6 +10,8 @@ trait BitSetFamily {
   val width: Int = terms.headOption.map(_.width).getOrElse(0)
 
   override def toString: String = terms.toSeq.sortBy((t: BitSet) => (t.onSet, t.offSet)).mkString("\n")
+
+  def isEmpty: Boolean = terms.forall(_.isEmpty)
 
   def cover(that: BitSetFamily): Boolean =
     if (terms.isEmpty)
@@ -17,6 +21,18 @@ trait BitSetFamily {
         case (left, (a, b)) => a.cover(b) & left
       }
 
+  def intersect(that: BitSetFamily): BitSetFamily = new BitSetFamily {
+    val terms = bsf.terms.flatMap(a => that.terms.map(b => a.intersect(b))).filterNot(_.isEmpty)
+  }
+
+  def subtract(that: BitSetFamily): BitSetFamily = new BitSetFamily {
+    val terms = bsf.terms.flatMap(a => that.terms.map(b => a.subtract(b))).filterNot(_.isEmpty)
+  }
+
+  def union(that: BitSetFamily): BitSetFamily = new BitSetFamily {
+    val terms = bsf.terms ++ that.terms
+  }
+
   override def equals(obj: Any): Boolean = {
     obj match {
       case that: BitSetFamily => if (width == that.width) this.cover(that) && that.cover(this) else false
@@ -25,7 +41,7 @@ trait BitSetFamily {
   }
 }
 
-trait BitSet extends BitSetFamily {
+trait BitSet extends BitSetFamily { b =>
   override val terms = Set(this)
   val onSet:  BigInt
   val offSet: BigInt
@@ -33,8 +49,27 @@ trait BitSet extends BitSetFamily {
 
   assert(width > 0)
 
-  def cover(that: BitSet) = ((onSet & ~that.onSet) == 0) & ((offSet & ~that.offSet) == 0)
+  def cover(that: BitSet) = ((onSet & that.onSet) == that.onSet) & ((offSet & that.offSet) == that.offSet)
 
+  def intersect(that: BitSet): BitSet = {
+    require(width == that.width)
+    new BitSet {
+      val onSet:          BigInt = b.onSet & that.onSet
+      val offSet:         BigInt = b.offSet & that.offSet
+      override val width: Int = b.width
+    }
+  }
+
+  def subtract(that: BitSet): BitSet = {
+    require(width == that.width)
+    new BitSet {
+      val onSet:          BigInt = b.onSet & ~that.onSet
+      val offSet:         BigInt = b.offSet & ~that.offSet
+      override val width: Int = b.width
+    }
+  }
+
+  override def isEmpty: Boolean = (onSet | offSet) == 0
   override def toString: String = Seq
     .tabulate(width) { i =>
       (onSet.testBit(i), offSet.testBit(i)) match {
