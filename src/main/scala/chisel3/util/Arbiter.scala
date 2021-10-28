4d24de78f7300b64e06908d1aa92195d5eb13b46
Abongwa Bonalais <81782853+Burnleydev1@users.noreply.github.com>
2021-10-28 07:37:05 +0100

    Update src/main/scala/chisel3/util/Arbiter.scala
    Co-authored-by: Megan Wachs <megan@sifive.com>

--- src/main/scala/chisel3/util/Arbiter.scala
+++ src/main/scala/chisel3/util/Arbiter.scala
@@ -14,6 +14,7 @@ import chisel3.internal.naming.chiselName  // can't use chisel3_ version because
   */
 class ArbiterIO[T <: Data](private val gen: T, val n: Int) extends Bundle {
 // See github.com/freechipsproject/chisel3/issues/765 for why gen is a private val and proposed replacement APIs.
+
 /** Input data, one per potential sender
   * 
   * @group Signals
