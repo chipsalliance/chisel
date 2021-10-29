c0fbc26694bf402227e55ed5efca250cb1a5d54a
Ocean Shen <shenao6626@gmail.com>
2021-10-29 16:54:36 +0800

    Refactor BitSet impl and add ScalaDocs

--- src/main/scala/chisel3/util/BitSet.scala
+++ src/main/scala/chisel3/util/BitSet.scala
@@ -2,112 +2,108 @@
 
 package chisel3.util
 
-trait BitSetFamily { bsf =>
-  val terms: Set[BitSet]
+trait BitSet { bsf =>
+  val terms: Set[BitPat]
 
-  // set width = 0 if terms is empty.
-  lazy val width: Int = {
-    assert(terms.map(_.width).size < 1)
+  /**
+    * Get specified width of said BitSet
+    *
+    * All BitPat contained in one set should have the same width
+    */
+  def getWidth: Int = {
+    assert(terms.map(_.width).size <= 1)
+    // set width = 0 if terms is empty.
     terms.headOption.map(_.width).getOrElse(0)
   }
 
-  override def toString: String = terms.toSeq.sortBy((t: BitSet) => (t.onSet, t.offSet)).mkString("\n")
+  override def toString: String = terms.toSeq.sortBy((t: BitPat) => (t.mask, t.value)).mkString("\n")
 
+  /**
+    * @return whether the BitSetFamily is empty (i.e. no value matches)
+    */
   def isEmpty: Boolean = terms.forall(_.isEmpty)
 
-  def cover(that: BitSetFamily): Boolean =
-    if (terms.isEmpty)
-      that.terms.isEmpty
-    else
-      terms.flatMap(a => that.terms.map(b => (a, b))).foldLeft(true) {
-        case (left, (a, b)) => a.cover(b) & left
-      }
-
-  def intersect(that: BitSetFamily): BitSetFamily = new BitSetFamily {
-    val terms = bsf.terms.flatMap(a => that.terms.map(b => a.intersect(b))).filterNot(_.isEmpty)
+  /**
+    * @return whether this BitSetFamily overlap with that BitSetFamily, i.e. !(intersect.isEmpty)
+    */
+  def overlap(that: BitSet): Boolean = {
+    !bsf.terms.flatMap(a => that.terms.map(b => (a, b))).forall { case (a, b) => !a.overlap(b) }
   }
 
-  def subtract(that: BitSetFamily): BitSetFamily = new BitSetFamily {
-    val terms = bsf.terms.flatMap(a => that.terms.map(b => a.subtract(b))).filterNot(_.isEmpty)
-  }
+  /**
+    * @param that BitSetFamily to be covered
+    * @return whether this BitSetFamily covers that (i.e. forall b matches that, b also matches this)
+    */
+  def cover(that: BitSet): Boolean =
+    that.subtract(this).isEmpty
 
-  def union(that: BitSetFamily): BitSetFamily = new BitSetFamily {
+  /**
+    * @return a BitSetFamily that only match a value when both operand match
+    */
+  def intersect(that: BitSet): BitSet =
+    bsf.terms
+      .flatMap(a => that.terms.map(b => a.intersect(b)))
+      .filterNot(_.isEmpty)
+      .fold(BitSet.emptyBitSet)(_.union(_))
+
+  /**
+    * Subtract that from this BitSetFamily
+    * @param that subtrahend
+    */
+  def subtract(that: BitSet): BitSet =
+    bsf.terms.map { a =>
+      that.terms.map(b => a.subtract(b)).fold(a)(_.intersect(_))
+    }.filterNot(_.isEmpty).fold(BitSet.emptyBitSet)(_.union(_))
+
+  /**
+    * Union of two BitSetFamily
+    */
+  def union(that: BitSet): BitSet = new BitSet {
     val terms = bsf.terms ++ that.terms
   }
 
+  /**
+    * Test whether two BitSetFamily matches the same set of value
+    *
+    * Caution: This method can be very expensive compared to ordinary == operator between two Objects
+    */
   override def equals(obj: Any): Boolean = {
     obj match {
-      case that: BitSetFamily => if (width == that.width) this.cover(that) && that.cover(this) else false
+      case that: BitSet => if (this.getWidth == that.getWidth) this.cover(that) && that.cover(this) else false
       case _ => false
     }
   }
 }
 
-trait BitSet extends BitSetFamily { b =>
-  override val terms = Set(this)
-  val onSet:  BigInt
-  val offSet: BigInt
-  val width:  Int
-
-  assert(width > 0)
-
-  def cover(that: BitSet) = ((onSet & that.onSet) == that.onSet) & ((offSet & that.offSet) == that.offSet)
-
-  def intersect(that: BitSet): BitSet = {
-    require(width == that.width)
-    new BitSet {
-      val onSet:               BigInt = b.onSet & that.onSet
-      val offSet:              BigInt = b.offSet & that.offSet
-      override lazy val width: Int = b.width
-    }
-  }
-
-  def subtract(that: BitSet): BitSet = {
-    require(width == that.width)
-    new BitSet {
-      val onSet:               BigInt = b.onSet & ~that.onSet
-      val offSet:              BigInt = b.offSet & ~that.offSet
-      override lazy val width: Int = b.width
-    }
-  }
-
-  override def isEmpty: Boolean = (onSet | offSet) == 0
-  override def toString: String = Seq
-    .tabulate(width) { i =>
-      (onSet.testBit(i), offSet.testBit(i)) match {
-        case (true, true)   => "-"
-        case (true, false)  => "1"
-        case (false, true)  => "0"
-        case (false, false) => "~"
-      }
-    }
-    .mkString
-}
-
 object BitSet {
-  def apply(str: String): BitSetFamily = {
-    new BitSetFamily {
+  val emptyBitSet: BitSet = new BitSet {
+    override val terms = Set()
+  }
+
+  def apply(str: String): BitSet = {
+    new BitSet {
       val terms = str
         .split('\n')
-        .map(str =>
-          new BitSet {
-            val onSet: BigInt = str.zipWithIndex.map {
-              case ('-' | '1', i) => BigInt(1) << i
-              case ('~' | '0', _) => BigInt(0)
-              case _              => throw new Exception("bitset parse error")
-            }.sum
-            val offSet: BigInt = str.zipWithIndex.map {
-              case ('-' | '0', i) => BigInt(1) << i
-              case ('~' | '1', _) => BigInt(0)
-              case _              => throw new Exception("bitset parse error")
-            }.sum
-            override lazy val width: Int = str.length
-          }
-        ).toSet
+        .map(str => BitPat(str))
+        .toSet
+      assert(terms.map(_.width).size <= 1)
     }
   }
 
-  def decode(input: chisel3.UInt, bitSets: Seq[BitSetFamily], errorBit: Boolean = false) =
+  /**
+    * Generate a decoder circuit that matches the input to each bitSet.
+    *
+    * The resulting circuit functions like the following but is optimized with a logic minifier
+    * when(input === bitSets(0)) { output := b000001 }
+    * .elsewhen (input === bitSets(1)) { output := b000010 }
+    * ....
+    * .otherwise { if (errorBit) output := b100000 else output := DontCare }
+    *
+    * @param input input to the decoder circuit, width should be equal to bitSets.width
+    * @param bitSets set of ports to be matched, all width should be the equal
+    * @param errorBit whether generate an additional decode error bit
+    */
+  def decode(input: chisel3.UInt, bitSets: Seq[BitSet], errorBit: Boolean = false): chisel3.UInt =
     chisel3.util.experimental.decode.decoder(
       input,
       chisel3.util.experimental.decode.TruthTable(
