0524267dabd2e5bdbb707afd448f0bfe3b0b5293
Jiuyang Liu <liu@jiuyang.me>
2021-11-18 14:44:05 +0800

    port pla to definition/instance api.

--- src/main/scala/chisel3/util/pla.scala
+++ src/main/scala/chisel3/util/pla.scala
@@ -3,37 +3,126 @@
 package chisel3.util
 
 import chisel3._
+import chisel3.experimental.hierarchy.{instantiable, public, Definition, Instance}
+
+/** Construct a [[https://en.wikipedia.org/wiki/Programmable_logic_array]] from specified table.
+  *
+  * Each position in the input matrix corresponds to an input variable where
+  * `0` implies the corresponding input literal appears complemented in the product term.
+  * `1` implies the input literal appears uncomplemented in the product term
+  * `?` implies the input literal does not appear in the product term.
+  *
+  * For each output
+  * a `1` means this product term makes the function value to `1`
+  * and a `0` or `?` means this product term make the function value to `0`
+  *
+  * @note There is one special case which we call it `? -> 1`. In this scenario, for some of the output functions (bits),
+  * all input terms that make this function value to `1` is purely composed by `?`. In a real pla, this will result in
+  * no connection to the gates in the AND Plane (verilog `z` on gate inputs), which in turn makes the outputs of the
+  * AND Plane undetermined (verilog `x` on outputs). This is not desired behavior in most cases, for example the
+  * minimization result of following truth table:
+  * 0 -> 1
+  * 1 -> 1
+  * which is:
+  * ? -> 1
+  * actually means something other than a verilog `x`. To ease the generation of minimized truth tables, this pla
+  * generation api will hard wire outputs to a `1` on this special case.
+  * This behavior is formally described as: if product terms that make one function value to `1` is solely consisted
+  * of don't-cares (`?`s), then this function is implemented as a constant `1`.
+  *
+  * @param table A [[Seq]] of inputs -> outputs mapping
+  * @param invert A [[BitPat]] specify which bit of the output should be inverted. `1` means the correspond position
+  *               of the output should be inverted in the PLA, a `0` or a `?` means direct output from the OR matrix.
+  */
+@instantiable
+class PLAModule(table: Seq[(BitPat, BitPat)], invert: BitPat = BitPat("b0")) extends Module {
+  require(table.nonEmpty, "pla table must not be empty")
+  private val (inputTerms, outputTerms) = table.unzip
+  require(
+    inputTerms.map(_.getWidth).distinct.size == 1,
+    "all `BitPat`s in the input part of specified PLA table must have the same width"
+  )
+  require(
+    outputTerms.map(_.getWidth).distinct.size == 1,
+    "all `BitPat`s in the output part of specified PLA table must have the same width"
+  )
+
+  // now all inputs / outputs have the same width
+  private def numberOfInputs = inputTerms.head.getWidth
+  private def numberOfOutputs = outputTerms.head.getWidth
+
+  private def inverterMask = invert.value & invert.mask
+  if (inverterMask.bitCount != 0)
+    require(
+      invert.getWidth == numberOfOutputs,
+      "non-zero inverter mask must have the same width as the output part of specified PLA table"
+    )
+
+  // IO definition:
+  @public val inputs = IO(Input(UInt(numberOfInputs.W)))
+  @public val outputs = IO(Output(UInt(numberOfOutputs.W)))
+
+  // Start constructing circuit:
+  // the AND matrix
+  // use `term -> AND line` map to reuse AND matrix output lines
+  private val andMatrixOutputs: Map[String, Bool] = inputTerms.map { t =>
+    val andMatrixInput = Seq
+      .tabulate(numberOfInputs) { i =>
+        if (t.mask.testBit(i)) {
+          Some(
+            if (t.value.testBit(i)) inputs(i)
+            else ~inputs(i)
+          )
+        } else {
+          None
+        }
+      }
+      .flatten
+    if (andMatrixInput.nonEmpty) t.toString -> Cat(andMatrixInput).andR() else t.toString -> true.B
+  }.toMap
+
+  // the OR matrix
+  private val orMatrixOutputs: UInt = Cat(
+    Seq
+      .tabulate(numberOfOutputs) { i =>
+        val andMatrixLines = table
+          // OR matrix composed by input terms which makes this output bit a `1`
+          .filter {
+            case (_, or) => or.mask.testBit(i) && or.value.testBit(i)
+          }.map {
+            case (inputTerm, _) =>
+              andMatrixOutputs(inputTerm.toString)
+          }
+        if (andMatrixLines.isEmpty) false.B
+        else Cat(andMatrixLines).orR()
+      }
+      .reverse
+  )
+
+  // the INV matrix, useful for decoders
+  private val invMatrixOutputs: UInt = Cat(
+    Seq
+      .tabulate(numberOfOutputs) { i =>
+        if (inverterMask.testBit(i)) ~orMatrixOutputs(i)
+        else orMatrixOutputs(i)
+      }
+      .reverse
+  )
+
+  outputs := invMatrixOutputs
+}
 
 object pla {
 
-  /** Construct a [[https://en.wikipedia.org/wiki/Programmable_logic_array]] from specified table.
-    *
-    * Each position in the input matrix corresponds to an input variable where
-    * `0` implies the corresponding input literal appears complemented in the product term.
-    * `1` implies the input literal appears uncomplemented in the product term
-    * `?` implies the input literal does not appear in the product term.
-    *
-    * For each output
-    * a `1` means this product term makes the function value to `1`
-    * and a `0` or `?` means this product term make the function value to `0`
-    *
-    * @note There is one special case which we call it `? -> 1`. In this scenario, for some of the output functions (bits),
-    * all input terms that make this function value to `1` is purely composed by `?`. In a real pla, this will result in
-    * no connection to the gates in the AND Plane (verilog `z` on gate inputs), which in turn makes the outputs of the
-    * AND Plane undetermined (verilog `x` on outputs). This is not desired behavior in most cases, for example the
-    * minimization result of following truth table:
-    * 0 -> 1
-    * 1 -> 1
-    * which is:
-    * ? -> 1
-    * actually means something other than a verilog `x`. To ease the generation of minimized truth tables, this pla
-    * generation api will hard wire outputs to a `1` on this special case.
-    * This behavior is formally described as: if product terms that make one function value to `1` is solely consisted
-    * of don't-cares (`?`s), then this function is implemented as a constant `1`.
+  private val definitions: collection.mutable.HashMap[(Seq[(BitPat, BitPat)], BitPat), Definition[PLAModule]] =
+    collection.mutable.HashMap()
+
+  /** Instance a [[PLA]], and return its io.
     *
     * @param table A [[Seq]] of inputs -> outputs mapping
     * @param invert A [[BitPat]] specify which bit of the output should be inverted. `1` means the correspond position
     *               of the output should be inverted in the PLA, a `0` or a `?` means direct output from the OR matrix.
+    *
     * @return the (input, output) [[Wire]] of [[UInt]] of the constructed pla.
     * {{{
     *   // A 1-of-8 decoder (like the 74xx138) can be constructed as follow
@@ -50,83 +139,7 @@ object pla {
     * }}}
     */
   def apply(table: Seq[(BitPat, BitPat)], invert: BitPat = BitPat("b0")): (UInt, UInt) = {
-    require(table.nonEmpty, "pla table must not be empty")
-
-    val (inputTerms, outputTerms) = table.unzip
-    require(
-      inputTerms.map(_.getWidth).distinct.size == 1,
-      "all `BitPat`s in the input part of specified PLA table must have the same width"
-    )
-    require(
-      outputTerms.map(_.getWidth).distinct.size == 1,
-      "all `BitPat`s in the output part of specified PLA table must have the same width"
-    )
-
-    // now all inputs / outputs have the same width
-    val numberOfInputs = inputTerms.head.getWidth
-    val numberOfOutputs = outputTerms.head.getWidth
-
-    val inverterMask = invert.value & invert.mask
-    if (inverterMask.bitCount != 0)
-      require(invert.getWidth == numberOfOutputs,
-        "non-zero inverter mask must have the same width as the output part of specified PLA table"
-      )
-
-    // input wires of the generated PLA
-    val inputs = Wire(UInt(numberOfInputs.W))
-    val invInputs = ~inputs
-
-    // output wires of the generated PLA
-    val outputs = Wire(UInt(numberOfOutputs.W))
-
-    // the AND matrix
-    // use `term -> AND line` map to reuse AND matrix output lines
-    val andMatrixOutputs: Map[String, Bool] = inputTerms.map { t =>
-      val andMatrixInput = Seq
-        .tabulate(numberOfInputs) { i =>
-          if (t.mask.testBit(i)) {
-            Some(
-              if (t.value.testBit(i)) inputs(i)
-              else invInputs(i)
-            )
-          } else {
-            None
-          }
-        }
-        .flatten
-      if (andMatrixInput.nonEmpty) t.toString -> Cat(andMatrixInput).andR() else t.toString -> true.B
-    }.toMap
-
-    // the OR matrix
-    val orMatrixOutputs: UInt = Cat(
-        Seq
-          .tabulate(numberOfOutputs) { i =>
-            val andMatrixLines = table
-              // OR matrix composed by input terms which makes this output bit a `1`
-              .filter {
-                case (_, or) => or.mask.testBit(i) && or.value.testBit(i)
-              }.map {
-                case (inputTerm, _) =>
-                  andMatrixOutputs(inputTerm.toString)
-              }
-            if (andMatrixLines.isEmpty) false.B
-            else Cat(andMatrixLines).orR()
-          }
-          .reverse
-      )
-
-    // the INV matrix, useful for decoders
-    val invMatrixOutputs: UInt = Cat(
-      Seq
-        .tabulate(numberOfOutputs) { i =>
-          if (inverterMask.testBit(i)) ~orMatrixOutputs(i)
-          else                          orMatrixOutputs(i)
-        }
-        .reverse
-    )
-
-    outputs := invMatrixOutputs
-
-    (inputs, outputs)
+    val pla = Instance(definitions.getOrElseUpdate((table, invert), Definition(new PLAModule(table, invert))))
+    (pla.inputs, pla.outputs)
   }
 }
