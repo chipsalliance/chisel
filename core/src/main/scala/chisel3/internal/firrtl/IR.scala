67c99b03176cd4d18492a7b5479792cc71ffd8db
Aditya Naik <91489422+adkian-sifive@users.noreply.github.com>
2021-11-19 14:39:59 -0500

    Update core/src/main/scala/chisel3/internal/firrtl/IR.scala
    Co-authored-by: Jack Koenig <jack.koenig3@gmail.com>

--- core/src/main/scala/chisel3/internal/firrtl/IR.scala
+++ core/src/main/scala/chisel3/internal/firrtl/IR.scala
@@ -85,7 +85,7 @@ case class Node(id: HasId) extends Arg {
     case Some(arg) => arg.name
     case None => id.instanceName
   }
-  override def simpleName: String = id.getOptionRef match {
+  override private[chisel3] def simpleName: String = id.getOptionRef match {
     case Some(arg) => arg.name
     case None => "?"
   }
