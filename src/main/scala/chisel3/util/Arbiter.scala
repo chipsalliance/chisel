ce3a33ac1d1673a95381aed0bb354e1badb5d4c7
Abongwa Bonalais <81782853+Burnleydev1@users.noreply.github.com>
2021-11-21 15:00:38 -0600

    added field grouping ScalaDoc for ArbiterIO

--- src/main/scala/chisel3/util/Arbiter.scala
+++ src/main/scala/chisel3/util/Arbiter.scala
@@ -17,9 +17,21 @@ import chisel3.internal.naming.chiselName  // can't use chisel3_ version because
   */
 class ArbiterIO[T <: Data](private val gen: T, val n: Int) extends Bundle {
   // See github.com/freechipsproject/chisel3/issues/765 for why gen is a private val and proposed replacement APIs.
-
+ 
+/** Input data, one per potential sender
+  * 
+  * @group Signals
+  */
   val in  = Flipped(Vec(n, Decoupled(gen)))
+/** Output data after arbitration
+  *
+  * @group Signals
+  */
   val out = Decoupled(gen)
+/** One-Hot vector indicating which output was chosen
+  *
+  * @group Signals
+  */
   val chosen = Output(UInt(log2Ceil(n).W))
 }
 
