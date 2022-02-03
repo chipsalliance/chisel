228ae7a8b69247e738d08f29f0db8b4de8f77515
Jack Koenig <koenig@sifive.com>
2022-02-02 17:35:39 -0800

    Change type of Bundle._elementsImpl to Iterable
    It was previously SeqMap (ListMap on Scala 2.12). This change gives us
    more freedom to optimize the implementation without breaking binary
    compatibility. It is scala.collection.Iterable because it is perfectly
    fine to return mutable collections (like Arrays) since the only use is
    to Iterate on them.

--- plugin/src/main/scala/chisel3/internal/plugin/BundleComponent.scala
+++ plugin/src/main/scala/chisel3/internal/plugin/BundleComponent.scala
@@ -41,12 +41,12 @@ private[plugin] class BundleComponent(val global: Global, arguments: ChiselPlugi
 
     def inferType(t: Tree): Type = localTyper.typed(t, nsc.Mode.TYPEmode).tpe
 
-    val bundleTpe:     Type = inferType(tq"chisel3.Bundle")
-    val dataTpe:       Type = inferType(tq"chisel3.Data")
-    val ignoreSeqTpe:  Type = inferType(tq"chisel3.IgnoreSeqInBundle")
-    val seqOfDataTpe:  Type = inferType(tq"scala.collection.Seq[chisel3.Data]")
-    val someOfDataTpe: Type = inferType(tq"scala.Option[chisel3.Data]")
-    val seqMapTpe:     Type = inferType(tq"scala.collection.immutable.SeqMap[String,Any]")
+    val bundleTpe:      Type = inferType(tq"chisel3.Bundle")
+    val dataTpe:        Type = inferType(tq"chisel3.Data")
+    val ignoreSeqTpe:   Type = inferType(tq"chisel3.IgnoreSeqInBundle")
+    val seqOfDataTpe:   Type = inferType(tq"scala.collection.Seq[chisel3.Data]")
+    val someOfDataTpe:  Type = inferType(tq"scala.Option[chisel3.Data]")
+    val itStringAnyTpe: Type = inferType(tq"scala.collection.Iterable[(String,Any)]")
 
     // Not cached because it should only be run once per class (thus once per Type)
     def isBundle(sym: Symbol): Boolean = { sym.tpe <:< bundleTpe }
@@ -220,10 +220,10 @@ private[plugin] class BundleComponent(val global: Global, arguments: ChiselPlugi
           val elementsImplSym =
             bundle.symbol.newMethod(TermName("_elementsImpl"), bundle.symbol.pos.focus, Flag.OVERRIDE | Flag.PROTECTED)
           elementsImplSym.resetFlag(Flags.METHOD)
-          elementsImplSym.setInfo(NullaryMethodType(seqMapTpe))
+          elementsImplSym.setInfo(NullaryMethodType(itStringAnyTpe))
 
           val elementsImpl = localTyper.typed(
-            DefDef(elementsImplSym, q"scala.collection.immutable.SeqMap.apply[String, Any](..$elementArgs)")
+            DefDef(elementsImplSym, q"scala.collection.immutable.Vector.apply[(String, Any)](..$elementArgs)")
           )
 
           Some(elementsImpl)
