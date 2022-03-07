fd305bc71b7b4791b6bf01c791a4284e93a1edca
Jiuyang Liu <liu@jiuyang.me>
2022-03-08 03:53:23 +0800

    Apply suggestions from code review
    Co-authored-by: Megan Wachs <megan@sifive.com>
    Co-authored-by: Jack Koenig <jack.koenig3@gmail.com>

--- src/main/scala/chisel3/experimental/util/algorithm/Bitwise.scala
+++ src/main/scala/chisel3/experimental/util/algorithm/Bitwise.scala
@@ -5,11 +5,12 @@ package chisel3.experimental.util.algorithm
 import chisel3._
 
 /** Map each bits to logical or of itself and all bits with lower index.
-  * Here 'left' means 'with lower index', as in arrays and lists, not to be confused with the 'left' as in 'shift left'
+  * Here `scanLeft` means "start at the left and look to the right, where left is the lowest index", a common operation on arrays and lists.
+  * This is consistent with the `left` as in "shift left" performed on bits, which means "start at the right (least significant bit) and move to the left".
   * @example {{{
-  * scanLeftOr("b00001000".U) // Returns "b11111000".U
-  * scanLeftOr("b00010100".U) // Returns "b11111100".U
-  * scanLeftOr("b00000000".U) // Returns "b00000000".U
+  * scanLeftOr("b00001000".U(8.W)) // Returns "b11111000".U
+  * scanLeftOr("b00010100".U(8.W)) // Returns "b11111100".U
+  * scanLeftOr("b00000000".U(8.W)) // Returns "b00000000".U
   * }}}
   */
 object scanLeftOr {
@@ -24,9 +25,8 @@ object scanLeftOr {
     helper(1, data)(width - 1, 0)
   }
 }
-
-/** Map each bits to logical or of itself and all bits with higher index.
-  * Here 'right' means 'with higher index', as in arrays and lists, not to be confused with the 'right' as in 'shift right'
+  * Here `scanRight` means "start at the right and look to the left, where right is the highest index", a common operation on arrays and lists.
+  * This is consistent with the `right` as in "shift right" performed on bits, which means "start at the left (most significant bit) and move to the right".
   * @example {{{
   * scanRightOr("b00001000".U) // Returns "b00001111".U
   * scanRightOr("b00010100".U) // Returns "b00011111".U
