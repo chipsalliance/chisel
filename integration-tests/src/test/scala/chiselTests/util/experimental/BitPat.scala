c3935a34bb81a2ea64a6b3b4c0bb7d43cba636e1
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-05-04 05:53:07 +0800

    BitSetRange API (#2449)
    * WIP: Add BitSetRange
    
    * BitSetRange: various code format changes
    
    * Try to fix scala random versioning
    
    * Initial impl of BitSet.toRange, along with some helper method in BitSet
    
    * BitPatRange: visibility and name changes:
    
    * BitSetRange: tests
    
    * BitSetRange: remove Random.between
    
    * Move BitSetRange to BitSet.fromRange. Remove BitSetRange and toBitSetRanges
    
    * Rename max_known_length -> MaxKnownLength in BitPat
    
    * Apply suggestions from code review
    
    Co-authored-by: Jack Koenig <jack.koenig3@gmail.com>
    
    * add api fromRange(start:  BigInt, length: BigInt)
    
    Co-authored-by: Jiuyang Liu <liu@jiuyang.me>
    Co-authored-by: Jack Koenig <jack.koenig3@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ integration-tests/src/test/scala/chiselTests/util/experimental/BitPat.scala
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chiselTests.util.experimental
+
+import chisel3._
+import chiseltest._
+import chiseltest.formal._
+import scala.util.Random
+import org.scalatest.flatspec.AnyFlatSpec
+import chisel3.util.experimental.BitSet
+
+class BitSetRangeTestModule(start: BigInt, length: BigInt, width: Int) extends Module {
+  val input = IO(Input(UInt(width.W)))
+
+  val range = BitSet.fromRange(start, length, width)
+  val testee = range.matches(input)
+  val ref = input >= start.U && input < (start + length).U
+
+  assert(testee === ref)
+}
+
+class BitSetRangeTest extends AnyFlatSpec with ChiselScalatestTester with Formal {
+  val rng = new Random(0x19260817)
+  val cases = 128
+  "BitSet.fromRange" should "be formally equivalent with bound checks" in {
+    for(i <- 1 to cases) {
+      val a = rng.nextLong() & Long.MaxValue
+      val b = rng.nextLong() & Long.MaxValue
+      val start = a.min(b)
+      val len = a.max(b) - start + 1
+      verify(new BitSetRangeTestModule(start, len, 64), Seq(BoundedCheck(1)))
+    }
+  }
+}
