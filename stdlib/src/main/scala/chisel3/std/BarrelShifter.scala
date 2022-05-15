6d4e6edc9a62c166af95b41b6aea9634d34275ed
Zenithal <i@zenithal.me>
2022-05-16 00:59:31 +0800

    Barrel Shifter/Rotater for Vec (#2518)
    * Add BarrelShifter API for Vec
    
    * Add BarrelShifter Test
    
    * fix for build.sc
    
    * Fix package name and move file
    
    * Fix sbt for stdlib
    
    Co-authored-by: Jiuyang Liu <liu@jiuyang.me>

---
+++ stdlib/src/main/scala/chisel3/std/BarrelShifter.scala
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3.std
+
+import chisel3._
+import chisel3.util.{Mux1H, UIntToOH}
+
+/** A Barrel Shifter implementation for Vec type.
+  *
+  * @param inputs           input signal to be shifted, should be a [[Vec]] type.
+  * @param shiftInput       input signal to indicate the shift number, encoded in UInt.
+  * @param shiftGranularity how many bits will be resolved in each layer.
+  *                         For a smaller `shiftGranularity`, latency will be high, but area is smaller.
+  *                         For a large `shiftGranularity`, latency will be low, but area is higher.
+  *
+  * @example {{{
+  * val input = 0xAABBCCDD.U(32.W)
+  * val bytes = VecInit(input.asBools.grouped(8).map(VecInit(_).asUInt).toSeq)
+  * val shamt = 1.U(2.W)
+  * val output = BarrelShifter.leftRotate(bytes, shamt).asUInt // big endian
+  * // output = 0xDDAABBCC.U(32.W)
+  * // The depth of Mux is 2, whereas originally
+  * // the depth of Mux is log(32) = 5
+  * val output = BarrelShifter.leftRotate(bytes, shamt, 2).asUInt // depth is 1 now
+  * }}}
+  */
+object BarrelShifter {
+  private trait ShiftType
+
+  private object LeftShift extends ShiftType
+
+  private object RightShift extends ShiftType
+
+  private object LeftRotate extends ShiftType
+
+  private object RightRotate extends ShiftType
+
+  private def apply[T <: Data](
+    inputs:           Vec[T],
+    shiftInput:       UInt,
+    shiftType:        ShiftType,
+    shiftGranularity: Int = 1
+  ): Vec[T] = {
+    require(shiftGranularity > 0)
+    val elementType: T = chiselTypeOf(inputs.head)
+    shiftInput
+      .asBools()
+      .grouped(shiftGranularity)
+      .map(VecInit(_).asUInt())
+      .zipWithIndex
+      .foldLeft(inputs) {
+        case (prev, (shiftBits, layer)) =>
+          Mux1H(
+            UIntToOH(shiftBits),
+            Seq.tabulate(1 << shiftBits.getWidth)(i => {
+              // For each layer of barrel shifter, it needs to
+              // Mux between shift 0 and i * 2^(depthOfLayer*granularity)
+              //
+              // e.g, when depth = 2 and granu = 1, the first layer Mux between 0 and 1
+              // while the second layer Mux between 0 and 2, thus Vec.shift(UInt(2.W))
+              //
+              // e.g, when depth = 2 and granu = 2, the first layer Mux between 0, 1, 2 and 3
+              // while the second layer Mux between 0, 4, 8, and 12, thus achieving Vec.shift(UInt(4.W))
+              //
+              // Also, shift no more than inputs length since prev.drop will not warn about overflow
+              // this is for Vec with length not the exponential of 2, e.g. 13
+              val layerShift: Int = (i * (1 << (layer * shiftGranularity))).min(prev.length)
+              VecInit(shiftType match {
+                case LeftRotate =>
+                  prev.drop(layerShift) ++ prev.take(layerShift)
+                case LeftShift =>
+                  prev.drop(layerShift) ++ Seq.fill(layerShift)(0.U.asTypeOf(elementType))
+                case RightRotate =>
+                  prev.takeRight(layerShift) ++ prev.dropRight(layerShift)
+                case RightShift =>
+                  Seq.fill(layerShift)(0.U.asTypeOf(elementType)) ++ prev.dropRight(layerShift)
+              })
+            })
+          )
+      }
+  }
+
+  def leftShift[T <: Data](inputs: Vec[T], shift: UInt, layerSize: Int = 1): Vec[T] =
+    apply(inputs, shift, LeftShift, layerSize)
+
+  def rightShift[T <: Data](inputs: Vec[T], shift: UInt, layerSize: Int = 1): Vec[T] =
+    apply(inputs, shift, RightShift, layerSize)
+
+  def leftRotate[T <: Data](inputs: Vec[T], shift: UInt, layerSize: Int = 1): Vec[T] =
+    apply(inputs, shift, LeftRotate, layerSize)
+
+  def rightRotate[T <: Data](inputs: Vec[T], shift: UInt, layerSize: Int = 1): Vec[T] =
+    apply(inputs, shift, RightRotate, layerSize)
+}
---
+++ stdlib/src/test/scala/chisel3/std/BarrelShifter.scala
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3.std
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import chiseltest.formal._
+import org.scalatest.flatspec.AnyFlatSpec
+import chisel3.std.BarrelShifter
+
+class VecLeftRotater[T <: Data](len: Int, gen: T, layerSize: Int = 1) extends Module {
+  require(len > 1)
+  val inputs:  Vec[T] = IO(Input(Vec(len, gen)))
+  val shift:   UInt = IO(Input(UInt((log2Ceil(len)).W))) // rotate at most len - 1
+  val outputs: Vec[T] = IO(Output(Vec(len, gen)))
+
+  outputs := BarrelShifter.leftRotate(inputs, shift, layerSize)
+
+  // Note: see note in VecLeftShifter
+  assert(inputs.asUInt().rotateRight(shift * gen.getWidth.U) === outputs.asUInt())
+}
+
+class VecLeftShifter[T <: Data](len: Int, gen: T, layerSize: Int = 1) extends Module {
+  require(len > 1)
+  val inputs:  Vec[T] = IO(Input(Vec(len, gen)))
+  val shift:   UInt = IO(Input(UInt((log2Ceil(len)).W))) // shift at most len - 1
+  val outputs: Vec[T] = IO(Output(Vec(len, gen)))
+
+  outputs := BarrelShifter.leftShift(inputs, shift, layerSize)
+
+  // Note: left shift for Vec is right shift for UInt
+  assert((inputs.asUInt() >> (shift * gen.getWidth.U)).pad(inputs.getWidth) === outputs.asUInt())
+}
+
+class VecRightRotater[T <: Data](len: Int, gen: T, layerSize: Int = 1) extends Module {
+  require(len > 1)
+  val inputs:  Vec[T] = IO(Input(Vec(len, gen)))
+  val shift:   UInt = IO(Input(UInt((log2Ceil(len)).W))) // rotate at most len - 1
+  val outputs: Vec[T] = IO(Output(Vec(len, gen)))
+
+  outputs := BarrelShifter.rightRotate(inputs, shift, layerSize)
+
+  // Note: see note in VecRightShifter
+  assert(inputs.asUInt().rotateLeft(shift * gen.getWidth.U) === outputs.asUInt())
+}
+
+class VecRightShifter[T <: Data](len: Int, gen: T, layerSize: Int = 1) extends Module {
+  require(len > 1)
+  val inputs:  Vec[T] = IO(Input(Vec(len, gen)))
+  val shift:   UInt = IO(Input(UInt((log2Ceil(len)).W))) // shift at most len - 1
+  val outputs: Vec[T] = IO(Output(Vec(len, gen)))
+
+  outputs := BarrelShifter.rightShift(inputs, shift, layerSize)
+
+  // Note: right shift for Vec is left shift for UInt
+  // on << shift width: UInt(this.width + that)
+  assert(((inputs.asUInt() << (shift * gen.getWidth.U))(inputs.getWidth - 1, 0)) === outputs.asUInt())
+}
+
+class BarrelShifterTester extends AnyFlatSpec with ChiselScalatestTester with Formal {
+  "VecLeftShifter" should "be formally equivalent with UInt Right Shift" in {
+    Seq(2, 3, 13).foreach(len => {
+      Seq(1, 2, 7).foreach(width => {
+        Seq(1, 2, 3).foreach(layerSize => {
+          verify(new VecLeftShifter(len, UInt(width.W), layerSize), Seq(BoundedCheck(1)))
+        })
+      })
+    })
+  }
+  "VecLeftRotater" should "be formally equivalent with UInt Right Rotate" in {
+    Seq(2, 3, 13).foreach(len => {
+      Seq(1, 2, 7).foreach(width => {
+        Seq(1, 2, 3).foreach(layerSize => {
+          verify(new VecLeftRotater(len, UInt(width.W), layerSize), Seq(BoundedCheck(1)))
+        })
+      })
+    })
+  }
+  "VecRightShifter" should "be formally equivalent with UInt Left Shift" in {
+    Seq(2, 3, 13).foreach(len => {
+      Seq(1, 2, 7).foreach(width => {
+        Seq(1, 2, 3).foreach(layerSize => {
+          verify(new VecRightShifter(len, UInt(width.W), layerSize), Seq(BoundedCheck(1)))
+        })
+      })
+    })
+  }
+  "VecRightRotater" should "be formally equivalent with UInt Left Rotate" in {
+    Seq(2, 3, 13).foreach(len => {
+      Seq(1, 2, 7).foreach(width => {
+        Seq(1, 2, 3).foreach(layerSize => {
+          verify(new VecRightRotater(len, UInt(width.W), layerSize), Seq(BoundedCheck(1)))
+        })
+      })
+    })
+  }
+}
