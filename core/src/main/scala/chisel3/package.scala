7e731aa087e0d7181418f85f492e4f0cb0b3d244
Girish Pai <girish.pai@sifive.com>
2022-05-27 12:47:49 -0700

    Further updates to scaladoc

--- core/src/main/scala/chisel3/package.scala
+++ core/src/main/scala/chisel3/package.scala
@@ -226,8 +226,9 @@ package object chisel3 {
       * Each expression (argument) referenced within the string is
       * converted to a particular Printable depending
       * on the format specifier and type.
-      *
-      * For Chisel types referenced within the String.
+      * 
+      * 
+      * ==== For Chisel types referenced within the String ====
       * %n - Returns [[Name]] Printable.
       * %N - Returns [[FullName]] Printable.
       * %b,%d,%x,%c = Only applicable for types of [[Bits]] or dreived from it. - returns ([[Binary]],[[Decimal]],
@@ -235,16 +236,17 @@ package object chisel3 {
       * Default if no specifier given is to call [[Data.toPrintable]] on the Chisel Type.
       *
       *
-      * For [[Printable]] type_:  No explicit format specifier supported - just return the Printable.
+      * ==== For [[Printable]] type_:  ====
+        No explicit format specifier supported - just return the Printable.
       *
-      * For regular scala types call String.format with the argument and specifier.
+      * ==== For regular scala types ====
+      * Call String.format with the argument and specifier.
       * Default is %s if no specifier is given. 
       * Wrap the result in [[PString]] Printable. 
       *
-      * Literal percents need to be escaped with a %.
-      *
-      * For the parts of the StringContext - remove format specifiers and
-      * if literal percents are present - convert them into [[Percent]] Printable.
+      * ==== For the parts of the StringContext ==== 
+      * Remove format specifiers and if literal percents (need to be escaped with %) 
+      * are present convert them into [[Percent]] Printable.
       * Rest of the string will be wrapped in [[PString]] Printable.
       *
       * @example
