950cfceea009263ecb654259e0141659e5c12247
Jared Barocsi <82000041+jared-barocsi@users.noreply.github.com>
2022-06-02 14:05:56 -0700

    Deprecate .U() and .S() syntax for literal bit extracts (#2534)
    (cherry picked from commit cadaf33a650ef898fdab2f81244e4ad6a07a9ea8)
    
    # Conflicts:
    #	macros/src/main/scala/chisel3/internal/sourceinfo/SourceInfoTransform.scala

--- src/test/scala/chiselTests/UIntOps.scala
+++ src/test/scala/chiselTests/UIntOps.scala
@@ -172,9 +172,18 @@ class MatchedRotateLeftAndRight(w: Int = 13) extends BasicTester {
 }
 
 class UIntLitExtractTester extends BasicTester {
-  assert("b101010".U(2) === false.B)
-  assert("b101010".U(3) === true.B)
-  assert("b101010".U(100) === false.B)
+  assert({
+    val lit = "b101010".U
+    lit(2) === false.B
+  })
+  assert({
+    val lit = "b101010".U
+    lit(3) === true.B
+  })
+  assert({
+    val lit = "b101010".U
+    lit(100) === false.B
+  })
   assert("b101010".U(3, 0) === "b1010".U)
   assert("b101010".U(9, 0) === "b0000101010".U)
 
