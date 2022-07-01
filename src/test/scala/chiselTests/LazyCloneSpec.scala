b3426fc1a73c086f5a58e2a83d0e284c6bb0a1c2
Zachary Yedidia <zyedidia@gmail.com>
2022-06-30 17:17:26 -0700

    Add two more tests

--- src/test/scala/chiselTests/LazyCloneSpec.scala
+++ src/test/scala/chiselTests/LazyCloneSpec.scala
@@ -5,28 +5,59 @@ package chiselTests
 import chisel3._
 import chisel3.stage.ChiselStage
 import chiselTests.ChiselFlatSpec
+import chisel3.stage.ChiselStage.elaborate
+
+object Counter {
+  var count = 0L
+}
+
+class Foo extends Bundle {
+  val a = UInt(8.W)
+  Counter.count += 1
+}
+
+class Bar(x: UInt) extends Bundle {
+  val a = x
+  Counter.count += 1
+}
 
 class LazyCloneSpec extends ChiselFlatSpec {
   behavior.of("LazyClone")
 
-  it should "lazily clone" in {
-    object Foo {
-      var count = 0L
-    }
-
-    class Foo extends Bundle {
-      val a = UInt(8.W)
-      Foo.count += 1
-    }
-
+  it should "not clone" in {
+    Counter.count = 0L
     class MyModule extends RawModule {
       val io = IO(Flipped(new Bundle {
-        val foo = Output(new Foo)
-        val bar = Input(new Foo)
+        val x = Output(new Foo)
+        val y = Input(new Foo)
       }))
     }
     ChiselStage.elaborate(new MyModule)
-    println(s"copies: ${Foo.count}")
-    Foo.count should be < 12L
+    Counter.count should be(2L)
+  }
+
+  it should "share with cloning" in {
+    Counter.count = 0L
+    class MyModule extends RawModule {
+      val foo = new Foo
+      val io = IO(Flipped(new Bundle {
+        val x = Output(foo)
+        val y = Input(foo)
+      }))
+    }
+    ChiselStage.elaborate(new MyModule)
+    Counter.count should be(3L)
+  }
+
+  it should "clone because of external ref" in {
+    Counter.count = 0L
+    class MyModule extends RawModule {
+      val io = IO(Flipped(new Bundle {
+        val foo = Output(new Bar(UInt(8.W)))
+        val bar = Input(new Bar(UInt(8.W)))
+      }))
+    }
+    ChiselStage.elaborate(new MyModule)
+    Counter.count should be(4L)
   }
 }
