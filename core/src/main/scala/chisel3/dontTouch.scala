06f964afaf134b2cbc7665ed8feab7c6479123fa
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2022-07-05 10:35:17 -0700

    Fixup compile options to work better for migrating

--- core/src/main/scala/chisel3/dontTouch.scala
+++ core/src/main/scala/chisel3/dontTouch.scala
@@ -34,7 +34,7 @@ object dontTouch {
     * @return Unmodified signal `data`
     */
   def apply[T <: Data](data: T)(implicit compileOptions: CompileOptions): T = {
-    if (compileOptions.checkSynthesizable) {
+    if (compileOptions.checkSynthesizable || compileOptions.migrateCheckSynthesizable) {
       requireIsHardware(data, "Data marked dontTouch")
     }
     annotate(new ChiselAnnotation { def toFirrtl = DontTouchAnnotation(data.toNamed) })
