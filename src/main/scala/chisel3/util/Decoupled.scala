b20df1d6cda03f6eef28ee480e0aade914c5face
Jared Barocsi <82000041+jared-barocsi@users.noreply.github.com>
2022-07-28 14:39:24 -0700

    Implement DecoupledIO.map utility (#2646)

--- src/main/scala/chisel3/util/Decoupled.scala
+++ src/main/scala/chisel3/util/Decoupled.scala
@@ -99,7 +99,22 @@ object ReadyValidIO {
   * of ready or valid.
   * @param gen the type of data to be wrapped in DecoupledIO
   */
-class DecoupledIO[+T <: Data](gen: T) extends ReadyValidIO[T](gen)
+class DecoupledIO[+T <: Data](gen: T) extends ReadyValidIO[T](gen) {
+
+  /** Applies the supplied functor to the bits of this interface, returning a new
+    * typed DecoupledIO interface.
+    * @param f The function to apply to this DecoupledIO's 'bits' with return type B
+    * @return a new DecoupledIO of type B
+    */
+  def map[B <: Data](f: T => B): DecoupledIO[B] = {
+    val _map_bits = f(bits)
+    val _map = Wire(Decoupled(chiselTypeOf(_map_bits)))
+    _map.bits := _map_bits
+    _map.valid := valid
+    ready := _map.ready
+    _map
+  }
+}
 
 /** This factory adds a decoupled handshaking protocol to a data bundle. */
 object Decoupled {
