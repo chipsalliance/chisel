e8b5b03c7c1a47e961d34c8f9d1122a7207da6c9
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-08-04 01:05:16 -0400

    Refactor circt into src/main/scala/circt, NFC
    Move the git-subtree added sifive/chisel-circt repository from "circt/"
    into "src/main/scala/circt/" and "src/test/scala/circtTests/".  Update
    the chisel-circt README, which now lives in the src directory, to not
    indicate that this is a published dependency anymore.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- circt/project/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
-  private val chiselVersion = "3.5.0"
-  lazy val chisel3 = "edu.berkeley.cs" %% "chisel3" % chiselVersion
-  lazy val chiselCompilerPlugin = "edu.berkeley.cs" %% "chisel3-plugin" % chiselVersion
-}
--- circt/project/plugins.sbt
+++
@@ -1,4 +0,0 @@
-addSbtPlugin("com.geirsson" % "sbt-ci-release" % "1.5.3")
-
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
-
