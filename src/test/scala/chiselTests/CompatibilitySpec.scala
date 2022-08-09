6833b908427f559d3e18ae77d8c225e5e000a03a
Megan Wachs <megan@sifive.com>
2022-08-09 09:21:56 -0700

    remove test for debug which was deleted

--- src/test/scala/chiselTests/CompatibilitySpec.scala
+++ src/test/scala/chiselTests/CompatibilitySpec.scala
@@ -461,19 +461,6 @@ class CompatibilitySpec extends ChiselFlatSpec with ScalaCheckDrivenPropertyChec
     ChiselStage.elaborate((new Foo))
   }
 
-  behavior.of("debug")
-
-  it should "still exist" in {
-    class Foo extends Module {
-      val io = IO(new Bundle {})
-
-      val data = UInt(width = 2)
-      debug(data)
-    }
-
-    ChiselStage.elaborate(new Foo)
-  }
-
   behavior.of("Data methods")
 
   behavior.of("Wire")
