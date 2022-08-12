42250376769467ce08b982b0372b4475a2502998
Zachary Yedidia <zyedidia@gmail.com>
2022-08-12 13:24:14 -0700

    Add test

--- src/test/scala/chiselTests/WarningSpec.scala
+++ src/test/scala/chiselTests/WarningSpec.scala
@@ -4,32 +4,39 @@ package chiselTests
 
 import chisel3._
 import chisel3.util._
-import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import chisel3.stage.ChiselStage
 import chisel3.experimental.ChiselEnum
 import chisel3.experimental.EnumType
-import chiselTests.ChiselFlatSpec
 
 class WarningSpec extends ChiselFlatSpec with Utils {
   behavior.of("Warnings")
 
-  "Warnings" should "be de-duplicated" in {
-    object MyEnum extends ChiselEnum {
-      val e0, e1, e2 = Value
-    }
+  object MyEnum extends ChiselEnum {
+    val e0, e1, e2 = Value
+  }
 
-    class MyModule extends Module {
-      val in = IO(Input(UInt(2.W)))
-      val out1 = IO(Output(MyEnum()))
-      val out2 = IO(Output(MyEnum()))
-      def func(out: EnumType): Unit = {
-        out := MyEnum(in)
-      }
-      func(out1)
-      func(out2)
+  class MyModule extends Module {
+    val in = IO(Input(UInt(2.W)))
+    val out1 = IO(Output(MyEnum()))
+    val out2 = IO(Output(MyEnum()))
+    def func(out: EnumType): Unit = {
+      out := MyEnum(in)
     }
+    func(out1)
+    func(out2)
+  }
+
+  "Warnings" should "be de-duplicated" in {
     val (log, _) = grabLog(ChiselStage.elaborate(new MyModule))
     def countSubstring(s: String, sub: String) =
       s.sliding(sub.length).count(_ == sub)
     countSubstring(log, "Casting non-literal UInt") should be(1)
+  }
+
+  "Warnings" should "be treated as errors with warningsAsErrors" in {
+    a[ChiselException] should be thrownBy extractCause[ChiselException] {
+      val args = Array("--warnings-as-errors")
+      (new ChiselStage).emitChirrtl(new MyModule, args)
+    }
   }
 }
