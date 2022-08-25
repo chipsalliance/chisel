fb8ea2a2fac227f2570da992d7877de2eb1cf801
Aditya Naik <91489422+adkian-sifive@users.noreply.github.com>
2022-08-24 18:16:59 -0700

    Bugfix - OpaqueSlot replace invalid localName (#2701)

--- core/src/main/scala/chisel3/internal/firrtl/Converter.scala
+++ core/src/main/scala/chisel3/internal/firrtl/Converter.scala
@@ -68,7 +68,7 @@ private[chisel3] object Converter {
       fir.Reference(name, fir.UnknownType)
     case Slot(imm, name) =>
       fir.SubField(convert(imm, ctx, info), name, fir.UnknownType)
-    case OpaqueSlot(imm, name) =>
+    case OpaqueSlot(imm) =>
       convert(imm, ctx, info)
     case Index(imm, ILit(idx)) =>
       fir.SubIndex(convert(imm, ctx, info), castToInt(idx, "Index"), fir.UnknownType)
