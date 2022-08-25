719da116b9ed129c7021155640221ccce10010b1
Aditya Naik <aditya.naik@sifive.com>
2022-08-25 10:16:31 -0700

    Fix clonetypes
    add missing whitespace

--- src/test/scala/chiselTests/RecordSpec.scala
+++ src/test/scala/chiselTests/RecordSpec.scala
@@ -136,14 +136,14 @@ trait RecordSpecUtils {
     val k = new InnerInnerRecord
     val elements = SeqMap("" -> k)
     override def opaqueType = elements.size == 1
-    override def cloneType: this.type = (new SingleElementRecord).asInstanceOf[this.type]
+    override def cloneType: this.type = (new InnerRecord).asInstanceOf[this.type]
   }
 
   class InnerInnerRecord extends Record {
     val k = new SingleElementRecord
     val elements = SeqMap("" -> k)
     override def opaqueType = elements.size == 1
-    override def cloneType: this.type = (new SingleElementRecord).asInstanceOf[this.type]
+    override def cloneType: this.type = (new InnerInnerRecord).asInstanceOf[this.type]
   }
 
   class NestedRecordModule extends Module {
@@ -153,6 +153,7 @@ trait RecordSpecUtils {
     inst.foo := in
     out := inst.bar
   }
+
   class InnerModule extends Module {
     val foo = IO(Input(new InnerRecord))
     val bar = IO(Output(new InnerRecord))
