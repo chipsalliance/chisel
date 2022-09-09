be6118fd3e26fe56fa86098c6ada3e6837497cb9
Zachary Yedidia <zyedidia@gmail.com>
2022-09-09 15:21:39 -0700

    Add return types and remove unnecessary function def

--- core/src/main/scala/chisel3/experimental/dataview/SubTypeOf.scala
+++ core/src/main/scala/chisel3/experimental/dataview/SubTypeOf.scala
@@ -11,43 +11,42 @@ object ChiselSubTypeOf {
   def genChiselSubTypeOf[A: c.WeakTypeTag, B: c.WeakTypeTag](c: Context): c.Tree = {
     import c.universe._
 
-    val ta = implicitly[c.WeakTypeTag[A]]
-    val tb = implicitly[c.WeakTypeTag[B]]
-    val tdata = implicitly[c.WeakTypeTag[Data]]
-    val empty = q""
+    def baseType(t: Type): Type =
+      if (t.baseClasses.length > 0)
+        t.baseType(t.baseClasses(0))
+      else
+        NoType
 
-    def baseType(t: Type): Type = t.baseType(t.baseClasses(0))
-
-    def couldBeEqual(a: Type, b: Type) =
+    def couldBeEqual(a: Type, b: Type): Boolean =
       // If one baseType is a subtype the other baseType, then these two
       // types could be equal, so we allow it and leave it to elaboration to
       // figure out. Otherwise, the types must be equal or we throw an error.
       (baseType(b) <:< baseType(a) || baseType(a) <:< baseType(b)) || a =:= b
 
-    // Returns true if 'a' is a structural subtype of 'b' (i.e. all fields of
-    // 'b' exist within 'a' with the same names and the same types).
-    def subtypeOf(a: Type, b: Type) = {
-      // Only look at public members that are getters and that are subtypes of Data.
-      val mb = b.members.filter(m => {
-        m.isPublic && m.isMethod && m.asMethod.isGetter && m.asMethod.returnType <:< tdata.tpe
-      })
-      // Go through every public member of b and make sure a member with the
-      // same name exists in a and it has the same structural type.
-      for (vb <- mb) {
-        val name = TermName(vb.name.toString)
-        val vaTyp = a.member(name).info.resultType
-        val vbTyp = vb.info.resultType
-        if (vaTyp == NoType || vbTyp == NoType || !couldBeEqual(vaTyp, vbTyp)) {
-          val err = if (vaTyp == NoType) s"${ta.tpe}.${name} does not exist" else s"${vaTyp} != ${vbTyp}"
-          c.error(
-            empty.pos,
-            s"${ta.tpe} is not a Chisel subtype of ${tb.tpe}: mismatch at ${tb.tpe}.${name}: $err. Did you mean .viewAs[${tb.tpe}]? " +
-              "Please see https://www.chisel-lang.org/chisel3/docs/cookbooks/dataview"
-          )
-        }
+    val a = implicitly[c.WeakTypeTag[A]].tpe
+    val b = implicitly[c.WeakTypeTag[B]].tpe
+    val tdata = implicitly[c.WeakTypeTag[Data]].tpe
+    val empty = q""
+
+    // Only look at public members that are getters and that are subtypes of Data.
+    val mb = b.members.filter(m => {
+      m.isPublic && m.isMethod && m.asMethod.isGetter && m.asMethod.returnType <:< tdata
+    })
+    // Go through every public member of b and make sure a member with the
+    // same name exists in a and it has the same structural type.
+    for (vb <- mb) {
+      val name = TermName(vb.name.toString)
+      val vaTyp = a.member(name).info.resultType
+      val vbTyp = vb.info.resultType
+      if (vaTyp == NoType || vbTyp == NoType || !couldBeEqual(vaTyp, vbTyp)) {
+        val err = if (vaTyp == NoType) s"${a}.${name} does not exist" else s"${vaTyp} != ${vbTyp}"
+        c.error(
+          empty.pos,
+          s"${a} is not a Chisel subtype of ${b}: mismatch at ${b}.${name}: $err. Did you mean .viewAs[${b}]? " +
+            "Please see https://www.chisel-lang.org/chisel3/docs/cookbooks/dataview"
+        )
       }
     }
-    subtypeOf(ta.tpe, tb.tpe)
 
     return empty
   }
