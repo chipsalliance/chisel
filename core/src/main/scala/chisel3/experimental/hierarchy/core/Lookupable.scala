44d71cc7cfe1f8dd3515fc9e28509e1b4e51e6a4
Jack Koenig <koenig@sifive.com>
2022-10-05 16:23:37 -0700

    Update core/src/main/scala/chisel3/experimental/hierarchy/core/Lookupable.scala

--- core/src/main/scala/chisel3/experimental/hierarchy/core/Lookupable.scala
+++ core/src/main/scala/chisel3/experimental/hierarchy/core/Lookupable.scala
@@ -343,7 +343,7 @@ object Lookupable {
             case Clone(x: InstantiableClone[_]) => getIoMap(x._innerContext)
             case Clone(x: InstanceClone[_]) => None
             case other => {
-              Builder.throwException(s"Internal Error! Unexpected case where we can't get IO Map: $other")
+              Builder.exception(s"Internal Error! Unexpected case where we can't get IO Map: $other")
             }
           }
         }
