cbf165b3111a160b126e66afa4ec1e6bbc4aedd0
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2022-10-19 12:05:09 -0700

    WIP fixing up CompatibiliityInteropSpec

--- src/test/scala/chiselTests/CompatibilityInteroperabilitySpec.scala
+++ src/test/scala/chiselTests/CompatibilityInteroperabilitySpec.scala
@@ -355,7 +355,8 @@ class CompatibilityInteroperabilitySpec extends ChiselFlatSpec {
   "A unidirectional but flipped Bundle with something close to NotStrict compileOptions, but not exactly" should "bulk connect in import chisel3._ code correctly" in {
     def checkException(thunk: => Unit): Unit = {
       val e = intercept[Exception] { thunk }
-      e.getMessage() should include (":<>=")
+      println(e.getMessage)
+      e.getMessage() should include ("Cannot use <>")
     }
     object Compat {
       import Chisel.{defaultCompileOptions => _, _}
@@ -394,16 +395,19 @@ class CompatibilityInteroperabilitySpec extends ChiselFlatSpec {
     class CorrectedTop(extraFlip: Boolean) extends RawModule {
       val port = IO(new MyBundle(extraFlip))
       val wire = Wire(new MyBundle(extraFlip))
-      port <> DontCare
-      wire <> DontCare
-      port <> wire
-      wire <> port
-      port.foo <> wire.foo
-      wire.foo <> port.foo
+      port :#= DontCare
+      wire :#= DontCare
+      port :<>= wire
+      if(extraFlip) {
+        port.foo :<>= wire.foo
+      } else {
+        wire.foo :<>= port.foo
+      }
     }
-    checkException(stage.ChiselStage.emitChirrtl(new Top(true), false, true))
-    checkException(stage.ChiselStage.emitChirrtl(new Top(false), false, true))
-    compile(new CorrectedTop(true))
-    compile(new CorrectedTop(false))
+    val e = intercept[Exception] { stage.ChiselStage.emitChirrtl(new Top(true), false, true) }
+    println(e)
+    //checkException(stage.ChiselStage.emitChirrtl(new Top(false), false, true))
+    //compile(new CorrectedTop(true))
+    //compile(new CorrectedTop(false))
   }
 }
