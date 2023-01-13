01f7255cb0fc4d657b11cd538d7408d3c83547ce
Jiuyang Liu <liu@jiuyang.me>
2023-01-13 16:02:36 +0800

    implement a simple firtool API

--- core/src/main/scala/chisel3/compiler/firtool.scala
+++ core/src/main/scala/chisel3/compiler/firtool.scala
@@ -1,35 +1,41 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package chisel3.compiler
-import chisel3.ChiselException
-import os.Path
-
-import scala.collection.mutable.ArrayBuffer
 
 object firtool {
-  def apply: CompilerApi  = new FirtoolImpl
+  def apply: CompilerApi = new FirtoolImpl
 }
 
-class FirtoolImpl extends CompilerApi {
+private[chisel3] class FirtoolImpl extends CompilerApi {
+  private var _consumed: Boolean = false
+  private var _circuit: chisel3.internal.firrtl.Circuit = null
+  private val _options: scala.collection.mutable.ArrayBuffer[String] = scala.collection.mutable.ArrayBuffer()
+
   /** Consume a [[chisel3.internal.firrtl.Circuit]], feed to compiler */
   def consume(circuit: chisel3.internal.firrtl.Circuit): Unit = {
-    if(_consumed) {
-      throw new ChiselException("")
+    if (_consumed) {
+      throw new chisel3.ChiselException("Already consume a circuit in this firrtl instance.")
     } else {
-      ???
+      _circuit = circuit
     }
     _consumed = true
   }
 
   /** Add option compiler. */
-  final def setOption(option: String): Unit = _options += option
+  final def setOption(option: String): Unit = {
+    require(option != "--annotation-file", "annotation-file is produced by Chisel")
+    _options += option
+  }
 
-  /** Get current configured options. */
-  final def getOptions: Seq[String] = _options.toSeq
+  final def emit: Unit = os.proc(
+    "firtool",
+    _options ++ Some(s"--annotation-file=${os.temp(firrtl.annotations.JsonProtocol.serialize(_circuit.firrtlAnnotations.toSeq))}"),
+    os.temp(chisel3.internal.firrtl.Converter.convertLazily(_circuit).serialize)
+  ).call()
 
-  /** Ask Compiler to emit files based on options. */
-  override def emit: Path = ???
-
-  private var _consumed: Boolean = false
-  private val _options: scala.collection.mutable.ArrayBuffer[String] = ArrayBuffer()
+  /** Get current configured options.
+    *
+    * @return the sequence of options in pair
+    */
+  override def getOptions: Seq[String] = _options
 }
