56f292982745ac3f98ecfb7d74c416cc7ddabc13
Aditya Naik <aditya.naik@sifive.com>
2023-01-18 15:20:26 -0800

    Add SourceInfoMacro

---
+++ core/src/main/scala/chisel3/internal/SourceInfoMacro.scala
@@ -0,0 +1,23 @@
+package chisel3.internal.sourceinfo
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+
+/** Provides a macro that returns the source information at the invocation point.
+  */
+@deprecated("Public APIs in chisel3.internal are deprecated", "Chisel 3.6")
+object SourceInfoMacro {
+  def generate_source_info(c: Context): c.Tree = {
+    import c.universe._
+    val p = c.enclosingPosition
+
+    val userDir = sys.props.get("user.dir") // Figure out what to do if not provided
+    val projectRoot = sys.props.get("chisel.project.root")
+    val root = projectRoot.orElse(userDir)
+
+    val path = root.map(r => p.source.file.canonicalPath.stripPrefix(r)).getOrElse(p.source.file.name)
+    val pathNoStartingSlash = if (path(0) == '/') path.tail else path
+
+    q"_root_.chisel3.experimental.SourceLine($pathNoStartingSlash, ${p.line}, ${p.column})"
+  }
+}
