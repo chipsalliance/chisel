82fc21b5f917e21ecb2b6d091a991e6fd583cdf7
Andrew Lenharth <darthscsi@users.noreply.github.com>
2023-01-24 19:32:05 -0600

    Demonstrate CIRCT Intrinsic (#2900)
    Add FIRRTL Intrinsic support with CIRCT sizeOf intrinsic.

--- src/test/scala/chiselTests/Reg.scala
+++ src/test/scala/chiselTests/Reg.scala
@@ -2,11 +2,11 @@
 
 package chiselTests
 
+import circt.stage.ChiselStage
 import chisel3._
 import chisel3.reflect.DataMirror
 import chisel3.testers.BasicTester
 import chisel3.util._
-import circt.stage.ChiselStage
 import org.scalacheck.Gen
 
 class RegSpec extends ChiselFlatSpec {
