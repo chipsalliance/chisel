74f97657725fdd3154afbf8e29d09bbf86b16882
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-02-16 23:40:52 -0500

    [chisel5] Remove RegisteredTransform
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- firrtl/src/test/scala/firrtlTests/options/ShellSpec.scala
+++ firrtl/src/test/scala/firrtlTests/options/ShellSpec.scala
@@ -23,11 +23,8 @@ class ShellSpec extends AnyFlatSpec with Matchers {
 
   behavior.of("Shell")
 
-  it should "detect all registered libraries and transforms" in {
+  it should "detect all registered libraries" in {
     val shell = new Shell("foo")
-
-    info("Found FooTransform")
-    shell.registeredTransforms.map(_.getClass.getName) should contain("firrtlTests.options.FooTransform")
 
     info("Found BarLibrary")
     shell.registeredLibraries.map(_.getClass.getName) should contain("firrtlTests.options.BarLibrary")
