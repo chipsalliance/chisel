8e39d381c92d084d2a729b12e2693143e89a9ef1
Aditya Naik <aditya.naik@sifive.com>
2023-02-23 09:51:27 -0800

    Actually catch problematic elements

--- core/src/main/scala/chisel3/Aggregate.scala
+++ core/src/main/scala/chisel3/Aggregate.scala
@@ -943,6 +943,11 @@ abstract class Record extends Aggregate {
             " See the cookbook entry 'How do I deal with the \"unable to clone\" error?' for more details."
         )
       }
+      if (field.binding.isDefined) {
+        throw RebindingException(
+          s"Cannot create Record ${this.className}; element ${field} of record is already a bound hardware."
+        )
+      }
     }
   }
 
@@ -1013,11 +1018,6 @@ abstract class Record extends Aggregate {
       if (child != sameChild) {
         throwException(
           s"${this.className} does not return the same objects when calling .elements multiple times. Did you make it a def by mistake?"
-        )
-      }
-      if (child.binding.isDefined) {
-        throw RebindingException(
-          s"Cannot create Record ${this.className}; element ${child} of record is already a bound hardware."
         )
       }
 
--- src/test/scala/chiselTests/RecordSpec.scala
+++ src/test/scala/chiselTests/RecordSpec.scala
@@ -423,10 +423,9 @@ class RecordSpec extends ChiselFlatSpec with Utils {
     err.getMessage should include("bundle plugin was unable to clone")
   }
 
-  "Attempting to initiate records with bound hardware values" should "error" in {
+  "Attempting to create a Record with bound elements" should "error" in {
     class MyRecord[T <: Data](gen: T) extends Record {
       val elements = SeqMap("a" -> gen)
-      override def cloneType: this.type = (new MyRecord(gen)).asInstanceOf[this.type]
     }
     val e = the[ChiselException] thrownBy {
       ChiselStage.elaborate(new Module {
