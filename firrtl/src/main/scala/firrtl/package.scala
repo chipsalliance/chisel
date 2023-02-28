410322c3fa02936ccf436b9064a0a4337687c110
Deborah Soung <debs@sifive.com>
2023-02-28 11:11:40 -0800

    cleanup rebase issues

--- firrtl/src/main/scala/firrtl/package.scala
+++ firrtl/src/main/scala/firrtl/package.scala
@@ -3,7 +3,6 @@
 import firrtl.annotations.Annotation
 
 package object firrtl {
-  import scala.language.implicitConversions
   implicit def seqToAnnoSeq(xs: Seq[Annotation]) = AnnotationSeq(xs)
   implicit def annoSeqToSeq(as: AnnotationSeq): Seq[Annotation] = as.toSeq
 }
