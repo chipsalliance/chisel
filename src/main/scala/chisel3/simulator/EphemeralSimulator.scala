016f22ed48a98b2a8f774dc6d1e43de58be448b3
George Lyon <GeorgeLyon@users.noreply.github.com>
2023-03-31 13:46:05 -0700

    Update EphemeralSimulator.scala

--- src/main/scala/chisel3/simulator/EphemeralSimulator.scala
+++ src/main/scala/chisel3/simulator/EphemeralSimulator.scala
@@ -4,9 +4,8 @@ import svsim._
 import chisel3.RawModule
 
 /** Provides a simple API for "ephemeral" invocations (where you don't care about the artifacts after the invocation completes) to
-  * simulate Chisel modules. To keep things really simple, it only provides the
-  * peek/poke API, which provides enough control while hiding some of the lower-level svsim
-  * complexity.
+  * simulate Chisel modules. To keep things really simple, `EphemeralSimulator` simulations can only be controlled using the
+  * peek/poke API, which provides enough control while hiding some of the lower-level svsim complexity.
   * @example
   * ```
   * import chisel3.simulator.EphemeralSimulator._
