0a522cff9db8c479df11ab246dc91bcb82521a21
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2023-03-31 09:59:30 -0700

    Added more documentation

--- core/src/main/scala/chisel3/internal/Context.scala
+++ core/src/main/scala/chisel3/internal/Context.scala
@@ -1,12 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chisel3.internal
 
+/** All values of contexts must cloneable to a top with more context */
 private[internal] trait CloneToContext {
   def cloneTo(c: Context): CloneToContext
 }
 
-// Datastructure to represent contexts (with back pointers to "provenance", or version of this context with less context information)
+/** Data-structure to represent hierarchical contexts
+  *
+  * Contains links from parent-to-child and child-to-parent
+  * Contains links to its provenance, i.e. from instance to its definition or its version of itself referenced with less context
+  *
+  * E.g. For context Top/foo:Foo/bar:Bar (with child baz:Baz), it contains pointers to the following contexts:
+  *  - Parent:     Top/foo:Foo
+  *  - Children:   Top/foo:Foo/bar:Bar/baz:Baz
+  *  - Provenance: Foo/bar:Bar
+  *
+  * @param key identifier used to pick which context's children to select
+  * @param parent if a parent is present; if None, this is a "top" context
+  * @param provenance if a provenance is preset; if None, this is an "origin" context
+  */
 private[chisel3] class Context(val key: String, val parent: Option[Context], val provenance: Option[Context]) {
+  // Cache of children keys that this context has
   private val localChildrenKeys = new collection.mutable.LinkedHashSet[String]()
+  // Cache of children Contexts
   private val children = new collection.mutable.LinkedHashMap[String, Context]()
 
   /** At this moment in time, all known children keys of this or any provenance Contexts */
@@ -23,9 +41,9 @@ private[chisel3] class Context(val key: String, val parent: Option[Context], val
   lazy val template: Context = {
     if (Some(top) == provenance.map(_.top)) provenance.get.template else this
   }
-  // entire parent context path
+  // Entire parent context path
   lazy val parentContextPath: Seq[Context] = (this +: parent.toSeq.flatMap(_.parentContextPath))
-  // entire provenance context path
+  // Entire provenance context path
   lazy val provenanceContextPath: Seq[Context] = (this +: provenance.toSeq.flatMap(_.provenanceContextPath))
 
   /* Helpers to determine whether this Context has these properties */
@@ -34,27 +52,28 @@ private[chisel3] class Context(val key: String, val parent: Option[Context], val
   lazy val isOrigin = origin == this
   lazy val isTemplate = template == this
 
-  /* Adding children to origin new Contexts */
-
-  private def newChildKey(k: String): Unit = {
-    if (isOrigin) localChildrenKeys += k else provenance.map(_.newChildKey(k))
+  /* Creates a new child key to `this.origin` */
+  private def createNewOriginChildKey(k: String): Unit = {
+    if (isOrigin) localChildrenKeys += k else provenance.map(_.createNewOriginChildKey(k))
   }
 
+  /* Creates a new child Context of `this`; can only call when `this.isOrigin` is true */
   def instantiateChild(childKey: String, provenance: Context): Context = {
     require(isOrigin)
     require(children.get(childKey).isEmpty)
-    newChildKey(childKey)
+    createNewOriginChildKey(childKey)
     val child = new Context(childKey, Some(this), Some(provenance))
     children.put(childKey, child)
     child
   }
 
-  /* Accessing contexts */
-
+  /** Return the appropriate child of `this`, error if this child key is not defined `this.origin` */
   def apply(childKey: String): Context = getChild(childKey).get
 
+  /** Optionally return the appropriate child of `this` if defined `this.origin` */
   def getChild(childKey: String): Option[Context] = getOrDeriveChild(childKey)
 
+  /** Accessing children contexts from `this`; either hit in the children cache, or build, cache, and return a new Context with provenance back to its origin */
   private def getOrDeriveChild(childKey: String): Option[Context] = {
     if (childrenKeys.contains(childKey)) {
       if (children.contains(childKey)) {
@@ -69,42 +88,31 @@ private[chisel3] class Context(val key: String, val parent: Option[Context], val
     } else None
   }
 
-  private def recurseParent(p: Context, top: Context): Option[Context] = {
-    if (p.origin == top) Some(p)
-    else {
-      if (p == p.top) None
-      else {
-        recurseParent(p.parent.get, top).map(_.apply(p.key))
-      }
+  /** Computes a new context to take the place of the previous `top`, rooted at the same top as `p` */
+  private def computeNewTop(p: Context, top: Context): Option[Context] = {
+    if (top == p.origin) {
+      Some(p)
+    } else {
+      if (p == p.top) None else computeNewTop(p.parent.get, top)
     }
   }
 
-  // either we share a top, or we don't
-  // If we share a top, return
-  // If we don't, then either
-  //  - (1) this.top subsumes other.top
-  //    - other.top is my provenance
-  //    - If any origin parent path == other.top, then retop
-  //  - (2) this.top is subsumed by other.top
-  //    - None
-  //  - (3) this.top and other.top have no relationship
-  //    - NoTone
+  /** Retops `this` to the top of `other` if `this.top` is subsumed by `origin` or any of its parents
+    */
   def copyTo(other: Context): Context = {
-    if (other.top == top) {
+    if (top == other.top) { // Already share same top-level context, so nothing needed
       this
-    } else {
-      if (other.origin == top) {
-        val newMe = parentPath.reverse.tail.foldLeft(other) { (p, key) =>
+    } else { // We need to retop this to a parent of other, if we can find one. Otherwise, return this unchanged
+      computeNewTop(other, top).map { newTop =>
+        parentPath.reverse.tail.foldLeft(newTop) { (p, key) =>
           p.apply(key)
         }
-        newMe
-      } else {
-        recurseParent(other, top).getOrElse(this)
-      }
+      }.getOrElse(this)
     }
   }
 
-  /* Values */
+  /** ***** Context-specific values *******
+    */
 
   private var valueVar: Option[CloneToContext] = None
   def setValue[T <: CloneToContext](v: T): T = {
@@ -113,28 +121,54 @@ private[chisel3] class Context(val key: String, val parent: Option[Context], val
     v
   }
 
+  /** Returns the first value defined along the provenance path; if its not defined sets the local value and returns it */
   def getOrSetValue[T <: CloneToContext](v: => T): T = if (hasValue) value.asInstanceOf[T] else setValue(v)
 
+  /** Returns the first value defined along the provenance path; if its not defined sets the local value and returns it */
+  def getLocalOrSetValue[T <: CloneToContext](v: => T): T = if (hasLocalValue) value.asInstanceOf[T] else setValue(v)
+
+  /** Returns the first value defined along the provenance path; errors if no value is present along provenance path */
   def value: CloneToContext = {
     require(hasValue, s"No value: cannot call value on $this:\n$visualize")
     getValue.get
   }
+
+  /** Optionally returns the first value defined along the provenance path; e.g. values with more top-context have higher priority */
   def getValue: Option[CloneToContext] = valueVar.orElse(provenance.flatMap(_.getValue).map(x => x.cloneTo(this)))
-  def hasLocalValue = valueVar.isDefined
+
+  /** If any value is defined in provenance chain */
   def hasValue = getValue.isDefined
 
-  // Helpful for debugging
+  /** Optionally returns the value defined on `this`; does not look down provenance chain */
+  def getLocalValue: Option[CloneToContext] = valueVar
+
+  /** If defined on `this`; does not look down provenance chain */
+  def hasLocalValue = valueVar.isDefined
+
+  /** ***** Debugging and Visualization Functions *******
+    */
+
+  /** Indicates this specific context object in memory; helpful for debugging */
   def id = this.hashCode().toString.take(3)
+
+  /** Associates `this.key` with its `template.key` */
   def keyWithDefinition = if (isTemplate) key else key + ":" + template.key
+
+  /** Associates `this.key` with its `template.key` and `this.id` */
   def keyWithId = keyWithDefinition + "(" + id + ")"
 
-  /* Helpers to visualize the Context tree information */
-
+  /** List of parent key's in order from `this` to `top` */
   def parentPath: Seq[String] = (key +: parent.toSeq.flatMap(_.parentPath))
 
+  /** List of provenance key's in order from `this` to `origin` */
   def provenancePath: Seq[String] = (key +: provenance.toSeq.flatMap(_.provenancePath))
 
-  def parentageAndProvenance(getKey: Context => String): String = {
+  /** A matrix of parent (vertical) and provenance (horizontal) chains customizable by how to translate each Context to a String */
+  def visualizeParentsAndProvenance(getKey: Context => String): String = {
+    def align(txt: String, size: Int): String = {
+      require(size >= txt.size, s"$size is not large enough to contain ${txt.size}")
+      txt + (" " * (size - txt.size))
+    }
     val sizes = collection.mutable.HashMap.empty[Int, Int]
     val parentProvenance: Seq[Seq[String]] =
       parentContextPath.map { p =>
@@ -150,19 +184,23 @@ private[chisel3] class Context(val key: String, val parent: Option[Context], val
     }.mkString("\n")
   }
 
-  def visualize = parentageAndProvenance(_.key)
-  def visualizeWithId = parentageAndProvenance(_.keyWithId)
-  def visualizeWithDefinition = parentageAndProvenance(_.keyWithDefinition)
+  /** A matrix of parent (vertical) and provenance (horizontal) keys */
+  def visualize = visualizeParentsAndProvenance(_.key)
 
-  def target:               String = parentContextPath.map(_.key).reverse.mkString("/")
-  def targetWithId:         String = parentContextPath.map(_.keyWithId).reverse.mkString("/")
+  /** A matrix of parent (vertical) and provenance (horizontal) keys with id */
+  def visualizeWithId = visualizeParentsAndProvenance(_.keyWithId)
+
+  /** A matrix of parent (vertical) and provenance (horizontal) keys with origin.key and id */
+  def visualizeWithDefinition = visualizeParentsAndProvenance(_.keyWithDefinition)
+
+  /** Parent chain of keys */
+  def target: String = parentContextPath.map(_.key).reverse.mkString("/")
+
+  /** Parent chain of keys with id */
+  def targetWithId: String = parentContextPath.map(_.keyWithId).reverse.mkString("/")
+
+  /** Parent chain of keys with id and origin.key */
   def targetWithDefinition: String = parentContextPath.map(_.keyWithId).reverse.mkString("/")
-
-  //Static
-  def align(txt: String, size: Int): String = {
-    require(size >= txt.size, s"$size is not large enough to contain ${txt.size}")
-    txt + (" " * (size - txt.size))
-  }
 
   /* Iterators to walk Context trees */
 
