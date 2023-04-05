df04b4f98929c0a95dfd64b64fed2f7d1af0d5a7
Deborah Soung <debs@sifive.com>
2023-04-05 15:16:31 -0700

    adding test for connectors

--- src/test/scala/chiselTests/experimental/ProbeSpec.scala
+++ src/test/scala/chiselTests/experimental/ProbeSpec.scala
@@ -6,7 +6,7 @@ import chiselTests.{ChiselFlatSpec, Utils}
 import circt.stage.ChiselStage
 
 class ProbeSpec extends ChiselFlatSpec with Utils {
-  "Ref" should "emit FIRRTL probe descriptors" in {
+  "U-Turn example" should "emit FIRRTL probe statements and expressions" in {
     val chirrtl = ChiselStage.emitCHIRRTL(
       new Module {
 
@@ -42,6 +42,7 @@ class ProbeSpec extends ChiselFlatSpec with Utils {
       },
       Array("--full-stacktrace")
     )
+
     chirrtl should include("output io : { flip in : Probe<UInt<1>>, out : Probe<UInt<1>>}")
     chirrtl should include("define u1.io.in = probe(io.x)")
     chirrtl should include("define u2.io.in = u1.io.out")
@@ -50,6 +51,44 @@ class ProbeSpec extends ChiselFlatSpec with Utils {
     chirrtl should include("release_initial(u1.io.out")
     chirrtl should include("force(clock, io.x, u2.io.out, u1.io.out)")
     chirrtl should include("release(clock, io.y, u2.io.out)")
+  }
+
+  "Connectors" should "work with probes" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(
+      new Module {
+
+        class FooBundle extends Bundle {
+          val bar = Bool()
+          val baz = Probe(UInt(8.W))
+        }
+
+        val io = IO(new Bundle {
+          val a = Input(new FooBundle)
+          val w = Output(Probe(new FooBundle))
+          val x = Output(new FooBundle)
+          val y = Output(new FooBundle)
+          val z = Output(new FooBundle)
+        })
+
+        // connecting two probe types
+        io.w := Probe.probe(io.a)
+
+        // connecting bundles containing probe types
+        io.x := io.a
+        io.z <> io.a
+        io.y :<>= io.a
+      },
+      Array("--full-stacktrace")
+    )
+
+    chirrtl should include("io.w.baz <= probe(io.a).baz")
+    chirrtl should include("io.w.bar <= probe(io.a).bar")
+    chirrtl should include("io.x.baz <= io.a.baz")
+    chirrtl should include("io.x.bar <= io.a.bar")
+    chirrtl should include("io.z.baz <= io.a.baz")
+    chirrtl should include("io.z.bar <= io.a.bar")
+    chirrtl should include("io.y.baz <= io.a.baz")
+    chirrtl should include("io.y.bar <= io.a.bar")
   }
 
 }
