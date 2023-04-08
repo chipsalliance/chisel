024cbfcb93bfeb5ca6d79548de64674fe54baab3
Andrew Lenharth <darthscsi@users.noreply.github.com>
2023-04-08 11:34:41 -0700

    More Circt intrinsic wrappers (IsX, PlusArgsTest, PlusArgsValue) (#2958)
    This extends the set of circt intrinsics with Chisel wrappers. These have no impact on code not using them.

---
+++ src/test/scala/chiselTests/util/PlusArgsTestSpec.scala
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chiselTests.util
+
+import chisel3._
+import chisel3.testers.BasicTester
+import chisel3.util.circt.PlusArgsTest
+import circt.stage.ChiselStage
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.io.Source
+
+private class PlusArgsTestTop extends Module {
+  val w = IO(Output(UInt(1.W)))
+  val x = IO(Output(UInt(1.W)))
+  val z = IO(Input(UInt(32.W)))
+  w := PlusArgsTest(UInt(32.W), "FOO")
+  x := PlusArgsTest(z, "BAR")
+}
+
+class PlusArgsTestSpec extends AnyFlatSpec with Matchers {
+  it should "Should work for types" in {
+    val fir = ChiselStage.emitCHIRRTL(new PlusArgsTestTop)
+    println(fir)
+    (fir.split('\n').map(_.trim) should contain).allOf(
+      "intmodule PlusArgsTestIntrinsic :",
+      "output found : UInt<1>",
+      "intrinsic = circt_plusargs_test",
+      "parameter FORMAT = \"FOO\"",
+      "parameter FORMAT = \"BAR\""
+    )
+  }
+}
--- src/test/scala/chiselTests/util/SizeOfSpec.scala
+++ src/test/scala/chiselTests/util/SizeOfSpec.scala
@@ -1,64 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chiselTests.util
 
 import chisel3._
-import chisel3.stage.ChiselGeneratorAnnotation
 import chisel3.testers.BasicTester
 import chisel3.util.circt.SizeOf
-
 import circt.stage.ChiselStage
-
-import firrtl.stage.FirrtlCircuitAnnotation
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.io.Source
 
-class MyBundle extends Bundle {
+private class SizeOfBundle extends Bundle {
   val a = UInt()
   val b = SInt()
 }
 
-class SizeOfTop extends Module {
-  val io = IO(new Bundle {
-    val w = Input(UInt(65.W))
-    val x = Input(new MyBundle)
-    val outw = UInt(32.W)
-    val outx = UInt(32.W)
-  })
-  io.outw := SizeOf(io.w)
-  io.outx := SizeOf(io.x)
+private class SizeOfTop extends Module {
+  val w = IO(Input(UInt(65.W)))
+  val x = IO(Input(new SizeOfBundle))
+  val outw = IO(Output(UInt(32.W)))
+  val outx = IO(Output(UInt(32.W)))
+  outw := SizeOf(w)
+  outx := SizeOf(x)
 }
 
-/** A test for intrinsics.  Since chisel is producing intrinsics as tagged
-  * extmodules (for now), we explicitly test the chirrtl and annotations rather
-  * than the processed firrtl or verilog.  It is worth noting that annotations
-  * are implemented (for now) in a way which makes the output valid for all
-  * firrtl compilers, hence we write a localized, not end-to-end test
-  */
 class SizeOfSpec extends AnyFlatSpec with Matchers {
   it should "Should work for types" in {
     val fir = ChiselStage.emitCHIRRTL(new SizeOfTop)
-    val a1 = """extmodule SizeOf_0""".r
-    (fir should include).regex(a1)
-    val b1 = """defname = SizeOf_0""".r
-    (fir should include).regex(b1)
-    val a2 = """extmodule SizeOf_1""".r
-    (fir should include).regex(a2)
-    val b2 = """defname = SizeOf_1""".r
-    (fir should include).regex(b2)
-
-    // The second elaboration uses a unique name since the Builder is reused (?)
-    val c = """Intrinsic\(~SizeOfTop\|SizeOf.*,circt.sizeof\)"""
-    ((new ChiselStage)
-      .execute(
-        args = Array("--target", "chirrtl"),
-        annotations = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new SizeOfTop))
-      )
-      .flatMap {
-        case FirrtlCircuitAnnotation(circuit) => circuit.annotations
-        case _                                => None
-      }
-      .mkString("\n") should include).regex(c)
+    println(fir)
+    (fir.split('\n').map(_.trim) should contain)
+      .allOf("intmodule SizeOfIntrinsic :", "input i : UInt<65>", "output size : UInt<32>", "intrinsic = circt_sizeof")
   }
 }
