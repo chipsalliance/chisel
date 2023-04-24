7440a27497bf56feb9566cdd21863e3b3cfbe511
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-04-24 15:51:38 -0400

    [Feature] Add Interface, ConformsTo
    Migrate work from seldridge/chisel-separable on separate compilation
    into upstream Chisel.  This defines an Interface and a Conformance.  An
    Interface is a Singleton Object that maps to a FIRRTL external module
    with pre-defined IO.  A Conformance is a type-class whose
    implementations define how a Chisel module aligns itself to the
    Interface.  Used together, these enable separate compilation of a client
    that uses an Interface from a component that implements the Interface.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ src/main/scala/chisel3/interface/Interface.scala
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: Apache-2.0
+package chisel3.interface
+
+import chisel3.{BlackBox => _, Module => _, _}
+import chisel3.experimental.{BaseModule, FlatIO}
+import chisel3.experimental.dataview._
+import scala.annotation.implicitNotFound
+
+@implicitNotFound(
+  "this method requires information from the separable compilation implementation, please bring one into scope as an `implicit val`. You can also consult the team that owns the implementation to refer to which one you should use!"
+)
+trait ConformsTo[Intf <: Interface, Mod <: BaseModule] {
+
+  /** Return the module that conforms to a port-level interface. */
+  private[interface] def genModule(): Mod
+
+  /** Define how this module hooks up to the port-level interface. */
+  private[interface] def portMap: Seq[(Mod, Intf#Ports) => (Data, Data)]
+
+}
+
+/** Functionality which is common */
+sealed trait InterfaceCommon {
+
+  private[interface] type Ports <: Record
+
+  /** Returns the Record that is the port-level interface. */
+  private[interface] def ports(): Ports
+
+}
+
+/** A generator of different Interfaces. Currently, this is just an Interface
+  * that is not a Singleton.
+  */
+trait InterfaceGenerator extends InterfaceCommon
+
+/** An interface between hardware units. Any module that implements this
+  * interface may be separately compiled from any module that instantiates this
+  * interface.
+  */
+trait Interface extends InterfaceCommon { self: Singleton =>
+
+  /** This types represents the type of a valid conformance to this Interface.
+    */
+  private type Conformance[Mod <: RawModule] = ConformsTo[this.type, Mod]
+
+  /** The name of this interface. This will be used as the name of any module
+    * that implements this interface.
+    * I.e., this is the name of the `BlackBox` and `Module` that are provided
+    * below. The implementation of this method is just coming up with a good
+    * name derived from the class name. (The name of the Interface in FIRRTL
+    * will be the name of the Scala class that extends the Interface.)
+    */
+  private[interface] def interfaceName: String = {
+    val className = getClass().getName()
+    className
+      .drop(className.lastIndexOf('.') + 1)
+      .split('$')
+      .toSeq
+      .filter {
+        case str if str.forall(_.isDigit) => false
+        case str                          => true
+      }
+      .last
+  }
+
+  sealed trait Entity { this: BaseModule =>
+    val io: Ports
+  }
+
+  object Wrapper {
+
+    /** The black box that has the same ports as this interface. This is what is
+      * instantiated by any user of this interface, i.e., a test harness.
+      */
+    final class BlackBox extends chisel3.BlackBox with Entity {
+      final val io = IO(ports())
+
+      override final def desiredName = interfaceName
+    }
+
+    /** The module that wraps any module which conforms to this Interface.
+      */
+    final class Module[B <: RawModule](
+    )(
+      implicit conformance: Conformance[B])
+        extends RawModule
+        with Entity {
+      final val io = FlatIO(ports())
+
+      // Use a dummy clock and reset connection when constructing the module.
+      // This is fine as we rely on DataView to catch missing connections to
+      // clock and reset.  The dummy clock and reset will never be used.
+      private val internal = withClockAndReset(
+        WireInit(Clock(), DontCare),
+        WireInit(Reset(), DontCare)
+      ) {
+        chisel3.Module(conformance.genModule())
+      }
+
+      private implicit val pm = PartialDataView[B, Ports](
+        _ => ports(),
+        conformance.portMap: _*
+      )
+      io :<>= internal.viewAs[Ports]
+
+      override def desiredName = interfaceName
+    }
+
+    /** A stub module that implements the interface. All IO of this module are
+      * just tied off.
+      */
+    final class Stub extends RawModule with Entity {
+      final val io = FlatIO(ports())
+      io := DontCare
+      dontTouch(io)
+    }
+
+  }
+
+}
