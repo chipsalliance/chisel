fbdf824552c7b22715eaa176b0382d0be1fb6201
Hideto Ueno <uenoku.tokotoko@gmail.com>
2023-06-22 05:33:01 -0700

    Rename intrinsics, add comments

--- src/main/scala/chisel3/util/circt/Synthesis.scala
+++ src/main/scala/chisel3/util/circt/Synthesis.scala
@@ -3,40 +3,52 @@
 package chisel3.util.circt
 
 import chisel3._
-import chisel3.experimental.IntrinsicModule
+import chisel3.experimental.{requireIsHardware, IntrinsicModule}
 import chisel3.internal.Builder
 
 import circt.Intrinsic
 
 /** A 2-to-1 mux cell intrinsic.
   */
-private class MuxCell2Intrinsic[T <: Data](gen: T) extends IntrinsicModule("circt_muxcell2") {
+private class Mux2CellIntrinsic[T <: Data](gen: T) extends IntrinsicModule("circt_mux2cell") {
   val sel = IO(Input(UInt(1.W)))
   val high = IO(Input(gen))
   val low = IO(Input(gen))
   val out = IO(Output(gen))
 }
 
-object MuxCell2 {
+/** Utility for constructing 2-to-1 MUX cell intrinsic. This intrinsic is lowered into verilog
+  * with vendor specic pragmas that guarantee utilization of 2-to-1 MUX cell in the synthesis process.
+  * Semantically `Mux2Cell(cond, con, alt)` is equivalent to `Mux(cond, con, alt)` for all `cond`, `con` and `alt`.
+  */
+object Mux2Cell {
 
-  /** Creates an intrinsic which is lowered to a 2-to-1 MUX cell in synthesis tools.
-    * @example {{{
-    * v := MuxCell2(sel, high, low)
+  /**
+    * Creates an intrinsic which will be lowered to a 2-to-1 MUX cell in synthesis tools.
+    * @param cond condition determining the input to choose
+    * @param con the value chosen when `cond` is true
+    * @param alt the value chosen when `cond` is false
+    * @example
+    * {{{
+    * val muxOut = Mux2Cell(data_in === 3.U, 3.U(4.W), 0.U(4.W))
     * }}}
     */
-
-  def apply[T <: Data](sel: UInt, high: T, low: T): T = {
-    val inst = Module(new MuxCell2Intrinsic(chisel3.chiselTypeOf(high)))
-    inst.sel := sel
-    inst.high := high
-    inst.low := low
+  def apply[T <: Data](cond: UInt, con: T, alt: T): T = {
+    requireIsHardware(cond, "MUX2 cell selector")
+    requireIsHardware(con, "MUX2 cell true value")
+    requireIsHardware(alt, "MUX2 cell false value")
+    val d = cloneSupertype(Seq(con, alt), "Mux2Cell")
+    val inst = Module(new Mux2CellIntrinsic(d))
+    inst.sel := cond
+    inst.high := con
+    inst.low := alt
     inst.out
   }
 }
 
 /** A 4-to-1 mux cell intrinsic.
   */
-private class MuxCell4Intrinsic[T <: Data](gen: T) extends IntrinsicModule("circt_muxcell4") {
+private class Mux4CellIntrinsic[T <: Data](gen: T) extends IntrinsicModule("circt_mux4cell") {
   val sel = IO(Input(UInt(2.W)))
   val v3 = IO(Input(gen))
   val v2 = IO(Input(gen))
@@ -45,18 +57,30 @@ private class MuxCell4Intrinsic[T <: Data](gen: T) extends IntrinsicModule("circ
   val out = IO(Output(gen))
 }
 
-object MuxCell4 {
+/** Utility for constructing 4-to-1 MUX cell intrinsic. This intrinsic is lowered into verilog
+  * with vendor specic pragmas that guarantee utilization of 4-to-1 MUX cell in the synthesis process.
+  */
+object Mux4Cell {
 
-  /** Creates an intrinsic which is lowered to a 4-to-1 MUX cell in synthesis tools.
-    * A selector must be a 2-bit unsigned integer. This instrinsic returns v0 when the
-    * selector is 0, and returns v3 when the selector is 3.
+  /**
+    * Creates an intrinsic which will be lowered to a 4-to-1 MUX cell in synthesis tools.
+    * @param sel 2-bit unsigned integer determining the input to choose.
+    * @param v3 the value chosen when `selector` is `3`
+    * @param v2 the value chosen when `selector` is `2`
+    * @param v1 the value chosen when `selector` is `1`
+    * @param v0 the value chosen when `selector` is `0`
     * @example {{{
-    * v := MuxCell4(sel, v3, v2, v1, v0)
+    * v := Mux4Cell(sel, v3, v2, v1, v0)
     * }}}
     */
-
   def apply[T <: Data](sel: UInt, v3: T, v2: T, v1: T, v0: T): T = {
-    val inst = Module(new MuxCell4Intrinsic(chisel3.chiselTypeOf(v3)))
+    requireIsHardware(sel, "4-to-1 mux selector")
+    requireIsHardware(v3, "MUX4 cell input value when selector == 3")
+    requireIsHardware(v2, "MUX4 cell input value when selector == 2")
+    requireIsHardware(v1, "MUX4 cell input value when selector == 1")
+    requireIsHardware(v0, "MUX4 cell input value when selector == 0")
+    val d = cloneSupertype(Seq(v3, v2, v1, v0), "Mux4Cell")
+    val inst = Module(new Mux4CellIntrinsic(d))
     inst.sel := sel
     inst.v3 := v3
     inst.v2 := v2
--- src/test/scala/chiselTests/util/circt/Synthesis.scala
+++ src/test/scala/chiselTests/util/circt/Synthesis.scala
@@ -4,7 +4,7 @@ package chiselTests.util.circt
 
 import chisel3._
 import chisel3.testers.BasicTester
-import chisel3.util.circt.{MuxCell2, MuxCell4}
+import chisel3.util.circt.{Mux2Cell, Mux4Cell}
 import circt.stage.ChiselStage
 
 import org.scalatest.flatspec.AnyFlatSpec
@@ -12,52 +12,52 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.io.Source
 
-private class MuxCell2Top extends Module {
+private class Mux2CellTop extends Module {
   val sel = IO(Input(UInt(1.W)))
   val high = IO(Input(UInt(32.W)))
   val low = IO(Input(UInt(32.W)))
   val out = IO(Output(UInt(32.W)))
-  out := MuxCell2(sel, high, low)
+  out := Mux2Cell(sel, high, low)
 }
 
-class MuxCell2Spec extends AnyFlatSpec with Matchers {
+class Mux2CellSpec extends AnyFlatSpec with Matchers {
   it should "work for types" in {
-    val fir = ChiselStage.emitCHIRRTL(new MuxCell2Top)
+    val fir = ChiselStage.emitCHIRRTL(new Mux2CellTop)
     (fir.split('\n').map(_.trim) should contain)
       .allOf(
-        "intmodule MuxCell2Intrinsic :",
+        "intmodule Mux2CellIntrinsic :",
         "input sel : UInt<1>",
         "input high : UInt<32>",
         "input low : UInt<32>",
         "output out : UInt<32>",
-        "intrinsic = circt_muxcell2"
+        "intrinsic = circt_mux2cell"
       )
   }
 }
 
-private class MuxCell4Top extends Module {
+private class Mux4CellTop extends Module {
   val sel = IO(Input(UInt(2.W)))
   val v3 = IO(Input(UInt(32.W)))
   val v2 = IO(Input(UInt(32.W)))
   val v1 = IO(Input(UInt(32.W)))
   val v0 = IO(Input(UInt(32.W)))
   val out = IO(Output(UInt(32.W)))
-  out := MuxCell4(sel, v3, v2, v1, v0)
+  out := Mux4Cell(sel, v3, v2, v1, v0)
 }
 
-class MuxCell4Spec extends AnyFlatSpec with Matchers {
+class Mux4CellSpec extends AnyFlatSpec with Matchers {
   it should "work for types" in {
-    val fir = ChiselStage.emitCHIRRTL(new MuxCell4Top)
+    val fir = ChiselStage.emitCHIRRTL(new Mux4CellTop)
     (fir.split('\n').map(_.trim) should contain)
       .allOf(
-        "intmodule MuxCell4Intrinsic :",
+        "intmodule Mux4CellIntrinsic :",
         "input sel : UInt<2>",
         "input v3 : UInt<32>",
         "input v2 : UInt<32>",
         "input v1 : UInt<32>",
         "input v0 : UInt<32>",
         "output out : UInt<32>",
-        "intrinsic = circt_muxcell4"
+        "intrinsic = circt_mux4cell"
       )
   }
 }
