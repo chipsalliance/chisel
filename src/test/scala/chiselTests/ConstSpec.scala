458fb16017b9970aa8f63d1f5513ba8eb0549bec
Daniel Resnick <danielzresnick@gmail.com>
2023-06-27 13:07:14 -0600

    Support bulk connection for const types (#3384)

--- src/test/scala/chiselTests/ConstSpec.scala
+++ src/test/scala/chiselTests/ConstSpec.scala
@@ -72,4 +72,73 @@ class ConstSpec extends ChiselFlatSpec with Utils {
     exc.getMessage should be("Cannot create Const of a Probe.")
   }
 
+  class FooBundle extends Bundle {
+    val a = UInt(4.W)
+    val b = Bool()
+  }
+
+  "Const to const connections" should "emit a direct passthrough connection" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Input(Const(new FooBundle)))
+      val out = IO(Output(Const(new FooBundle)))
+      out := in
+    })
+    chirrtl should include("connect out, in")
+  }
+
+  "Const to non-const connections" should "emit a direct passthrough connection" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Input(Const(new FooBundle)))
+      val out = IO(Output(new FooBundle))
+      out := in
+    })
+    chirrtl should include("connect out, in")
+  }
+
+  "Const to nested const connections" should "emit a direct passthrough connection" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Input(Const(new Bundle {
+        val foo = new FooBundle
+      })))
+      val out = IO(Output(Const(new Bundle {
+        val foo = Const(new FooBundle)
+      })))
+      out := in
+    })
+    chirrtl should include("connect out, in")
+  }
+
+  class BidirectionalBundle extends Bundle {
+    val a = UInt(4.W)
+    val b = Flipped(Bool())
+  }
+
+  "Const to const bidirection connections using ':<>='" should "emit a direct passthrough connection" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Const(Flipped(new BidirectionalBundle)))
+      val out = IO(Const(new BidirectionalBundle))
+      out :<>= in
+    })
+    chirrtl should include("connect out, in")
+  }
+
+  "Const to const bidirection connections using '<>'" should "emit a direct passthrough connection" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Const(Flipped(new BidirectionalBundle)))
+      val out = IO(Const(new BidirectionalBundle))
+      out <> in
+    })
+    chirrtl should include("connect out, in")
+  }
+
+  "Const to const coercing mono connections using ':#='" should "emit elementwise connections" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
+      val in = IO(Const(Flipped(new BidirectionalBundle)))
+      val out = IO(Output(Const(new BidirectionalBundle)))
+      out :#= in
+    })
+    chirrtl should include("connect out.b, in.b")
+    chirrtl should include("connect out.a, in.a")
+  }
+
 }
