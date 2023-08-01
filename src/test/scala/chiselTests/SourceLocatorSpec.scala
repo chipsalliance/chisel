63063cd0e3c78de6f1f85e85699e27af82757393
Jack Koenig <koenig@sifive.com>
2023-08-01 16:58:22 -0700

    Optimize source locator escaping when emitting FIRRTL (#3439)
    (cherry picked from commit 6de7cce485ea503493dcd3907d1c6e36be66621e)

--- src/test/scala/chiselTests/SourceLocatorSpec.scala
+++ src/test/scala/chiselTests/SourceLocatorSpec.scala
@@ -2,7 +2,10 @@
 
 package chiselTests
 
+import circt.stage.ChiselStage.emitCHIRRTL
 import chisel3._
+import chisel3.experimental.SourceLine
+import firrtl.ir.FileInfo
 
 class SourceLocatorSpec extends ChiselFunSpec with Utils {
   describe("(0) Relative source paths") {
@@ -12,6 +15,30 @@ class SourceLocatorSpec extends ChiselFunSpec with Utils {
       }
       val chirrtl = circt.stage.ChiselStage.emitCHIRRTL(new Top)
       chirrtl should include("@[src/test/scala/chiselTests/SourceLocatorSpec.scala")
+    }
+  }
+
+  describe("(1) Source locators with special characters") {
+    val filename = "I need escaping\n\\\t].scala"
+    val escaped = "I need escaping\\n\\\\\\t\\].scala"
+    val info = SourceLine(filename, 123, 456)
+    it("(1.a): are properly escaped when converting to FIRRTL") {
+      val firrtl = FileInfo.fromUnescaped(filename)
+      firrtl should equal(FileInfo(escaped))
+    }
+    it("(1.b): are properly escaped to FIRRTL through Chisel elaboration") {
+      implicit val sl = info
+
+      val chirrtl = emitCHIRRTL(new RawModule {
+        val in = IO(Input(UInt(8.W)))
+        val out = IO(Output(UInt(8.W)))
+        out := in
+      })
+      chirrtl should include(escaped)
+    }
+    it("(1.c): can be properly unescaped") {
+      val escapedInfo = FileInfo(escaped)
+      escapedInfo.unescaped should equal(filename)
     }
   }
 }
