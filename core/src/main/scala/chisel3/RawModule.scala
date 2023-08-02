8a650688eb6698e1a9c75f4de14ad1079e92aedf
Mike Urbach <mikeurbach@gmail.com>
2023-08-02 17:42:06 -0600

    Document use of atModuleBodyEnd with Definition.

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -72,6 +72,9 @@ abstract class RawModule extends BaseModule {
     *      val baz = ...
     *    }
     *  }}}
+    *
+    * If atModuleBodyEnd is used in a Definition, any generated hardware will be included in the Definition. However, it
+    * is currently not possible to annotate any val within atModuleBodyEnd as @public.
     */
   protected def atModuleBodyEnd(gen: => Unit): Unit = {
     _atModuleBodyEnd += { () => gen }
