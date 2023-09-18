a79a20ea169148f2f73244ae306b9ec05fb6135c
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-09-18 14:47:51 -0400

    Add FixedIOModule, FixedIOBlackBox
    Add a new module hierarchy that promotes the type of the IO of that module
    to a type parameter.  This module hierarchy includes:
    
      1. FixedIOBaseModule
      2. FixedIORawModule
      3. FixedIOBlackBox
    
    Both (2) and (3) inherit from (1).  These modules are all "fixed" in the
    type of their IO.  It follows that no more IO are allowed to be created
    other than what is in the type.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ src/main/scala/chisel3/PolymorphicIOModule.scala
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3
+
+import chisel3.experimental.{BaseModule, ExtModule, FlatIO, Param}
+
+/** A module or external module whose IO is generated from a specific generator.
+  * This module may have no additional IO created other than what is specified
+  * by its `ioGenerator` abstract member.
+  */
+sealed trait FixedIOBaseModule[A <: Data] extends BaseModule {
+
+  /** A generator of IO */
+  protected def ioGenerator: A
+
+  final val io = FlatIO(ioGenerator)
+  disallowIOCreation()
+
+}
+
+/** A Chisel module whose IO is determined by an IO generator.  This module
+  * cannot have additional IO created by modules that extend it.
+  *
+  * @param ioGenerator
+  */
+class FixedIORawModule[A <: Data](final val ioGenerator: A) extends RawModule with FixedIOBaseModule[A]
+
+/** A Chisel blackbox whose IO is determined by an IO generator.  This module
+  * cannot have additional IO created by modules that extend it.
+  *
+  * @param ioGenerator
+  * @param params
+  */
+class FixedIOExtModule[A <: Data](final val ioGenerator: A, params: Map[String, Param] = Map.empty[String, Param])
+    extends ExtModule(params)
+    with FixedIOBaseModule[A]
