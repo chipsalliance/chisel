c3877182c8725da7628312831f447477ec87e482
Aditya Naik <aditya.naik@sifive.com>
2023-09-21 16:24:43 -0700

    Add DedupGroupAnnotation phase
    fmt

---
+++ src/main/scala/chisel3/stage/phases/AddDedupGroupAnnotations.scala
@@ -0,0 +1,33 @@
+package chisel3.stage.phases
+
+import firrtl.AnnotationSeq
+import firrtl.options.{Dependency, Phase}
+import firrtl.options.Viewer.view
+import firrtl.transforms.DedupGroupAnnotation
+
+import chisel3.experimental.BaseModule
+import chisel3.stage._
+import chisel3.stage.CircuitSerializationAnnotation._
+import chisel3.ChiselException
+import chisel3.experimental.dedupGroup
+
+class AddDedupGroupAnnotations extends Phase {
+  override def prerequisites = Seq.empty
+  override def optionalPrerequisites = Seq(Dependency[Convert])
+  override def optionalPrerequisiteOf = Seq.empty
+  override def invalidates(a: Phase) = false
+
+  def transform(annotations: AnnotationSeq): AnnotationSeq = {
+    val chiselOptions = view[ChiselOptions](annotations)
+    val circuit = chiselOptions.chiselCircuit.getOrElse {
+      throw new ChiselException(
+        s"Unable to locate the elaborated circuit, did ${classOf[Elaborate].getName} run correctly"
+      )
+    }
+    val skipAnnos = annotations.collect { case x: DedupGroupAnnotation => x.target }
+    val annos = circuit.components.collect {
+      case x if !(skipAnnos.contains(x.id.toTarget)) => DedupGroupAnnotation(x.id.toTarget, x.id.desiredName)
+    }
+    annos ++ annotations
+  }
+}
