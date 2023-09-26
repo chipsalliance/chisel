cfec60a3189abc6c5f7f8bb36b3c276521aec29e
Aditya Naik <aditya.naik@sifive.com>
2023-09-26 12:07:34 -0700

    Add filter for D/I blackbox instances

--- src/main/scala/chisel3/stage/phases/AddDedupGroupAnnotations.scala
+++ src/main/scala/chisel3/stage/phases/AddDedupGroupAnnotations.scala
@@ -4,12 +4,14 @@ import firrtl.AnnotationSeq
 import firrtl.options.{Dependency, Phase}
 import firrtl.options.Viewer.view
 import firrtl.transforms.DedupGroupAnnotation
+import scala.annotation.nowarn
 
 import chisel3.experimental.BaseModule
 import chisel3.stage._
 import chisel3.stage.CircuitSerializationAnnotation._
 import chisel3.ChiselException
 import chisel3.experimental.dedupGroup
+import chisel3.internal.firrtl.DefBlackBox
 
 class AddDedupGroupAnnotations extends Phase {
   override def prerequisites = Seq.empty
@@ -24,8 +26,14 @@ class AddDedupGroupAnnotations extends Phase {
         s"Unable to locate the elaborated circuit, did ${classOf[Elaborate].getName} run correctly"
       )
     }
+
     val skipAnnos = annotations.collect { case x: DedupGroupAnnotation => x.target }
-    val annos = circuit.components.collect {
+
+    @nowarn("msg=class Port")
+    val annos = circuit.components.filterNot {
+      case x @ DefBlackBox(id, _, _, _, _) => id.toString contains "Instance"
+      case x                               => false
+    }.collect {
       case x if !(skipAnnos.contains(x.id.toTarget)) => DedupGroupAnnotation(x.id.toTarget, x.id.desiredName)
     }
     annos ++ annotations
