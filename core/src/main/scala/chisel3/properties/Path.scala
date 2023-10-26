6abb3acc9fe54b0a5be3ed88faf1885b29594be4
Mike Urbach <mikeurbach@gmail.com>
2023-10-26 11:05:02 -0700

    Updated Path property target type serialization. (#3601)

--- core/src/main/scala/chisel3/properties/Path.scala
+++ core/src/main/scala/chisel3/properties/Path.scala
@@ -10,14 +10,23 @@ import firrtl.ir.{PathPropertyLiteral}
 /** Represent a Path type for referencing a hardware instance or member in a Property[Path]
   */
 sealed abstract class Path {
-  private[chisel3] def toTarget(): IsMember
+  private[chisel3] def toTarget():   IsMember
+  private[chisel3] def isMemberPath: Boolean
 
   private[chisel3] def convert(): PathPropertyLiteral = {
     val target = toTarget()
-    val targetType = target match {
-      case _: ModuleTarget    => "OMModuleTarget"
-      case _: InstanceTarget  => "OMInstanceTarget"
-      case _: ReferenceTarget => "OMReferenceTarget"
+    val targetType = if (isMemberPath) {
+      target match {
+        case _: ModuleTarget    => "OMMemberInstanceTarget"
+        case _: InstanceTarget  => "OMMemberInstanceTarget"
+        case _: ReferenceTarget => "OMMemberReferenceTarget"
+      }
+    } else {
+      target match {
+        case _: ModuleTarget    => "OMInstanceTarget"
+        case _: InstanceTarget  => "OMInstanceTarget"
+        case _: ReferenceTarget => "OMReferenceTarget"
+      }
     }
     PathPropertyLiteral(s"$targetType:${target.serialize}")
   }
@@ -27,33 +36,45 @@ object Path {
 
   /** Construct a Path that refers to a Module
     */
-  def apply(module: BaseModule): Path = {
+  def apply(module: BaseModule): Path = apply(module, false)
+  def apply(module: BaseModule, isMemberPath: Boolean): Path = {
+    val _isMemberPath = isMemberPath // avoid name shadowing below
     new Path {
-      def toTarget(): IsMember = module.toAbsoluteTarget
+      def toTarget():   IsMember = module.toAbsoluteTarget
+      def isMemberPath: Boolean = _isMemberPath
     }
   }
 
   /** Construct a Path that refers to a Data
     */
-  def apply(data: Data): Path = {
+  def apply(data: Data): Path = apply(data, false)
+  def apply(data: Data, isMemberPath: Boolean): Path = {
+    val _isMemberPath = isMemberPath // avoid name shadowing below
     new Path {
-      def toTarget(): IsMember = data.toAbsoluteTarget
+      def toTarget():   IsMember = data.toAbsoluteTarget
+      def isMemberPath: Boolean = _isMemberPath
     }
   }
 
   /** Construct a Path that refers to a Memory
     */
-  def apply(mem: MemBase[_]): Path = {
+  def apply(mem: MemBase[_]): Path = apply(mem, false)
+  def apply(mem: MemBase[_], isMemberPath: Boolean): Path = {
+    val _isMemberPath = isMemberPath // avoid name shadowing below
     new Path {
-      def toTarget(): IsMember = mem.toAbsoluteTarget
+      def toTarget():   IsMember = mem.toAbsoluteTarget
+      def isMemberPath: Boolean = _isMemberPath
     }
   }
 
   /** Construct a Path from a target
     */
-  def apply(target: IsMember): Path = {
+  def apply(target: IsMember): Path = apply(target, false)
+  def apply(target: IsMember, isMemberPath: Boolean): Path = {
+    val _isMemberPath = isMemberPath // avoid name shadowing below
     new Path {
-      def toTarget(): IsMember = target
+      def toTarget():   IsMember = target
+      def isMemberPath: Boolean = _isMemberPath
     }
   }
 }
--- src/test/scala/chiselTests/properties/PropertySpec.scala
+++ src/test/scala/chiselTests/properties/PropertySpec.scala
@@ -168,6 +168,7 @@ class PropertySpec extends ChiselFlatSpec with MatchesAndOmits {
       val propOutA = IO(Output(Property[Path]()))
       val propOutB = IO(Output(Property[Path]()))
       val propOutC = IO(Output(Property[Path]()))
+      val propOutD = IO(Output(Property[Path]()))
       override def desiredName = "Top"
       val inst = Module(new RawModule {
         val data = WireInit(false.B)
@@ -177,12 +178,37 @@ class PropertySpec extends ChiselFlatSpec with MatchesAndOmits {
       propOutA := Property(inst)
       propOutB := Property(inst.data)
       propOutC := Property(inst.mem)
+      propOutD := Property(this)
     })
 
     matchesAndOmits(chirrtl)(
       """propassign propOutA, path("OMInstanceTarget:~Top|Top/inst:Foo")""",
       """propassign propOutB, path("OMReferenceTarget:~Top|Top/inst:Foo>data")""",
-      """propassign propOutC, path("OMReferenceTarget:~Top|Top/inst:Foo>mem")"""
+      """propassign propOutC, path("OMReferenceTarget:~Top|Top/inst:Foo>mem")""",
+      """propassign propOutD, path("OMInstanceTarget:~Top|Top")"""
+    )()
+  }
+
+  it should "support member path target types when requested" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
+      val propOutA = IO(Output(Property[Path]()))
+      val propOutB = IO(Output(Property[Path]()))
+      val propOutC = IO(Output(Property[Path]()))
+      override def desiredName = "Top"
+      val inst = Module(new RawModule {
+        val data = WireInit(false.B)
+        val mem = SyncReadMem(1, Bool())
+        override def desiredName = "Foo"
+      })
+      propOutA := Property(Path(inst, true))
+      propOutB := Property(Path(inst.data, true))
+      propOutC := Property(Path(inst.mem, true))
+    })
+
+    matchesAndOmits(chirrtl)(
+      """propassign propOutA, path("OMMemberInstanceTarget:~Top|Top/inst:Foo")""",
+      """propassign propOutB, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>data")""",
+      """propassign propOutC, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>mem")"""
     )()
   }
 
