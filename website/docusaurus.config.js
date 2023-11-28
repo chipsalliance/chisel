5dd0d06959214efdc6bc467da00076e79d3c773f
Jack Koenig <koenig@sifive.com>
2023-11-28 09:16:57 -0800

    [website] Fix website URL
    https://chisel-lang.org is not the same thing as
    https://www.chisel-lang.org. This is causing issues for the Algolia web
    crawler.
    
    h/t @richardxia for pointing this out to me

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   favicon: 'img/chisel-tool-icon.svg',
 
   // Set the production url of your site here
-  url: 'https://chisel-lang.org',
+  url: 'https://www.chisel-lang.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
