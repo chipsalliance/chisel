301ff7ede5cc4613e8bb292a2df0f0ef14b592f4
Jack Koenig <koenig@sifive.com>
2024-01-22 15:49:20 -0800

    Resolve backport conflicts

--- .github/workflows/build-scala-cli-template/chisel-template.scala
+++ .github/workflows/build-scala-cli-template/chisel-template.scala
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 //> using repository "sonatype:snapshots"
 //> using scala "2.13.10"
-//> using lib "edu.berkeley.cs::chisel3::@VERSION@"
+//> using dep "edu.berkeley.cs::chisel3::@VERSION@"
 //> using plugin "edu.berkeley.cs:::chisel3-plugin::@VERSION@"
-=======
-//> using repository "sonatype-s01:snapshots"
-//> using scala "2.13.12"
-//> using dep "org.chipsalliance::chisel::@VERSION@"
-//> using plugin "org.chipsalliance:::chisel-plugin::@VERSION@"
->>>>>>> 589f10672 (Update Scala CLI Template (#3757))
 //> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
 
 import chisel3._
