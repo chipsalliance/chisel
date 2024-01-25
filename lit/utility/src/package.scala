f5fc4100677a08512787d4acbbddc489fab71e5f
Jiuyang Liu <liu@jiuyang.me>
2024-01-25 15:51:18 +0800

    add getConverter for prototyping APIs

--- lit/utility/src/package.scala
+++ lit/utility/src/package.scala
@@ -4,19 +4,22 @@ import chisel3.panamaconverter.PanamaCIRCTConverter
 package object lit {
   object utility {
     object panamaconverter {
-      def streamString(module: => RawModule, stream: PanamaCIRCTConverter => geny.Writable): String = Seq(
+      def getConverter(module: => RawModule): PanamaCIRCTConverter = Seq(
         new chisel3.stage.phases.Elaborate,
         chisel3.panamaconverter.stage.Convert
       ).foldLeft(
         firrtl.AnnotationSeq(Seq(chisel3.stage.ChiselGeneratorAnnotation(() => module)))
-      ) { case (annos, phase) => phase.transform(annos) }
-        .collectFirst {
-          case chisel3.panamaconverter.stage.PanamaCIRCTConverterAnnotation(converter) =>
-            val string = new java.io.ByteArrayOutputStream
-            stream(converter).writeBytesTo(string)
-            new String(string.toByteArray)
-        }
-        .get
+      ) { case (annos, phase) => phase.transform(annos) }.collectFirst {
+        case chisel3.panamaconverter.stage.PanamaCIRCTConverterAnnotation(converter) =>
+          converter
+      }.get
+
+      def streamString(module: => RawModule, stream: PanamaCIRCTConverter => geny.Writable): String = {
+        val converter = getConverter(module)
+        val string = new java.io.ByteArrayOutputStream
+        stream(converter).writeBytesTo(string)
+        new String(string.toByteArray)
+      }
 
       def firrtlString(module: => RawModule): String = streamString(module, _.firrtlStream)
 
