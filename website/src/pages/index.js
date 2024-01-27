5b198c9c77a265eac2ad34e1c962570b9de37ebe
Jack Koenig <koenig@sifive.com>
2024-01-27 12:42:05 -0800

    [website] Get title and description from siteConfig

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title="Home"
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
