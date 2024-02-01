eeb45a4ee7604b8510364f3e188bb338f4cd7d25
Jack Koenig <koenig@sifive.com>
2024-01-31 21:27:21 -0800

    Report source locator in when scoping error messages (#3804)
    (cherry picked from commit d21a66358fb5e93c0c8e5d62d9138dd1c26d8853)

--- core/src/main/scala/chisel3/When.scala
+++ core/src/main/scala/chisel3/When.scala
@@ -71,7 +71,7 @@ object when {
   *  added by preprocessing the command queue.
   */
 final class WhenContext private[chisel3] (
-  sourceInfo:  SourceInfo,
+  _sourceInfo: SourceInfo,
   cond:        Option[() => Bool],
   block:       => Any,
   firrtlDepth: Int,
@@ -79,6 +79,8 @@ final class WhenContext private[chisel3] (
   altConds: List[() => Bool]) {
 
   private var scopeOpen = false
+
+  private[chisel3] def sourceInfo: SourceInfo = _sourceInfo
 
   /** Returns the local condition, inverted for an otherwise */
   private[chisel3] def localCond: Bool = {
