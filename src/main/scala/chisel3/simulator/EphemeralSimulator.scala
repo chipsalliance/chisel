1ff43c9398d3babd7a6606409e931b3557bdaa6a
Jack Koenig <koenig@sifive.com>
2024-02-22 15:19:31 -0800

    [svsim] Make EphemeralSimulator multi-processing friendly (#3847)

--- src/main/scala/chisel3/simulator/EphemeralSimulator.scala
+++ src/main/scala/chisel3/simulator/EphemeralSimulator.scala
@@ -2,16 +2,19 @@ package chisel3.simulator
 
 import svsim._
 import chisel3.RawModule
+import java.nio.file.Files
+import java.io.File
+import scala.reflect.io.Directory
 
 /** Provides a simple API for "ephemeral" invocations (where you don't care about the artifacts after the invocation completes) to
   * simulate Chisel modules. To keep things really simple, `EphemeralSimulator` simulations can only be controlled using the
   * peek/poke API, which provides enough control while hiding some of the lower-level svsim complexity.
   * @example
-  * ```
+  * {{{
   * import chisel3.simulator.EphemeralSimulator._
   * ...
   * simulate(new MyChiselModule()) { module => ... }
-  * ```
+  * }}}
   */
 object EphemeralSimulator extends PeekPokeAPI {
 
@@ -19,9 +22,7 @@ object EphemeralSimulator extends PeekPokeAPI {
     module: => T
   )(body:   (T) => Unit
   ): Unit = {
-    synchronized {
-      simulator.simulate(module)({ module => body(module.wrapped) }).result
-    }
+    makeSimulator.simulate(module)({ module => body(module.wrapped) }).result
   }
 
   private class DefaultSimulator(val workspacePath: String) extends SingleBackendSimulator[verilator.Backend] {
@@ -32,15 +33,14 @@ object EphemeralSimulator extends PeekPokeAPI {
 
     // Try to clean up temporary workspace if possible
     sys.addShutdownHook {
-      Runtime.getRuntime().exec(Array("rm", "-rf", workspacePath)).waitFor()
+      (new Directory(new File(workspacePath))).deleteRecursively()
     }
   }
-  private lazy val simulator: DefaultSimulator = {
-    val temporaryDirectory = System.getProperty("java.io.tmpdir")
+  private def makeSimulator: DefaultSimulator = {
     // TODO: Use ProcessHandle when we can drop Java 8 support
     // val id = ProcessHandle.current().pid().toString()
     val id = java.lang.management.ManagementFactory.getRuntimeMXBean().getName()
     val className = getClass().getName().stripSuffix("$")
-    new DefaultSimulator(Seq(temporaryDirectory, className, id).mkString("/"))
+    new DefaultSimulator(Files.createTempDirectory(s"${className}_${id}_").toString)
   }
 }
