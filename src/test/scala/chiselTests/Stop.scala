1b9c47c1733752a2d7b0e0d865c93d9ddae6af79
Nandor Licker <nandor.licker@sifive.com>
2024-02-28 23:13:42 -0800

    Print an optional Printable passed to stop ops

--- src/test/scala/chiselTests/Stop.scala
+++ src/test/scala/chiselTests/Stop.scala
@@ -4,16 +4,25 @@ package chiselTests
 
 import chisel3._
 import chisel3.testers.BasicTester
+import _root_.circt.stage.ChiselStage
 
 class StopTester() extends BasicTester {
-  stop()
+  chisel3.stop()
+}
+
+class StopWithMessageTester() extends BasicTester {
+  val cycle = RegInit(0.U(4.W))
+  cycle := cycle + 1.U
+  when(cycle === 4.U) {
+    chisel3.stop(cf"cycle: $cycle")
+  }
 }
 
 class StopImmediatelyTester extends BasicTester {
   val cycle = RegInit(0.asUInt(4.W))
   cycle := cycle + 1.U
   when(cycle === 4.U) {
-    stop()
+    chisel3.stop()
   }
   assert(cycle =/= 5.U, "Simulation did not exit upon executing stop()")
 }
@@ -21,6 +30,11 @@ class StopImmediatelyTester extends BasicTester {
 class StopSpec extends ChiselFlatSpec {
   "stop()" should "stop and succeed the testbench" in {
     assertTesterPasses { new StopTester }
+  }
+
+  it should "emit an optional message with arguments" in {
+    val chirrtl = ChiselStage.emitCHIRRTL(new StopWithMessageTester)
+    chirrtl should include("\"cycle: %d\", cycle")
   }
 
   it should "end the simulation immediately" in {
