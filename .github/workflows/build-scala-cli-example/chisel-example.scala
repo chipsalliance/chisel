a649f55aa18493c5313d6b33df67a27ef4788c4f
Jack Koenig <koenig@sifive.com>
2024-03-07 15:01:41 -0800

    Rename Scala CLI template to example (#3917)
    This avoids name aliasing with the chisel-template repository.
    
    ---------
    
    Co-authored-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>
    (cherry picked from commit bb3c6406f0c4391479ed3a29822d3d427951d315)
    
    # Conflicts:
    #	.github/workflows/build-scala-cli-example.yml
    #	.github/workflows/build-scala-cli-example/action.yml
    #	.github/workflows/build-scala-cli-example/chisel-example.scala
    #	.github/workflows/scala-cli-template.yml
    #	.github/workflows/test.yml
    #	docs/src/installation.md

--- .github/workflows/build-scala-cli-example/chisel-example.scala
+++ .github/workflows/build-scala-cli-example/chisel-example.scala
@@ -1,7 +1,13 @@
 //> using repository "sonatype-s01:snapshots"
+<<<<<<< HEAD:.github/workflows/build-scala-cli-template/chisel-template.scala
 //> using scala "2.13.10"
 //> using dep "org.chipsalliance::chisel::@VERSION@"
 //> using plugin "org.chipsalliance:::chisel-plugin::@VERSION@"
+=======
+//> using scala "2.13.12"
+//> using dep "org.chipsalliance::chisel:@VERSION@"
+//> using plugin "org.chipsalliance:::chisel-plugin:@VERSION@"
+>>>>>>> bb3c6406f (Rename Scala CLI template to example (#3917)):.github/workflows/build-scala-cli-example/chisel-example.scala
 //> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
 
 import chisel3._
