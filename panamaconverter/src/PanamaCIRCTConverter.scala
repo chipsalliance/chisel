fd5f0c7b23e71cfba7d37546ef1471228231bf2d
Asuna <SpriteOvO@gmail.com>
2024-03-31 13:28:11 +0200

    Specify `desiredName` as `defname` for `extmodule`

--- panamaconverter/src/PanamaCIRCTConverter.scala
+++ panamaconverter/src/PanamaCIRCTConverter.scala
@@ -922,13 +922,14 @@ class PanamaCIRCTConverter(val circt: PanamaCIRCT, fos: Option[FirtoolOptions], 
   def visitDefBlackBox(defBlackBox: DefBlackBox): Unit = {
     val ports = util.convert(defBlackBox.ports ++ defBlackBox.id.secretPorts, defBlackBox.topDir)
     val nameAttr = circt.mlirStringAttrGet(defBlackBox.name)
+    val desiredNameAttr = circt.mlirStringAttrGet(defBlackBox.id.desiredName)
 
     val builder = util
       .OpBuilder("firrtl.extmodule", firCtx.circuitBlock, circt.unkLoc)
       .withRegionNoBlock()
       .withNamedAttr("sym_name", nameAttr)
       .withNamedAttr("sym_visibility", circt.mlirStringAttrGet("private"))
-      .withNamedAttr("defname", nameAttr)
+      .withNamedAttr("defname", desiredNameAttr)
       .withNamedAttr("parameters", circt.mlirArrayAttrGet(defBlackBox.params.map(p => util.convert(p._1, p._2)).toSeq))
       .withNamedAttr("convention", circt.firrtlAttrGetConvention(FIRRTLConvention.Scalarized)) // TODO: Make an option `scalarizeExtModules` for it
       .withNamedAttr("annotations", circt.emptyArrayAttr)
