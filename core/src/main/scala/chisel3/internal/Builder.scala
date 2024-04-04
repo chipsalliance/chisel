ce05462c239deec8aee0704efbd3a71e8b64ec49
Jack Koenig <koenig@sifive.com>
2024-04-03 21:18:00 -0700

    Support literals in DataView (#3964)

--- core/src/main/scala/chisel3/internal/Builder.scala
+++ core/src/main/scala/chisel3/internal/Builder.scala
@@ -940,7 +940,7 @@ private[chisel3] object Builder extends LazyLogging {
       val localTarget = view.toTarget
       val absTarget = view.toAbsoluteTarget
       val elts = getRecursiveFields.lazily(view, "").collect { case (elt: Element, _) => elt }
-      for (elt <- elts) {
+      for (elt <- elts if !elt.isLit) {
         // This is a hack to not crash when .viewAs is called on non-hardware
         // It can be removed in Chisel 6.0.0 when it becomes illegal to call .viewAs on non-hardware
         val targetOfViewOpt =
