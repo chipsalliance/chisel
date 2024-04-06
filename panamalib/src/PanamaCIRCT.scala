0e3fae83c72e68970267adbf1d6ad02ddb07da18
Asuna <SpriteOvO@gmail.com>
2024-04-06 23:05:54 +0200

    [binder] Support constructing integers larger than 64 bits

--- panamalib/src/PanamaCIRCT.scala
+++ panamalib/src/PanamaCIRCT.scala
@@ -500,6 +500,8 @@ class PanamaCIRCT {
 
   def firrtlAttrGetMemDir(dir: FIRRTLMemDir) = MlirAttribute(CAPI.firrtlAttrGetMemDir(arena, mlirCtx, dir.value))
 
+  def firrtlAttrGetIntegerFromString(tpe: MlirType, numBits: Int, str: String, radix: Byte) = MlirAttribute(CAPI.firrtlAttrGetIntegerFromString(arena, tpe.get, numBits, newString(str).get, radix))
+
   def firrtlValueFoldFlow(value: MlirValue, flow: Int): Int = CAPI.firrtlValueFoldFlow(value.get, flow)
 
   def firrtlImportAnnotationsFromJSONRaw(annotationsStr: String): Option[MlirAttribute] = {
