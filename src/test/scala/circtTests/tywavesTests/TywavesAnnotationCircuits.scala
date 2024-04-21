e6de3b6249f0a8ff7ac66f677ca495a344ff7ab5
Raffaele Meloni <raffaele.meloni99@gmail.com>
2024-04-21 16:13:18 +0200

    Refactor of TywavesAnnotationSpec test structure
    - Move module and data types tests in different subdirectories
    - Add readmes with output samples of the annotations

--- src/test/scala/circtTests/tywavesTests/TywavesAnnotationCircuits.scala
+++ src/test/scala/circtTests/tywavesTests/TywavesAnnotationCircuits.scala
@@ -4,7 +4,37 @@ import chisel3._
 import chisel3.experimental.{Analog, IntrinsicModule}
 import chisel3.experimental.hierarchy.{instantiable, Definition, Instance}
 import chisel3.properties.Class
-import chisel3.util.MixedVec
+import chisel3.stage.ChiselGeneratorAnnotation
+import chisel3.util.{MixedVec, SRAM, SRAMInterface}
+import circt.stage.ChiselStage
+import org.scalatest.AppendedClues.convertToClueful
+import org.scalatest.matchers.should.Matchers
+
+/** Utility functions for testing [[chisel3.tywaves.TywavesAnnotation]] */
+object TestUtils extends Matchers {
+
+  def countSubstringOccurrences(mainString: String, subString: String): Int = {
+    val pattern = subString.r
+    pattern.findAllMatchIn(mainString).length
+  }
+
+  def createExpected(target: String, typeName: String, binding: String = ""): String = {
+    val realTypeName = binding match {
+      case "" => typeName
+      case _  => s"$binding\\[$typeName\\]"
+    }
+    s"""\"target\":\"$target\",\\s+\"typeName\":\"$realTypeName\"""".stripMargin
+  }
+
+  def checkAnno(expectedMatches: Seq[(String, Int)], refString: String): Unit = {
+    def totalAnnoCheck(n: Int): (String, Int) = (""""class":"chisel3.tywaves.TywavesAnnotation"""", n + 1)
+
+    (expectedMatches :+ totalAnnoCheck(expectedMatches.map(_._2).sum)).foreach {
+      case (pattern, count) =>
+        (countSubstringOccurrences(refString, pattern) should be(count)).withClue(s"Pattern: $pattern")
+    }
+  }
+}
 
 object TywavesAnnotationCircuits {
 
@@ -22,6 +52,11 @@ object TywavesAnnotationCircuits {
     override def toString: String = "Reg"
   }
 
+  /** Represent a module which internal elements can have different bindings:
+    *  - Reg
+    *  - Wire
+    *  - IO
+    */
   abstract class TywavesTestModule(bindingChoice: BindingChoice) extends RawModule {
     def body: Unit
     bindingChoice match {
@@ -33,125 +68,160 @@ object TywavesAnnotationCircuits {
     }
   }
 
-  // Empty circuit
-  class TopCircuit extends RawModule
+  /** Circuits for module annotations tests */
+  object ModuleCircuits {
+    // Empty circuit
+    class TopCircuit extends RawModule
 
-  // Circuit with submodule
-  class MyModule extends RawModule
-  class TopCircuitSubModule extends RawModule { val mod = Module(new MyModule) }
+    // Circuit with submodule
+    class MyModule extends RawModule
 
-  // Circuit with submodule and submodule with submodule
-  class TopCircuitMultiModule extends RawModule {
-    // 4 times MyModule in total
-    val mod1 = Module(new MyModule)
-    val mod2 = Module(new MyModule)
-    val mods = Seq.fill(2)(Module(new MyModule))
-  }
+    class TopCircuitSubModule extends RawModule {
+      val mod = Module(new MyModule)
+    }
 
-  // Circuit with submodule and submodule with submodule
-  class TopCircuitWithParams extends RawModule {
-    class MyModule(val width: Int) extends RawModule
-    val mod1: MyModule = Module(new MyModule(8))
-    val mod2: MyModule = Module(new MyModule(16))
-    val mod3: MyModule = Module(new MyModule(32))
-  }
+    // Circuit with submodule and submodule with submodule
+    class TopCircuitMultiModule extends RawModule {
+      // 4 times MyModule in total
+      val mod1 = Module(new MyModule)
+      val mod2 = Module(new MyModule)
+      val mods = Seq.fill(2)(Module(new MyModule))
+    }
 
-  // Circuit with submodule and submodule with submodule
-  class TopCircuitBlackBox extends RawModule {
-    class MyBlackBox extends BlackBox(Map("PARAM1" -> "TRUE", "PARAM2" -> "DEFAULT")) { val io = IO(new Bundle {}) }
-    val myBlackBox1:  MyBlackBox = Module(new MyBlackBox)
-    val myBlackBox2:  MyBlackBox = Module(new MyBlackBox)
-    val myBlackBoxes: Seq[MyBlackBox] = Seq.fill(2)(Module(new MyBlackBox))
-  }
+    // Circuit with submodule and submodule with submodule
+    class TopCircuitWithParams extends RawModule {
+      class MyModule(val width: Int) extends RawModule
 
-  // Circuit using intrinsics
-  class ExampleIntrinsicModule(str: String) extends IntrinsicModule("OtherIntrinsic", Map("STRING" -> str)) {}
-  class TopCircuitIntrinsic extends RawModule {
-    val myIntrinsicModule1: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("Hello"))
-    val myIntrinsicModule2: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("World"))
-    val myIntrinsicModules: Seq[ExampleIntrinsicModule] = Seq.fill(2)(Module(new ExampleIntrinsicModule("Hello")))
-  }
+      val mod1: MyModule = Module(new MyModule(8))
+      val mod2: MyModule = Module(new MyModule(16))
+      val mod3: MyModule = Module(new MyModule(32))
+    }
 
-  // An abstract description of a CSR, represented as a Class.
-  @instantiable
-  class CSRDescription extends Class
-  class CSRModule(csrDescDef: Definition[CSRDescription]) extends RawModule {
-    val csrDescription = Instance(csrDescDef)
-  }
-  class TopCircuitClasses extends RawModule {
-    val csrDescDef = Definition(new CSRDescription)
-    val csrModule1: CSRModule = Module(new CSRModule(csrDescDef))
-    val csrModule2: CSRModule = Module(new CSRModule(csrDescDef))
-    val csrModules: Seq[CSRModule] = Seq.fill(2)(Module(new CSRModule(csrDescDef)))
-  }
+    // Circuit with submodule and submodule with submodule
+    class TopCircuitBlackBox extends RawModule {
+      class MyBlackBox extends BlackBox(Map("PARAM1" -> "TRUE", "PARAM2" -> "DEFAULT")) {
+        val io = IO(new Bundle {})
+      }
 
-  // Test Clock, Reset and AsyncReset
-  class TopCircuitClockReset extends RawModule {
-    val clock: Clock = IO(Input(Clock()))
-    // Types od reset https://www.chisel-lang.org/docs/explanations/reset
-    val syncReset:  Bool = IO(Input(Bool()))
-    val reset:      Reset = IO(Input(Reset()))
-    val asyncReset: AsyncReset = IO(Input(AsyncReset()))
-  }
+      val myBlackBox1:  MyBlackBox = Module(new MyBlackBox)
+      val myBlackBox2:  MyBlackBox = Module(new MyBlackBox)
+      val myBlackBoxes: Seq[MyBlackBox] = Seq.fill(2)(Module(new MyBlackBox))
+    }
 
-  // Test implicit Clock and Reset
-  class TopCircuitImplicitClockReset extends Module
+    // Circuit using intrinsics
+    class ExampleIntrinsicModule(str: String) extends IntrinsicModule("OtherIntrinsic", Map("STRING" -> str)) {}
 
-  // TODO: add the other ground types
-  class TopCircuitGroundTypes(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
-    override def body: Unit = {
-      val uint: UInt = bindingChoice(UInt(8.W))
-      val sint: SInt = bindingChoice(SInt(8.W))
-      val bool: Bool = bindingChoice(Bool())
-      if (bindingChoice != RegBinding) { val analog: Analog = bindingChoice(Analog(1.W)) }
-      //        val fixedPoint: FixedPoint TODO: does fixed point still exist?
-      //        val interval: Interval = bindingChoice(Interval())) TODO: does interval still exist?
-      val bits: UInt = bindingChoice(Bits(8.W))
+    class TopCircuitIntrinsic extends RawModule {
+      val myIntrinsicModule1: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("Hello"))
+      val myIntrinsicModule2: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("World"))
+      val myIntrinsicModules: Seq[ExampleIntrinsicModule] = Seq.fill(2)(Module(new ExampleIntrinsicModule("Hello")))
+    }
+
+    // An abstract description of a CSR, represented as a Class.
+    @instantiable
+    class CSRDescription extends Class
+
+    class CSRModule(csrDescDef: Definition[CSRDescription]) extends RawModule {
+      val csrDescription = Instance(csrDescDef)
+    }
+
+    class TopCircuitClasses extends RawModule {
+      val csrDescDef = Definition(new CSRDescription)
+      val csrModule1: CSRModule = Module(new CSRModule(csrDescDef))
+      val csrModule2: CSRModule = Module(new CSRModule(csrDescDef))
+      val csrModules: Seq[CSRModule] = Seq.fill(2)(Module(new CSRModule(csrDescDef)))
     }
   }
 
-  // Test Bundles
-  class MyEmptyBundle extends Bundle
-  class MyBundle extends Bundle {
-    val a: UInt = UInt(8.W)
-    val b: SInt = SInt(8.W)
-    val c: Bool = Bool()
-  }
-  class TopCircuitBundles(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
-    override def body: Unit = {
-      val a: Bundle = bindingChoice(new Bundle {})
-      val b: MyEmptyBundle = bindingChoice(new MyEmptyBundle)
-      val c: MyBundle = bindingChoice(new MyBundle)
+  /** Circuits for DataTypes annotations test */
+  object DataTypesCircuits {
+    // Test Clock, Reset and AsyncReset
+    class TopCircuitClockReset extends RawModule {
+      val clock: Clock = IO(Input(Clock()))
+      // Types od reset https://www.chisel-lang.org/docs/explanations/reset
+      val syncReset:  Bool = IO(Input(Bool()))
+      val reset:      Reset = IO(Input(Reset()))
+      val asyncReset: AsyncReset = IO(Input(AsyncReset()))
+    }
+
+    // Test implicit Clock and Reset
+    class TopCircuitImplicitClockReset extends Module
+
+    // TODO: add the other ground types
+    class TopCircuitGroundTypes(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
+      override def body: Unit = {
+        val uint: UInt = bindingChoice(UInt(8.W))
+        val sint: SInt = bindingChoice(SInt(8.W))
+        val bool: Bool = bindingChoice(Bool())
+        if (bindingChoice != RegBinding) {
+          val analog: Analog = bindingChoice(Analog(1.W))
+        }
+        //        val fixedPoint: FixedPoint TODO: does fixed point still exist?
+        //        val interval: Interval = bindingChoice(Interval())) TODO: does interval still exist?
+        val bits: UInt = bindingChoice(Bits(8.W))
+      }
+    }
+
+    // Test Bundles
+    class MyEmptyBundle extends Bundle
+
+    class MyBundle extends Bundle {
+      val a: UInt = UInt(8.W)
+      val b: SInt = SInt(8.W)
+      val c: Bool = Bool()
+    }
+
+    class TopCircuitBundles(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
+      override def body: Unit = {
+        val a: Bundle = bindingChoice(new Bundle {})
+        val b: MyEmptyBundle = bindingChoice(new MyEmptyBundle)
+        val c: MyBundle = bindingChoice(new MyBundle)
+      }
+    }
+
+    class MyNestedBundle extends Bundle {
+      val a: Bool = Bool()
+      val b: MyBundle = new MyBundle
+      val c: MyBundle = Flipped(new MyBundle)
+    }
+
+    class TopCircuitBundlesNested(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
+      override def body: Unit = {
+        val a: MyNestedBundle = bindingChoice(new MyNestedBundle)
+      }
+    }
+
+    // Test Vecs
+    class TopCircuitVecs(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
+      override def body: Unit = {
+        val a: Vec[SInt] = bindingChoice(Vec(5, SInt(23.W)))
+        val b: Vec[Vec[SInt]] = bindingChoice(Vec(5, Vec(3, SInt(23.W))))
+        val c = bindingChoice(
+          Vec(
+            5,
+            new Bundle {
+              val x: UInt = UInt(8.W)
+            }
+          )
+        )
+        // TODO: check if this should have a better representation, now its type is represented as other Records
+        val d = bindingChoice(MixedVec(UInt(3.W), SInt(10.W)))
+      }
+    }
+
+    // Test Bundles with Vecs
+    class TopCircuitBundleWithVec(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
+      override def body: Unit = {
+        val a = bindingChoice(new Bundle {
+          val vec = Vec(5, UInt(8.W))
+        })
+      }
+    }
+
+    // Test signals in submodules
+    class TopCircuitTypeInSubmodule(bindingChoice: BindingChoice) extends RawModule {
+      val mod = Module(new TopCircuitGroundTypes(bindingChoice))
     }
   }
 
-  class MyNestedBundle extends Bundle {
-    val a: Bool = Bool()
-    val b: MyBundle = new MyBundle
-    val c: MyBundle = Flipped(new MyBundle)
-  }
-  class TopCircuitBundlesNested(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
-    override def body: Unit = { val a: MyNestedBundle = bindingChoice(new MyNestedBundle) }
-  }
-
-  // Test Vecs
-  class TopCircuitVecs(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
-    override def body: Unit = {
-      val a: Vec[SInt] = bindingChoice(Vec(5, SInt(23.W)))
-      val b: Vec[Vec[SInt]] = bindingChoice(Vec(5, Vec(3, SInt(23.W))))
-      val c = bindingChoice(Vec(5, new Bundle { val x: UInt = UInt(8.W) }))
-      // TODO: check if this should have a better representation, now its type is represented as other Records
-      val d = bindingChoice(MixedVec(UInt(3.W), SInt(10.W)))
-    }
-  }
-
-  // Test Bundles with Vecs
-  class TopCircuitBundleWithVec(bindingChoice: BindingChoice) extends TywavesTestModule(bindingChoice) {
-    override def body: Unit = { val a = bindingChoice(new Bundle { val vec = Vec(5, UInt(8.W)) }) }
-  }
-
-  // Test signals in submodules
-  class TopCircuitTypeInSubmodule(bindingChoice: BindingChoice) extends RawModule {
-    val mod = Module(new TopCircuitGroundTypes(bindingChoice))
-  }
 }
--- src/test/scala/circtTests/tywavesTests/dataTypesTests/TypeAnnotationDataTypesSpec.scala
+++ src/test/scala/circtTests/tywavesTests/dataTypesTests/TypeAnnotationDataTypesSpec.scala
@@ -1,191 +1,16 @@
-package circtTests.tywavesTests
+package circtTests.tywavesTests.dataTypesTests
 
 import chisel3._
-import chisel3.experimental.Analog
 import chisel3.stage.ChiselGeneratorAnnotation
-import chisel3.util.MixedVec
 import circt.stage.ChiselStage
-import org.scalatest.AppendedClues.convertToClueful
-import org.scalatest.exceptions.TestFailedException
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.exceptions.TestFailedException
 
-import TywavesAnnotationCircuits._
-
-object TywavesAnnotationSpec {
-  import chisel3._
-
-  // A user defined type from Bundle: FooBundle
-  class FooBundle extends Bundle {
-    val a: UInt = Input(UInt(8.W))
-    val b: SInt = Input(SInt(8.W))
-    val c: Bool = Input(Bool())
-  }
-
-  // A user defined type as nested bundle: BazBundle
-  class BarBundle extends Bundle {
-    val a: Bool = Input(Bool())
-    val b: FooBundle = new FooBundle
-    val c: FooBundle = Flipped(new FooBundle)
-  }
-
-  // A class
-  class Foo extends RawModule {
-    // A user defined type
-    val a: FooBundle = IO(Flipped(new FooBundle))
-    // An anonymous bundle
-    val b = IO(new Bundle {
-      val a: UInt = Input(UInt(8.W))
-      val b: SInt = Input(SInt(8.W))
-      val c: Bool = Input(Bool())
-    })
-
-    // A nested bundle
-    val c: BarBundle = IO(new BarBundle)
-
-    a <> b
-    c.b <> c.c
-  }
-
-  // A class with a submodule in it
-  class Bar extends RawModule {
-    val a:   BarBundle = IO(new BarBundle)
-    val foo: Foo = Module(new Foo)
-    val bar: Foo = Module(new Foo)
-
-    a <> foo.c
-    a <> bar.c
-  }
-
-}
-
-class TywavesAnnotationSpec extends AnyFunSpec with Matchers with chiselTests.Utils {
-  private val baseDir = os.pwd / "test_run_dir" / this.getClass.getSimpleName
-
-  def countSubstringOccurrences(mainString: String, subString: String): Int = {
-    val pattern = subString.r
-    pattern.findAllMatchIn(mainString).length
-  }
-
-  def createExpected(target: String, typeName: String, binding: String = ""): String = {
-    val realTypeName = binding match {
-      case "" => typeName
-      case _  => s"$binding\\[$typeName\\]"
-    }
-    s"""\"target\":\"$target\",\\s+\"typeName\":\"$realTypeName\"""".stripMargin
-  }
-
-  def checkAnno(expectedMatches: Seq[(String, Int)], refString: String): Unit = {
-    def totalAnnoCheck(n: Int): (String, Int) = (""""class":"chisel3.tywaves.TywavesAnnotation"""", n + 1)
-
-    (expectedMatches :+ totalAnnoCheck(expectedMatches.map(_._2).sum)).foreach {
-      case (pattern, count) =>
-        (countSubstringOccurrences(refString, pattern) should be(count)).withClue(s"Pattern: $pattern")
-    }
-  }
-
-  describe("Module Annotations") {
-    val targetDir = os.pwd / "test_run_dir" / "TywavesAnnotationSpec" / "Module Annotations"
-    val args: Array[String] = Array("--target", "chirrtl", "--target-dir", targetDir.toString)
-
-    it("should annotate a circuit") {
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuit)))
-      os.read(targetDir / "TopCircuit.fir") should include("TywavesAnnotation").and(
-        include("\"target\":\"~TopCircuit|TopCircuit\"").and(
-          include("\"typeName\":\"TopCircuit\"")
-        )
-      )
-    }
-
-    it("should annotate a module in a circuit") {
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitSubModule)))
-      os.read(targetDir / "TopCircuitSubModule.fir") should include("TywavesAnnotation").and(
-        include("\"target\":\"~TopCircuitSubModule|MyModule\"").and(include("\"typeName\":\"MyModule\""))
-      )
-    }
-
-    it("should annotate a module in a circuit multiple times") {
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitMultiModule)))
-      // The file should include it
-      val string = os.read(targetDir / "TopCircuitMultiModule.fir")
-      countSubstringOccurrences(string, "\"class\":\"chisel3.tywaves.TywavesAnnotation\"") should be(5)
-      countSubstringOccurrences(string, "\"typeName\":\"MyModule\"") should be(4)
-    }
-
-    it("should annotate variants of Parametrized module in a circuit") {
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitWithParams)))
-      // The file should include it
-      val string = os.read(targetDir / "TopCircuitWithParams.fir")
-      countSubstringOccurrences(string, "\"class\":\"chisel3.tywaves.TywavesAnnotation\"") should be(4)
-      countSubstringOccurrences(string, "\"typeName\":\"MyModule") should be(3)
-      // This now fails!!! TODO: it is something that I want to have, PARAMETERIZED MODULES
-      countSubstringOccurrences(string, "\"typeName\":\"MyModule(8)\"") should be(1)
-      countSubstringOccurrences(string, "\"typeName\":\"MyModule(16)\"") should be(1)
-      countSubstringOccurrences(string, "\"typeName\":\"MyModule(32)\"") should be(1)
-    }
-
-    it("should annotate black boxes") {
-
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitBlackBox)))
-      val string = os.read(targetDir / "TopCircuitBlackBox.fir")
-      countSubstringOccurrences(string, "\"class\":\"chisel3.tywaves.TywavesAnnotation\"") should be(5)
-      countSubstringOccurrences(string, "\"typeName\":\"MyBlackBox") should be(4)
-    }
-
-    it("should annotate intrinsic modules") {
-      import chisel3.experimental.IntrinsicModule
-      class ExampleIntrinsicModule(str: String) extends IntrinsicModule("OtherIntrinsic", Map("STRING" -> str)) {}
-      class TopCircuitIntrinsic extends RawModule {
-        val myIntrinsicModule1: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("Hello"))
-        val myIntrinsicModule2: ExampleIntrinsicModule = Module(new ExampleIntrinsicModule("World"))
-        val myIntrinsicModules: Seq[ExampleIntrinsicModule] = Seq.fill(2)(Module(new ExampleIntrinsicModule("Hello")))
-      }
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitIntrinsic)))
-      val string = os.read(targetDir / "TopCircuitIntrinsic.fir")
-      countSubstringOccurrences(string, "\"class\":\"chisel3.tywaves.TywavesAnnotation\"") should be(5)
-      countSubstringOccurrences(string, "\"typeName\":\"ExampleIntrinsicModule") should be(4)
-    }
-
-    it("should annotate classes modules") {
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitClasses)))
-      val string = os.read(targetDir / "TopCircuitClasses.fir")
-      countSubstringOccurrences(string, "\"typeName\":\"CSRModule\"") should be(4)
-      // TODO: this fails, I don't know why it has twice the same class in the components
-      countSubstringOccurrences(string, "\"typeName\":\"CSRDescription") should be(1)
-      countSubstringOccurrences(string, "\"class\":\"chisel3.tywaves.TywavesAnnotation\"") should be(6)
-    }
-
-    it("should annotate parametric module") {
-      class MyModule[T <: Data](gen: T) extends RawModule {
-        val a: T = IO(Input(gen))
-      }
-
-      class TopCircuitParametric extends RawModule {
-        val myModule1: MyModule[UInt] = Module(new MyModule(UInt(8.W)))
-        val myModule2: MyModule[SInt] = Module(new MyModule(SInt(8.W)))
-        val myModule3: MyModule[Bool] = Module(new MyModule(Bool()))
-        val (x, y, z) = (IO(Input(UInt(8.W))), IO(Input(SInt(8.W))), IO(Input(Bool())))
-        x <> myModule1.a
-        y <> myModule2.a
-        z <> myModule3.a
-      }
-
-      (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitParametric)))
-      val string = os.read(targetDir / "TopCircuitParametric.fir")
-
-      val expectedMatches = Seq(
-        (createExpected("~TopCircuitParametric\\|MyModule>a", "UInt<8>", "IO"), 1),
-        (createExpected("~TopCircuitParametric\\|MyModule_1>a", "SInt<8>", "IO"), 1),
-        (createExpected("~TopCircuitParametric\\|MyModule_2>a", "Bool", "IO"), 1),
-        (createExpected("~TopCircuitParametric\\|TopCircuitParametric>x", "UInt<8>", "IO"), 1),
-        (createExpected("~TopCircuitParametric\\|TopCircuitParametric>y", "SInt<8>", "IO"), 1),
-        (createExpected("~TopCircuitParametric\\|TopCircuitParametric>z", "Bool", "IO"), 1),
-        // TODO: the goal is to have MyModule[UInt<8>], MyModule[SInt<8>], MyModule[Bool]
-        ("\"typeName\":\"MyModule\"", 3)
-      )
-      checkAnno(expectedMatches, string)
-    }
-  }
+class TypeAnnotationDataTypesSpec extends AnyFunSpec with Matchers with chiselTests.Utils {
+  import circtTests.tywavesTests.TywavesAnnotationCircuits.DataTypesCircuits._
+  import circtTests.tywavesTests.TywavesAnnotationCircuits.{BindingChoice, PortBinding, RegBinding, WireBinding}
+  import circtTests.tywavesTests.TestUtils._
 
   def typeTests(args: Array[String], targetDir: os.Path, b: BindingChoice): Unit = {
     def addClockReset(nameTop: String, nameInst: Option[String] = None) = if (b == RegBinding)
@@ -299,13 +124,14 @@ class TywavesAnnotationSpec extends AnyFunSpec with Matchers with chiselTests.Ut
   describe("Port Annotations") {
     val targetDir = os.pwd / "test_run_dir" / "TywavesAnnotationSpec" / "Port Annotations"
     val args: Array[String] = Array("--target", "chirrtl", "--target-dir", targetDir.toString)
+
     it("should fail") {
       class TopCircuitFail extends Module
       (new ChiselStage(true)).execute(args, Seq(ChiselGeneratorAnnotation(() => new TopCircuitFail)))
       val string = os.read(targetDir / "TopCircuitFail.fir")
       val expectedMatches = Seq(
         (createExpected("~TopCircuitFail\\|TopCircuitFail>clock", "Clock", "IO"), 1)
-//        (createExpected("~TopCircuitFail\\|TopCircuitFail>reset", "Bool", "IO"), 1)
+        //        (createExpected("~TopCircuitFail\\|TopCircuitFail>reset", "Bool", "IO"), 1)
       )
       assertThrows[TestFailedException] { checkAnno(expectedMatches, string) }
     }
@@ -348,4 +174,5 @@ class TywavesAnnotationSpec extends AnyFunSpec with Matchers with chiselTests.Ut
     val args: Array[String] = Array("--target", "chirrtl", "--target-dir", targetDir.toString)
     typeTests(args, targetDir, RegBinding)
   }
+
 }
