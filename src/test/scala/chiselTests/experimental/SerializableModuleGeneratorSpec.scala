48fcc1524d6923cd7e0b69ffe6212437e16fa6b8
Jiuyang Liu <liu@jiuyang.me>
2024-04-30 08:07:22 +0800

    wip: tweak test for debug

--- src/test/scala/chiselTests/experimental/SerializableModuleGeneratorSpec.scala
+++ src/test/scala/chiselTests/experimental/SerializableModuleGeneratorSpec.scala
@@ -3,102 +3,26 @@
 package chiselTests
 package experimental
 import chisel3._
-import chisel3.experimental.{SerializableModule, SerializableModuleGenerator, SerializableModuleParameter}
-import upickle.default._
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
+import upickle.default
+
+// generate at compile time, but not implement it.
 object GCDSerializableModuleParameter {
-  implicit def rwP: ReadWriter[GCDSerializableModuleParameter] = macroRW
+  implicit def rw: default.ReadWriter[GCDSerializableModuleParameter] = upickle.default.macroRW
 }
 
 case class GCDSerializableModuleParameter(width: Int) extends SerializableModuleParameter
 
 class GCDSerializableModule(val parameter: GCDSerializableModuleParameter)
     extends Module
-    with SerializableModule[GCDSerializableModuleParameter] {
-  val io = IO(new Bundle {
-    val a = Input(UInt(parameter.width.W))
-    val b = Input(UInt(parameter.width.W))
-    val e = Input(Bool())
-    val z = Output(UInt(parameter.width.W))
-  })
-  val x = Reg(UInt(parameter.width.W))
-  val y = Reg(UInt(parameter.width.W))
-  val z = Reg(UInt(parameter.width.W))
-  val e = Reg(Bool())
-  when(e) {
-    x := io.a
-    y := io.b
-    z := 0.U
-  }
-  when(x =/= y) {
-    when(x > y) {
-      x := x - y
-    }.otherwise {
-      y := y - x
-    }
-  }.otherwise {
-    z := x
-  }
-  io.z := z
+    with SerializableModule[GCDSerializableModuleParameter]{
+  val a = 4
 }
 
-class SerializableModuleGeneratorSpec extends ChiselFlatSpec with Utils {
-  val g = SerializableModuleGenerator(
-    classOf[GCDSerializableModule],
-    GCDSerializableModuleParameter(32)
-  )
-
-  val g0 = SerializableModuleGenerator[GCDSerializableModule, GCDSerializableModuleParameter](GCDSerializableModuleParameter(32))
-
-  "SerializableModuleGenerator" should "be serialized and deserialized" in {
-    assert(
-      g == upickle.default.read[SerializableModuleGenerator[GCDSerializableModule, GCDSerializableModuleParameter]](
-        upickle.default.write(g)
-      )
-    )
-  }
-
-  "SerializableModuleGenerator[M,P](p: P) and SerializableModuleGenerator(classOf[M], p: P)" should "be equal" in {
-    assert(g == g0)
-  }
-
-  "SerializableModuleGenerator" should "be able to elaborate" in {
-    circt.stage.ChiselStage.emitCHIRRTL(g.module())
-  }
-
-  "SerializableModuleGenerator" should "be able to elaborate with D/I" in {
-    val cir = circt.stage.ChiselStage.emitCHIRRTL(
-      new Module {
-        val i32 =
-          SerializableModuleGenerator(classOf[GCDSerializableModule], GCDSerializableModuleParameter(32)).instance()
-        val i16 =
-          SerializableModuleGenerator(classOf[GCDSerializableModule], GCDSerializableModuleParameter(16)).instance()
-        val ii32 =
-          SerializableModuleGenerator(classOf[GCDSerializableModule], GCDSerializableModuleParameter(32)).instance()
-      }
-    )
-    cir should include("inst i32 of GCDSerializableModule")
-    cir should include("inst i16 of GCDSerializableModule_1")
-    cir should include("inst ii32 of GCDSerializableModule")
-  }
-
-  case class FooParameter() extends SerializableModuleParameter
-
-  class InnerFoo(val parameter: FooParameter) extends RawModule with SerializableModule[FooParameter]
-
-  "InnerClass" should "not be able to serialize" in {
-    assert(
-      intercept[java.lang.IllegalArgumentException](
-        circt.stage.ChiselStage.emitCHIRRTL(
-          {
-            SerializableModuleGenerator(
-              classOf[InnerFoo],
-              FooParameter()
-            ).module()
-          }
-        )
-      ).getMessage.contains(
-        "You define your class chiselTests.experimental.SerializableModuleGeneratorSpec$FooParameter inside other class."
-      )
-    )
-  }
+object GCDSerializableModule extends chisel3.experimental.SerializableModuleMain[GCDSerializableModuleParameter, GCDSerializableModule] {
+  import scala.reflect.runtime.universe
+  // this should be
+  implicit val pRW: default.ReadWriter[GCDSerializableModuleParameter] = GCDSerializableModuleParameter.rw
+  implicit val mTypeTag: universe.TypeTag[GCDSerializableModule] = implicitly[universe.TypeTag[GCDSerializableModule]]
+  implicit val pTypeTag: universe.TypeTag[GCDSerializableModuleParameter] = implicitly[universe.TypeTag[GCDSerializableModuleParameter]]
 }
