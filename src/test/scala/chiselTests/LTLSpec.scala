f1f42f6732eb057c34f56000f9229165b96d8671
Will Dietz <will.dietz@sifive.com>
2024-05-07 12:32:55 -0500

    [LTL] Test for source locations from "user" code.
    verif_* intrinsics do not have this yet, so don't check them.

--- src/test/scala/chiselTests/LTLSpec.scala
+++ src/test/scala/chiselTests/LTLSpec.scala
@@ -5,6 +5,7 @@ package chiselTests
 import chisel3._
 import chisel3.ltl._
 import chisel3.testers.BasicTester
+import chisel3.experimental.SourceLine
 import _root_.circt.stage.ChiselStage
 
 import org.scalatest.flatspec.AnyFlatSpec
@@ -34,6 +35,7 @@ class LTLSpec extends AnyFlatSpec with Matchers {
   it should "support sequence delay operations" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a, b, c = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val s0: Sequence = a.delay(1)
       val s1: Sequence = b.delayRange(2, 4)
       val s2: Sequence = c.delayAtLeast(5)
@@ -41,40 +43,44 @@ class LTLSpec extends AnyFlatSpec with Matchers {
       val s4: Sequence = a ##* b
       val s5: Sequence = a ##+ b
     })
+    val sourceLoc = "@[Foo.scala 1:2]"
     chirrtl should include("input a : UInt<1>")
     chirrtl should include("input b : UInt<1>")
     chirrtl should include("input c : UInt<1>")
-    chirrtl should include("node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, a)")
-    chirrtl should include("node delay_1 = intrinsic(circt_ltl_delay<delay = 2, length = 2> : UInt<1>, b)")
-    chirrtl should include("node delay_2 = intrinsic(circt_ltl_delay<delay = 5> : UInt<1>, c)")
-    chirrtl should include("node delay_3 = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, b)")
-    chirrtl should include("node concat = intrinsic(circt_ltl_concat : UInt<1>, a, delay_3")
-    chirrtl should include("node delay_4 = intrinsic(circt_ltl_delay<delay = 0> : UInt<1>, b)")
-    chirrtl should include("node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_4")
-    chirrtl should include("node delay_5 = intrinsic(circt_ltl_delay<delay = 1> : UInt<1>, b)")
-    chirrtl should include("node concat_2 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_5")
+    chirrtl should include(f"node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, a) $sourceLoc")
+    chirrtl should include(f"node delay_1 = intrinsic(circt_ltl_delay<delay = 2, length = 2> : UInt<1>, b) $sourceLoc")
+    chirrtl should include(f"node delay_2 = intrinsic(circt_ltl_delay<delay = 5> : UInt<1>, c) $sourceLoc")
+    chirrtl should include(f"node delay_3 = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, b) $sourceLoc")
+    chirrtl should include(f"node concat = intrinsic(circt_ltl_concat : UInt<1>, a, delay_3) $sourceLoc")
+    chirrtl should include(f"node delay_4 = intrinsic(circt_ltl_delay<delay = 0> : UInt<1>, b) $sourceLoc")
+    chirrtl should include(f"node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_4) $sourceLoc")
+    chirrtl should include(f"node delay_5 = intrinsic(circt_ltl_delay<delay = 1> : UInt<1>, b) $sourceLoc")
+    chirrtl should include(f"node concat_2 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_5) $sourceLoc")
   }
 
   it should "support sequence concat operations" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a, b, c, d, e = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val s0: Sequence = a.concat(b)
       val s1: Sequence = Sequence.concat(c, d, e) // (c concat d) concat e
     })
+    val sourceLoc = "@[Foo.scala 1:2]"
     chirrtl should include("input a : UInt<1>")
     chirrtl should include("input b : UInt<1>")
     chirrtl should include("input c : UInt<1>")
     chirrtl should include("input d : UInt<1>")
     chirrtl should include("input e : UInt<1>")
-    chirrtl should include("intrinsic(circt_ltl_concat : UInt<1>, a, b")
-    chirrtl should include("node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, c, d")
-    chirrtl should include("intrinsic(circt_ltl_concat : UInt<1>, concat_1, e")
+    chirrtl should include(f"intrinsic(circt_ltl_concat : UInt<1>, a, b) $sourceLoc")
+    chirrtl should include(f"node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, c, d) $sourceLoc")
+    chirrtl should include(f"intrinsic(circt_ltl_concat : UInt<1>, concat_1, e) $sourceLoc")
   }
 
   it should "support and, or, and clock operations" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a, b = IO(Input(Bool()))
       val clock = IO(Input(Clock()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val s0: Sequence = a.delay()
       val s1: Sequence = s0.and(b)
       val s2: Sequence = s0.or(b)
@@ -84,63 +90,77 @@ class LTLSpec extends AnyFlatSpec with Matchers {
       val p2: Property = p0.or(b)
       val p3: Property = p0.clock(clock)
     })
+    val sourceLoc = "@[Foo.scala 1:2]"
+
     // Sequences
-    chirrtl should include("node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, a)")
-    chirrtl should include("node and = intrinsic(circt_ltl_and : UInt<1>, delay, b)")
-    chirrtl should include("node or = intrinsic(circt_ltl_or : UInt<1>, delay, b)")
-    chirrtl should include("node clock_1 = intrinsic(circt_ltl_clock : UInt<1>, delay, clock)")
+    chirrtl should include(f"node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, a) $sourceLoc")
+    chirrtl should include(f"node and = intrinsic(circt_ltl_and : UInt<1>, delay, b) $sourceLoc")
+    chirrtl should include(f"node or = intrinsic(circt_ltl_or : UInt<1>, delay, b) $sourceLoc")
+    chirrtl should include(f"node clock_1 = intrinsic(circt_ltl_clock : UInt<1>, delay, clock) $sourceLoc")
 
     // Properties
-    chirrtl should include("node eventually = intrinsic(circt_ltl_eventually : UInt<1>, a)")
-    chirrtl should include("node and_1 = intrinsic(circt_ltl_and : UInt<1>, eventually, b)")
-    chirrtl should include("node or_1 = intrinsic(circt_ltl_or : UInt<1>, eventually, b)")
-    chirrtl should include("node clock_2 = intrinsic(circt_ltl_clock : UInt<1>, eventually, clock)")
+    chirrtl should include(f"node eventually = intrinsic(circt_ltl_eventually : UInt<1>, a) $sourceLoc")
+    chirrtl should include(f"node and_1 = intrinsic(circt_ltl_and : UInt<1>, eventually, b) $sourceLoc")
+    chirrtl should include(f"node or_1 = intrinsic(circt_ltl_or : UInt<1>, eventually, b) $sourceLoc")
+    chirrtl should include(f"node clock_2 = intrinsic(circt_ltl_clock : UInt<1>, eventually, clock) $sourceLoc")
   }
 
   it should "support property not operation" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val p0: Property = Property.not(a)
     })
-    chirrtl should include("intrinsic(circt_ltl_not : UInt<1>, a)")
+    val sourceLoc = "@[Foo.scala 1:2]"
+    chirrtl should include(f"intrinsic(circt_ltl_not : UInt<1>, a) $sourceLoc")
   }
 
   it should "support property implication operation" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a, b = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val p0: Property = Property.implication(a, b)
       val p1: Property = a |-> b
       val p2: Property = Property.implicationNonOverlapping(a, b)
       val p3: Property = a |=> b
     })
+    val sourceLoc = "@[Foo.scala 1:2]"
 
     // Overlapping
-    chirrtl should include("intrinsic(circt_ltl_implication : UInt<1>, a, b)")
-    chirrtl should include("intrinsic(circt_ltl_implication : UInt<1>, a, b)")
+    chirrtl should include(f"intrinsic(circt_ltl_implication : UInt<1>, a, b) $sourceLoc")
+    chirrtl should include(f"intrinsic(circt_ltl_implication : UInt<1>, a, b) $sourceLoc")
 
     // Non-overlapping (emitted as `a ## true |-> b`)
-    chirrtl should include("node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, UInt<1>(0h1))")
-    chirrtl should include("node concat = intrinsic(circt_ltl_concat : UInt<1>, a, delay)")
-    chirrtl should include("node implication_2 = intrinsic(circt_ltl_implication : UInt<1>, concat, b)")
-    chirrtl should include("node delay_1 = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, UInt<1>(0h1))")
-    chirrtl should include("node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_1)")
-    chirrtl should include("node implication_3 = intrinsic(circt_ltl_implication : UInt<1>, concat_1, b)")
+    chirrtl should include(
+      f"node delay = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, UInt<1>(0h1)) $sourceLoc"
+    )
+    chirrtl should include(f"node concat = intrinsic(circt_ltl_concat : UInt<1>, a, delay) $sourceLoc")
+    chirrtl should include(f"node implication_2 = intrinsic(circt_ltl_implication : UInt<1>, concat, b) $sourceLoc")
+    chirrtl should include(
+      f"node delay_1 = intrinsic(circt_ltl_delay<delay = 1, length = 0> : UInt<1>, UInt<1>(0h1)) $sourceLoc"
+    )
+    chirrtl should include(f"node concat_1 = intrinsic(circt_ltl_concat : UInt<1>, a, delay_1) $sourceLoc")
+    chirrtl should include(f"node implication_3 = intrinsic(circt_ltl_implication : UInt<1>, concat_1, b) $sourceLoc")
   }
 
   it should "support property eventually operation" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val p0: Property = a.eventually
     })
-    chirrtl should include("intrinsic(circt_ltl_eventually : UInt<1>, a)")
+    val sourceLoc = "@[Foo.scala 1:2]"
+    chirrtl should include(f"intrinsic(circt_ltl_eventually : UInt<1>, a) $sourceLoc")
   }
 
   it should "support property disable operation" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
       val a, b = IO(Input(Bool()))
+      implicit val info = SourceLine("Foo.scala", 1, 2)
       val p0: Property = a.disable(b.asDisable)
     })
-    chirrtl should include("intrinsic(circt_ltl_disable : UInt<1>, a, b)")
+    val sourceLoc = "@[Foo.scala 1:2]"
+    chirrtl should include(f"intrinsic(circt_ltl_disable : UInt<1>, a, b) $sourceLoc")
   }
 
   it should "support simple property asserts/assumes/covers" in {
@@ -166,12 +186,14 @@ class LTLSpec extends AnyFlatSpec with Matchers {
     for ((prop, (intrinsic, op)) <- properties) {
       val chirrtl = ChiselStage.emitCHIRRTL(new Module {
         val a = IO(Input(Bool()))
+        implicit val info = SourceLine("Foo.scala", 1, 2)
         prop(a)
       })
+      val sourceLoc = "@[Foo.scala 1:2]"
       chirrtl should include("node has_been_reset = intrinsic(circt_has_been_reset : UInt<1>, clock, reset)")
       chirrtl should include("node disable = eq(has_been_reset, UInt<1>(0h0))")
-      chirrtl should include("node disable_1 = intrinsic(circt_ltl_disable : UInt<1>, a, disable)")
-      chirrtl should include("node clock_1 = intrinsic(circt_ltl_clock : UInt<1>, disable_1, clock)")
+      chirrtl should include(f"node disable_1 = intrinsic(circt_ltl_disable : UInt<1>, a, disable) $sourceLoc")
+      chirrtl should include(f"node clock_1 = intrinsic(circt_ltl_clock : UInt<1>, disable_1, clock) $sourceLoc")
       chirrtl should include(f"node $op = intrinsic(circt_verif_$op : UInt<1>, clock_1)")
     }
   }
