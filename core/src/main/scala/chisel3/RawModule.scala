afd7bfc7fabc9e8a4b9bade7fd9272deeba0e7ed
Will Dietz <will.dietz@sifive.com>
2024-05-23 20:42:12 -0500

    Fix boring tap of non-passive source from parent. (#4083)
    Expected result is fully aligned, which is what happens
    when reading from a probe.
    
    When the source is already at the LCA (from parent)
    there's no probe and the secret connections only support
    a few commands.
    
    For lack of a way to do, e.g., `:#=` here,
    use `read(probe(x))` to get the fully aligned result
    that's expected and bored down to the child.
    
    This isn't ideal but fixes this using only the sorts of expressions
    and commands that we've already committed to supporting.
    
    Fixes #3557.
    
    (cherry picked from commit 0dbedc3154b6163061b65d4a94a0a0fd7e8079c9)
    
    # Conflicts:
    #	core/src/main/scala/chisel3/RawModule.scala

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -10,6 +10,7 @@ import chisel3.experimental.hierarchy.{InstanceClone, ModuleClone}
 import chisel3.properties.{DynamicObject, Property, StaticObject}
 import chisel3.internal.Builder._
 import chisel3.internal.firrtl.ir._
+import chisel3.reflect.DataMirror
 import _root_.firrtl.annotations.{IsModule, ModuleTarget}
 import scala.collection.immutable.VectorBuilder
 import scala.collection.mutable.ArrayBuffer
@@ -209,8 +210,19 @@ abstract class RawModule extends BaseModule {
       case (false, true)                                => Connect(si, left.lref, ProbeRead(Node(right)))
       case (false, false) =>
         (left, right) match {
+<<<<<<< HEAD
           case (_: Property[_], _: Property[_]) => PropAssign(si, left.lref, Node(right))
           case (_, _) => Connect(si, left.lref, Node(right))
+=======
+          case (_: Property[_], _: Property[_]) => PropAssign(si, Node(left), Node(right))
+          // Use `connect lhs, read(probe(rhs))` if lhs is passive version of rhs.
+          // This provides solution for this: https://github.com/chipsalliance/chisel/issues/3557
+          case (_, _)
+              if !DataMirror.checkAlignmentTypeEquivalence(left, right) &&
+                DataMirror.checkAlignmentTypeEquivalence(left, Output(chiselTypeOf(right))) =>
+            Connect(si, Node(left), ProbeRead(ProbeExpr(Node(right))))
+          case (_, _) => Connect(si, Node(left), Node(right))
+>>>>>>> 0dbedc315 (Fix boring tap of non-passive source from parent. (#4083))
         }
     }
     val secretCommands = if (_closed) {
