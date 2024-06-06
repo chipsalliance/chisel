292af69c3d643739a3714210be694df3290f405e
Asherah Connor <ashe@kivikakk.ee>
2024-06-06 18:48:42 +0300

    PeekPokeAPI: include source location on failed expect() calls. (#4144)
    * simulator: add SourceInfo to expect calls and report.
    * simulator: add test for failed expects.
    * simulator: attempt to extract source line.
    * simulator: make testableData.expect's sourceInfo parameter explicit.
    * simulator: add factory method for giving failed expect sourceInfo/extraContext.
    
    (cherry picked from commit 45dd82a70027c21b1615d79962c03652fe4fa6d1)
    
    # Conflicts:
    #	src/test/scala/chiselTests/simulator/SimulatorSpec.scala

--- src/test/scala/chiselTests/simulator/SimulatorSpec.scala
+++ src/test/scala/chiselTests/simulator/SimulatorSpec.scala
@@ -70,6 +70,122 @@ class SimulatorSpec extends AnyFunSpec with Matchers {
       assert(result === 12)
     }
 
+<<<<<<< HEAD
+=======
+    it("reports failed expects correctly") {
+      val simulator = new VerilatorSimulator("test_run_dir/simulator/GCDSimulator")
+      val thrown = the[PeekPokeAPI.FailedExpectationException[_]] thrownBy {
+        simulator
+          .simulate(new GCD()) { module =>
+            import PeekPokeAPI._
+            val gcd = module.wrapped
+            gcd.io.a.poke(24.U)
+            gcd.io.b.poke(36.U)
+            gcd.io.loadValues.poke(1.B)
+            gcd.clock.step()
+            gcd.io.loadValues.poke(0.B)
+            gcd.clock.step(10)
+            gcd.io.result.expect(5)
+          }
+          .result
+      }
+      thrown.getMessage must include("Observed value '12' != 5.")
+      (thrown.getMessage must include).regex(
+        """ @\[src/test/scala/chiselTests/simulator/SimulatorSpec\.scala \d+:\d+\]"""
+      )
+      thrown.getMessage must include("gcd.io.result.expect(5)")
+      thrown.getMessage must include("                    ^")
+    }
+
+    it("runs a design that includes an external module") {
+      class Bar extends ExtModule with HasExtModuleInline {
+        val a = IO(Output(Bool()))
+        setInline(
+          "Bar.sv",
+          """|module Bar(
+             |  output a
+             |);
+             | assign a = 1'b1;
+             |endmodule
+             |""".stripMargin
+        )
+      }
+
+      class Baz extends ExtModule with HasExtModuleResource {
+        val a = IO(Output(Bool()))
+        addResource("/chisel3/simulator/Baz.sv")
+      }
+
+      class Qux extends ExtModule with HasExtModulePath {
+        val a = IO(Output(Bool()))
+        addPath("src/test/resources/chisel3/simulator/Qux.sv")
+      }
+
+      class Foo extends RawModule {
+        val a, b, c = IO(Output(Bool()))
+        a :<= Module(new Bar).a
+        b :<= Module(new Baz).a
+        c :<= Module(new Qux).a
+      }
+
+      new VerilatorSimulator("test_run_dir/simulator/extmodule")
+        .simulate(new Foo) { module =>
+          import PeekPokeAPI._
+          val foo = module.wrapped
+          foo.a.expect(1)
+          foo.b.expect(1)
+          foo.c.expect(1)
+        }
+        .result
+    }
+
+    it("runs a design with debug mode (-g) and --strip-debug-info") {
+      import circt.stage.ChiselStage
+
+      class Bar extends Module {
+        val a = IO(Input(Bool()))
+        val b = IO(Input(Bool()))
+        val out = IO(Output(Bool()))
+
+        out := a & b
+      }
+
+      // Check now the debug info is stripped
+      val expectedSV = ChiselStage.emitSystemVerilog(new Bar, firtoolOpts = Array("--strip-debug-info", "-g"))
+
+      new VerilatorSimulator("test_run_dir/simulator/bar_debug_mode") {
+        override val firtoolArgs = Seq("--strip-debug-info", "-g")
+      }
+        .simulate(new Bar) { module =>
+          import PeekPokeAPI._
+          val bar = module.wrapped
+
+          bar.a.poke(false.B)
+          bar.b.poke(false.B)
+          bar.out.expect(false.B)
+          bar.clock.step()
+
+          bar.a.poke(true.B)
+          bar.b.poke(false.B)
+          bar.out.expect(false.B)
+          bar.clock.step()
+
+          bar.a.poke(true.B)
+          bar.b.poke(true.B)
+          bar.out.expect(true.B)
+          bar.clock.step()
+        }
+        .result
+
+      // Check the expected SV and the generated SV are the same
+      val source = io.Source.fromFile("test_run_dir/simulator/bar_debug_mode/primary-sources/Bar.sv")
+      val actualSV = source.mkString
+      assert(actualSV === expectedSV)
+      source.close()
+
+    }
+
+>>>>>>> 45dd82a70 (PeekPokeAPI: include source location on failed expect() calls. (#4144))
     it("simulate a circuit with zero-width ports") {
       val width = 0
       // Run a simulation with zero width foo
