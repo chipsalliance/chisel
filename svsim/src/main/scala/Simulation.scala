1ee19f5c2445dc01ca1c2b2af3dd1c08105e3091
Kamyar Mohajerani <kammoh@gmail.com>
2024-06-18 15:38:45 -0400

    wip: simulator exceptions should be serializable

--- svsim/src/main/scala/Simulation.scala
+++ svsim/src/main/scala/Simulation.scala
@@ -34,7 +34,7 @@ final class Simulation private[svsim] (
     processBuilder.directory(new File(cwd))
     processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT)
     val environment = settings.environment ++ Seq(
-      Some("SVSIM_EXECUTION_SCRIPT" -> executionScriptPath),
+      Option.when(executionScriptLimit.map(_ >= 0).getOrElse(true))("SVSIM_EXECUTION_SCRIPT" -> executionScriptPath),
       executionScriptLimit.map("SVSIM_EXECUTION_SCRIPT_LIMIT" -> _.toString)
     ).flatten
     environment.foreach { (pair) =>
@@ -56,10 +56,14 @@ final class Simulation private[svsim] (
       controller.setTraceEnabled(true)
     }
     val bodyOutcome = Try {
-      val result = body(controller)
+      body(controller)
+    }
+    val completionOutcome = Try {
+      if (traceEnabled) {
+        controller.setTraceEnabled(false)
+      }
       // Exceptions thrown from commands still in the queue when `body` returns should supercede returning `result`
       controller.completeInFlightCommands()
-      result
     }
 
     // Always attempt graceful shutdown, even if `body` failed.
@@ -83,6 +87,7 @@ final class Simulation private[svsim] (
       throw new Exception(s"Nonzero exit status: ${process.exitValue()}")
     }
 
+    completionOutcome.get
     // Issues during graceful shutdown are considered test failures
     gracefulShutdownOutcome.get
 
