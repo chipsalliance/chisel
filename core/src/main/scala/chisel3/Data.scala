09234181eeb469c37476afd41bc8538b634c3d0b
Megan Wachs <megan@sifive.com>
2024-07-03 10:37:25 -0700

    make requireTypeEquivalent public and add a message

--- core/src/main/scala/chisel3/Data.scala
+++ core/src/main/scala/chisel3/Data.scala
@@ -607,15 +607,18 @@ abstract class Data extends HasId with NamedComponent with SourceInfoDoc {
 
   /** Require that two things are type equivalent, and if they are not, print a helpful error message as
     * to why not.
+    * 
+    * @param that the Data to compare to for type equivalence
+    * @param message if they are not type equivalent, contextual message to add to the exception thrown
     */
-  private[chisel3] def requireTypeEquivalent(that: Data): Unit = {
+  def requireTypeEquivalent(that: Data, message: Option[String] = None): Unit = {
     require(
       this.typeEquivalent(that), {
         val reason = this
           .findFirstTypeMismatch(that, strictTypes = true, strictWidths = true, strictProbeInfo = true)
           .map(s => s"\nbecause $s")
           .getOrElse("")
-        s"$this is not typeEquivalent to $that$reason"
+        s"${message.getOrElse("")}$this is not typeEquivalent to $that$reason"
       }
     )
   }
