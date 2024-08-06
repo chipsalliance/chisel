39a5429c89e511e1d51897eca801fa12744f50f1
Jack Koenig <koenig@sifive.com>
2024-08-06 11:21:39 -0700

    Update .github/scripts/check-maven-for-llvm-firtool-version.sh
    Co-authored-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- .github/scripts/check-maven-for-llvm-firtool-version.sh
+++ .github/scripts/check-maven-for-llvm-firtool-version.sh
@@ -6,4 +6,4 @@ VERSION=${1#firtool-}
 POM="https://repo1.maven.org/maven2/org/chipsalliance/llvm-firtool/$VERSION/llvm-firtool-$VERSION.pom"
 
 # Return 1 if not found
-curl -o/dev/null -sfIL $POM || false
+curl -o/dev/null -sfIL "$POM" || false
