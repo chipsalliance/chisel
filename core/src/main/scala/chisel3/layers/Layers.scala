3b64e70af4ffa02796bd174667b6d5f3c2a99197
Jack Koenig <koenig@sifive.com>
2024-08-09 10:22:58 -0700

    Update Verification layers to use LayerConfig
    Fixes a "ships passing in the night" breakage due to layer.Convention
    being deprecated.

--- core/src/main/scala/chisel3/layers/Layers.scala
+++ core/src/main/scala/chisel3/layers/Layers.scala
@@ -1,17 +1,17 @@
 package chisel3.layers
 
 import chisel3.experimental.UnlocatableSourceInfo
-import chisel3.layer.{Convention, Layer}
+import chisel3.layer.{Layer, LayerConfig}
 
 /** The root [[chisel3.layer.Layer]] for all shared verification collateral. */
-object Verification extends Layer(Convention.Bind)(_parent = Layer.Root, _sourceInfo = UnlocatableSourceInfo) {
+object Verification extends Layer(LayerConfig.Extract())(_parent = Layer.Root, _sourceInfo = UnlocatableSourceInfo) {
 
   /** The [[chisel3.layer.Layer]] where all assertions will be placed. */
-  object Assert extends Layer(Convention.Bind)(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
+  object Assert extends Layer(LayerConfig.Extract())(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
 
   /** The [[chisel3.layer.Layer]] where all assumptions will be placed. */
-  object Assume extends Layer(Convention.Bind)(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
+  object Assume extends Layer(LayerConfig.Extract())(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
 
   /** The [[chisel3.layer.Layer]] where all covers will be placed. */
-  object Cover extends Layer(Convention.Bind)(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
+  object Cover extends Layer(LayerConfig.Extract())(_parent = Verification, _sourceInfo = UnlocatableSourceInfo)
 }
