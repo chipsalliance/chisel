5dd085c91bfa82012018cacc56eb710f616cc2fb
Mike Urbach <mike.urbach@sifive.com>
2024-08-09 16:35:05 -0600

    Support BoringUtils.bore on OpaqueType wrapping a Property. (#4337)
    Co-authored-by: Jack Koenig <koenig@sifive.com>

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -4,7 +4,7 @@ package chisel3
 
 import scala.util.Try
 import scala.language.experimental.macros
-import chisel3.experimental.{BaseModule, SourceInfo, UnlocatableSourceInfo}
+import chisel3.experimental.{BaseModule, OpaqueType, SourceInfo, UnlocatableSourceInfo}
 import chisel3.internal._
 import chisel3.internal.binding._
 import chisel3.experimental.hierarchy.{InstanceClone, ModuleClone}
@@ -231,24 +231,34 @@ abstract class RawModule extends BaseModule {
       .getOrElse(
         throwException(s"BoringUtils currently only support identity views, ${_right} has multiple targets.")
       )
-    val rhs = (left.probeInfo.nonEmpty, right.probeInfo.nonEmpty) match {
-      case (true, true)                                 => ProbeDefine(si, left.lref, Node(right))
-      case (true, false) if left.probeInfo.get.writable => ProbeDefine(si, left.lref, RWProbeExpr(Node(right)))
-      case (true, false)                                => ProbeDefine(si, left.lref, ProbeExpr(Node(right)))
-      case (false, true)                                => Connect(si, left.lref, ProbeRead(Node(right)))
-      case (false, false)                               =>
-        // For non-probe, directly create Nodes for lhs, skipping visibility check to support BoringUtils.drive.
-        (left, right) match {
-          case (_: Property[_], _: Property[_]) => PropAssign(si, Node(left), Node(right))
-          // Use `connect lhs, read(probe(rhs))` if lhs is passive version of rhs.
-          // This provides solution for this: https://github.com/chipsalliance/chisel/issues/3557
-          case (_, _)
-              if !DataMirror.checkAlignmentTypeEquivalence(left, right) &&
-                DataMirror.checkAlignmentTypeEquivalence(left, Output(chiselTypeOf(right))) =>
-            Connect(si, Node(left), ProbeRead(ProbeExpr(Node(right))))
-          case (_, _) => Connect(si, Node(left), Node(right))
-        }
+
+    def computeConnection(left: Data, right: Data): Command = {
+      (left.probeInfo.nonEmpty, right.probeInfo.nonEmpty) match {
+        case (true, true)                                 => ProbeDefine(si, left.lref, Node(right))
+        case (true, false) if left.probeInfo.get.writable => ProbeDefine(si, left.lref, RWProbeExpr(Node(right)))
+        case (true, false)                                => ProbeDefine(si, left.lref, ProbeExpr(Node(right)))
+        case (false, true)                                => Connect(si, left.lref, ProbeRead(Node(right)))
+        case (false, false)                               =>
+          // For non-probe, directly create Nodes for lhs, skipping visibility check to support BoringUtils.drive.
+          (left, right) match {
+            case (lhsOpaque: Record, rhsOpaque: Record)
+                if lhsOpaque._isOpaqueType && rhsOpaque._isOpaqueType && DataMirror.isProperty(
+                  lhsOpaque.allElements.head
+                ) && DataMirror.isProperty(rhsOpaque.allElements.head) =>
+              computeConnection(lhsOpaque.allElements.head, rhsOpaque.allElements.head)
+            case (_: Property[_], _: Property[_]) => PropAssign(si, Node(left), Node(right))
+            // Use `connect lhs, read(probe(rhs))` if lhs is passive version of rhs.
+            // This provides solution for this: https://github.com/chipsalliance/chisel/issues/3557
+            case (_, _)
+                if !DataMirror.checkAlignmentTypeEquivalence(left, right) &&
+                  DataMirror.checkAlignmentTypeEquivalence(left, Output(chiselTypeOf(right))) =>
+              Connect(si, Node(left), ProbeRead(ProbeExpr(Node(right))))
+            case (_, _) => Connect(si, Node(left), Node(right))
+          }
+      }
     }
+
+    val rhs = computeConnection(left, right)
     val secretCommands = if (_closed) {
       _component.get.asInstanceOf[DefModule].secretCommands
     } else {
