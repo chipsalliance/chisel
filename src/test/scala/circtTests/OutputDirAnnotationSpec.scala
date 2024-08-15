a0f380d616e7c8032721dd38b4b02e193339aac5
Will Dietz <will.dietz@sifive.com>
2024-08-15 13:26:52 -0500

    Add support for circt.OutputDirAnnotation.

---
+++ src/test/scala/circtTests/OutputDirAnnotationSpec.scala
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package circtTests
+
+import chisel3._
+import circt.outputDir
+import circt.stage.ChiselStage
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import chiselTests.experimental.hierarchy.Utils
+
+class OutputDirAnnotationSpec extends AnyFunSpec with Matchers {
+  describe("output directory annotation works") {
+    it("should put module where requested") {
+      class TestModule extends RawModule with Public {}
+      class ParentModule extends RawModule {
+        val test = outputDir(Module(new TestModule), "foo")
+      }
+
+      val chirrtl = ChiselStage.emitCHIRRTL(new ParentModule)
+      (chirrtl.split('\n').map(_.takeWhile(_ != '@').trim) should contain).allOf(
+        """"class":"circt.OutputDirAnnotation",""",
+        """"target":"~ParentModule|TestModule",""",
+        """"dirname":"foo""""
+      )
+
+      val sv = ChiselStage.emitSystemVerilog(new ParentModule)
+      sv should include(""""foo/TestModule.sv"""")
+    }
+  }
+}
