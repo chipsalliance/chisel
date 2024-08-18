793eb2e43a15915de46b1083ebce3de3abf5cef5
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2024-08-17 20:25:57 -0400

    fixup! Make '--module' support arguments

--- src/test/scala/chiselTests/stage/ChiselAnnotationsSpec.scala
+++ src/test/scala/chiselTests/stage/ChiselAnnotationsSpec.scala
@@ -48,24 +48,25 @@ class ChiselAnnotationsSpec extends AnyFlatSpec with Matchers {
 
   behavior.of("ChiselGeneratorAnnotation when stringly constructing from Module names")
 
-  it should "elaborate from a String" in {
-    val annotation = ChiselGeneratorAnnotation("chiselTests.stage.ChiselAnnotationsSpecFoo")
+  it should "elaborate a module without parameters" in {
+    val annotation = ChiselGeneratorAnnotation("chiselTests.stage.ChiselAnnotationsSpecFoo()")
+    val res = annotation.elaborate
+    res(0) shouldBe a[ChiselCircuitAnnotation]
+    res(1) shouldBe a[DesignAnnotation[_]]
+  }
+
+  it should "elaborate a module with parameters" in {
+    val annotation = ChiselGeneratorAnnotation("""chiselTests.stage.ChiselAnnotationsSpecBaz("hello")""")
     val res = annotation.elaborate
     res(0) shouldBe a[ChiselCircuitAnnotation]
     res(1) shouldBe a[DesignAnnotation[_]]
   }
 
   it should "throw an exception if elaboration from a String refers to nonexistant class" in {
-    val bar = "chiselTests.stage.ChiselAnnotationsSpecBar"
+    val bar = "chiselTests.stage.ChiselAnnotationsSpecBar()"
     val annotation = ChiselGeneratorAnnotation(bar)
-    intercept[OptionsException] { annotation.elaborate }.getMessage should startWith(s"Unable to locate module '$bar'")
-  }
-
-  it should "throw an exception if elaboration from a String refers to an anonymous class" in {
-    val baz = "chiselTests.stage.ChiselAnnotationsSpecBaz"
-    val annotation = ChiselGeneratorAnnotation(baz)
     intercept[OptionsException] { annotation.elaborate }.getMessage should startWith(
-      s"Unable to create instance of module '$baz'"
+      s"Unable to run module generator '$bar' because it or one of its arguments could not be found"
     )
   }
 
