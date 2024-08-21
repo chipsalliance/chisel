d85b628849c26f0c4f2314a0ba695e244c9dc60b
Deborah Soung <debs@sifive.com>
2024-08-21 15:27:50 -0700

    Add `Path.apply()` to support `HasTarget` (#4364)

--- src/test/scala/chiselTests/properties/PropertySpec.scala
+++ src/test/scala/chiselTests/properties/PropertySpec.scala
@@ -9,6 +9,7 @@ import circt.stage.ChiselStage
 import chisel3.properties.ClassType
 import chisel3.properties.AnyClassType
 import chisel3.util.experimental.BoringUtils
+import chisel3.util.SRAM
 
 class PropertySpec extends ChiselFlatSpec with MatchesAndOmits {
   behavior.of("Property")
@@ -190,25 +191,35 @@ class PropertySpec extends ChiselFlatSpec with MatchesAndOmits {
   }
 
   it should "support member path target types when requested" in {
-    val chirrtl = ChiselStage.emitCHIRRTL(new RawModule {
+    val chirrtl = ChiselStage.emitCHIRRTL(new Module {
       val propOutA = IO(Output(Property[Path]()))
       val propOutB = IO(Output(Property[Path]()))
       val propOutC = IO(Output(Property[Path]()))
+      val propOutD = IO(Output(Property[Path]()))
       override def desiredName = "Top"
-      val inst = Module(new RawModule {
+      val inst = Module(new Module {
         val data = WireInit(false.B)
         val mem = SyncReadMem(1, Bool())
+        val sram = SRAM(
+          size = 1,
+          tpe = Bool(),
+          numReadPorts = 1,
+          numWritePorts = 1,
+          numReadwritePorts = 0
+        )
         override def desiredName = "Foo"
       })
       propOutA := Property(Path(inst, true))
       propOutB := Property(Path(inst.data, true))
       propOutC := Property(Path(inst.mem, true))
+      propOutD := Property(Path(inst.sram.underlying.get, true))
     })
 
     matchesAndOmits(chirrtl)(
       """propassign propOutA, path("OMMemberInstanceTarget:~Top|Top/inst:Foo")""",
       """propassign propOutB, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>data")""",
-      """propassign propOutC, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>mem")"""
+      """propassign propOutC, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>mem")""",
+      """propassign propOutD, path("OMMemberReferenceTarget:~Top|Top/inst:Foo>sram_sram")"""
     )()
   }
 
