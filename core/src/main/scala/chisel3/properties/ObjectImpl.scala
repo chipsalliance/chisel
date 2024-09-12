c1c03d1b116360d3269136fec0e03a352916d8fb
Jack Koenig <koenig@sifive.com>
2024-09-12 12:26:10 -0700

    Create Scala 2 veneer for properties/Object.scala

--- core/src/main/scala/chisel3/properties/ObjectImpl.scala
+++ core/src/main/scala/chisel3/properties/ObjectImpl.scala
@@ -2,38 +2,19 @@
 
 package chisel3.properties
 
-import scala.language.experimental.macros
-
 import chisel3.{Module, RawModule, SpecifiedDirection}
 import chisel3.experimental.{BaseModule, SourceInfo}
 import chisel3.internal.firrtl.ir.{DefClass, DefObject, Node}
-import chisel3.internal.sourceinfo.InstTransform
 import chisel3.internal.{throwException, Builder, HasId, NamedComponent}
 import chisel3.internal.binding.ObjectFieldBinding
 
 import scala.collection.immutable.HashMap
 import scala.language.existentials
 
-/** Represents an instance of a Class.
-  *
-  * This cannot be instantiated directly, instead see Class.unsafeGetDynamicObject.
-  *
-  * The DynamicObject is generally unsafe, in that its getField method does not check the name, type, or direction of
-  * the accessed field. It may be used with care, and a more typesafe version called StaticObject has been added, which
-  * works with the Definition / Instance APIs.
-  *
-  * To create a DynamicObject directly, wrap a Class with DynamicObject.apply. For example:
-  *
-  *  {{{
-  *    val obj = DynamicObject(new Class {
-  *      override def desiredName = "Test"
-  *      val in = IO(Input(Property[Int]()))
-  *      val out = IO(Output(Property[Int]()))
-  *      out := in
-  *    })
-  *  }}}
-  */
-class DynamicObject private[chisel3] (val className: ClassType) extends HasId with NamedComponent {
+private[chisel3] trait DynamicObjectImpl extends HasId with NamedComponent {
+
+  val className: ClassType
+
   private val tpe = Property[className.Type]()
 
   _parent.foreach(_.addId(this))
@@ -79,21 +60,9 @@ class DynamicObject private[chisel3] (val className: ClassType) extends HasId wi
   }
 }
 
-object DynamicObject {
+private[chisel3] trait ObjectDynamicObjectImpl {
 
-  /** A wrapper method to wrap Class instantiations and return a DynamicObject.
-    *
-    * This is necessary to help Chisel track internal state. This can be used instead of `Definition.apply` if a
-    * DynamicObject is required. If possible, it is safer to user `Definition.apply` and StaticObject.
-    *
-    * @param bc the Class being created
-    *
-    * @return a DynamicObject representing an instance of the Class
-    */
-  def apply[T <: Class](bc: => T): DynamicObject = macro InstTransform.apply[T]
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Class](bc: => T)(implicit sourceInfo: SourceInfo): DynamicObject = {
+  protected def _applyImpl[T <: Class](bc: => T)(implicit sourceInfo: SourceInfo): DynamicObject = {
     // Instantiate the Class definition.
     val cls = Module.evaluate[T](bc)
 
