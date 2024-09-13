361e0ce30f56c62697f6f4a851959fc5f48beb0c
Jack Koenig <koenig@sifive.com>
2024-09-12 20:47:13 -0700

    Create Scala 2 veneer for Clock.scala

--- core/src/main/scala/chisel3/ClockImpl.scala
+++ core/src/main/scala/chisel3/ClockImpl.scala
@@ -2,19 +2,15 @@
 
 package chisel3
 
-import scala.language.experimental.macros
 import chisel3.experimental.SourceInfo
 import chisel3.internal.Builder.pushOp
 import chisel3.internal.firrtl.ir._
-import chisel3.internal.sourceinfo._
 import chisel3.internal.firrtl.ir.PrimOp.AsUIntOp
 
-object Clock {
-  def apply(): Clock = new Clock
-}
-
 // TODO: Document this.
-sealed class Clock(private[chisel3] val width: Width = Width(1)) extends Element {
+private[chisel3] trait ClockImpl extends Element {
+  private[chisel3] val width: Width = Width(1)
+
   override def toString: String = stringAccessor("Clock")
 
   def cloneType: this.type = Clock().asInstanceOf[this.type]
@@ -30,10 +26,7 @@ sealed class Clock(private[chisel3] val width: Width = Width(1)) extends Element
   /** Not really supported */
   def toPrintable: Printable = PString("CLOCK")
 
-  /** Returns the contents of the clock wire as a [[Bool]]. */
-  final def asBool: Bool = macro SourceInfoTransform.noArg
-
-  def do_asBool(implicit sourceInfo: SourceInfo): Bool = this.asUInt.asBool
+  protected def _asBoolImpl(implicit sourceInfo: SourceInfo): Bool = this.asUInt.asBool
 
   override private[chisel3] def _asUIntImpl(first: Boolean)(implicit sourceInfo: SourceInfo): UInt = pushOp(
     DefPrim(sourceInfo, UInt(this.width), AsUIntOp, ref)
