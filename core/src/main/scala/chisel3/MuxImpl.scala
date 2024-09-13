22d88a958631cdbe3167aeec555eaf558d6bda29
Jack Koenig <koenig@sifive.com>
2024-09-12 20:55:56 -0700

    Create Scala 2 veneer for core/src/main/scala/chisel3/Mux.scala

--- core/src/main/scala/chisel3/MuxImpl.scala
+++ core/src/main/scala/chisel3/MuxImpl.scala
@@ -2,32 +2,15 @@
 
 package chisel3
 
-import scala.language.experimental.macros
-
 import chisel3.internal._
 import chisel3.internal.Builder.pushOp
 import chisel3.experimental.{requireIsHardware, SourceInfo}
-import chisel3.internal.sourceinfo.MuxTransform
 import chisel3.internal.firrtl.ir._
 import chisel3.internal.firrtl.ir.PrimOp._
 
-object Mux extends SourceInfoDoc {
+private[chisel3] trait MuxImpl {
 
-  /** Creates a mux, whose output is one of the inputs depending on the
-    * value of the condition.
-    *
-    * @param cond condition determining the input to choose
-    * @param con the value chosen when `cond` is true
-    * @param alt the value chosen when `cond` is false
-    * @example
-    * {{{
-    * val muxOut = Mux(data_in === 3.U, 3.U(4.W), 0.U(4.W))
-    * }}}
-    */
-  def apply[T <: Data](cond: Bool, con: T, alt: T): T = macro MuxTransform.apply[T]
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
+  protected def _applyImpl[T <: Data](
     cond: Bool,
     con:  T,
     alt:  T
