4d6b1839f7cf5b0db70706a08d4ccf7724afd0a5
Jack Koenig <koenig@sifive.com>
2024-09-13 15:21:34 -0700

    Create Scala 2 veneers for src/main/scala/chisel3/util/Reg.scala

--- src/main/scala/chisel3/util/RegImpl.scala
+++ src/main/scala/chisel3/util/RegImpl.scala
@@ -6,35 +6,16 @@ import scala.language.experimental.macros
 
 import chisel3._
 import chisel3.experimental.SourceInfo
-import chisel3.internal.sourceinfo.SourceInfoTransform
 
-object RegEnable {
+private[chisel3] trait RegEnableImpl {
 
-  /** Returns a register with the specified next, update enable gate, and no reset initialization.
-    *
-    * @example {{{
-    * val regWithEnable = RegEnable(nextVal, ena)
-    * }}}
-    */
-  def apply[T <: Data](next: T, enable: Bool): T = macro SourceInfoTransform.nextEnableArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](next: T, enable: Bool)(implicit sourceInfo: SourceInfo): T = {
+  protected def _applyImpl[T <: Data](next: T, enable: Bool)(implicit sourceInfo: SourceInfo): T = {
     val r = Reg(chiselTypeOf(next))
     when(enable) { r := next }
     r
   }
 
-  /** Returns a register with the specified next, update enable gate, and reset initialization.
-    *
-    * @example {{{
-    * val regWithEnableAndReset = RegEnable(nextVal, 0.U, ena)
-    * }}}
-    */
-  def apply[T <: Data](next: T, init: T, enable: Bool): T = macro SourceInfoTransform.nextInitEnableArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
+  protected def _applyImpl[T <: Data](
     next:   T,
     init:   T,
     enable: Bool
@@ -47,48 +28,9 @@ object RegEnable {
   }
 }
 
-object ShiftRegister {
+private[chisel3] trait ShiftRegisterImpl {
 
-  /** Returns the n-cycle delayed version of the input signal.
-    *
-    * @param in input to delay
-    * @param n number of cycles to delay
-    * @param en enable the shift
-    *
-    * @example {{{
-    * val regDelayTwo = ShiftRegister(nextVal, 2, ena)
-    * }}}
-    */
-  def apply[T <: Data](in: T, n: Int, en: Bool): T = macro SourceInfoTransform.inNEnArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
-    in: T,
-    n:  Int,
-    en: Bool = true.B
-  )(
-    implicit sourceInfo: SourceInfo
-  ): T =
-    _apply_impl(in, n, en)
-
-  /** Returns the n-cycle delayed version of the input signal.
-    *
-    * Enable is assumed to be true.
-    *
-    * @param in input to delay
-    * @param n number of cycles to delay
-    *
-    * @example {{{
-    * val regDelayTwo = ShiftRegister(nextVal, 2)
-    * }}}
-    */
-  def apply[T <: Data](in: T, n: Int): T = macro SourceInfoTransform.inNArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](in: T, n: Int)(implicit sourceInfo: SourceInfo): T =
-    _apply_impl(in, n)
-
-  private def _apply_impl[T <: Data](
+  protected def _applyImpl[T <: Data](
     in: T,
     n:  Int,
     en: Bool = true.B
@@ -97,21 +39,7 @@ object ShiftRegister {
   ): T =
     ShiftRegisters(in, n, en).lastOption.getOrElse(in)
 
-  /** Returns the n-cycle delayed version of the input signal with reset initialization.
-    *
-    * @param in input to delay
-    * @param n number of cycles to delay
-    * @param resetData reset value for each register in the shift
-    * @param en enable the shift
-    *
-    * @example {{{
-    * val regDelayTwoReset = ShiftRegister(nextVal, 2, 0.U, ena)
-    * }}}
-    */
-  def apply[T <: Data](in: T, n: Int, resetData: T, en: Bool): T = macro SourceInfoTransform.inNResetDataEnArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
+  protected def _applyImpl[T <: Data](
     in:        T,
     n:         Int,
     resetData: T,
@@ -121,29 +49,7 @@ object ShiftRegister {
   ): T =
     ShiftRegisters(in, n, resetData, en).lastOption.getOrElse(in)
 
-  /** Returns the n-cycle delayed version of the input signal (SyncReadMem-based ShiftRegister implementation).
-    *
-    * @param in input to delay
-    * @param n  number of cycles to delay
-    * @param en enable the shift
-    * @param useDualPortSram dual port or single port SRAM based implementation
-    * @param name name of SyncReadMem object
-    */
-  def mem[T <: Data](in: T, n: Int, en: Bool, useDualPortSram: Boolean, name: Option[String]): T =
-    macro SourceInfoTransform.inNEnUseDualPortSramNameArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_mem[T <: Data](
-    in:              T,
-    n:               Int,
-    en:              Bool,
-    useDualPortSram: Boolean,
-    name:            Option[String]
-  )(
-    implicit sourceInfo: SourceInfo
-  ): T = _apply_impl_mem(in, n, en, useDualPortSram, name)
-
-  private def _apply_impl_mem[T <: Data](
+  protected def _applyImplMem[T <: Data](
     in:              T,
     n:               Int,
     en:              Bool = true.B,
@@ -210,26 +116,9 @@ object ShiftRegister {
   }
 }
 
-object ShiftRegisters {
+private[chisel3] trait ShiftRegistersImpl {
 
-  /** Returns a sequence of delayed input signal registers from 1 to n.
-    *
-    * @param in input to delay
-    * @param n  number of cycles to delay
-    * @param en enable the shift
-    */
-  def apply[T <: Data](in: T, n: Int, en: Bool): Seq[T] = macro SourceInfoTransform.inNEnArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
-    in: T,
-    n:  Int,
-    en: Bool
-  )(
-    implicit sourceInfo: SourceInfo
-  ): Seq[T] = _apply_impl(in, n, en)
-
-  private def _apply_impl[T <: Data](
+  protected def _applyImpl[T <: Data](
     in: T,
     n:  Int,
     en: Bool = true.B
@@ -238,30 +127,7 @@ object ShiftRegisters {
   ): Seq[T] =
     Seq.iterate(in, n + 1)(util.RegEnable(_, en)).drop(1)
 
-  /** Returns a sequence of delayed input signal registers from 1 to n.
-    *
-    * Enable is assumed to be true.
-    *
-    * @param in input to delay
-    * @param n  number of cycles to delay
-    */
-  def apply[T <: Data](in: T, n: Int): Seq[T] = macro SourceInfoTransform.inNArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](in: T, n: Int)(implicit sourceInfo: SourceInfo): Seq[T] =
-    _apply_impl(in, n)
-
-  /** Returns delayed input signal registers with reset initialization from 1 to n.
-    *
-    * @param in        input to delay
-    * @param n         number of cycles to delay
-    * @param resetData reset value for each register in the shift
-    * @param en        enable the shift
-    */
-  def apply[T <: Data](in: T, n: Int, resetData: T, en: Bool): Seq[T] = macro SourceInfoTransform.inNResetDataEnArg
-
-  /** @group SourceInfoTransformMacro */
-  def do_apply[T <: Data](
+  protected def _applyImpl[T <: Data](
     in:        T,
     n:         Int,
     resetData: T,
