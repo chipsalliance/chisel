4f5d81e8d573cd221d42471705a35e7104eb5a3c
Jiuyang Liu <liu@jiuyang.me>
2024-10-03 20:35:58 +0800

    fix up tests

--- src/test/scala/chiselTests/util/SRAMSpec.scala
+++ src/test/scala/chiselTests/util/SRAMSpec.scala
@@ -3,8 +3,7 @@
 package chiselTests.util
 
 import chisel3._
-import chisel3.properties.{Path, Property}
-import chisel3.util.{MemoryReadWritePort, SRAM, SRAMInterface}
+import chisel3.util.{MemoryReadWritePort, SRAM}
 import chisel3.experimental.{annotate, ChiselAnnotation, OpaqueType}
 import chiselTests.ChiselFlatSpec
 import _root_.circt.stage.ChiselStage.{emitCHIRRTL, emitSystemVerilog}
@@ -37,7 +36,7 @@ class SRAMSpec extends ChiselFlatSpec {
     val chirrtl = chirrtlCircuit.serialize
     chirrtl should include("module Top :")
     chirrtl should include(
-      "wire sram : { readPorts : { flip address : UInt<5>, flip enable : UInt<1>, data : UInt<8>}[0], writePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip data : UInt<8>}[0], readwritePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip isWrite : UInt<1>, readData : UInt<8>, flip writeData : UInt<8>}[1], description : { depth : Integer, dataWidth : Integer, masked : Bool, read : Integer, write : Integer, readwrite : Integer, maskGranularity : Integer}"
+      "wire sram : { readPorts : { flip address : UInt<5>, flip enable : UInt<1>, data : UInt<8>}[0], writePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip data : UInt<8>}[0], readwritePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip isWrite : UInt<1>, readData : UInt<8>, flip writeData : UInt<8>}[1], description : Inst<SRAMDescription>}"
     )
     chirrtl should include("mem sram_sram")
     chirrtl should include("data-type => UInt<8>")
@@ -52,6 +51,10 @@ class SRAMSpec extends ChiselFlatSpec {
     chirrtl should include("connect sram.readwritePorts[0].readData, sram_sram.RW0.rdata")
     chirrtl should include("connect sram_sram.RW0.wdata, sram.readwritePorts[0].writeData")
     chirrtl should include("connect sram_sram.RW0.wmode, sram.readwritePorts[0].isWrite")
+    chirrtl should include(
+      "propassign sram_descriptionInstance.hierarchyIn, path(\"OMReferenceTarget:~Top|Top>sram_sram\")"
+    )
+    chirrtl should include("propassign sram.description, sram_descriptionInstance")
 
     val dummyAnno = annos.collectFirst { case DummyAnno(t) => (t.toString) }
     dummyAnno should be(Some("~Top|Top>sram_sram"))
@@ -78,7 +81,7 @@ class SRAMSpec extends ChiselFlatSpec {
     chirrtl should include("module Top :")
     chirrtl should include("mem carrot :")
     chirrtl should include(
-      "wire sramInterface : { readPorts : { flip address : UInt<5>, flip enable : UInt<1>, data : UInt<8>}[0], writePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip data : UInt<8>}[0], readwritePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip isWrite : UInt<1>, readData : UInt<8>, flip writeData : UInt<8>}[1], description : { depth : Integer, dataWidth : Integer, masked : Bool, read : Integer, write : Integer, readwrite : Integer, maskGranularity : Integer}"
+      "wire sramInterface : { readPorts : { flip address : UInt<5>, flip enable : UInt<1>, data : UInt<8>}[0], writePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip data : UInt<8>}[0], readwritePorts : { flip address : UInt<5>, flip enable : UInt<1>, flip isWrite : UInt<1>, readData : UInt<8>, flip writeData : UInt<8>}[1], description : Inst<SRAMDescription>}"
     )
 
     val dummyAnno = annos.collectFirst { case DummyAnno(t) => (t.toString) }
@@ -226,42 +229,5 @@ class SRAMSpec extends ChiselFlatSpec {
 
     // check CIRCT can compile the output
     val sv = emitSystemVerilog(new Top)
-  }
-
-  it should "be possible to access SRAM description information" in {
-
-    class Top extends Module {
-      val size = IO(Output(Property[Int]()))
-      val sram = SRAM(
-        size = 32,
-        tpe = UInt(8.W),
-        numReadPorts = 0,
-        numWritePorts = 0,
-        numReadwritePorts = 1
-      )
-      size := sram.description.get.depth
-    }
-    // TODO we need a way with ChiselSim to evaluate properties
-    val chirrtl = emitCHIRRTL(new Top)
-    chirrtl should include("output size : Integer")
-    chirrtl should include("propassign size, sram.description.depth")
-  }
-
-  it should "be possible to create an SramInterface Wire" in {
-    class Top extends Module {
-      val sramIntf = Wire(
-        new SRAMInterface(
-          memSize = 32,
-          tpe = Vec(4, UInt(8.W)),
-          numReadPorts = 0,
-          numWritePorts = 0,
-          numReadwritePorts = 1,
-          masked = false
-        )
-      )
-      sramIntf := DontCare
-    }
-    // should not give uninitialized errors for description properties
-    emitSystemVerilog(new Top)
   }
 }
