fbb42ecc2d207054804a5f47c139e406ecb66a29
Aditya Naik <aditya.naik@sifive.com>
2024-10-07 03:51:33 -0700

    Fix quickfix types

--- core/src/main/scala/chisel3/RawModule.scala
+++ core/src/main/scala/chisel3/RawModule.scala
@@ -258,10 +258,10 @@ abstract class RawModule extends BaseModule {
 
 /** Enforce that the Module.reset be Asynchronous (AsyncReset) */
 trait RequireAsyncReset extends Module {
-  override final def resetType: Module.ResetType.Type = Module.ResetType.Asynchronous
+  override final def resetType: Module.ResetType.Asynchronous.type = Module.ResetType.Asynchronous
 }
 
 /** Enforce that the Module.reset be Synchronous (Bool) */
 trait RequireSyncReset extends Module {
-  override final def resetType: Module.ResetType.Type = Module.ResetType.Synchronous
+  override final def resetType: Module.ResetType.Synchronous.type = Module.ResetType.Synchronous
 }
