df32801327decdb51465150d13713ee0c318b528
unlsycn <unlsycn@unlsycn.com>
2024-11-04 10:57:31 +0000

    feat: add AutoBlackBox

---
+++ src/main/scala/chisel3/util/experimental/AutoBlackBox.scala
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3.util.experimental
+
+import chisel3._
+
+import scala.collection.immutable.SeqMap
+
+class AutoBlackBox(
+  val verilog:      String,
+  val signalFilter: String => Boolean = _ => true)
+    extends FixedIOExtModule(
+      ioGenerator = new AutoBundleFromVerilog(
+        SeqMap.from(SlangUtils.verilogModuleIO(SlangUtils.getVerilogAst(verilog)))
+      )(signalFilter)
+    ) {
+  override def desiredName = SlangUtils.verilogModuleName(SlangUtils.getVerilogAst(verilog))
+}
+
+class AutoBundleFromVerilog(
+  allElements:  SeqMap[String, Data]
+)(signalFilter: String => Boolean)
+    extends Record {
+  val elements: SeqMap[String, Data] = allElements.filter(n => signalFilter(n._1)).map {
+    case (k, v) =>
+      k -> chisel3.reflect.DataMirror.internal.chiselTypeClone(v)
+  }
+  def apply(data: String) = elements.getOrElse(
+    data,
+    throw new ChiselException(
+      s"$data not found in Verilog IO: \n ${allElements.keys.mkString("\n")}"
+    )
+  )
+}
