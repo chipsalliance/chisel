2159d813f2d9e3df05257ab504ed9f7ffe18ef86
Aditya Naik <aditya.naik@sifive.com>
2024-11-20 12:23:20 -0800

    Fix D/I sources to cross compile

---
+++ core/src/main/scala-2/chisel3/experimental/hierarchy/core/Hierarchy.scala
@@ -0,0 +1,68 @@
+package chisel3.experimental.hierarchy.core
+
+import chisel3._
+import scala.collection.mutable.{HashMap, HashSet}
+import chisel3.experimental.BaseModule
+import _root_.firrtl.annotations.IsModule
+import scala.reflect.runtime.universe.TypeTag
+
+trait Hierarchy[+A] extends HierarchyImpl[A] {
+/** Determine whether underlying proto is of type provided.
+    *
+    * @note IMPORTANT: this function requires summoning a TypeTag[B], which will fail if B is an inner class.
+    * @note IMPORTANT: this function IGNORES type parameters, akin to normal type erasure.
+    * @note IMPORTANT: this function relies on Java reflection for underlying proto, but Scala reflection for provided type
+    *
+    * E.g. isA[List[Int]] will return true, even if underlying proto is of type List[String]
+    * @return Whether underlying proto is of provided type (with caveats outlined above)
+    */
+  def isA[B: TypeTag]: Boolean = {
+    val tptag = implicitly[TypeTag[B]]
+    // drop any type information for the comparison, because the proto will not have that information.
+    val name = tptag.tpe.toString.takeWhile(_ != '[')
+    inBaseClasses(name)
+  }
+
+}
+
+object Hierarchy {
+  implicit class HierarchyBaseModuleExtensions[T <: BaseModule](i: Hierarchy[T]) {
+
+    /** Returns the toTarget of this hierarchy
+      * @return target of this hierarchy
+      */
+    def toTarget: IsModule = i match {
+      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toTarget
+      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toTarget
+      case _ => throw new InternalErrorException("Match error: toTarget i=$i")
+    }
+
+    /** Returns the toAbsoluteTarget of this hierarchy
+      * @return absoluteTarget of this Hierarchy
+      */
+    def toAbsoluteTarget: IsModule = i match {
+      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toAbsoluteTarget
+      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toAbsoluteTarget
+      case _ => throw new InternalErrorException("Match error: toAbsoluteTarget i=$i")
+    }
+
+    /** Returns the toRelativeTarget of this hierarchy
+      * @return relativeTarget of this Hierarchy
+      */
+    def toRelativeTarget(root: Option[BaseModule]): IsModule = i match {
+      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toRelativeTarget(root)
+      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toRelativeTarget(root)
+      case _ => throw new InternalErrorException("Match error: toAbsoluteTarget i=$i")
+    }
+
+    /** Returns the toRelativeTarget of this hierarchy
+      * @return relativeTarget of this Hierarchy
+      */
+    def toRelativeTargetToHierarchy(root: Option[Hierarchy[BaseModule]]): IsModule = i match {
+      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toRelativeTargetToHierarchy(root)
+      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toRelativeTargetToHierarchy(root)
+      case _ => throw new InternalErrorException("Match error: toAbsoluteTarget i=$i")
+    }
+
+  }
+}
--- core/src/main/scala/chisel3/experimental/hierarchy/core/HierarchyImpl.scala
+++ core/src/main/scala/chisel3/experimental/hierarchy/core/HierarchyImpl.scala
@@ -5,7 +5,6 @@ package chisel3.experimental.hierarchy.core
 import chisel3._
 
 import scala.collection.mutable.{HashMap, HashSet}
-import scala.reflect.runtime.universe.TypeTag
 import chisel3.experimental.BaseModule
 import _root_.firrtl.annotations.IsModule
 
@@ -15,7 +14,7 @@ import scala.annotation.implicitNotFound
   *
   * Enables writing functions which are Instance/Definition agnostic
   */
-sealed trait Hierarchy[+A] {
+trait HierarchyImpl[+A] {
   private[chisel3] def underlying: Underlying[A]
   private[chisel3] def proto: A = underlying match {
     case Proto(value) => value
@@ -25,22 +24,6 @@ sealed trait Hierarchy[+A] {
   /** Updated by calls to [[_lookup]], to avoid recloning returned Data's */
   private[chisel3] val cache = HashMap[Data, Data]()
   private[chisel3] def getInnerDataContext: Option[BaseModule]
-
-  /** Determine whether underlying proto is of type provided.
-    *
-    * @note IMPORTANT: this function requires summoning a TypeTag[B], which will fail if B is an inner class.
-    * @note IMPORTANT: this function IGNORES type parameters, akin to normal type erasure.
-    * @note IMPORTANT: this function relies on Java reflection for underlying proto, but Scala reflection for provided type
-    *
-    * E.g. isA[List[Int]] will return true, even if underlying proto is of type List[String]
-    * @return Whether underlying proto is of provided type (with caveats outlined above)
-    */
-  def isA[B: TypeTag]: Boolean = {
-    val tptag = implicitly[TypeTag[B]]
-    // drop any type information for the comparison, because the proto will not have that information.
-    val name = tptag.tpe.toString.takeWhile(_ != '[')
-    inBaseClasses(name)
-  }
 
   // This code handles a special-case where, within an mdoc context, the type returned from
   //  scala reflection (typetag) looks different than when returned from java reflection.
@@ -95,45 +78,3 @@ sealed trait Hierarchy[+A] {
 
 // Used to effectively seal Hierarchy, without requiring Definition and Instance to be in this file.
 private[chisel3] trait SealedHierarchy[+A] extends Hierarchy[A]
-
-object Hierarchy {
-  implicit class HierarchyBaseModuleExtensions[T <: BaseModule](i: Hierarchy[T]) {
-
-    /** Returns the toTarget of this hierarchy
-      * @return target of this hierarchy
-      */
-    def toTarget: IsModule = i match {
-      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toTarget
-      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toTarget
-      case _ => throw new InternalErrorException(s"Match error: toTarget i=$i")
-    }
-
-    /** Returns the toAbsoluteTarget of this hierarchy
-      * @return absoluteTarget of this Hierarchy
-      */
-    def toAbsoluteTarget: IsModule = i match {
-      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toAbsoluteTarget
-      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toAbsoluteTarget
-      case _ => throw new InternalErrorException(s"Match error: toAbsoluteTarget i=$i")
-    }
-
-    /** Returns the toRelativeTarget of this hierarchy
-      * @return relativeTarget of this Hierarchy
-      */
-    def toRelativeTarget(root: Option[BaseModule]): IsModule = i match {
-      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toRelativeTarget(root)
-      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toRelativeTarget(root)
-      case _ => throw new InternalErrorException(s"Match error: toAbsoluteTarget i=$i")
-    }
-
-    /** Returns the toRelativeTarget of this hierarchy
-      * @return relativeTarget of this Hierarchy
-      */
-    def toRelativeTargetToHierarchy(root: Option[Hierarchy[BaseModule]]): IsModule = i match {
-      case d: Definition[T] => new Definition.DefinitionBaseModuleExtensions(d).toRelativeTargetToHierarchy(root)
-      case i: Instance[T]   => new Instance.InstanceBaseModuleExtensions(i).toRelativeTargetToHierarchy(root)
-      case _ => throw new InternalErrorException(s"Match error: toAbsoluteTarget i=$i")
-    }
-
-  }
-}
