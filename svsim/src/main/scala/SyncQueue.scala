669d3e85af5ca8682961f88dc536e2be7fc4eaf5
Kamyar Mohajerani <kammoh@gmail.com>
2024-12-01 13:18:54 -0500

    wip

--- svsim/src/main/scala/SyncQueue.scala
+++ svsim/src/main/scala/SyncQueue.scala
@@ -4,15 +4,26 @@ package svsim
 
 import java.util.concurrent.ConcurrentLinkedQueue
 
-/**
-  * A simple wrapper for a thread-safe queue
+/** A simple wrapper for a thread-safe queue
   */
 final class SyncQueue[A] extends Iterable[A] {
   private val queue = new ConcurrentLinkedQueue[A]()
 
-  def enqueue(elem: A): Unit = queue.add(elem)
+  @inline def enqueue(elem: A): Unit = queue.add(elem)
 
-  def dequeue(): A = queue.remove()
+  @inline def dequeue(): A = queue.remove()
+
+  @inline def addOne(elem: A): this.type = {
+    enqueue(elem)
+    this
+  }
+
+  def addAll(elems: IterableOnce[A]): this.type = {
+    elems.iterator.foreach(enqueue)
+    this
+  }
+
+  @inline def peekHead: A = queue.peek()
 
   def iterator: Iterator[A] = {
     new Iterator[A] {
@@ -22,6 +33,9 @@ final class SyncQueue[A] extends Iterable[A] {
   }
 
   def clear(): Unit = queue.clear()
+
+  @inline def length: Int = queue.size()
+
 }
 
 object SyncQueue {
