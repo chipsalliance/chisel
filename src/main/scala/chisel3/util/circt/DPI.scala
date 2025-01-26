6a450525a8abd8053d2f5cfd8ab45fe5f4cfdc71
Qiming Chu <cchuqiming@gmail.com>
2025-01-26 14:21:16 +0800

    update DPI function call examples in chisel3.util.circt.dpi

--- src/main/scala/chisel3/util/circt/DPI.scala
+++ src/main/scala/chisel3/util/circt/DPI.scala
@@ -36,7 +36,7 @@ object RawClockedNonVoidFunctionCall {
     *
     * Please refer https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi for DPI function ABI.
     * @example {{{
-    * val a = RawClockedNonVoidFunctionCall("dpi_func_foo", UInt(1.W), clock, enable, b, c)
+    * val a = RawClockedNonVoidFunctionCall("dpi_func_foo", UInt(1.W))(clock, enable, b, c)
     * }}}
     */
   def apply[T <: Data](
@@ -69,7 +69,7 @@ object RawUnclockedNonVoidFunctionCall {
     *
     * Please refer https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi for DPI function ABI.
     * @example {{{
-    * val a = RawUnclockedNonVoidFunctionCall("dpi_func_foo", UInt(1.W), enable, b, c)
+    * val a = RawUnclockedNonVoidFunctionCall("dpi_func_foo", UInt(1.W))(enable, b, c)
     * }}}
     */
   def apply[T <: Data](
@@ -96,7 +96,7 @@ object RawClockedVoidFunctionCall {
     *
     * Please refer https://github.com/llvm/circt/blob/main/docs/Dialects/FIRRTL/FIRRTLIntrinsics.md#dpi-intrinsic-abi for DPI function ABI.
     * @example {{{
-    * RawClockedVoidFunctionCall("dpi_func_foo", UInt(1.W), clock, enable, b, c)
+    * RawClockedVoidFunctionCall("dpi_func_foo", UInt(1.W))(clock, enable, b, c)
     * }}}
     */
   def apply(
