1436ea23c20696cc2b276dfe58e1225e62ca4248
Trevor McKay <trevor.mckay@sifive.com>
2025-01-30 19:40:38 -0800

    rename TestHarness to TestHarnessGenerator

--- src/main/scala/chisel3/experimental/inlinetest/InlineTest.scala
+++ src/main/scala/chisel3/experimental/inlinetest/InlineTest.scala
@@ -24,13 +24,13 @@ class TestParameters[M <: RawModule, R] private[inlinetest] (
   *  @tparam M the type of the DUT module
   *  @tparam R the type of the result returned by the test body
   */
-trait TestHarness[M <: RawModule, R] {
+trait TestHarnessGenerator[M <: RawModule, R] {
 
   /** Generate a testharness module given the test parameters. */
   def generate(test: TestParameters[M, R]): RawModule with Public
 }
 
-object TestHarness {
+object TestHarnessGenerator {
 
   /** The minimal implementation of a unit testharness. Has a clock input and a synchronous reset
     *  input. Connects these to the DUT and does nothing else.
@@ -42,7 +42,7 @@ object TestHarness {
     test.body(dut)
   }
 
-  implicit def unitTestHarness[M <: RawModule]: TestHarness[M, Unit] = new TestHarness[M, Unit] {
+  implicit def unitTestHarness[M <: RawModule]: TestHarnessGenerator[M, Unit] = new TestHarnessGenerator[M, Unit] {
     def generate(test: TestParameters[M, Unit]): RawModule with Public = new UnitTestHarness(test)
   }
 }
@@ -71,7 +71,7 @@ trait HasTests[M <: RawModule] { module: M =>
     *
     *  @param body the circuit to elaborate inside the testharness
     */
-  protected final def test[R](testName: String)(body: Instance[M] => R)(implicit th: TestHarness[M, R]): Unit =
+  protected final def test[R](testName: String)(body: Instance[M] => R)(implicit th: TestHarnessGenerator[M, R]): Unit =
     elaborateParentModule { moduleDefinition =>
       val test = new TestParameters[M, R](desiredName, testName, moduleDefinition, body)
       th.generate(test)
