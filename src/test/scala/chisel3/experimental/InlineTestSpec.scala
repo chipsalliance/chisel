d8aaaa74510e7269d44dc24f04dbf3c6c35c8a63
Trevor McKay <trevor.mckay@sifive.com>
2025-01-30 20:44:07 -0800

    add example that utilizes DUT type in testharness

--- src/test/scala/chisel3/experimental/InlineTestSpec.scala
+++ src/test/scala/chisel3/experimental/InlineTestSpec.scala
@@ -11,28 +11,66 @@ class TestResultBundle extends Bundle {
   val code = Output(UInt(8.W))
 }
 
-class TestHarnessWithResultIO[M <: RawModule](test: TestParameters[M, TestResultBundle]) extends Module {
-  override def resetType = Module.ResetType.Synchronous
-  override val desiredName = s"test_${test.dutName}_${test.testName}"
-  val dut = Instance(test.dutDefinition)
-  val result = IO(new TestResultBundle)
-  result := test.body(dut)
+// Here is a testharness that consumes some kind of hardware from the test body, e.g.
+// a finish and pass/fail interface.
+object TestHarnessWithResultIO {
+  class TestHarnessModule[M <: RawModule](test: TestParameters[M, TestResultBundle]) extends Module {
+    override def resetType = Module.ResetType.Synchronous
+    override val desiredName = s"test_${test.dutName}_${test.testName}"
+    val dut = Instance(test.dutDefinition)
+    val result = IO(new TestResultBundle)
+    result := test.body(dut)
+  }
+  implicit def testharnessGenerator[M <: RawModule]: TestHarnessGenerator[M, TestResultBundle] =
+    new TestHarnessGenerator[M, TestResultBundle] {
+      def generate(test: TestParameters[M, TestResultBundle]): RawModule with Public =
+        new TestHarnessModule(test) with Public
+    }
 }
 
-object TestHarnessWithResultIO {
-  implicit def enhancedTestHarness[M <: RawModule]: TestHarness[M, TestResultBundle] =
-    new TestHarness[M, TestResultBundle] {
-      def generate(test: TestParameters[M, TestResultBundle]): RawModule with Public =
-        new TestHarnessWithResultIO(test) with Public
+object TestHarnessWithMonitorSocket {
+  // Here is a testharness that expects some sort of interface on its DUT, e.g. a probe
+  // socket to which to attach a monitor.
+  class TestHarnessModule[M <: RawModule with HasMonitorSocket](test: TestParameters[M, Unit]) extends Module {
+    override def resetType = Module.ResetType.Synchronous
+    override val desiredName = s"test_${test.dutName}_${test.testName}"
+    val dut = Instance(test.dutDefinition)
+    test.body(dut)
+    val monitor = Module(new ProtocolMonitor(dut.monProbe.cloneType))
+    monitor.io :#= probe.read(dut.monProbe)
+  }
+  implicit def testharnessGenerator[M <: RawModule with HasMonitorSocket]: TestHarnessGenerator[M, Unit] =
+    new TestHarnessGenerator[M, Unit] {
+      def generate(test: TestParameters[M, Unit]): RawModule with Public =
+        new TestHarnessModule(test) with Public
     }
 }
 
 @instantiable
-class ModuleWithTests(ioWidth: Int = 32) extends Module with HasTests[ModuleWithTests] {
-  @public val io = IO(new Bundle {
-    val in = Input(UInt(ioWidth.W))
-    val out = Output(UInt(ioWidth.W))
-  })
+trait HasMonitorSocket { this: RawModule =>
+  protected def makeProbe(bundle: ProtocolBundle): ProtocolBundle = {
+    val monProbe = IO(probe.Probe(chiselTypeOf(bundle)))
+    probe.define(monProbe, probe.ProbeValue(bundle))
+    monProbe
+  }
+  @public val monProbe: ProtocolBundle
+}
+
+class ProtocolBundle(width: Int) extends Bundle {
+  val in  = Flipped(UInt(width.W))
+  val out = UInt(width.W)
+}
+
+class ProtocolMonitor(bundleType: ProtocolBundle) extends Module {
+  val io = IO(Input(bundleType))
+  assert(io.in === io.out, "in === out")
+}
+
+@instantiable
+class ModuleWithTests(ioWidth: Int = 32) extends Module with HasMonitorSocket with HasTests[ModuleWithTests] {
+  @public val io = IO(new ProtocolBundle(ioWidth))
+
+  override val monProbe = makeProbe(io)
 
   io.out := io.in
 
@@ -46,21 +84,31 @@ class ModuleWithTests(ioWidth: Int = 32) extends Module with HasTests[ModuleWith
     assert(instance.io.out =/= 0.U): Unit
   }
 
-  import TestHarnessWithResultIO._
-  test("enhanced") { instance =>
-    val result = Wire(new TestResultBundle)
-    val timer = RegInit(0.U)
-    timer := timer + 1.U
-    instance.io.in := 5.U(ioWidth.W)
-    val outValid = instance.io.out =/= 0.U
-    when(outValid) {
-      result.code := 0.U
-      result.finish := timer > 1000.U
-    }.otherwise {
-      result.code := 1.U
-      result.finish := true.B
+  {
+    import TestHarnessWithResultIO._
+    test("with_result") { instance =>
+      val result = Wire(new TestResultBundle)
+      val timer = RegInit(0.U)
+      timer := timer + 1.U
+      instance.io.in := 5.U(ioWidth.W)
+      val outValid = instance.io.out =/= 0.U
+      when(outValid) {
+        result.code := 0.U
+        result.finish := timer > 1000.U
+      }.otherwise {
+        result.code := 1.U
+        result.finish := true.B
+      }
+      result
     }
-    result
+  }
+
+  {
+    import TestHarnessWithMonitorSocket._
+    test("with_monitor") { instance =>
+      instance.io.in := 5.U(ioWidth.W)
+      assert(instance.io.out =/= 0.U): Unit
+    }
   }
 }
 
@@ -80,11 +128,21 @@ class HasTestsSpec extends ChiselFlatSpec with FileCheck {
       | CHECK: input reset : UInt<1>
       | CHECK: inst dut of ModuleWithTests
       |
-      | CHECK: public module test_ModuleWithTests_enhanced
+      | CHECK: public module test_ModuleWithTests_with_result
       | CHECK: input clock : Clock
       | CHECK: input reset : UInt<1>
       | CHECK: output result : { finish : UInt<1>, code : UInt<8>}
       | CHECK: inst dut of ModuleWithTests
+      |
+      | CHECK: public module test_ModuleWithTests_with_monitor
+      | CHECK: input clock : Clock
+      | CHECK: input reset : UInt<1>
+      | CHECK: inst dut of ModuleWithTests
+      | CHECK: inst monitor of ProtocolMonitor
+      | CHECK: connect monitor.clock, clock
+      | CHECK: connect monitor.reset, reset
+      | CHECK: connect monitor.io.out, read(dut.monProbe).out
+      | CHECK: connect monitor.io.in, read(dut.monProbe).in
       """
     )
   }
