d7029abfc252c04d7ad9d22fda68c4b0badfa45b
Megan Wachs <megan@sifive.com>
2025-02-03 16:43:07 -0800

    add missing test, scalafmt

--- src/test/scala/chisel3/internal/ContainsProbeSpec.scala
+++ src/test/scala/chisel3/internal/ContainsProbeSpec.scala
@@ -8,31 +8,31 @@ import circt.stage.ChiselStage
 import chiselTests.ChiselFunSpec
 
 class ContainsProbeSpec extends ChiselFunSpec {
-  it("1. should return false for elements that dont"){
-     class TestMod extends RawModule {
+  it("1. should return false for elements that dont") {
+    class TestMod extends RawModule {
       val a = IO(Input(Bool()))
       val b = Bool()
 
       require(!containsProbe(a))
       require(!containsProbe(b))
-     }
+    }
     ChiselStage.emitCHIRRTL(new TestMod)
   }
 
   it("2. should return true for elements that do") {
-           class TestMod extends RawModule {
+    class TestMod extends RawModule {
 
       val a = IO(Probe(Bool()))
       val b = Probe(Bool())
-    
+
       require(containsProbe(a))
       require(containsProbe(b))
-     }
-         ChiselStage.emitCHIRRTL(new TestMod)
+    }
+    ChiselStage.emitCHIRRTL(new TestMod)
 
   }
 
-  it("3. should return false for aggregates"){
+  it("3. should return false for aggregates") {
     class TestMod extends RawModule {
 
       val a = IO(DecoupledIO(Bool()))
@@ -40,10 +40,9 @@ class ContainsProbeSpec extends ChiselFunSpec {
 
       require(!containsProbe(a))
       require(!containsProbe(b))
-     }
-         ChiselStage.emitCHIRRTL(new TestMod)
+    }
+    ChiselStage.emitCHIRRTL(new TestMod)
   }
-
 
   it("4. return true for aggregates that are probes") {
     class TestMod extends RawModule {
@@ -53,10 +52,10 @@ class ContainsProbeSpec extends ChiselFunSpec {
 
       require(containsProbe(a))
       require(containsProbe(b))
-     }
+    }
     ChiselStage.emitCHIRRTL(new TestMod)
   }
-  it("5. return true for aggregates that contain probes"){
+  it("5. return true for aggregates that contain probes") {
     class TestMod extends RawModule {
 
       val a = IO(DecoupledIO(Probe(Bool())))
@@ -67,7 +66,8 @@ class ContainsProbeSpec extends ChiselFunSpec {
       require(containsProbe(a))
       require(containsProbe(b))
       require(containsProbe(c))
-     }
+      require(containsProbe(d))
+    }
     ChiselStage.emitCHIRRTL(new TestMod)
   }
 }
