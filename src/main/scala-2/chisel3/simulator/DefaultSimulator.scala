28e5342a2077e59e6dd03aedd83edca8e6ef9dd9
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-05 18:19:24 -0500

    fixup! [chiselsim] Add Default (non-Ephemeral) Simulator

--- src/main/scala-2/chisel3/simulator/DefaultSimulator.scala
+++ src/main/scala-2/chisel3/simulator/DefaultSimulator.scala
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chisel3.simulator
 
 import svsim._
--- src/test/scala/chiselTests/simulator/DefaultSimulatorSpec.scala
+++ src/test/scala/chiselTests/simulator/DefaultSimulatorSpec.scala
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chiselTests.simulator
 
 import chisel3._
