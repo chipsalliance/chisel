e50556f8dcc1fb4834e3fc54c03da4654d89678d
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-04 22:44:22 -0500

    [chiselsim] Add Default (non-Ephemeral) Simulator
    Add a new simulator, DefaultSimulator, to Chiselsim.  This fills a major,
    long-standing hole in this API by adding a simulator that does not delete
    all the files that it creates after a test runs.
    
    This uses a type class, `HasTestingDirectory`, to allow for control over
    where the directory is created.  A default implementation is provided
    which uses a nanosecond resolution timestamp to differentiate the tests.
    E.g., example output from running the new test included in this commit
    multiple times is:
    
        test_run_dir/
        └── DefaultSimulator
            ├── 2025-02-05T16-58-02.175175
            ├── 2025-02-05T16-58-11.941263
            └── 2025-02-05T16-58-17.721776
    
    Note: a Scalatest-based type class implementation is intentionally _not_
    provided as part of this PR.  This will be provided in a follow-on in such
    a way that Chiselsim does not inherit a dependency on Scalatest.  Ideally,
    users of Scalatest will use the future Scalatest-based type class
    implementation to get a subdirectory which is not a timestamp, but derived
    from the name of that part of the test.  I.e., a directory like:
    
        test_run_dir/
        └── DefaultSimulator
            ├── writes_files_to_a_permanent_directory_on_disk
            └── doesnt_delete_the_root_directory
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ src/test/scala/chiselTests/simulator/DefaultSimulatorSpec.scala
@@ -0,0 +1,48 @@
+package chiselTests.simulator
+
+import chisel3._
+import chisel3.layer.{block, Layer, LayerConfig}
+import chisel3.ltl.AssertProperty
+import chisel3.simulator.{HasTestingDirectory, LayerControl}
+import chisel3.simulator.DefaultSimulator._
+import java.nio.file.FileSystems
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import scala.reflect.io.Directory
+
+class DefaultSimulatorSpec extends AnyFunSpec with Matchers {
+  class Foo extends Module {
+    stop()
+  }
+
+  describe("DefaultSimulator") {
+    it("writes files to a permanent directory on disk") {
+
+      /** An implementation that always writes to the subdirectory "test_run_dir/<class-name>/foo/" */
+      implicit val fooDirectory = new HasTestingDirectory {
+        override def getDirectory(testClassName: String) =
+          FileSystems.getDefault().getPath("test_run_dir", testClassName, "foo")
+      }
+
+      val directory = Directory(FileSystems.getDefault().getPath("test_run_dir", "DefaultSimulator", "foo").toFile())
+      directory.deleteRecursively()
+
+      simulate(new Foo()) { _ => }
+
+      info(s"found expected directory: '$directory'")
+      assert(directory.exists)
+      assert(directory.isDirectory)
+
+      val allFiles = directory.deepFiles.toSeq.map(_.toString).toSet
+      for (
+        file <- Seq(
+          "test_run_dir/DefaultSimulator/foo/workdir-default/Makefile",
+          "test_run_dir/DefaultSimulator/foo/primary-sources/Foo.sv"
+        )
+      ) {
+        info(s"found expected file: '$file'")
+        allFiles should contain(file)
+      }
+    }
+  }
+}
