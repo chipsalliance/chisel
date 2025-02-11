98ad6842c4da15c4aef64f77c1298252a393440a
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-11 14:56:23 -0500

    [chiselsim] Add HasSimulator type class
    Add a new type class, `HasSimulator`, which provides a method to get a
    simulator.  Provide two implementations: one for Verilator and one for
    VCS.  Additionally, provide a low-priority default that maps to Verilator.
    
    This is working towards refactoring Chiselsim to improve the
    customizability of its APIs while not dramatically changing how a user
    interacts with it.  Additionally, this is trying to work towards providing
    Scalatest-specific implementations of `HasSimulator` which allow the
    simulator to be chosen on the command line.  Much of the code for this is
    currently sequestered in either ChiselSpec (part of the tests which are
    not published) or in internal code.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ src/main/scala/chisel3/simulator/HasSimulator.scala
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3.simulator
+
+import java.nio.file.Files
+
+/** Type class for providing a simulator. */
+trait HasSimulator {
+
+  /** Return a simulator.  This require providing a type class implementation of
+    * [[HasTestingDirectory]].
+    */
+  def getSimulator(implicit testingDirectory: HasTestingDirectory): Simulator[_]
+
+}
+
+/** Type class implementations of [[HasSimulator]]. */
+object HasSimulator {
+
+  /** This object provides implementations of [[HasSimulator]].  To set the
+    * simulator in your test, please import one of the implementations.
+    *
+    * E.g., to use Verilator import:
+    *
+    * {{{
+    * import chisel3.simulator.HasSimulator.simulators.verilator
+    * }}}
+    *
+    * Or, to use VCS import:
+    *
+    * {{{
+    * import chisel3.simulator.HasSimulator.simulators.vcs
+    * }}}
+    *
+    * Note: if you do _not_ import one of these, the default will be to use
+    * Verilator due to the low-priority implicit default
+    * [[HasSimulator.default]].
+    */
+  object simulators {
+
+    /** A [[HasSimulator]] implementation for a Verilator simulator. */
+    def verilator: HasSimulator = new HasSimulator {
+      override def getSimulator(implicit testingDirectory: HasTestingDirectory): Simulator[svsim.verilator.Backend] =
+        new Simulator[svsim.verilator.Backend] {
+          override val backend = svsim.verilator.Backend.initializeFromProcessEnvironment()
+          override val tag = "verilator"
+          override val commonCompilationSettings = svsim.CommonCompilationSettings()
+          override val backendSpecificCompilationSettings = svsim.verilator.Backend.CompilationSettings()
+          override val workspacePath = Files.createDirectories(testingDirectory.getDirectory).toString
+        }
+    }
+
+    /** A [[HasSimulator]] implementation for a VCS simulator. */
+    def vcs: HasSimulator = new HasSimulator {
+      override def getSimulator(implicit testingDirectory: HasTestingDirectory): Simulator[svsim.vcs.Backend] =
+        new Simulator[svsim.vcs.Backend] {
+          override val backend = svsim.vcs.Backend.initializeFromProcessEnvironment().getOrElse {
+            throw new Exception(
+              "Unable to load VCS from the environment. (Did you forget to set VCS_HOME and LM_LICENSE_FILE?)"
+            )
+          }
+          override val tag = "vcs"
+          override val commonCompilationSettings = svsim.CommonCompilationSettings()
+          override val backendSpecificCompilationSettings = svsim.vcs.Backend.CompilationSettings()
+          override val workspacePath = Files.createDirectories(testingDirectory.getDirectory).toString
+        }
+    }
+
+  }
+
+  /** Low-priority default implementation of [[HasSimulator]] that uses Verilator.
+    * This is the default that will be used if the user does provide an
+    * alternative.
+    */
+  implicit def default: HasSimulator = simulators.verilator
+
+}
---
+++ src/test/scala-2/chiselTests/simulator/HasSimulatorSpec.scala
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chiselTests.simulator
+
+import chisel3.simulator.{HasSimulator, HasTestingDirectory, Simulator}
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class HasSimulatorSpec extends AnyFunSpec with Matchers {
+
+  /** Return a field from the simulator so that we can check its type. */
+  def foo(implicit a: HasSimulator): String = a.getSimulator.tag
+
+  describe("HasSimulator") {
+
+    it("should use Verilator as a low-priority default") {
+
+      foo should be("verilator")
+
+    }
+
+    it("should allow overriding via an implicit val") {
+
+      implicit val bar: HasSimulator = new HasSimulator {
+        override def getSimulator(implicit testingDirectory: HasTestingDirectory): Simulator[svsim.verilator.Backend] =
+          new Simulator[svsim.verilator.Backend] {
+            override val backend = svsim.verilator.Backend.initializeFromProcessEnvironment()
+            override val tag = "still-verilator"
+            override val commonCompilationSettings = svsim.CommonCompilationSettings()
+            override val backendSpecificCompilationSettings = svsim.verilator.Backend.CompilationSettings()
+            override val workspacePath = ""
+          }
+      }
+
+      foo should be("still-verilator")
+
+    }
+
+  }
+
+}
