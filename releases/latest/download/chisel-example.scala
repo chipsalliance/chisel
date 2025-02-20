bd1614306633e59b9a7af30583ae33eb80a5d668
jackkoenig <jackkoenig@users.noreply.github.com>
2025-02-20 20:07:59 +0000

    deploy: af7fb1be309e9b484895aa6d4f19d0503fc05f8a

---
+++ releases/latest/download/chisel-example.scala
@@ -0,0 +1,35 @@
+//> using repository "sonatype-s01:snapshots"
+//> using scala "2.13.15"
+//> using dep "org.chipsalliance::chisel:6.6.0"
+//> using plugin "org.chipsalliance:::chisel-plugin:6.6.0"
+//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
+
+import chisel3._
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+class Foo extends Module {
+  val a, b, c = IO(Input(Bool()))
+  val d, e, f = IO(Input(Bool()))
+  val foo, bar = IO(Input(UInt(8.W)))
+  val out = IO(Output(UInt(8.W)))
+
+  val myReg = RegInit(0.U(8.W))
+  out := myReg
+
+  when(a && b && c) {
+    myReg := foo
+  }
+  when(d && e && f) {
+    myReg := bar
+  }
+}
+
+object Main extends App {
+  println(
+    ChiselStage.emitSystemVerilog(
+      gen = new Foo,
+      firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info", "-default-layer-specialization=enable")
+    )
+  )
+}
