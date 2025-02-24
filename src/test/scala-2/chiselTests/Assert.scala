9d2d393a63e8c42fb41efb77717dd3a19b4eb6b5
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-24 12:46:59 -0500

    [test] Migrate AssertSpec to ChiselSim
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/Assert.scala
+++ src/test/scala-2/chiselTests/Assert.scala
@@ -6,10 +6,11 @@ import chisel3._
 import circt.stage.ChiselStage
 import chisel3.simulator.scalatest.ChiselSim
 import chisel3.simulator.stimulus.RunUntilFinished
-import chisel3.testers.BasicTester
-import chisel3.util._
+import chisel3.util.Counter
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class FailingAssertTester() extends BasicTester {
+class FailingAssertTester() extends Module {
   assert(false.B)
   // Wait to come out of reset
   val (_, done) = Counter(!reset.asBool, 4)
@@ -18,7 +19,7 @@ class FailingAssertTester() extends BasicTester {
   }
 }
 
-class SucceedingAssertTester() extends BasicTester {
+class SucceedingAssertTester() extends Module {
   assert(true.B)
   // Wait to come out of reset
   val (_, done) = Counter(!reset.asBool, 4)
@@ -35,7 +36,7 @@ class PipelinedResetModule extends Module {
 }
 
 // This relies on reset being asserted for 3 or more cycles
-class PipelinedResetTester extends BasicTester {
+class PipelinedResetTester extends Module {
   val pipelinedResetModule = Module(new PipelinedResetModule)
 
   pipelinedResetModule.reset := RegNext(RegNext(RegNext(reset)))
@@ -46,31 +47,31 @@ class PipelinedResetTester extends BasicTester {
   }
 }
 
-class ModuloAssertTester extends BasicTester {
+class ModuloAssertTester extends Module {
   assert((4.U % 2.U) === 0.U)
   stop()
 }
 
-class FormattedAssertTester extends BasicTester {
+class FormattedAssertTester extends Module {
   val foobar = Wire(UInt(32.W))
   foobar := 123.U
   assert(foobar === 123.U, "Error! Wire foobar =/= %x! This is 100%% wrong.\n", foobar)
   stop()
 }
 
-class BadUnescapedPercentAssertTester extends BasicTester {
+class BadUnescapedPercentAssertTester extends Module {
   assert(1.U === 1.U, "I'm 110% sure this is an invalid message")
   stop()
 }
 
-class PrintableFormattedAssertTester extends BasicTester {
+class PrintableFormattedAssertTester extends Module {
   val foobar = Wire(UInt(32.W))
   foobar := 123.U
   assert(foobar === 123.U, cf"Error! Wire foobar =/= $foobar%x This is 100%% wrong.\n")
   stop()
 }
 
-class PrintableBadUnescapedPercentAssertTester extends BasicTester {
+class PrintableBadUnescapedPercentAssertTester extends Module {
   assert(1.U === 1.U, p"I'm 110% sure this is an invalid message")
   stop()
 }
@@ -98,32 +99,32 @@ class PrintableScopeTester extends Module {
   val printablePort = cf"$in"
 }
 
-class AssertPrintableWireScope extends BasicTester {
+class AssertPrintableWireScope extends Module {
   val mod = Module(new PrintableScopeTester)
   assert(1.U === 2.U, mod.printableWire)
   stop()
 }
 
-class AssertPrintablePortScope extends BasicTester {
+class AssertPrintablePortScope extends Module {
   val mod = Module(new PrintableScopeTester)
   mod.in := 255.U
   assert(1.U === 1.U, mod.printablePort)
   stop()
 }
 
-class AssertPrintableFailingWhenScope extends BasicTester {
+class AssertPrintableFailingWhenScope extends Module {
   val mod = Module(new PrintableWhenScopeTester)
   assert(1.U === 1.U, mod.printable)
   stop()
 }
 
-class AssumePrintableWireScope extends BasicTester {
+class AssumePrintableWireScope extends Module {
   val mod = Module(new PrintableScopeTester)
   assume(1.U === 1.U, mod.printableWire)
   stop()
 }
 
-class AssumePrintablePortScope extends BasicTester {
+class AssumePrintablePortScope extends Module {
   val mod = Module(new PrintableScopeTester)
   mod.in := 255.U
   assume(1.U === 1.U, mod.printablePort)
@@ -144,27 +145,27 @@ class PrintableWhenScopeTester extends Module {
   }
 }
 
-class AssertSpec extends ChiselFlatSpec with Utils with ChiselSim {
+class AssertSpec extends AnyFlatSpec with Matchers with ChiselSim {
   "A failing assertion" should "fail the testbench" in {
     intercept[Exception] {
       simulate(new FailingAssertTester)(RunUntilFinished(3))
     }.getMessage() should include("One or more assertions failed")
   }
   "A succeeding assertion" should "not fail the testbench" in {
-    assertTesterPasses { new SucceedingAssertTester }
+    simulate { new SucceedingAssertTester }(RunUntilFinished(5))
   }
   "An assertion" should "not assert until we come out of reset" in {
-    assertTesterPasses { new PipelinedResetTester }
+    simulate { new PipelinedResetTester }(RunUntilFinished(5))
   }
 
   "Assert Printables" should "respect port scoping" in {
-    assertTesterPasses { new AssertPrintablePortScope }
+    simulate { new AssertPrintablePortScope }(RunUntilFinished(3))
   }
   "Assert Printables" should "respect wire scoping" in {
     a[ChiselException] should be thrownBy { ChiselStage.emitCHIRRTL(new AssertPrintableWireScope) }
   }
   "Assume Printables" should "respect port scoping" in {
-    assertTesterPasses { new AssumePrintablePortScope }
+    simulate { new AssumePrintablePortScope }(RunUntilFinished(3))
   }
 
   "Assume Printables" should "respect wire scoping" in {
@@ -176,10 +177,10 @@ class AssertSpec extends ChiselFlatSpec with Utils with ChiselSim {
   }
 
   "Assertions" should "allow the modulo operator % in the message" in {
-    assertTesterPasses { new ModuloAssertTester }
+    simulate { new ModuloAssertTester }(RunUntilFinished(3))
   }
   they should "allow printf-style format strings with arguments" in {
-    assertTesterPasses { new FormattedAssertTester }
+    simulate { new FormattedAssertTester }(RunUntilFinished(3))
   }
   they should "allow printf-style format strings in Assumes" in {
     val chirrtl = ChiselStage.emitCHIRRTL(new PrintableAssumeTester)
@@ -188,21 +189,17 @@ class AssertSpec extends ChiselFlatSpec with Utils with ChiselSim {
     )
   }
   they should "not allow unescaped % in the message" in {
-    a[java.util.UnknownFormatConversionException] should be thrownBy {
-      extractCause[java.util.UnknownFormatConversionException] {
-        ChiselStage.emitCHIRRTL { new BadUnescapedPercentAssertTester }
-      }
+    intercept[java.util.UnknownFormatConversionException] {
+      ChiselStage.emitCHIRRTL { new BadUnescapedPercentAssertTester }
     }
   }
 
   they should "allow printable format strings with arguments" in {
-    assertTesterPasses { new FormattedAssertTester }
+    simulate { new FormattedAssertTester }(RunUntilFinished(3))
   }
   they should "not allow unescaped % in the printable message" in {
-    a[java.util.UnknownFormatConversionException] should be thrownBy {
-      extractCause[java.util.UnknownFormatConversionException] {
-        ChiselStage.emitCHIRRTL { new BadUnescapedPercentAssertTester }
-      }
+    intercept[java.util.UnknownFormatConversionException] {
+      ChiselStage.emitCHIRRTL { new BadUnescapedPercentAssertTester }
     }
   }
 
