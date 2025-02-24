21648639d71036a8c69863b5226d34a45e927af7
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-24 11:35:17 -0500

    [test] Migrate AdderTree to ChiselSim
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/AdderTree.scala
+++ src/test/scala-2/chiselTests/AdderTree.scala
@@ -3,7 +3,9 @@
 package chiselTests
 
 import chisel3._
-import chisel3.testers.BasicTester
+import chisel3.simulator.scalatest.ChiselSim
+import chisel3.simulator.stimulus.RunUntilFinished
+import org.scalatest.propspec.AnyPropSpec
 
 class AdderTree[T <: Bits with Num[T]](genType: T, vecSize: Int) extends Module {
   val io = IO(new Bundle {
@@ -13,7 +15,7 @@ class AdderTree[T <: Bits with Num[T]](genType: T, vecSize: Int) extends Module 
   io.numOut := io.numIn.reduceTree((a: T, b: T) => (a + b))
 }
 
-class AdderTreeTester(bitWidth: Int, numsToAdd: List[Int]) extends BasicTester {
+class AdderTreeTester(bitWidth: Int, numsToAdd: List[Int]) extends Module {
   val genType = UInt(bitWidth.W)
   val dut = Module(new AdderTree(genType, numsToAdd.size))
   dut.io.numIn := VecInit(numsToAdd.map(x => x.asUInt(bitWidth.W)))
@@ -22,12 +24,12 @@ class AdderTreeTester(bitWidth: Int, numsToAdd: List[Int]) extends BasicTester {
   stop()
 }
 
-class AdderTreeSpec extends ChiselPropSpec {
+class AdderTreeSpec extends AnyPropSpec with PropertyUtils with ChiselSim {
   property("All numbers should be added correctly by an Adder Tree") {
     forAll(safeUIntN(20)) {
       case (w: Int, v: List[Int]) => {
         whenever(v.size > 0 && w > 0) {
-          assertTesterPasses { new AdderTreeTester(w, v.map(x => math.abs(x) % (1 << w)).toList) }
+          simulate { new AdderTreeTester(w, v.map(x => math.abs(x) % (1 << w)).toList) }(RunUntilFinished(2))
         }
       }
     }
