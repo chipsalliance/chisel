76a7659fccb2465bc378ea7ee54a072e77bd39a7
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-24 17:09:41 -0500

    [test] Migrate EnumSpec to ChiselSim
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/EnumSpec.scala
+++ src/test/scala-2/chiselTests/EnumSpec.scala
@@ -3,17 +3,20 @@
 package chiselTests
 
 import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import chisel3.simulator.stimulus.RunUntilFinished
 import chisel3.util.Enum
-import chisel3.testers.BasicTester
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class EnumSpec extends ChiselFlatSpec {
+class EnumSpec extends AnyFlatSpec with ChiselSim {
 
   "1-entry Enums" should "work" in {
-    assertTesterPasses(new BasicTester {
+    simulate(new Module {
       val onlyState :: Nil = Enum(1)
       val wire = WireDefault(onlyState)
       chisel3.assert(wire === onlyState)
       stop()
-    })
+    })(RunUntilFinished(3))
   }
 }
