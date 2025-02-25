16694a24b1bda0fd2a65bff4cbe6cc985dcb0c95
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-24 19:09:06 -0500

    [test] Migrate ShiftRegister*Spec to ChiselSim
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/Reg.scala
+++ src/test/scala-2/chiselTests/Reg.scala
@@ -5,13 +5,16 @@ package chiselTests
 import circt.stage.ChiselStage
 import chisel3._
 import chisel3.reflect.DataMirror
-import chisel3.testers.BasicTester
+import chisel3.simulator.scalatest.ChiselSim
+import chisel3.simulator.stimulus.RunUntilFinished
 import chisel3.util._
 import org.scalacheck.Gen
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 class RegSpec extends ChiselFlatSpec {
   "Reg" should "be of the same type and width as t" in {
-    class RegOutTypeWidthTester extends BasicTester {
+    class RegOutTypeWidthTester extends Module {
       val reg = Reg(UInt(2.W))
       DataMirror.widthOf(reg) should be(2.W)
     }
@@ -19,7 +22,7 @@ class RegSpec extends ChiselFlatSpec {
   }
 
   "RegNext" should "be of unknown width" in {
-    class RegUnknownWidthTester extends BasicTester {
+    class RegUnknownWidthTester extends Module {
       val reg1 = RegNext(2.U(3.W))
       DataMirror.widthOf(reg1).known should be(false)
       val reg2 = RegNext(2.U(3.W), 4.U)
@@ -31,7 +34,7 @@ class RegSpec extends ChiselFlatSpec {
   }
 
   "RegInit" should "have width only if specified in the literal" in {
-    class RegForcedWidthTester extends BasicTester {
+    class RegForcedWidthTester extends Module {
       val reg1 = RegInit(20.U)
       DataMirror.widthOf(reg1).known should be(false)
       val reg2 = RegInit(20.U(7.W))
@@ -41,7 +44,7 @@ class RegSpec extends ChiselFlatSpec {
   }
 }
 
-class ShiftTester(n: Int) extends BasicTester {
+class ShiftTester(n: Int) extends Module {
   val (cntVal, done) = Counter(true.B, n)
   val start = 23.U
   val sr = ShiftRegister(cntVal + start, n)
@@ -51,7 +54,7 @@ class ShiftTester(n: Int) extends BasicTester {
   }
 }
 
-class ShiftResetTester(n: Int) extends BasicTester {
+class ShiftResetTester(n: Int) extends Module {
   val (cntVal, done) = Counter(true.B, n - 1)
   val start = 23.U
   val sr = ShiftRegister(cntVal + 23.U, n, 1.U, true.B)
@@ -61,17 +64,17 @@ class ShiftResetTester(n: Int) extends BasicTester {
   }
 }
 
-class ShiftRegisterSpec extends ChiselPropSpec {
+class ShiftRegisterSpec extends AnyPropSpec with ScalaCheckPropertyChecks with ChiselSim {
   property("ShiftRegister should shift") {
-    forAll(Gen.choose(0, 4)) { (shift: Int) => assertTesterPasses { new ShiftTester(shift) } }
+    forAll(Gen.choose(0, 4)) { (shift: Int) => simulate { new ShiftTester(shift) }(RunUntilFinished(shift + 2)) }
   }
 
   property("ShiftRegister should reset all values inside") {
-    forAll(Gen.choose(0, 4)) { (shift: Int) => assertTesterPasses { new ShiftResetTester(shift) } }
+    forAll(Gen.choose(0, 4)) { (shift: Int) => simulate { new ShiftResetTester(shift) }(RunUntilFinished(shift + 2)) }
   }
 }
 
-class ShiftsTester(n: Int) extends BasicTester {
+class ShiftsTester(n: Int) extends Module {
   val (cntVal, done) = Counter(true.B, n)
   val start = 23.U
   val srs = ShiftRegisters(cntVal + start, n)
@@ -83,8 +86,8 @@ class ShiftsTester(n: Int) extends BasicTester {
   }
 }
 
-class ShiftRegistersSpec extends ChiselPropSpec {
+class ShiftRegistersSpec extends AnyPropSpec with ScalaCheckPropertyChecks with ChiselSim {
   property("ShiftRegisters should shift") {
-    forAll(Gen.choose(0, 4)) { (shift: Int) => assertTesterPasses { new ShiftsTester(shift) } }
+    forAll(Gen.choose(0, 4)) { (shift: Int) => simulate { new ShiftsTester(shift) }(RunUntilFinished(shift + 2)) }
   }
 }
