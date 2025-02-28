d4d6591ef854776981be1e2029cd1883a251f26d
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-02-28 12:29:25 -0500

    [test] Modernize InvalidateAPISpec
    Avoid to-be-deprecated utilities.  Avoid wrapping ChiselStage and use it
    directly.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/InvalidateAPISpec.scala
+++ src/test/scala-2/chiselTests/InvalidateAPISpec.scala
@@ -6,21 +6,11 @@ import chisel3._
 import chisel3.stage.ChiselGeneratorAnnotation
 import chisel3.util.Counter
 import circt.stage.ChiselStage
-import firrtl.util.BackendCompilationUtilities._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.propspec.AnyPropSpec
 
 class InvalidateAPISpec extends AnyPropSpec with Matchers {
 
-  def myGenerateFirrtl(t: => Module): String = ChiselStage.emitCHIRRTL(t)
-  def compileFirrtl(t: => Module): Unit = {
-    val testDir = createTestDirectory(this.getClass.getSimpleName)
-
-    (new ChiselStage).execute(
-      Array[String]("-td", testDir.getAbsolutePath, "--target", "verilog"),
-      Seq(ChiselGeneratorAnnotation(() => t))
-    )
-  }
   class TrivialInterface extends Bundle {
     val in = Input(Bool())
     val out = Output(Bool())
@@ -32,7 +22,7 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       io.out := DontCare
       io.out := io.in
     }
-    val firrtlOutput = myGenerateFirrtl(new ModuleWithDontCare)
+    val firrtlOutput = ChiselStage.emitCHIRRTL(new ModuleWithDontCare)
     firrtlOutput should include("invalidate io.out")
   }
 
@@ -41,7 +31,7 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       val io = IO(new TrivialInterface)
       io.out := io.in
     }
-    val firrtlOutput = myGenerateFirrtl(new ModuleWithoutDontCare)
+    val firrtlOutput = ChiselStage.emitCHIRRTL(new ModuleWithoutDontCare)
     (firrtlOutput should not).include("invalidate")
   }
 
@@ -50,7 +40,7 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       val io = IO(new TrivialInterface)
       io <> DontCare
     }
-    val firrtlOutput = myGenerateFirrtl(new ModuleWithoutDontCare)
+    val firrtlOutput = ChiselStage.emitCHIRRTL(new ModuleWithoutDontCare)
     firrtlOutput should include("invalidate io.out")
     firrtlOutput should include("invalidate io.in")
   }
@@ -63,7 +53,7 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       })
       io.outs <> DontCare
     }
-    val firrtlOutput = myGenerateFirrtl(new ModuleWithoutDontCare)
+    val firrtlOutput = ChiselStage.emitCHIRRTL(new ModuleWithoutDontCare)
     for (i <- 0 until nElements)
       firrtlOutput should include(s"invalidate io.outs[$i]")
   }
@@ -76,7 +66,7 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       })
       io.ins := DontCare
     }
-    val firrtlOutput = myGenerateFirrtl(new ModuleWithoutDontCare)
+    val firrtlOutput = ChiselStage.emitCHIRRTL(new ModuleWithoutDontCare)
     for (i <- 0 until nElements)
       firrtlOutput should include(s"invalidate io.ins[$i]")
   }
@@ -158,6 +148,6 @@ class InvalidateAPISpec extends AnyPropSpec with Matchers {
       val foo = IO(Output(Clock()))
       foo := DontCare
     }
-    myGenerateFirrtl(new ClockConnectedToDontCare) should include("invalidate foo")
+    ChiselStage.emitCHIRRTL(new ClockConnectedToDontCare) should include("invalidate foo")
   }
 }
