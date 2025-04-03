051a819f68e8be2a74ea308f2712064cfac07575
Jack Koenig <koenig@sifive.com>
2025-04-03 13:24:26 -0700

    Rewrite VerificationSpec using FileCheck

--- src/test/scala-2/chiselTests/VerificationSpec.scala
+++ src/test/scala-2/chiselTests/VerificationSpec.scala
@@ -3,9 +3,8 @@
 package chiselTests
 
 import chisel3._
+import chisel3.testing.scalatest.FileCheck
 import circt.stage.ChiselStage
-import java.io.File
-import org.scalatest.matchers.should.Matchers
 import org.scalatest.propspec.AnyPropSpec
 
 class SimpleTest extends Module {
@@ -21,34 +20,26 @@ class SimpleTest extends Module {
   }
 }
 
-class VerificationSpec extends AnyPropSpec with Matchers {
-
-  def assertContains(s: Seq[String], x: String): Unit = {
-    val containsLine = s.map(_.contains(x)).reduce(_ || _)
-    assert(containsLine, s"\n  $x\nwas not found in`\n  ${s.mkString("\n  ")}``")
-  }
+class VerificationSpec extends AnyPropSpec with FileCheck {
 
   property("basic equality check should work") {
-    val fir = ChiselStage.emitCHIRRTL(new SimpleTest)
-    val lines = fir.split("(\n|@)").map(_.trim).toIndexedSeq
-
-    // reset guard around the verification statement
-    assertContains(lines, "when _T_1 :")
-    assertContains(lines, "cover(clock, _T, UInt<1>(0h1), \"\")")
-
-    assertContains(lines, "assume(clock, _T_3, UInt<1>(0h1), \"Assumption failed")
-    assertContains(lines, "node _T_5 = eq(io.out, io.in)")
-    assertContains(lines, "node _T_6 = eq(reset, UInt<1>(0h0))")
-    assertContains(
-      lines,
-      """intrinsic(circt_chisel_ifelsefatal<format = "Assertion failed: io.in:%d is equal to io.out:%d\n", label = "chisel3_builtin">, clock, _T_5, _T_6, io.in, io.out)"""
-    )
+    ChiselStage.emitCHIRRTL(new SimpleTest).fileCheck() {
+      """|CHECK:      node _T = eq(io.in, UInt<2>(0h3))
+         |CHECK:      when _T_1 :
+         |CHECK-NEXT:     cover(clock, _T, UInt<1>(0h1), "")
+         |CHECK:      node _T_3 = neq(io.in, UInt<2>(0h2))
+         |CHECK:      assume(clock, _T_3, UInt<1>(0h1), "Assumption failed at
+         |CHECK:      node _T_5 = eq(io.out, io.in)
+         |CHECK:      node _T_6 = eq(reset, UInt<1>(0h0))
+         |CHECK:      intrinsic(circt_chisel_ifelsefatal<format = "Assertion failed: io.in:%d is equal to io.out:%d\n", label = "chisel3_builtin">, clock, _T_5, _T_6, io.in, io.out)
+         |""".stripMargin
+    }
   }
 
-  property("annotation of verification constructs should work") {
+  property("labeling of verification constructs should work") {
 
-    /** Circuit that contains and annotates verification nodes. */
-    class AnnotationTest extends Module {
+    /** Circuit that contains and labels verification nodes. */
+    class LabelTest extends Module {
       val io = IO(new Bundle {
         val in = Input(UInt(8.W))
         val out = Output(UInt(8.W))
@@ -58,20 +49,13 @@ class VerificationSpec extends AnyPropSpec with Matchers {
       val assm = chisel3.assume(io.in =/= 2.U)
       val asst = chisel3.assert(io.out === io.in)
     }
-
-    // compile circuit
-    val testDir = new File("test_run_dir", "VerificationAnnotationTests")
-    ChiselStage.emitSystemVerilogFile(gen = new AnnotationTest, args = Array("-td", testDir.getPath))
-
-    // read in FIRRTL file
-    val svFile = new File(testDir, "AnnotationTest.sv")
-    svFile should exist
-    val svLines = scala.io.Source.fromFile(svFile).getLines().toList
-
     // check that verification appear in verilog output
-    exactly(1, svLines) should include("cover__cov: cov")
-    exactly(1, svLines) should include("assume__assm:")
-    exactly(1, svLines) should include("assume(io_in != 8'h2)")
-    exactly(1, svLines) should include("$error(\"Assumption failed")
+    ChiselStage.emitSystemVerilog(new LabelTest).fileCheck() {
+      """|CHECK:      cover__cov: cover(io_in == 8'h3);
+         |CHECK:      assume__assm:
+         |CHECK-NEXT:   assume(io_in != 8'h2)
+         |CHECK-NEXT:   $error("Assumption failed
+         |""".stripMargin
+    }
   }
 }
