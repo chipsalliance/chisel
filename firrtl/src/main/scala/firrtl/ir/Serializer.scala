669bca298459da89570d5c2ea0cc45058ee11294
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-04-09 18:04:02 -0400

    [firrtl] Bump serializer version to 5.1.0 (#4879)
    Bump the version of serialized FIRRTL to version 5.1.0.  This was
    mistakenly not bumped when the version was updated in Chisel.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- firrtl/src/main/scala/firrtl/ir/Serializer.scala
+++ firrtl/src/main/scala/firrtl/ir/Serializer.scala
@@ -17,7 +17,7 @@ object Serializer {
   val Indent = "  "
 
   // The version supported by the serializer.
-  val version = Version(4, 2, 0)
+  val version = Version(5, 1, 0)
 
   /** Converts a `FirrtlNode` into its string representation with
     * default indentation.
