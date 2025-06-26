bab1bd9f62d1aeb617e9612bae96aba2916493f6
Aditya Naik <aditya.naik@sifive.com>
2025-06-26 13:25:39 -0700

    Add comment to Scala 3 selectDynamic

--- core/src/main/scala-3/chisel3/BundleSelect.scala
+++ core/src/main/scala-3/chisel3/BundleSelect.scala
@@ -2,6 +2,15 @@
 
 package chisel3
 
+// Unlike in Scala 2, in Scala 3 the compiler no longer infers the
+// refined structural type for an anonymous subclass - it infers the
+// unrefined type for anonymous Bundles, which will always be
+// Bundle.
+//
+// This extension method allows accessing elements of an
+// anonymous Bundle even if it's refined type is not inferred -
+// without it the compiler will try to grab the element from class
+// Bundle itself and will obviously error.
 extension (b: Bundle) {
   def selectDynamic(field: String): Any = b.elements(field)
 }
