d3417d9d16dbffa458b501edaa29379b0cab6db4
Aditya Naik <aditya.naik@sifive.com>
2025-07-10 16:26:19 -0700

    Code review updates

--- core/src/main/scala-2/chisel3/SelectableCompat.scala
+++ core/src/main/scala-2/chisel3/SelectableCompat.scala
@@ -3,7 +3,7 @@
 package chisel3
 
 /** Does nothing in Scala 2; this stub trait is for maintaining
-  * compatibility with Scala 3's selectable, required for Scala 3
-  * Records.
+  * compatibility with Scala 3's selectable, required to support
+  * structurally-typed Bundles in Scala 3.
   */
 trait Selectable
--- plugin/src/main/scala-3/chisel3/internal/plugin/BundlePhase.scala
+++ plugin/src/main/scala-3/chisel3/internal/plugin/BundlePhase.scala
@@ -2,14 +2,14 @@
 
 package chisel3.internal.plugin
 
-import dotty.tools.dotc.*
+import dotty.tools.dotc.report
 import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.ast.tpd.TreeOps
 import dotty.tools.dotc.ast.untpd
-import dotty.tools.dotc.ast.tpd.*
 import dotty.tools.dotc.core.Contexts.*
 import dotty.tools.dotc.core.Symbols.*
 import dotty.tools.dotc.core.Names
-import dotty.tools.dotc.core.StdNames.*
+import dotty.tools.dotc.core.StdNames.nme
 import dotty.tools.dotc.core.Constants.Constant
 import dotty.tools.dotc.typer.TyperPhase
 import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
@@ -21,7 +21,7 @@ import dotty.tools.dotc.core.Definitions
 import scala.collection.mutable
 
 object BundleHelpers {
-  def cloneTypeFull(tree: Tree)(using Context): Tree = {
+  def cloneTypeFull(tree: tpd.Tree)(using Context): tpd.Tree = {
     val cloneSym =
       requiredMethod("chisel3.reflect.DataMirror.internal.chiselTypeClone")
     tpd.Apply(
@@ -34,28 +34,27 @@ object BundleHelpers {
   }
 
   def generateAutoCloneType(
-    record:     tpd.TypeDef,
-    thiz:       tpd.This,
-    conArgsOpt: Option[List[List[tpd.Tree]]],
-    isBundle:   Boolean
-  )(using Context): Option[tpd.DefDef] = {
-    conArgsOpt.flatMap { conArgs =>
-      val newExpr = tpd.New(record.symbol.typeRef, conArgs.flatten)
-      val recordTpe = requiredClassRef("chisel3.Record")
-      val cloneTypeSym = newSymbol(
-        record.symbol,
-        Names.termName("_cloneTypeImpl"),
-        Flags.Method | Flags.Override | Flags.Protected,
-        MethodType(Nil, Nil, recordTpe)
-      )
-      Some(tpd.DefDef(cloneTypeSym.asTerm, _ => newExpr))
-    }
+    record:  tpd.TypeDef,
+    thiz:    tpd.This,
+    conArgs: List[List[tpd.Tree]]
+  )(using Context): tpd.DefDef = {
+    val newExpr = tpd.New(record.symbol.typeRef, conArgs.flatten)
+    val recordTpe = requiredClassRef("chisel3.Record")
+    val cloneTypeSym = newSymbol(
+      record.symbol,
+      Names.termName("_cloneTypeImpl"),
+      Flags.Method | Flags.Override | Flags.Protected,
+      MethodType(Nil, Nil, recordTpe)
+    )
+    tpd.DefDef(cloneTypeSym.asTerm, _ => newExpr)
   }
 
+  // Creates a list of constructor parameter accessors from the
+  // argument value. Returns a Some if a constructor is found, or
+  // None otherwise.
   def extractConArgs(
-    record:   tpd.TypeDef,
-    thiz:     tpd.This,
-    isBundle: Boolean
+    record: tpd.TypeDef,
+    thiz:   tpd.This
   )(using Context): Option[List[List[tpd.Tree]]] = {
     val template = record.rhs.asInstanceOf[tpd.Template]
     val primaryConstructorOpt = Option(template.constr)
@@ -68,63 +67,65 @@ object BundleHelpers {
 
     val constructor = primaryConstructorOpt.get
     val paramLookup = paramAccessors.map(sym => sym.name.toString -> sym).toMap
+    val symAccessors = (x: Names.TermName) => record.symbol.asClass.paramAccessors.find(_.name == x)
+
     if (constructor.symbol.is(Flags.Private)) {
-      None
-    } else {
-      Some(constructor.termParamss.map(_.collect { case vp =>
-        val p: Symbol = paramLookup(vp.name.toString)
-        val select =
-          if (
-            record.symbol.asClass.paramAccessors
-              .find(_.name == vp.name)
-              .forall(_.is(Flags.Private))
-          ) None
-          else Some(tpd.Select(thiz, p.name))
-        if (select.isEmpty) return None
-        else {
-          val cloned: tpd.Tree =
-            if (ChiselTypeHelpers.isData(vp.tpt.tpe))
-              cloneTypeFull(select.get)
-            else select.get
-          if (vp.tpt.tpe.isRepeatedParam)
-            tpd.SeqLiteral(List(cloned), cloned)
-          else
-            cloned
-        }
-      }))
+      val msg = "Private bundle constructors cannot automatically be cloned, try making it package private"
+      report.error(msg, constructor.srcPos)
+      return None
     }
+
+    constructor.termParamss.map(_.map {
+      case param if symAccessors(param.name).forall(_.is(Flags.Private)) =>
+        report.warning("Private Bundle constructor parameters render the constructor uncloneable")
+        return None
+    })
+
+    Some(constructor.termParamss.map(_.collect { case vp =>
+      val p: Symbol = paramLookup(vp.name.toString)
+      val select = tpd.Select(thiz, p.name)
+      val cloned: tpd.Tree =
+        if (ChiselTypeHelpers.isData(vp.tpt.tpe))
+          cloneTypeFull(select)
+        else select
+      if (vp.tpt.tpe.isRepeatedParam)
+        tpd.SeqLiteral(List(cloned), cloned)
+      else
+        cloned
+    }))
   }
 
-  private def makeVector(values: List[Tree])(using Context): Tree = {
+  private def makeVector(values: List[tpd.Tree])(using Context): tpd.Tree = {
     val elemTpe = defn.TupleClass.typeRef.appliedTo(List(defn.StringType, defn.AnyType))
-    val vectorModule = ref(requiredModule("scala.collection.immutable.Vector").termRef)
+    val vectorModule = tpd.ref(requiredModule("scala.collection.immutable.Vector").termRef)
 
-    val vec = Apply(
-      TypeApply(Select(vectorModule, nme.apply), List(TypeTree(elemTpe))),
-      List(SeqLiteral(values, TypeTree(elemTpe)))
+    tpd.Apply(
+      tpd.TypeApply(tpd.Select(vectorModule, nme.apply), List(tpd.TypeTree(elemTpe))),
+      List(tpd.SeqLiteral(values, tpd.TypeTree(elemTpe)))
     )
-    vec
   }
 
   /** Creates the tuple containing the given elements */
-  def tupleTree(elems: List[Tree])(using Context): Tree = {
+  def tupleTree(elems: List[tpd.Tree])(using Context): tpd.Tree = {
     val arity = elems.length
-    if arity == 0 then ref(defn.EmptyTupleModule)
+    if arity == 0 then tpd.ref(defn.EmptyTupleModule)
     else if arity <= Definitions.MaxTupleArity then
       // TupleN[elem1Tpe, ...](elem1, ...)
-      ref(defn.TupleType(arity).nn.typeSymbol.companionModule)
+      tpd
+        .ref(defn.TupleType(arity).nn.typeSymbol.companionModule)
         .select(nme.apply)
         .appliedToTypes(elems.map(_.tpe.widenIfUnstable))
         .appliedToArgs(elems)
     else
       // TupleXXL.apply(elems*) // TODO add and use Tuple.apply(elems*) ?
-      ref(defn.TupleXXLModule)
+      tpd
+        .ref(defn.TupleXXLModule)
         .select(nme.apply)
-        .appliedToVarargs(elems.map(_.asInstance(defn.ObjectType)), TypeTree(defn.ObjectType))
+        .appliedToVarargs(elems.map(_.asInstance(defn.ObjectType)), tpd.TypeTree(defn.ObjectType))
         .asInstance(defn.tupleType(elems.map(elem => elem.tpe.widenIfUnstable)))
   }
 
-  def getBundleFields(record: tpd.TypeDef)(using Context): List[Tree] = {
+  def getBundleFields(record: tpd.TypeDef)(using Context): List[tpd.Tree] = {
     val bundleSym = record.symbol.asClass
     val recordTpe = requiredClass("chisel3.Record")
     def isBundleDataField(m: Symbol): Boolean = {
@@ -135,9 +136,9 @@ object BundleHelpers {
       )
     }
 
-    val currentFields: List[Tree] = bundleSym.info.decls.toList.collect {
+    val currentFields: List[tpd.Tree] = bundleSym.info.decls.toList.collect {
       case m if isBundleDataField(m) =>
-        val name = m.name.show.trim
+        val name = m.name.show
         val thisRef: tpd.Tree = tpd.This(bundleSym.asClass)
         val sel:     tpd.Tree = tpd.Select(thisRef, m.termRef)
         tupleTree(List(tpd.Literal(Constant(name)), sel))
@@ -147,7 +148,7 @@ object BundleHelpers {
 
   def generateElements(record: tpd.TypeDef)(using Context): tpd.DefDef = {
     val bundleSym = record.symbol.asClass
-    val currentFields: List[Tree] = getBundleFields(record)
+    val currentFields: List[tpd.Tree] = getBundleFields(record)
     val dataTpe = requiredClassRef("chisel3.Data")
     val rhs = makeVector(currentFields)
 
@@ -183,10 +184,11 @@ class ChiselBundlePhase extends PluginPhase {
       val thiz:     tpd.This = tpd.This(record.symbol.asClass)
 
       // =========== Generate _cloneTypeImpl ==================
-      val conArgs: Option[List[List[tpd.Tree]]] =
-        BundleHelpers.extractConArgs(record, thiz, isBundle)
-      val cloneTypeImplOpt =
-        BundleHelpers.generateAutoCloneType(record, thiz, conArgs, isBundle)
+      val conArgsOpt: Option[List[List[tpd.Tree]]] =
+        BundleHelpers.extractConArgs(record, thiz)
+      val cloneTypeImplOpt: Option[tpd.DefDef] = Option.when(!conArgsOpt.isEmpty) {
+        BundleHelpers.generateAutoCloneType(record, thiz, conArgsOpt.get)
+      }
 
       // =========== Generate val elements (Bundles only) =====
       val elementsImplOpt: Option[tpd.DefDef] = Option.when(isBundle) {
@@ -208,7 +210,7 @@ class ChiselBundlePhase extends PluginPhase {
       }
 
       record match {
-        case td @ TypeDef(name, tmpl: tpd.Template) => {
+        case td @ tpd.TypeDef(name, tmpl: tpd.Template) => {
           val newDefs = elementsImplOpt ++: usingPluginOpt ++: cloneTypeImplOpt.toList
           val newTemplate =
             if (tmpl.body.size >= 1)
