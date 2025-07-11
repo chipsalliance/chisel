f0e4874d1ad4da6a85abc9f8fb699f8219bce661
Jack Koenig <koenig@sifive.com>
2025-07-11 15:00:26 -0700

    Re-enable SNAPSHOT publishing
    Remove custom Mill publishing logic, use upstream flow for release
    publishing. This involves generating the list of plugin versions we
    publish in bash + jq.
    
    The publishing flow does not seem to work for SNAPSHOTs, so instead call
    .publish on the relevant modules for SNAPSHOT publishing.

---
+++ .github/scripts/publish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Ideally Snapshots could use mill.javalib.SonatypeCentralPublishModule/ too
+# but it does not work for them on 1.0.1. It seems to publish to the wrong place.
+IS_SNAPSHOT=$(./mill show unipublish.isSnapshot)
+PLUGIN_VERSIONS=$(./mill show plugin.publishableVersions)
+PLUGIN_MODULES=$(jq -r 'map("plugin.cross[" + . + "]")' <<< "${PLUGIN_VERSIONS}")
+MODULES=$(jq -r '. + ["unipublish"]' <<< "${PLUGIN_MODULES}")
+if [[ "${IS_SNAPSHOT}" = "true" ]]; then
+  for mod in $(jq -r '.[]' <<< "$MODULES"); do
+    ./mill ${mod}.publish
+  done
+else
+  VERSION=$(./mill show unipublish.publishVersion | tr -d \")
+  BUNDLE_NAME=$(./mill show unipublish.artifactMetadata | jq -r '.group + "." + .id + "-" + .version')
+  MODULES_LIST=$(jq -r 'join(",")' <<< "${MODULES}")
+  ./mill mill.javalib.SonatypeCentralPublishModule/ \
+    --shouldRelease "false" \
+    --bundleName "$BUNDLE_NAME" \
+    --publishArtifacts "{${MODULES_LIST}}.publishArtifacts"
+fi
