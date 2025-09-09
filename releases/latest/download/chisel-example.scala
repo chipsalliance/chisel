c852c983af74d917dfd1573369e2cff2901de8c2
seldridge <seldridge@users.noreply.github.com>
2025-09-09 19:30:15 +0000

    deploy: 69b02764fd8e7368956a5d896a658d5759ea3a56

--- releases/latest/download/chisel-example.scala
+++ releases/latest/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.16
-//> using dep org.chipsalliance::chisel:7.0.0-RC4
-//> using plugin org.chipsalliance:::chisel-plugin:7.0.0-RC4
+//> using dep org.chipsalliance::chisel:7.0.0
+//> using plugin org.chipsalliance:::chisel-plugin:7.0.0
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.16
-//> using dep org.chipsalliance::chisel:7.0.0-RC4+6-a1443b64-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.0.0-RC4+6-a1443b64-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.0.0+7-69b02764-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.0.0+7-69b02764-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
