7bcce1785bd8ff97fbbc0f8335a894a37f8a3e3d
seldridge <seldridge@users.noreply.github.com>
2025-09-18 01:59:08 +0000

    deploy: 35bc32e75bd2da5843674ec65f425575154b6d33

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.16
-//> using dep org.chipsalliance::chisel:7.0.0+11-30b60ed3-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.0.0+11-30b60ed3-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.0.0+12-35bc32e7-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.0.0+12-35bc32e7-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
