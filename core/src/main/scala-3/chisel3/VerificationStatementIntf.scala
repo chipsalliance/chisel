92d5572387159ea26e1f2a1a30e0e2d245d219c0
Aditya Naik <aditya.naik@sifive.com>
2025-10-06 13:09:46 -0700

    Add apply methods to assert, assume, cover

--- core/src/main/scala-3/chisel3/VerificationStatementIntf.scala
+++ core/src/main/scala-3/chisel3/VerificationStatementIntf.scala
@@ -2,14 +2,65 @@
 
 package chisel3
 
-private[chisel3] trait Assert$Intf { self: assert.type =>
-  // TODO implement
+import chisel3.experimental.{BaseModule, SourceInfo}
+
+trait VerifPrintMacrosDoc
+
+val emptySourceLine: VerifStmtMacrosCompat.SourceLineInfo = ("???", 0)
+
+private[chisel3] trait Assert$Intf extends VerifPrintMacrosDoc { self: assert.type =>
+
+  def apply(
+    cond:    Bool,
+    message: String,
+    data:    Bits*
+  )(
+    implicit sourceInfo: SourceInfo
+  ): Assert = VerifStmtMacrosCompat.assert._applyWithSourceLinePrintable(
+    cond,
+    emptySourceLine,
+    Some(Printable.pack(message, data: _*))
+  )
+
+  def apply(
+    cond:    Bool,
+    message: Printable
+  )(
+    implicit sourceInfo: SourceInfo
+  ): Assert = VerifStmtMacrosCompat.assert._applyWithSourceLinePrintable(cond, emptySourceLine, Some(message))
+
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Assert =
+    VerifStmtMacrosCompat.assert._applyWithSourceLinePrintable(cond, emptySourceLine, None)
 }
 
-private[chisel3] trait Assume$Intf { self: assume.type =>
-  // TODO implement
+private[chisel3] trait Assume$Intf extends VerifPrintMacrosDoc { self: assume.type =>
+
+  def apply(
+    cond:    Bool,
+    message: String,
+    data:    Bits*
+  )(
+    implicit sourceInfo: SourceInfo
+  ): Assume = VerifStmtMacrosCompat.assume._applyWithSourceLinePrintable(
+    cond,
+    emptySourceLine,
+    Some(Printable.pack(message, data: _*))
+  )
+
+  def apply(
+    cond:    Bool,
+    message: Printable
+  )(
+    implicit sourceInfo: SourceInfo
+  ): Assume = VerifStmtMacrosCompat.assume._applyWithSourceLinePrintable(cond, emptySourceLine, Some(message))
+
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Assume =
+    VerifStmtMacrosCompat.assume._applyWithSourceLinePrintable(cond, emptySourceLine, None)
 }
 
-private[chisel3] trait Cover$Impl { self: cover.type =>
-  // TODO implement
+private[chisel3] trait Cover$Impl extends VerifPrintMacrosDoc { self: cover.type =>
+  def apply(cond: Bool, message: String)(implicit sourceInfo: SourceInfo): Cover =
+    VerifStmtMacrosCompat.cover._applyWithSourceLine(cond, emptySourceLine, Some(message))
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Cover =
+    VerifStmtMacrosCompat.cover._applyWithSourceLine(cond, emptySourceLine, None)
 }
--- core/src/main/scala-3/chisel3/VerificationStatementMacros.scala
+++ core/src/main/scala-3/chisel3/VerificationStatementMacros.scala
@@ -4,6 +4,9 @@ package chisel3
 
 import chisel3.experimental.SourceInfo
 import chisel3.internal._
+import chisel3.internal.firrtl.ir._
+import chisel3.util.circt.IfElseFatalIntrinsic
+import chisel3.layer.block
 
 import scala.annotation.nowarn
 
@@ -27,4 +30,72 @@ object VerifStmtMacrosCompat {
       case None => p"$kind failed at $lineMsg\n"
     }
   }
+
+  object assert {
+    private def emitIfElseFatalIntrinsic(
+      clock:     Clock,
+      predicate: Bool,
+      enable:    Bool,
+      format:    Printable
+    )(
+      implicit sourceInfo: SourceInfo
+    ): chisel3.assert.Assert = {
+      block(layers.Verification.Assert, skipIfAlreadyInBlock = true, skipIfLayersEnabled = true) {
+        val id = Builder.forcedUserModule // It should be safe since we push commands anyway.
+        val args = Printable.unpackFirrtlArgs(format)
+        IfElseFatalIntrinsic(id, format, "chisel3_builtin", clock, predicate, enable, args: _*)(sourceInfo)
+      }
+      new chisel3.assert.Assert()
+    }
+    def _applyWithSourceLinePrintable(
+      cond:    Bool,
+      line:    SourceLineInfo,
+      message: Option[Printable]
+    )(
+      implicit sourceInfo: SourceInfo
+    ): chisel3.assert.Assert = {
+      message.foreach(Printable.checkScope(_))
+      val pable = formatFailureMessage("Assertion", line, cond, message)
+      emitIfElseFatalIntrinsic(Module.clock, cond, !Module.reset.asBool, pable)
+    }
+  }
+
+  object assume {
+    def _applyWithSourceLinePrintable(
+      cond:    Bool,
+      line:    SourceLineInfo,
+      message: Option[Printable]
+    )(
+      implicit sourceInfo: SourceInfo
+    ): chisel3.assume.Assume = {
+      val id = new chisel3.assume.Assume()
+      block(layers.Verification.Assume, skipIfAlreadyInBlock = true, skipIfLayersEnabled = true) {
+        message.foreach(Printable.checkScope(_))
+        when(!Module.reset.asBool) {
+          val formattedMsg = formatFailureMessage("Assumption", line, cond, message)
+          Builder.pushCommand(Verification(id, Formal.Assume, sourceInfo, Module.clock.ref, cond.ref, formattedMsg))
+        }
+      }
+      id
+    }
+  }
+
+  object cover {
+    def _applyWithSourceLine(
+      cond:    Bool,
+      line:    SourceLineInfo,
+      message: Option[String]
+    )(
+      implicit sourceInfo: SourceInfo
+    ): chisel3.cover.Cover = {
+      val id = new chisel3.cover.Cover()
+      block(layers.Verification.Cover, skipIfAlreadyInBlock = true, skipIfLayersEnabled = true) {
+        when(!Module.reset.asBool) {
+          Builder.pushCommand(Verification(id, Formal.Cover, sourceInfo, Module.clock.ref, cond.ref, ""))
+        }
+      }
+      id
+    }
+  }
+
 }
