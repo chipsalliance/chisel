5c6a119c9746a0b754e2683877c45786df2aca09
jackkoenig <jackkoenig@users.noreply.github.com>
2025-10-28 18:56:24 +0000

    deploy: 6c8d9d82d188f3d5692d2107459eec36f0ae0689

--- api/latest/lib/jquery.min.js.LICENSE.txt
+++
@@ -1,1 +0,0 @@
-/*! jQuery v3.7.1 | (c) OpenJS Foundation and other contributors | jquery.org/license */
--- assets/js/8158.f6825f02.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkchisel_lang=globalThis.webpackChunkchisel_lang||[]).push([[8158],{8158:(a,l,h)=>{h.r(l),h.d(l,{DocSearchModal:()=>c.a1});var c=h(3219)}}]);
