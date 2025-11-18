863e25395b0cdf7d385970b98286ceac0720e8c5
tmckay-sifive <tmckay-sifive@users.noreply.github.com>
2025-11-18 23:56:19 +0000

    deploy: aea576a8bb0ef8a9f2e38bd76272dfc7761881f8

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.17
-//> using dep org.chipsalliance::chisel:7.3.0+5-071d8011-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.3.0+5-071d8011-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.3.0+6-aea576a8-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.3.0+6-aea576a8-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
