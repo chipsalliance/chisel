e659f70c4be1c6d5044f0664a437c60361c573ea
Aditya Naik <aditya.naik@sifive.com>
2025-11-25 13:26:05 -0800

    Add scala.reflect.Selectable
    Update SelectableCompat

--- core/src/main/scala-2/chisel3/SelectableCompat.scala
+++ core/src/main/scala-2/chisel3/SelectableCompat.scala
@@ -2,8 +2,13 @@
 
 package chisel3
 
-/** Does nothing in Scala 2; this stub trait is for maintaining
-  * compatibility with Scala 3's selectable, required to support
-  * structurally-typed Bundles in Scala 3.
+/** These traits do nothing in Scala 2 other than maintaining
+  * compatibility with Scala 3's Selectable and
+  * scala.reflect.Selectable, required to support structural selection
+  * in Scala 3.
+  *
+  * SelectableCompat => scala.reflect.Selectable
+  * Selectable => scala.Selectable
   */
+trait SelectableCompat
 trait Selectable
