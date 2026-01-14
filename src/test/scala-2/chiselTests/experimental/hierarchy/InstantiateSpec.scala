fa42b60066dcd1c7782540789454b72e1ac70109
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2026-01-14 12:34:18 -0500

    [core] Add elideBlocks to Instantiate cache key
    Extend the Instantiate cache key to track if the instantiation is in an
    `elideBlocks` scope.  This fixes a bug where an `Instantiate` call inside
    an `elideBlocks` scope will instantiate a module which has layer
    blocks (and defeat the intent of using `elideBlocks`).
    
    Fixes #5138.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/test/scala-2/chiselTests/experimental/hierarchy/InstantiateSpec.scala
+++ src/test/scala-2/chiselTests/experimental/hierarchy/InstantiateSpec.scala
@@ -196,6 +196,13 @@ object InstantiateSpec {
   class LookupableChiselType extends Module {
     @public val bundleType = new MyBundle(MyBundleParameters(4, 8, true))
   }
+
+  @instantiable
+  class HasLayer extends Module {
+    layer.block(layers.Verification.Assert) {
+      val a = WireInit(false.B)
+    }
+  }
 }
 
 class ParameterizedReset(hasAsyncNotSyncReset: Boolean) extends Module {
@@ -515,6 +522,21 @@ class InstantiateSpec extends AnyFunSpec with Matchers with FileCheck {
       }).fileCheck()(
         """|CHECK: intmodule InstantiableIntrinsic :
            |CHECK: module Top :
+           |""".stripMargin
+      )
+    }
+
+    it("should include 'elideBlocks' when computing the hash") {
+      class Foo extends Module {
+        val bar = Instantiate(new HasLayer)
+        layer.elideBlocks {
+          val bar2 = Instantiate(new HasLayer)
+        }
+      }
+
+      emitCHIRRTL(new Foo).fileCheck()(
+        """|CHECK-COUNT-3: module
+           |CHECK-NOT:     module
            |""".stripMargin
       )
     }
