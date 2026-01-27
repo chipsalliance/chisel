36dff4ab1990d3f2a56bd9a2f107e1c0a333acb3
trmckay <trmckay@users.noreply.github.com>
2026-01-27 22:51:18 +0000

    deploy: 00e5355f66c4d0171164390bc6579eeed8445e45

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.18
-//> using dep org.chipsalliance::chisel:7.7.0+34-94b8570e-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.7.0+34-94b8570e-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.7.0+35-00e5355f-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.7.0+35-00e5355f-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
