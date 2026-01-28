4cf83d645c7f2b16af0e2274a89fb454aa0ad2c3
Aditya Naik <91489422+adkian-sifive@users.noreply.github.com>
2026-01-28 15:22:28 -0800

    [Scala3] Add Switch macro and move SwitchSpec (#5179)

--- src/main/scala-3/chisel3/util/Switch.scala
+++ src/main/scala-3/chisel3/util/Switch.scala
@@ -5,8 +5,80 @@
 
 package chisel3.util
 
-import chisel3._
+import chisel3.*
+import scala.quoted.*
 
+/** Conditional logic to form a switch block. See [[is$ is]] for the case API.
+  *
+  * @example {{{
+  * switch (myState) {
+  *   is (state1) {
+  *     // some logic here that runs when myState === state1
+  *   }
+  *   is (state2) {
+  *     // some logic here that runs when myState === state2
+  *   }
+  * }
+  * }}}
+  */
 object switch {
-  def apply[T <: Element](cond: T)(x: => Any): Unit = println("Not supported yet")
+  inline def apply[T <: Element](cond: T)(inline x: Any): Unit =
+    ${ SwitchMacros.impl[T]('cond, 'x) }
+}
+
+private object SwitchMacros {
+
+  def impl[T <: Element: Type](cond: Expr[T], x: Expr[Any])(using Quotes): Expr[Unit] = {
+    import quotes.reflect.*
+
+    val isApplySymbol = Symbol.requiredModule("chisel3.util.is").methodMember("apply").toSet
+    // Since switch.apply is inlined it will always be wrapped in an Inline block
+    val statements: List[Statement] = x.asTerm match {
+      case Inlined(_, _, Block(head, tail)) => head :+ tail
+    }
+
+    // List of params and blocks as in `is(params) { block }`
+    val isCallExprs: List[Expr[(Iterable[T], () => Any)]] = statements.flatMap {
+      case Apply(Apply(fun, List(paramArg)), List(blockArg)) if isApplySymbol(fun.symbol) =>
+        Some(buildIsCallExpr[T](paramArg, blockArg))
+
+      case term =>
+        report.errorAndAbort(
+          s"Cannot include blocks that do not begin with is() in switch. Got: ${term.show}",
+          term.pos
+        )
+    }
+
+    if (isCallExprs.isEmpty) {
+      '{ new SwitchContext[T]($cond, None, Set.empty); () }
+    } else {
+      val isCallsList: Expr[List[(Iterable[T], () => Any)]] = Expr.ofList(isCallExprs)
+      '{
+        val ctx = new SwitchContext[T]($cond, None, Set.empty)
+        $isCallsList.foldLeft(ctx) { case (acc, (params, block)) =>
+          acc.is(params)(block())
+        }
+        ()
+      }
+    }
+  }
+
+  private def buildIsCallExpr[T <: Element: Type](
+    using Quotes
+  )(paramArg: quotes.reflect.Term, blockArg: quotes.reflect.Term): Expr[(Iterable[T], () => Any)] = {
+    import quotes.reflect.*
+
+    val paramsExpr: Expr[Iterable[T]] = paramArg.asExpr match {
+      case '{ $iter: Iterable[T] } => iter
+      case '{ $elem: T }           => '{ Seq($elem) }
+      case other =>
+        report.errorAndAbort(s"is() parameter must be an Element or Iterable[Element], got: ${other.show}")
+    }
+
+    val blockExpr: Expr[() => Any] = blockArg.asExpr match {
+      case '{ $b: t } => '{ () => $b }
+    }
+
+    '{ ($paramsExpr, $blockExpr) }
+  }
 }
