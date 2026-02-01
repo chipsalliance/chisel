4f314bcb585a4335c8deae8d18a9840d61e9ff8f
Jack Koenig <koenig@sifive.com>
2026-02-01 14:25:54 -0800

    [scala3] Split Intrinsic.scala implicit SourceInfo APIs
    Split public methods using implicit SourceInfo in
    core/src/main/scala/chisel3/Intrinsic.scala into Scala 2 and Scala 3
    specific interfaces.
    
    Intrinsic.apply and IntrinsicExpr.apply are now exposed through
    interface traits with Scala 2 using 'implicit SourceInfo' and Scala 3
    using 'using SourceInfo'.
    
    Co-authored-by: Augment Code <code@augmentcode.com>

---
+++ core/src/main/scala-3/chisel3/IntrinsicIntf.scala
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chisel3
+
+import chisel3.experimental.SourceInfo
+
+private[chisel3] trait Intrinsic$Intf { self: Intrinsic.type =>
+
+  /** Create an intrinsic statement.
+    *
+    * @param intrinsic name of the intrinsic
+    * @param params parameter name/value pairs, if any.  Parameter names must be unique.
+    * @param data inputs
+    *
+    * @example {{{
+    * Intrinsic("test", "Foo" -> 5)(f, g)
+    * }}}
+    */
+  def apply(intrinsic: String, params: (String, Param)*)(data: Data*)(using sourceInfo: SourceInfo): Unit =
+    _applyImpl(intrinsic, params: _*)(data: _*)
+}
+
+private[chisel3] trait IntrinsicExpr$Intf { self: IntrinsicExpr.type =>
+
+  /** Create an intrinsic expression.
+    *
+    * @param intrinsic name of the intrinsic
+    * @param ret return type of the expression
+    * @param params parameter name/value pairs, if any.  Parameter names must be unique.
+    * @param data inputs
+    * @return intrinsic expression that returns the specified return type
+    *
+    * @example {{{
+    * val test = IntrinsicExpr("test", UInt(32.W), "Foo" -> 5)(f, g) + 3.U
+    * }}}
+    */
+  def apply[T <: Data](
+    intrinsic: String,
+    ret:       => T,
+    params:    (String, Param)*
+  )(data: Data*)(
+    using sourceInfo: SourceInfo
+  ): T = _applyImpl(intrinsic, ret, params: _*)(data: _*)
+}
--- core/src/main/scala/chisel3/Intrinsic.scala
+++ core/src/main/scala/chisel3/Intrinsic.scala
@@ -9,39 +9,22 @@ import chisel3.internal.Builder
 import chisel3.internal.binding.OpBinding
 import chisel3.internal.Builder.pushCommand
 
-object Intrinsic {
+object Intrinsic extends Intrinsic$Intf {
 
-  /** Create an intrinsic statement.
-    *
-    * @param intrinsic name of the intrinsic
-    * @param params parameter name/value pairs, if any.  Parameter names must be unique.
-    * @param data inputs
-    *
-    * @example {{{
-    * Intrinsic("test", "Foo" -> 5)(f, g)
-    * }}}
-    */
-  def apply(intrinsic: String, params: (String, Param)*)(data: Data*)(implicit sourceInfo: SourceInfo): Unit = {
+  private[chisel3] def _applyImpl(
+    intrinsic: String,
+    params:    (String, Param)*
+  )(data: Data*)(
+    implicit sourceInfo: SourceInfo
+  ): Unit = {
     require(params.map(_._1).distinct.size == params.size, "parameter names must be unique")
     pushCommand(DefIntrinsic(sourceInfo, intrinsic, data.map(_.ref), params))
   }
 }
 
-object IntrinsicExpr {
+object IntrinsicExpr extends IntrinsicExpr$Intf {
 
-  /** Create an intrinsic expression.
-    *
-    * @param intrinsic name of the intrinsic
-    * @param ret return type of the expression
-    * @param params parameter name/value pairs, if any.  Parameter names must be unique.
-    * @param data inputs
-    * @return intrinsic expression that returns the specified return type
-    *
-    * @example {{{
-    * val test = IntrinsicExpr("test", UInt(32.W), "Foo" -> 5)(f, g) + 3.U
-    * }}}
-    */
-  def apply[T <: Data](
+  private[chisel3] def _applyImpl[T <: Data](
     intrinsic: String,
     ret:       => T,
     params:    (String, Param)*
