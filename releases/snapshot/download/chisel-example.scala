a6b211c553d528e17afa855b4a2dc85c53336c6f
azidar <azidar@users.noreply.github.com>
2026-02-11 22:29:11 +0000

    deploy: 9fe07222648ff72fa83922ba7ccff8f2c46d8d9f

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.18
-//> using dep org.chipsalliance::chisel:7.8.0+13-d8fbcf77-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.8.0+13-d8fbcf77-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.8.0+14-9fe07222-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.8.0+14-9fe07222-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
