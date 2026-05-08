4aa615898a5ddff8063abc5aa86dde00feb36dba
jackkoenig <jackkoenig@users.noreply.github.com>
2026-05-08 04:38:01 +0000

    deploy: f88b0539df6a6b8557e4062b7a3f974d356f751e

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.18
-//> using dep org.chipsalliance::chisel:7.11.0+18-218bbc8d-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.11.0+18-218bbc8d-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.11.0+19-f88b0539-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.11.0+19-f88b0539-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
