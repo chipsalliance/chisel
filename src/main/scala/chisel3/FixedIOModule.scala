7cd46700172e06801463fc497b42661a09132752
Jack Koenig <koenig@sifive.com>
2026-05-28 08:42:04 -0700

    Add DataProduct to FixedIOModule as well

--- src/main/scala/chisel3/FixedIOModule.scala
+++ src/main/scala/chisel3/FixedIOModule.scala
@@ -5,6 +5,7 @@ package chisel3
 import chisel3.experimental.hierarchy.{instantiable, public}
 import chisel3.experimental.hierarchy.core.{Definition, Instance, Lookupable}
 import chisel3.experimental.{BaseModule, UnlocatableSourceInfo}
+import chisel3.experimental.dataview.DataProduct
 
 /** A module or external module whose IO is generated from a specific generator.
   * This module may have no additional IO created other than what is specified
@@ -19,17 +20,24 @@ sealed trait FixedIOBaseModule[A] extends BaseModule {
   // Concrete subclasses implement this via an implicit constructor parameter.
   protected implicit def lookupable: Lookupable[A]
 
+  protected implicit def dataProduct: DataProduct[A]
+
   // No @public, lookups implemented manually in companion object.
   final val io: A = {
-    implicit val l: Lookupable[A] = lookupable
-    val dataElems = l.in(ioGenerator)
-    val names = LazyList.from(0).map(i => ('a' + i).toChar.toString)
-    val ports = dataElems.zip(names).map { case (d, name) =>
+    val a = ioGenerator
+    val dataElems = lookupable.in(a)
+    // TODO add check for name collisions
+    val nameLookup = dataProduct.dataIterator(a, "").toMap
+    val io = dataElems.map { d =>
       val p = FlatIO(d)(UnlocatableSourceInfo)
+      val name = {
+        val n = nameLookup(d)
+        if (n == "") "io" else n
+      }
       p.suggestName(name)
       p
     }
-    l.out(ioGenerator, ports.iterator)
+    lookupable.out(ioGenerator, io.iterator)
   }
   endIOCreation()
 
@@ -43,7 +51,7 @@ object FixedIOBaseModule {
   implicit class DefinitionOps[A](___module: Definition[FixedIOBaseModule[A]]) {
     def io: A = {
       implicit val _mg: chisel3.internal.MacroGenerated = new chisel3.internal.MacroGenerated {}
-      implicit val _l:  Lookupable.Aux[A, A] =
+      implicit val _l: Lookupable.Aux[A, A] =
         ___module.proto.lookupable.asInstanceOf[Lookupable.Aux[A, A]]
       ___module._lookup(_.io)
     }
@@ -52,7 +60,7 @@ object FixedIOBaseModule {
   implicit class InstanceOps[A](___module: Instance[FixedIOBaseModule[A]]) {
     def io: A = {
       implicit val _mg: chisel3.internal.MacroGenerated = new chisel3.internal.MacroGenerated {}
-      implicit val _l:  Lookupable.Aux[A, A] =
+      implicit val _l: Lookupable.Aux[A, A] =
         ___module.proto.lookupable.asInstanceOf[Lookupable.Aux[A, A]]
       ___module._lookup(_.io)
     }
@@ -64,8 +72,10 @@ object FixedIOBaseModule {
   *
   * @param ioGenerator
   */
-class FixedIORawModule[A](final val ioGenerator: A)(implicit val lookupable: Lookupable[A])
-    extends RawModule
+class FixedIORawModule[A](final val ioGenerator: A)(
+  implicit val lookupable: Lookupable[A],
+  val dataProduct:         DataProduct[A]
+) extends RawModule
     with FixedIOBaseModule[A]
 
 /** A Chisel module whose IO (in addition to [[clock]] and [[reset]]) is determined
@@ -74,8 +84,10 @@ class FixedIORawModule[A](final val ioGenerator: A)(implicit val lookupable: Loo
   *
   * @param ioGenerator
   */
-class FixedIOModule[A](final val ioGenerator: A)(implicit val lookupable: Lookupable[A])
-    extends Module
+class FixedIOModule[A](final val ioGenerator: A)(
+  implicit val lookupable: Lookupable[A],
+  val dataProduct:         DataProduct[A]
+) extends Module
     with FixedIOBaseModule[A]
 
 /** A Chisel blackbox whose IO is determined by an IO generator.  This module
@@ -85,6 +97,7 @@ class FixedIOModule[A](final val ioGenerator: A)(implicit val lookupable: Lookup
   * @param params
   */
 class FixedIOExtModule[A](final val ioGenerator: A, params: Map[String, Param] = Map.empty[String, Param])(
-  implicit val lookupable: Lookupable[A]
+  implicit val lookupable: Lookupable[A],
+  val dataProduct:         DataProduct[A]
 ) extends ExtModule(params)
     with FixedIOBaseModule[A]
