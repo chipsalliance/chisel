362d602360e5a682da227dc0a33d7c954e6770d9
Aditya Naik <aditya.naik@sifive.com>
2026-05-27 19:05:49 -0700

    [Scala3] Implement DynamicObject.apply on Scala 3

--- core/src/main/scala-3/chisel3/properties/ObjectIntf.scala
+++ core/src/main/scala-3/chisel3/properties/ObjectIntf.scala
@@ -2,6 +2,18 @@
 
 package chisel3.properties
 
+import chisel3.experimental.SourceInfo
+
 private[chisel3] trait DynamicObjectObjIntf { self: DynamicObject.type =>
-  // TODO implement
+
+  /** A wrapper method to wrap Class instantiations and return a DynamicObject.
+    *
+    * This is necessary to help Chisel track internal state. This can be used instead of `Definition.apply` if a
+    * DynamicObject is required. If possible, it is safer to user `Definition.apply` and StaticObject.
+    *
+    * @param bc the Class being created
+    *
+    * @return a DynamicObject representing an instance of the Class
+    */
+  def apply[T <: Class](bc: => T)(using SourceInfo): DynamicObject = _applyImpl(bc)
 }
