4a8fbfce61b7e8e1e9a7af579972011fb669538d
dtzSiFive <dtzSiFive@users.noreply.github.com>
2026-08-27 22:10:05 +0000

    deploy: b02fbfda18fa4424de0d65e0d429de9e2789c0c5

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
 //> using scala 2.13.18
-//> using dep org.chipsalliance::chisel:7.14.0+6-da9d38e7-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:7.14.0+6-da9d38e7-SNAPSHOT
+//> using dep org.chipsalliance::chisel:7.14.0+8-b02fbfda-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:7.14.0+8-b02fbfda-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
